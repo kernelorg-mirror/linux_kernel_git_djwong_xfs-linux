6c147844897922da907d83acca4625267752e8d3
Darrick J. Wong <darrick.wong@oracle.com>
2019-01-16 10:12:38 -0800

    xfs: scrub the realtime rmapbt
    Check the realtime reverse mapping btree against the rtbitmap, and
    modify the rtbitmap scrub to check against the rtrmapbt.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/scrub.h
+++ fs/xfs/scrub/scrub.h
@@ -99,6 +99,7 @@ int xchk_parent(struct xfs_scrub *sc);
 #ifdef CONFIG_XFS_RT
 int xchk_rtbitmap(struct xfs_scrub *sc);
 int xchk_rtsummary(struct xfs_scrub *sc);
+int xchk_rtrmapbt(struct xfs_scrub *sc);
 #else
 static inline int
 xchk_rtbitmap(struct xfs_scrub *sc)
@@ -107,6 +108,11 @@ xchk_rtbitmap(struct xfs_scrub *sc)
 }
 static inline int
 xchk_rtsummary(struct xfs_scrub *sc)
+{
+	return -ENOENT;
+}
+static inline int
+xchk_rtrmapbt(struct xfs_scrub *sc)
 {
 	return -ENOENT;
 }
