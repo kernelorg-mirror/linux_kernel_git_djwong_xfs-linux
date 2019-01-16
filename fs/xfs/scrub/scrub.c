07254f09ef5f77a34cd53a9b1e0cfaaaad52c811
Darrick J. Wong <darrick.wong@oracle.com>
2019-01-16 10:12:14 -0800

    xfs: introduce online scrub freeze
    Introduce a new 'online scrub freeze' that we can use to lock out all
    filesystem modifications and background activity so that we can perform
    global scans in order to rebuild metadata.  This introduces a new IFLAG
    to the scrub ioctl to indicate that userspace is willing to allow a
    freeze.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/scrub.c
+++ fs/xfs/scrub/scrub.c
@@ -171,6 +171,8 @@ xchk_teardown(
 	struct xfs_inode	*ip_in,
 	int			error)
 {
+	int				err2;
+
 	xchk_ag_free(sc, &sc->sa);
 	if (sc->tp) {
 		if (error == 0 && (sc->sm->sm_flags & XFS_SCRUB_IFLAG_REPAIR))
@@ -186,6 +188,12 @@ xchk_teardown(
 		    !xfs_internal_inum(sc->mp, sc->ip->i_ino))
 			xfs_irele(sc->ip);
 		sc->ip = NULL;
+	}
+	if (sc->flags & XCHK_FS_FROZEN) {
+		err2 = xchk_fs_thaw(sc);
+		if (!error && err2)
+			error = err2;
+		sc->flags &= ~XCHK_FS_FROZEN;
 	}
 	if (sc->flags & XCHK_HAS_QUOTAOFFLOCK) {
 		mutex_unlock(&sc->mp->m_quotainfo->qi_quotaofflock);
