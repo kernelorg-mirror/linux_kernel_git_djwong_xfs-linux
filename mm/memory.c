f3f3db3b466f4d669383ae9e831a4519cc12016b
Darrick J. Wong <darrick.wong@oracle.com>
2019-04-05 18:05:12 -0700

    mm/fs: don't allow writes to immutable files
    The chattr manpage has this to say about immutable files:
    
    "A file with the 'i' attribute cannot be modified: it cannot be deleted
    or renamed, no link can be created to this file, most of the file's
    metadata can not be modified, and the file can not be opened in write
    mode."
    
    Once the flag is set, it is enforced for quite a few file operations,
    such as fallocate, fpunch, fzero, rm, touch, open, etc.  However, we
    don't check for immutability when doing a write(), a PROT_WRITE mmap(),
    a truncate(), or a write to a previously established mmap.
    
    If a program has an open write fd to a file that the administrator
    subsequently marks immutable, the program still can change the file
    contents.  Weird!
    
    The ability to write to an immutable file does not follow the manpage
    promise that immutable files cannot be modified.  Worse yet it's
    inconsistent with the behavior of other syscalls which don't allow
    modifications of immutable files.
    
    Therefore, add the necessary checks to make the write, mmap, and
    truncate behavior consistent with what the manpage says and consistent
    with other syscalls on filesystems which support IMMUTABLE.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- mm/memory.c
+++ mm/memory.c
@@ -2149,6 +2149,9 @@ static vm_fault_t do_page_mkwrite(struct vm_fault *vmf)
 
 	vmf->flags = FAULT_FLAG_WRITE|FAULT_FLAG_MKWRITE;
 
+	if (vmf->vma->vm_file && IS_IMMUTABLE(file_inode(vmf->vma->vm_file)))
+		return VM_FAULT_SIGBUS;
+
 	ret = vmf->vma->vm_ops->page_mkwrite(vmf);
 	/* Restore original flags so that caller is not surprised */
 	vmf->flags = old_flags;
