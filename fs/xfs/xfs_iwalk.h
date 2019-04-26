369f82d91c782353d0203e49bb9ae5d35edc7e99
Darrick J. Wong <darrick.wong@oracle.com>
2019-04-26 10:39:00 -0700

    xfs: refactor INUMBERS to use iwalk functions
    Now that we have generic functions to walk inode records, refactor the
    INUMBERS implementation to use it.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_iwalk.h
+++ fs/xfs/xfs_iwalk.h
@@ -14,4 +14,11 @@ int xfs_iwalk(struct xfs_mount *mp, xfs_ino_t startino, xfs_iwalk_fn iwalk_fn,
 int xfs_iwalk_threaded(struct xfs_mount *mp, xfs_ino_t startino,
 		xfs_iwalk_fn iwalk_fn, void *data);
 
+/* Walk all inode btree records in the filesystem starting from @startino. */
+typedef int (*xfs_inobt_walk_fn)(struct xfs_mount *mp, xfs_agnumber_t agno,
+		const struct xfs_inobt_rec_incore *irec, void *data);
+#define XFS_INOBT_WALK_ABORT	(XFS_IWALK_ABORT)
+int xfs_inobt_walk(struct xfs_mount *mp, xfs_ino_t startino,
+		xfs_inobt_walk_fn inobt_walk_fn, void *data);
+
 #endif /* __XFS_IWALK_H__ */
