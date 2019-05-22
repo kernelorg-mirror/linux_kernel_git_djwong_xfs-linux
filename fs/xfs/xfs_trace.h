45ea061dbe279508a0e4b10c0165caecb15b3ba3
Darrick J. Wong <darrick.wong@oracle.com>
2019-05-21 17:07:26 -0700

    xfs: track unlinked inactive inode quota counters
    Set up quota counters to track the number of inodes and blocks that will
    be freed from inactivating unlinked inodes.  We'll use this in the
    deferred inactivation patch to hide the effects of deferred processing.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_trace.h
+++ fs/xfs/xfs_trace.h
@@ -903,6 +903,7 @@ DEFINE_EVENT(xfs_dquot_class, name, \
 	TP_PROTO(struct xfs_dquot *dqp), \
 	TP_ARGS(dqp))
 DEFINE_DQUOT_EVENT(xfs_dqadjust);
+DEFINE_DQUOT_EVENT(xfs_dqreclaim_inactive);
 DEFINE_DQUOT_EVENT(xfs_dqreclaim_want);
 DEFINE_DQUOT_EVENT(xfs_dqreclaim_dirty);
 DEFINE_DQUOT_EVENT(xfs_dqreclaim_busy);
