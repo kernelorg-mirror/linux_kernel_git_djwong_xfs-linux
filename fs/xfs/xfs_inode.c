6184c7b40d4b965b02167514493c90e9252fb6d8
Dave Chinner <dchinner@redhat.com>
2019-05-21 17:08:11 -0700

    xfs: don't free EOF blocks on read close
    When we have a workload that does open/read/close in parallel with other
    allocation, the file becomes rapidly fragmented. This is due to close()
    calling xfs_release() and removing the speculative preallocation beyond
    EOF.
    
    The existing open/*/close heuristic in xfs_release() does not catch this
    as a sync writer does not leave delayed allocation blocks allocated on
    the inode for later writeback that can be detected in xfs_release() and
    hence XFS_IDIRTY_RELEASE never gets set.
    
    In xfs_file_release(), we know more about the released file context, and
    so we need to communicate some of the details to xfs_release() so it can
    do the right thing here and skip EOF block truncation. This defers the
    EOF block cleanup for synchronous write contexts to the background EOF
    block cleaner which will clean up within a few minutes.
    
    Before:
    
    Test 1: sync write fragmentation counts
    
    /mnt/scratch/file.0: 919
    /mnt/scratch/file.1: 916
    /mnt/scratch/file.2: 919
    /mnt/scratch/file.3: 920
    /mnt/scratch/file.4: 920
    /mnt/scratch/file.5: 921
    /mnt/scratch/file.6: 916
    /mnt/scratch/file.7: 918
    
    After:
    
    Test 1: sync write fragmentation counts
    
    /mnt/scratch/file.0: 24
    /mnt/scratch/file.1: 24
    /mnt/scratch/file.2: 11
    /mnt/scratch/file.3: 24
    /mnt/scratch/file.4: 3
    /mnt/scratch/file.5: 24
    /mnt/scratch/file.6: 24
    /mnt/scratch/file.7: 23
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    [darrick: wordsmithing, fix commit message]
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -1211,10 +1211,11 @@ out:
 
 int
 xfs_release(
-	xfs_inode_t	*ip)
+	struct xfs_inode	*ip,
+	bool			want_free_eofblocks)
 {
-	xfs_mount_t	*mp = ip->i_mount;
-	int		error;
+	struct xfs_mount	*mp = ip->i_mount;
+	int			error;
 
 	if (!S_ISREG(VFS_I(ip)->i_mode) || (VFS_I(ip)->i_mode == 0))
 		return 0;
@@ -1250,7 +1251,7 @@ xfs_release(
 	if (VFS_I(ip)->i_nlink == 0)
 		return 0;
 
-	if (xfs_can_free_eofblocks(ip, false)) {
+	if (want_free_eofblocks && xfs_can_free_eofblocks(ip, false)) {
 
 		/*
 		 * Check if the inode is being opened, written and closed
