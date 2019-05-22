d9ef536ecbdf78641ef752f252f0640e85e4a817
Darrick J. Wong <darrick.wong@oracle.com>
2019-05-21 17:07:33 -0700

    xfs: repair the rmapbt
    Rebuild the reverse mapping btree from all primary metadata.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/bitmap.c
+++ fs/xfs/scrub/bitmap.c
@@ -33,6 +33,7 @@ xfs_bitmap_set(
 	bmr->start = start;
 	bmr->len = len;
 	list_add_tail(&bmr->list, &bitmap->list);
+	bitmap->weight += len;
 
 	return 0;
 }
@@ -57,6 +58,7 @@ xfs_bitmap_init(
 	struct xfs_bitmap	*bitmap)
 {
 	INIT_LIST_HEAD(&bitmap->list);
+	bitmap->weight = 0;
 }
 
 /* Compare two btree extents. */
@@ -159,6 +161,7 @@ xfs_bitmap_disunion(
 			state |= LEFT_ALIGNED;
 		if (sub_start + sub_len == br->start + br->len)
 			state |= RIGHT_ALIGNED;
+		bitmap->weight -= sub_len;
 		switch (state) {
 		case LEFT_ALIGNED:
 			/* Coincides with only the left. */
@@ -295,4 +298,12 @@ xfs_bitmap_set_btblocks(
 	struct xfs_btree_cur	*cur)
 {
 	return xfs_btree_visit_blocks(cur, xfs_bitmap_collect_btblock, bitmap);
+}
+
+/* Compute the weight of this bitmap. */
+uint64_t
+xfs_bitmap_hweight(
+	struct xfs_bitmap	*bitmap)
+{
+	return bitmap->weight;
 }
