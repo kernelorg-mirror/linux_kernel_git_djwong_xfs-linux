35ec3341fa302f2e58e5d82f1a2c543b490d8106
Darrick J. Wong <darrick.wong@oracle.com>
2019-05-21 17:07:29 -0700

    xfs: retry fs writes when there isn't space
    Any time we try a file write that fails due to ENOSPC or EDQUOT, force
    inactivation work to free up some resources and try one more time.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_iomap.c
+++ fs/xfs/xfs_iomap.c
@@ -548,6 +548,7 @@ xfs_file_iomap_begin_delay(
 	struct xfs_iext_cursor	icur, ccur;
 	xfs_fsblock_t		prealloc_blocks = 0;
 	bool			eof = false, cow_eof = false, shared = false;
+	bool			flush_inactive = true;
 	int			whichfork = XFS_DATA_FORK;
 	int			error = 0;
 
@@ -555,7 +556,7 @@ xfs_file_iomap_begin_delay(
 	ASSERT(!xfs_get_extsz_hint(ip));
 
 	xfs_ilock(ip, XFS_ILOCK_EXCL);
-
+start_over:
 	if (unlikely(XFS_TEST_ERROR(
 	    (XFS_IFORK_FORMAT(ip, XFS_DATA_FORK) != XFS_DINODE_FMT_EXTENTS &&
 	     XFS_IFORK_FORMAT(ip, XFS_DATA_FORK) != XFS_DINODE_FMT_BTREE),
@@ -700,6 +701,14 @@ retry:
 		break;
 	case -ENOSPC:
 	case -EDQUOT:
+		if (flush_inactive) {
+			flush_inactive = false;
+			whichfork = XFS_DATA_FORK;
+			xfs_iunlock(ip, XFS_ILOCK_EXCL);
+			xfs_inactive_force(mp);
+			xfs_ilock(ip, XFS_ILOCK_EXCL);
+			goto start_over;
+		}
 		/* retry without any preallocation */
 		trace_xfs_delalloc_enospc(ip, offset, count);
 		if (prealloc_blocks) {
