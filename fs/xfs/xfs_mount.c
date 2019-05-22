265fea03faad32e458450fcc262a132b98df6402
Darrick J. Wong <darrick.wong@oracle.com>
2019-05-21 17:08:14 -0700

    xfs: create a polled function to force inode inactivation
    Create a polled version of xfs_inactive_force so that we can force
    inactivation while holding a lock (usually the umount lock) without
    tripping over the softlockup timer.  This is for callers that hold vfs
    locks while calling inactivation, which is currently unmount, iunlink
    processing during mount, and rw->ro remount.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_mount.c
+++ fs/xfs/xfs_mount.c
@@ -1036,7 +1036,7 @@ xfs_unmountfs(
 	 * Since this can involve finobt updates, do it now before we lose the
 	 * per-AG space reservations.
 	 */
-	xfs_inactive_force(mp);
+	xfs_inactive_force_poll(mp);
 
 	xfs_stop_block_reaping(mp);
 	xfs_fs_unreserve_ag_blocks(mp);
