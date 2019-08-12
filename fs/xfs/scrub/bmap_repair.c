a7814d44067a8a5bdee5eb9eeb786d2244791560
Darrick J. Wong <darrick.wong@oracle.com>
2019-08-12 14:40:45 -0700

    xfs: convert block map rebuild code to use btree bulk loading
    Convert the bmap repair functions to use btree bulk loading.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/bmap_repair.c
+++ fs/xfs/scrub/bmap_repair.c
@@ -41,40 +41,26 @@
  * Basically, we gather all the rmap records for the inode and fork we're
  * fixing, reset the incore fork, then re-add all the records.
  */
-
-/* Smallest possible record to represent a single contiguous physical map. */
-#define XREP_BMAP_UNWRITTEN	((uint64_t)1ULL << 63)
-struct xrep_bmap_extent {
-	/* starting offset; upper bit means unwritten */
-	xfs_fileoff_t	startoff;
-	xfs_fsblock_t	startblock;
-	xfs_filblks_t	blockcount;
-} __packed;
-
-static inline xfs_fileoff_t
-xrep_bmap_startoff(
-	const struct xrep_bmap_extent	*ext)
-{
-	return ext->startoff & ~XREP_BMAP_UNWRITTEN;
-}
-
 struct xrep_bmap {
 	/* List of new bmap records. */
 	struct xfbma		*bmap_records;
 
 	/* Old bmbt blocks */
-	struct xbitmap		*btlist;
+	struct xbitmap		old_bmbt_blocks;
+
+	/* New fork. */
+	struct xrep_newbt	new_fork_info;
 
 	struct xfs_scrub	*sc;
 
-	/* Inode we're fixing. */
-	xfs_ino_t		ino;
-
-	/* How many blocks did we find in the other fork? */
-	xfs_rfsblock_t		otherfork_blocks;
+	/* How many blocks did we find allocated to this file? */
+	xfs_rfsblock_t		nblocks;
 
 	/* How many bmbt blocks did we find for this fork? */
-	xfs_rfsblock_t		bmbt_blocks;
+	xfs_rfsblock_t		old_bmbt_block_count;
+
+	/* Iterator for the bmap records. */
+	uint64_t		iter;
 
 	/* Which fork are we fixing? */
 	int			whichfork;
@@ -88,7 +74,8 @@ xrep_bmap_walk_rmap(
 	void			*priv)
 {
 	struct xrep_bmap	*rb = priv;
-	struct xrep_bmap_extent	rbe;
+	struct xfs_bmbt_rec	rbe;
+	struct xfs_bmbt_irec	irec;
 	struct xfs_mount	*mp = cur->bc_mp;
 	xfs_fsblock_t		fsbno;
 	int			error = 0;
@@ -97,34 +84,54 @@ xrep_bmap_walk_rmap(
 		return error;
 
 	/* Skip extents which are not owned by this inode and fork. */
-	if (rec->rm_owner != rb->ino) {
+	if (rec->rm_owner != rb->sc->ip->i_ino)
 		return 0;
-	} else if (rb->whichfork == XFS_DATA_FORK &&
-		 (rec->rm_flags & XFS_RMAP_ATTR_FORK)) {
-		rb->otherfork_blocks += rec->rm_blockcount;
-		return 0;
-	} else if (rb->whichfork == XFS_ATTR_FORK &&
-		 !(rec->rm_flags & XFS_RMAP_ATTR_FORK)) {
-		rb->otherfork_blocks += rec->rm_blockcount;
-		return 0;
-	}
 
-	/* Delete the old bmbt blocks later. */
+	rb->nblocks += rec->rm_blockcount;
+
+	/* If this rmap isn't for the fork we want, we're done. */
+	if (rb->whichfork == XFS_DATA_FORK &&
+	    (rec->rm_flags & XFS_RMAP_ATTR_FORK))
+		return 0;
+	if (rb->whichfork == XFS_ATTR_FORK &&
+	    !(rec->rm_flags & XFS_RMAP_ATTR_FORK))
+		return 0;
+
+	/* Remember any old bmbt blocks we find so we can delete them later. */
 	if (rec->rm_flags & XFS_RMAP_BMBT_BLOCK) {
 		fsbno = XFS_AGB_TO_FSB(mp, cur->bc_private.a.agno,
 				rec->rm_startblock);
-		rb->bmbt_blocks += rec->rm_blockcount;
-		return xbitmap_set(rb->btlist, fsbno, rec->rm_blockcount);
+		rb->old_bmbt_block_count += rec->rm_blockcount;
+		return xbitmap_set(&rb->old_bmbt_blocks, fsbno,
+				rec->rm_blockcount);
 	}
 
-	/* Remember this rmap. */
-	rbe.startoff = rec->rm_offset;
-	rbe.startblock = XFS_AGB_TO_FSB(mp, cur->bc_private.a.agno,
-			rec->rm_startblock);
-	rbe.blockcount = rec->rm_blockcount;
+	/* Remember this rmap as a series of bmap records. */
+	irec.br_startoff = rec->rm_offset;
+	irec.br_startblock = XFS_AGB_TO_FSB(mp, cur->bc_private.a.agno,
+					rec->rm_startblock);
 	if (rec->rm_flags & XFS_RMAP_UNWRITTEN)
-		rbe.startoff |= XREP_BMAP_UNWRITTEN;
-	return xfbma_append(rb->bmap_records, &rbe);
+		irec.br_state = XFS_EXT_UNWRITTEN;
+	else
+		irec.br_state = XFS_EXT_NORM;
+
+	do {
+		xfs_extlen_t len = min_t(xfs_filblks_t, rec->rm_blockcount,
+					 MAXEXTLEN);
+
+		irec.br_blockcount = len;
+		xfs_bmbt_disk_set_all(&rbe, &irec);
+
+		trace_xrep_bmap_found(rb->sc->ip, rb->whichfork, &irec);
+
+		error = xfbma_append(rb->bmap_records, &rbe);
+
+		irec.br_startblock += len;
+		irec.br_startoff += len;
+		rec->rm_blockcount -= len;
+	} while (error == 0 && rec->rm_blockcount > 0);
+
+	return error;
 }
 
 /* Compare two bmap extents. */
@@ -133,8 +140,11 @@ xrep_bmap_extent_cmp(
 	const void			*a,
 	const void			*b)
 {
-	xfs_fileoff_t			ao = xrep_bmap_startoff(a);
-	xfs_fileoff_t			bo = xrep_bmap_startoff(b);
+	xfs_fileoff_t			ao;
+	xfs_fileoff_t			bo;
+
+	ao = xfs_bmbt_disk_get_startoff((struct xfs_bmbt_rec *)a);
+	bo = xfs_bmbt_disk_get_startoff((struct xfs_bmbt_rec *)b);
 
 	if (ao > bo)
 		return 1;
@@ -166,56 +176,6 @@ xrep_bmap_scan_ag(
 		error = 0;
 	xfs_btree_del_cursor(cur, error);
 	xfs_trans_brelse(sc->tp, agf_bp);
-	return error;
-}
-
-struct xrep_add_bmap {
-	struct xfs_scrub	*sc;
-	int			baseflags;
-};
-
-/* Insert bmap records into an inode fork, given an rmap. */
-STATIC int
-xrep_bmap_insert_rec(
-	const void			*item,
-	void				*priv)
-{
-	const struct xrep_bmap_extent	*rbe = item;
-	struct xfs_bmbt_irec		bmap = {
-		.br_startblock	= rbe->startblock,
-		.br_startoff	= xrep_bmap_startoff(rbe),
-		.br_blockcount	= rbe->blockcount,
-	};
-	struct xrep_add_bmap		*x = priv;
-	xfs_extlen_t			extlen;
-	int				flags = x->baseflags;
-	int				error = 0;
-
-	if (rbe->startoff & XREP_BMAP_UNWRITTEN)
-		flags |= XFS_BMAPI_PREALLOC;
-	while (bmap.br_blockcount > 0) {
-		extlen = min_t(xfs_filblks_t, bmap.br_blockcount, MAXEXTLEN);
-
-		/* Re-add the extent to the fork. */
-		error = xfs_bmapi_remap(x->sc->tp, x->sc->ip, bmap.br_startoff,
-				extlen, bmap.br_startblock, flags);
-		if (error)
-			goto out;
-
-		bmap.br_startblock += extlen;
-		bmap.br_startoff += extlen;
-		bmap.br_blockcount -= extlen;
-
-		error = xfs_defer_finish(&x->sc->tp);
-		if (error)
-			goto out;
-		/* Make sure we roll the transaction. */
-		error = xfs_trans_roll_inode(&x->sc->tp, x->sc->ip);
-		if (error)
-			goto out;
-	}
-
-out:
 	return error;
 }
 
@@ -271,164 +231,264 @@ xrep_bmap_check_inputs(
  */
 STATIC int
 xrep_bmap_find_mappings(
-	struct xfs_scrub	*sc,
-	int			whichfork,
-	struct xfbma		*bmap_records,
-	struct xbitmap		*old_bmbt_blocks,
-	xfs_rfsblock_t		*old_bmbt_block_count,
-	xfs_rfsblock_t		*otherfork_blocks)
+	struct xrep_bmap	*rb)
 {
-	struct xrep_bmap	rb = {
-		.sc		= sc,
-		.bmap_records	= bmap_records,
-		.btlist		= old_bmbt_blocks,
-		.ino		= sc->ip->i_ino,
-		.whichfork	= whichfork,
-	};
+	struct xfs_scrub	*sc = rb->sc;
 	xfs_agnumber_t		agno;
-	unsigned int		resblks;
-	int			error;
+	int			error = 0;
 
 	/* Iterate the rmaps for extents. */
 	for (agno = 0; agno < sc->mp->m_sb.sb_agcount; agno++) {
-		error = xrep_bmap_scan_ag(&rb, agno);
+		error = xrep_bmap_scan_ag(rb, agno);
 		if (error)
 			return error;
 	}
+
+	return 0;
+}
+
+/* Retrieve bmap data for bulk load. */
+STATIC int
+xrep_bmap_get_data(
+	struct xfs_btree_cur	*cur,
+	void			*priv)
+{
+	struct xfs_bmbt_rec	rec;
+	struct xfs_bmbt_irec	*irec = &cur->bc_rec.b;
+	struct xrep_bmap	*rb = priv;
+	int			error;
+
+	do {
+		error = xfbma_get(rb->bmap_records, rb->iter++, &rec);
+	} while (error == 0 && xfbma_is_null(rb->bmap_records, &rec));
+
+	xfs_bmbt_disk_get_all(&rec, irec);
+	return error;
+}
+
+/* Feed one of the new btree blocks to the bulk loader. */
+STATIC int
+xrep_bmap_alloc_block(
+	struct xfs_btree_cur	*cur,
+	union xfs_btree_ptr	*ptr,
+	void			*priv)
+{
+	struct xrep_bmap        *rb = priv;
+
+	return xrep_newbt_alloc_block(cur, &rb->new_fork_info, ptr);
+}
+
+/* Figure out how much space we need to create the incore btree root block. */
+STATIC size_t
+xrep_bmap_iroot_size(
+	struct xfs_btree_cur	*cur,
+	unsigned int		nr_this_level,
+	void			*priv)
+{
+	return XFS_BMAP_BROOT_SPACE_CALC(cur->bc_mp, nr_this_level);
+}
+
+/* Update the inode counters. */
+STATIC int
+xrep_bmap_reset_counters(
+	struct xrep_bmap	*rb)
+{
+	struct xfs_scrub	*sc = rb->sc;
+	int64_t			delta;
+	int			error;
+
+	/*
+	 * Update the inode block counts to reflect the extents we found in the
+	 * rmapbt.
+	 */
+	delta = rb->new_fork_info.ifake.if_blocks - rb->old_bmbt_block_count;
+	sc->ip->i_d.di_nblocks = rb->nblocks + delta;
+	xfs_trans_log_inode(sc->tp, sc->ip, XFS_ILOG_CORE);
+
+	/*
+	 * Adjust the quota counts by the difference in size between the old
+	 * and new bmbt.
+	 */
+	if (delta == 0 || !XFS_IS_QUOTA_ON(sc->mp))
+		return 0;
+
+	error = xrep_ino_dqattach(sc);
+	if (error)
+		return error;
+
+	xfs_trans_mod_dquot_byino(sc->tp, sc->ip, XFS_TRANS_DQ_BCOUNT, delta);
+	return 0;
+}
+
+/* Create a new iext tree and load it with block mappings. */
+STATIC int
+xrep_bmap_extents_load(
+	struct xrep_bmap	*rb,
+	struct xfs_btree_cur	*bmap_cur)
+{
+	struct xfs_iext_cursor	icur;
+	struct xbtree_ifakeroot	*ifake = &rb->new_fork_info.ifake;
+	struct xfs_ifork	*ifp = ifake->if_fork;
+	unsigned int		i;
+	int			error;
+
+	ASSERT(ifp->if_bytes == 0);
+
+	/* Add all the records to the incore extent tree. */
+	rb->iter = 0;
+	xfs_iext_first(ifp, &icur);
+	for (i = 0; i < ifake->if_extents; i++) {
+		error = xrep_bmap_get_data(bmap_cur, rb);
+		if (error)
+			return error;
+		xfs_iext_insert_raw(ifp, &icur, &bmap_cur->bc_rec.b);
+		xfs_iext_next(ifp, &icur);
+	}
+	ifp->if_flags = XFS_IFEXTENTS;
+
+	return 0;
+}
+
+/* Reserve new btree blocks and bulk load all the bmap records. */
+STATIC int
+xrep_bmap_btree_load(
+	struct xrep_bmap	*rb,
+	struct xfs_btree_cur	**bmap_curp)
+{
+	struct xfs_btree_bload	bmap_bload = {
+		.get_data	= xrep_bmap_get_data,
+		.alloc_block	= xrep_bmap_alloc_block,
+		.iroot_size	= xrep_bmap_iroot_size,
+	};
+	struct xfs_scrub	*sc = rb->sc;
+	struct xbtree_ifakeroot	*ifake = &rb->new_fork_info.ifake;
+	int			error;
+
+	/* Compute how many blocks we'll need. */
+	error = xfs_btree_bload_compute_geometry(*bmap_curp, &bmap_bload,
+			ifake->if_extents);
+	if (error)
+		return error;
+	xfs_btree_del_cursor(*bmap_curp, error);
+	*bmap_curp = NULL;
 
 	/*
 	 * Guess how many blocks we're going to need to rebuild an entire bmap
 	 * from the number of extents we found, and pump up our transaction to
 	 * have sufficient block reservation.
 	 */
-	resblks = xfs_bmbt_calc_size(sc->mp, xfbma_length(bmap_records));
-	error = xfs_trans_reserve_more(sc->tp, resblks, 0);
+	error = xfs_trans_reserve_more(sc->tp, bmap_bload.nr_blocks, 0);
 	if (error)
 		return error;
 
-	*otherfork_blocks = rb.otherfork_blocks;
-	*old_bmbt_block_count = rb.bmbt_blocks;
-	return 0;
-}
-
-/* Update the inode counters. */
-STATIC int
-xrep_bmap_reset_counters(
-	struct xfs_scrub	*sc,
-	xfs_rfsblock_t		old_bmbt_block_count,
-	xfs_rfsblock_t		otherfork_blocks,
-	int			*log_flags)
-{
-	int			error;
-
-	xfs_trans_ijoin(sc->tp, sc->ip, 0);
-
 	/*
-	 * We're going to use the bmap routines to reconstruct a fork from rmap
-	 * records.  Those functions increment di_nblocks for us, so we need to
-	 * subtract out all the data and bmbt blocks from the fork we're about
-	 * to rebuild.  otherfork_blocks reflects all the data and bmbt blocks
-	 * for the other fork, so this assignment effectively performs the
-	 * subtraction for us.
+	 * Reserve the space we'll need for the new btree.  Drop the cursor
+	 * while we do this because that can roll the transaction and cursors
+	 * can't handle that.
 	 */
-	sc->ip->i_d.di_nblocks = otherfork_blocks;
-	*log_flags |= XFS_ILOG_CORE;
-
-	if (!old_bmbt_block_count)
-		return 0;
-
-	/* Release quota counts for the old bmbt blocks. */
-	error = xrep_ino_dqattach(sc);
+	error = xrep_newbt_reserve_space(&rb->new_fork_info,
+			bmap_bload.nr_blocks);
 	if (error)
 		return error;
-	xfs_trans_mod_dquot_byino(sc->tp, sc->ip, XFS_TRANS_DQ_BCOUNT,
-			-(int64_t)old_bmbt_block_count);
-	return 0;
+
+	/* Add all observed bmap records. */
+	rb->iter = 0;
+	*bmap_curp = xfs_bmbt_stage_cursor(sc->mp, sc->tp, sc->ip,
+			&rb->new_fork_info.ifake);
+	return xfs_btree_bload(*bmap_curp, &bmap_bload, rb);
 }
 
-/* Initialize a new fork and implant it in the inode. */
-STATIC void
-xrep_bmap_reset_fork(
-	struct xfs_scrub	*sc,
-	int			whichfork,
-	bool			has_mappings,
-	int			*log_flags)
-{
-	/* Set us back to extents format with zero records. */
-	XFS_IFORK_FMT_SET(sc->ip, whichfork, XFS_DINODE_FMT_EXTENTS);
-	XFS_IFORK_NEXT_SET(sc->ip, whichfork, 0);
-
-	/* Reinitialize the in-core fork. */
-	if (XFS_IFORK_PTR(sc->ip, whichfork) != NULL)
-		xfs_idestroy_fork(sc->ip, whichfork);
-	if (whichfork == XFS_DATA_FORK) {
-		memset(&sc->ip->i_df, 0, sizeof(struct xfs_ifork));
-		sc->ip->i_df.if_flags |= XFS_IFEXTENTS;
-	} else if (whichfork == XFS_ATTR_FORK) {
-		if (has_mappings) {
-			sc->ip->i_afp = NULL;
-		} else {
-			sc->ip->i_afp = kmem_zone_zalloc(xfs_ifork_zone,
-					KM_SLEEP);
-			sc->ip->i_afp->if_flags |= XFS_IFEXTENTS;
-		}
-	}
-
-	/*
-	 * Now that we've reinitialized the in-memory fork and set the inode
-	 * back to extents format with zero extents, any extents that we
-	 * subsequently map into the file will reinitialize the on-disk fork
-	 * area for us.  All we have to do is log the inode core to preserve
-	 * the format and extent count fields.
-	 */
-	*log_flags |= XFS_ILOG_CORE;
-}
-
-/* Make our changes permanent so that we can start rebuilding the fork. */
+/*
+ * Use the collected bmap information to stage a new bmap fork.  If this is
+ * successful we'll return with the new fork information logged to the repair
+ * transaction but not yet committed.
+ */
 STATIC int
-xrep_bmap_commit_new(
-	struct xfs_scrub	*sc,
-	int			log_flags)
-{
-	xfs_trans_log_inode(sc->tp, sc->ip, log_flags);
-	return xfs_trans_roll_inode(&sc->tp, sc->ip);
-}
-
-/* Build new fork mappings and dispose of the old bmbt blocks. */
-STATIC int
-xrep_bmap_rebuild_tree(
-	struct xfs_scrub	*sc,
-	int			whichfork,
-	struct xfbma		*bmap_records,
-	struct xbitmap		*old_bmbt_blocks)
+xrep_bmap_build_new_fork(
+	struct xrep_bmap	*rb)
 {
 	struct xfs_owner_info	oinfo;
-	struct xrep_add_bmap	x = {
-		.sc		= sc,
-		.baseflags	= XFS_BMAPI_NORMAP,
-	};
+	struct xfs_scrub	*sc = rb->sc;
+	struct xfs_btree_cur	*bmap_cur;
+	struct xbtree_ifakeroot	*ifake = &rb->new_fork_info.ifake;
 	int			error;
-
-	if (whichfork == XFS_ATTR_FORK)
-		x.baseflags |= XFS_BMAPI_ATTRFORK;
 
 	/*
 	 * Sort the bmap extents by startblock to avoid btree splits when we
 	 * rebuild the bmbt btree.
 	 */
-	error = xfbma_sort(bmap_records, xrep_bmap_extent_cmp);
+	error = xfbma_sort(rb->bmap_records, xrep_bmap_extent_cmp);
 	if (error)
 		return error;
 
-	/* Dispose of all the old bmbt blocks. */
-	xfs_rmap_ino_bmbt_owner(&oinfo, sc->ip->i_ino, whichfork);
-	error = xrep_reap_extents(sc, old_bmbt_blocks, &oinfo,
+	/*
+	 * Prepare to construct the new fork by initializing the new btree
+	 * structure and creating a fake ifork in the ifakeroot structure.
+	 */
+	xfs_rmap_ino_bmbt_owner(&oinfo, sc->ip->i_ino, rb->whichfork);
+	xrep_newbt_init_inode(&rb->new_fork_info, sc, rb->whichfork, &oinfo);
+	bmap_cur = xfs_bmbt_stage_cursor(sc->mp, sc->tp, sc->ip,
+			&rb->new_fork_info.ifake);
+
+	/*
+	 * Figure out the size and format of the new fork, then fill it with
+	 * all the bmap records we've found.  Join the inode to the transaction
+	 * so that we can roll the transaction while holding the inode locked.
+	 */
+	xfs_trans_ijoin(sc->tp, sc->ip, 0);
+	ifake->if_extents = xfbma_length(rb->bmap_records);
+	if (XFS_BMDR_SPACE_CALC(ifake->if_extents) <=
+	    XFS_DFORK_SIZE(&sc->ip->i_d, sc->mp, rb->whichfork)) {
+		ifake->if_format = XFS_DINODE_FMT_EXTENTS;
+		error = xrep_bmap_extents_load(rb, bmap_cur);
+	} else {
+		ifake->if_format = XFS_DINODE_FMT_BTREE;
+		error = xrep_bmap_btree_load(rb, &bmap_cur);
+	}
+	if (error)
+		goto err_cur;
+
+	/*
+	 * Install the new fork in the inode.  After this point the old mapping
+	 * data are no longer accessible and the new tree is live.  We delete
+	 * the cursor immediately after committing the staged root because the
+	 * staged fork might be in extents format.
+	 */
+	xfs_bmbt_commit_staged_btree(bmap_cur, rb->whichfork);
+	xfs_btree_del_cursor(bmap_cur, 0);
+
+	/* Reset the inode counters now that we've changed the fork. */
+	error = xrep_bmap_reset_counters(rb);
+	if (error)
+		goto err_newbt;
+
+	/* Dispose of any unused blocks and the accounting infomation. */
+	xrep_newbt_destroy(&rb->new_fork_info, error);
+
+	return xfs_trans_roll_inode(&sc->tp, sc->ip);
+err_cur:
+	if (bmap_cur)
+		xfs_btree_del_cursor(bmap_cur, error);
+err_newbt:
+	xrep_newbt_destroy(&rb->new_fork_info, error);
+	return error;
+}
+
+/*
+ * Now that we've logged the new inode btree, invalidate all of the old blocks
+ * and free them, if there were any.
+ */
+STATIC int
+xrep_bmap_remove_old_tree(
+	struct xrep_bmap	*rb)
+{
+	struct xfs_scrub*	sc = rb->sc;
+	struct xfs_owner_info	oinfo;
+
+	/* Free the old bmbt blocks if they're not in use. */
+	xfs_rmap_ino_bmbt_owner(&oinfo, sc->ip->i_ino, rb->whichfork);
+	return xrep_reap_extents(sc, &rb->old_bmbt_blocks, &oinfo,
 			XFS_AG_RESV_NONE);
-	if (error)
-		return error;
-
-	/* "Remap" the extents into the fork. */
-	return xfbma_iter_del(bmap_records, xrep_bmap_insert_rec, &x);
 }
 
 /* Repair an inode fork. */
@@ -437,50 +497,45 @@ xrep_bmap(
 	struct xfs_scrub	*sc,
 	int			whichfork)
 {
-	struct xbitmap		old_bmbt_blocks;
-	struct xfbma		*bmap_records;
-	xfs_rfsblock_t		old_bmbt_block_count;
-	xfs_rfsblock_t		otherfork_blocks;
-	int			log_flags = 0;
+	struct xrep_bmap	*rb;
 	int			error = 0;
 
 	error = xrep_bmap_check_inputs(sc, whichfork);
 	if (error)
 		return error;
 
+	rb = kmem_zalloc(sizeof(struct xrep_bmap), KM_NOFS | KM_MAYFAIL);
+	if (!rb)
+		return -ENOMEM;
+	rb->sc = sc;
+	rb->whichfork = whichfork;
+
 	/* Set up some storage */
-	bmap_records = xfbma_init(sizeof(struct xrep_bmap_extent));
-	if (IS_ERR(bmap_records))
-		return PTR_ERR(bmap_records);
+	rb->bmap_records = xfbma_init(sizeof(struct xfs_bmbt_rec));
+	if (IS_ERR(rb->bmap_records)) {
+		error = PTR_ERR(rb->bmap_records);
+		goto out_rb;
+	}
 
 	/* Collect all reverse mappings for this fork's extents. */
-	xbitmap_init(&old_bmbt_blocks);
-	error = xrep_bmap_find_mappings(sc, whichfork, bmap_records,
-			&old_bmbt_blocks, &old_bmbt_block_count,
-			&otherfork_blocks);
+	xbitmap_init(&rb->old_bmbt_blocks);
+	error = xrep_bmap_find_mappings(rb);
 	if (error)
-		goto out;
+		goto out_bitmap;
 
-	/*
-	 * Blow out the in-core fork and zero the on-disk fork.  This is the
-	 * point at which we are no longer able to bail out gracefully.
-	 */
-	error = xrep_bmap_reset_counters(sc, old_bmbt_block_count,
-			otherfork_blocks, &log_flags);
+	/* Rebuild the bmap information. */
+	error = xrep_bmap_build_new_fork(rb);
 	if (error)
-		goto out;
-	xrep_bmap_reset_fork(sc, whichfork, xfbma_length(bmap_records) == 0,
-			&log_flags);
-	error = xrep_bmap_commit_new(sc, log_flags);
-	if (error)
-		goto out;
+		goto out_bitmap;
 
-	/* Now rebuild the fork extent map information. */
-	error = xrep_bmap_rebuild_tree(sc, whichfork, bmap_records,
-			&old_bmbt_blocks);
-out:
-	xbitmap_destroy(&old_bmbt_blocks);
-	xfbma_destroy(bmap_records);
+	/* Kill the old tree. */
+	error = xrep_bmap_remove_old_tree(rb);
+
+out_bitmap:
+	xbitmap_destroy(&rb->old_bmbt_blocks);
+	xfbma_destroy(rb->bmap_records);
+out_rb:
+	kmem_free(rb);
 	return error;
 }
 
