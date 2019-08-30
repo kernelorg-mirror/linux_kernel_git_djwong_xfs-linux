2dc855e611b8ec86e60b7edd345ea522ef4de36d
Darrick J. Wong <darrick.wong@oracle.com>
2019-08-30 15:45:48 -0700

    xfs: cross-reference realtime bitmap to realtime rmapbt scrubber
    When we're checking the realtime rmapbt, cross-reference the entries
    with the realtime bitmap too.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/rtbitmap.c
+++ fs/xfs/scrub/rtbitmap.c
@@ -136,13 +136,10 @@ xchk_xref_is_used_rt_space(
 	do_div(startext, sc->mp->m_sb.sb_rextsize);
 	do_div(endext, sc->mp->m_sb.sb_rextsize);
 	extcount = endext - startext + 1;
-	xfs_ilock(sc->mp->m_rbmip, XFS_ILOCK_SHARED | XFS_ILOCK_RTBITMAP);
 	error = xfs_rtalloc_extent_is_free(sc->mp, sc->tp, startext, extcount,
 			&is_free);
 	if (!xchk_should_check_xref(sc, &error, NULL))
-		goto out_unlock;
+		return;
 	if (is_free)
 		xchk_ino_xref_set_corrupt(sc, sc->mp->m_rbmip->i_ino);
-out_unlock:
-	xfs_iunlock(sc->mp->m_rbmip, XFS_ILOCK_SHARED | XFS_ILOCK_RTBITMAP);
 }
