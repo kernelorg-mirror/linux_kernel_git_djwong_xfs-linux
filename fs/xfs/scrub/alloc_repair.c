fee0a0fd177ba708537c0fa18f92f31e72f2a103
Darrick J. Wong <darrick.wong@oracle.com>
2019-08-30 15:45:38 -0700

    xfs: widen xfs_rmap_irec fields to handle realtime rmapbt
    Change the startblock and blockcount fields of xfs_rmap_irec to be 64
    bits wide.  This enables us to use the same high level rmap code for
    either tree.  We'll also collect all the resulting breakage fixes here.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/alloc_repair.c
+++ fs/xfs/scrub/alloc_repair.c
@@ -404,10 +404,8 @@ xrep_abt_dispose_reservations(
 	for_each_xrep_newbt_reservation(&ra->new_bnobt_info, resv, n) {
 		/* Add a deferred rmap for each extent we used. */
 		if (resv->used > 0)
-			xfs_rmap_alloc_extent(sc->tp,
-					XFS_FSB_TO_AGNO(sc->mp, resv->fsbno),
-					XFS_FSB_TO_AGBNO(sc->mp, resv->fsbno),
-					resv->used, XFS_RMAP_OWN_AG);
+			xfs_rmap_alloc_extent(sc->tp, resv->fsbno, resv->used,
+					XFS_RMAP_OWN_AG);
 
 		/*
 		 * Add a deferred free for each block we didn't use and now
