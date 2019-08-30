1034eb85aa650bfc1bf7034837e8dbc3f1f671f7
Darrick J. Wong <darrick.wong@oracle.com>
2019-08-30 15:45:11 -0700

    xfs: parallelize inode inactivation
    Split the inode inactivation work into per-AG work items so that we can
    take advantage of parallelization.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/common.c
+++ fs/xfs/scrub/common.c
@@ -910,6 +910,7 @@ xchk_stop_reaping(
 {
 	sc->flags |= XCHK_REAPING_DISABLED;
 	xfs_stop_block_reaping(sc->mp);
+	xfs_inactive_cancel_work(sc->mp);
 }
 
 /* Restart background reaping of resources. */
@@ -917,6 +918,7 @@ void
 xchk_start_reaping(
 	struct xfs_scrub	*sc)
 {
+	xfs_inactive_schedule_work(sc->mp, 0);
 	xfs_start_block_reaping(sc->mp);
 	sc->flags &= ~XCHK_REAPING_DISABLED;
 }
