1f61e93c2789d08ef5488b2ee8026ed98a43ca2a
Darrick J. Wong <darrick.wong@oracle.com>
2019-08-30 15:45:09 -0700

    xfs: deferred inode inactivation
    Instead of calling xfs_inactive directly from xfs_fs_destroy_inode,
    defer the inactivation phase to a separate workqueue.  With this we
    avoid blocking memory reclaim on filesystem metadata updates that are
    necessary to free an in-core inode, such as post-eof block freeing, COW
    staging extent freeing, and truncating and freeing unlinked inodes.  Now
    that work is deferred to a workqueue where we can do the freeing in
    batches.
    
    We introduce two new inode flags -- NEEDS_INACTIVE and INACTIVATING.
    The first flag helps our worker find inodes needing inactivation, and
    the second flag marks inodes that are in the process of being
    inactivated.  A concurrent xfs_iget on the inode can still resurrect the
    inode by clearing NEEDS_INACTIVE (or bailing if INACTIVATING is set).
    
    Unfortunately, deferring the inactivation has one huge downside --
    eventual consistency.  Since all the freeing is deferred to a worker
    thread, one can rm a file but the space doesn't come back immediately.
    This can cause some odd side effects with quota accounting and statfs,
    so we also force inactivation scans in order to maintain the existing
    behaviors, at least outwardly.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_inode.h
+++ fs/xfs/xfs_inode.h
@@ -227,6 +227,7 @@ static inline bool xfs_inode_has_cow_data(struct xfs_inode *ip)
 #define XFS_IRECLAIMABLE	(1 << 2) /* inode can be reclaimed */
 #define __XFS_INEW_BIT		3	 /* inode has just been allocated */
 #define XFS_INEW		(1 << __XFS_INEW_BIT)
+#define XFS_NEED_INACTIVE	(1 << 4) /* see XFS_INACTIVATING below */
 #define XFS_ITRUNCATED		(1 << 5) /* truncated down so flush-on-close */
 #define XFS_IDIRTY_RELEASE	(1 << 6) /* dirty release already seen */
 #define __XFS_IFLOCK_BIT	7	 /* inode is being flushed right now */
@@ -244,13 +245,23 @@ static inline bool xfs_inode_has_cow_data(struct xfs_inode *ip)
 #define XFS_ICOWBLOCKS		(1 << 12)/* has the cowblocks tag set */
 
 /*
+ * If we need to update on-disk metadata before this IRECLAIMABLE inode can be
+ * freed, then NEED_INACTIVE will be set.  Once we start the updates, the
+ * INACTIVATING bit will be set to keep iget away from this inode.  After the
+ * inactivation completes, both flags will be cleared and the inode is a
+ * plain old IRECLAIMABLE inode.
+ */
+#define XFS_INACTIVATING	(1 << 13)
+
+/*
  * Per-lifetime flags need to be reset when re-using a reclaimable inode during
  * inode lookup. This prevents unintended behaviour on the new inode from
  * ocurring.
  */
 #define XFS_IRECLAIM_RESET_FLAGS	\
 	(XFS_IRECLAIMABLE | XFS_IRECLAIM | \
-	 XFS_IDIRTY_RELEASE | XFS_ITRUNCATED)
+	 XFS_IDIRTY_RELEASE | XFS_ITRUNCATED | XFS_NEED_INACTIVE | \
+	 XFS_INACTIVATING)
 
 /*
  * Synchronize processes attempting to flush the in-core inode back to disk.
@@ -514,6 +525,8 @@ extern struct kmem_zone	*xfs_inode_zone;
 bool xfs_inode_verify_forks(struct xfs_inode *ip);
 int xfs_has_eofblocks(struct xfs_inode *ip, bool *has);
 bool xfs_inode_needs_inactivation(struct xfs_inode *ip);
+void xfs_inode_inactivation_prep(struct xfs_inode *ip);
+void xfs_inode_inactivation_cleanup(struct xfs_inode *ip);
 
 int xfs_iunlink_init(struct xfs_perag *pag);
 void xfs_iunlink_destroy(struct xfs_perag *pag);
