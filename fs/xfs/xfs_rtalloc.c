a2b01dd3bbc2806847711c3d7b161fc92137a1ae
Darrick J. Wong <darrick.wong@oracle.com>
2019-10-09 18:28:41 -0700

    xfs: report realtime rmap btree corruption errors to the health system
    Whenever we encounter corrupt realtime rmap btree blocks, we should
    report that to the health monitoring system for later reporting.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -1326,6 +1326,7 @@ xfs_rtmount_inodes(
 		if (XFS_IS_CORRUPT(mp,
 				   mp->m_rrmapip->i_d.di_format !=
 				   XFS_DINODE_FMT_RMAP)) {
+			xfs_rt_mark_sick(mp, XFS_SICK_RT_RMAPBT);
 			error = -EFSCORRUPTED;
 			goto out_rrmap;
 		}
