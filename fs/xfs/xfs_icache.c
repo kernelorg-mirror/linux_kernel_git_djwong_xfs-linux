a6e74be69af6999c29a8276fc05f8f2ebdae8154
Darrick J. Wong <darrick.wong@oracle.com>
2019-10-09 18:10:38 -0700

    xfs: report inode corruption errors to the health system
    Whenever we encounter corrupt inode records, we should report that to
    the health monitoring system for later reporting.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_icache.c
+++ fs/xfs/xfs_icache.c
@@ -24,6 +24,7 @@
 #include "xfs_reflink.h"
 #include "xfs_da_format.h"
 #include "xfs_dir2.h"
+#include "xfs_health.h"
 #include "xfs_imeta.h"
 
 #include <linux/iversion.h>
@@ -382,6 +383,7 @@ xfs_iget_check_free_state(
 			xfs_warn(ip->i_mount,
 "Corruption detected! Free inode 0x%llx not marked free! (mode 0x%x)",
 				ip->i_ino, VFS_I(ip)->i_mode);
+			xfs_inode_mark_sick(ip, XFS_SICK_INO_CORE);
 			return -EFSCORRUPTED;
 		}
 
@@ -389,6 +391,7 @@ xfs_iget_check_free_state(
 			xfs_warn(ip->i_mount,
 "Corruption detected! Free inode 0x%llx has blocks allocated!",
 				ip->i_ino);
+			xfs_inode_mark_sick(ip, XFS_SICK_INO_CORE);
 			return -EFSCORRUPTED;
 		}
 		return 0;
@@ -657,6 +660,7 @@ xfs_iget_cache_miss(
 		goto out_destroy;
 
 	if (!xfs_inode_verify_forks(ip)) {
+		xfs_inode_mark_sick(ip, XFS_SICK_INO_CORE);
 		error = -EFSCORRUPTED;
 		goto out_destroy;
 	}
