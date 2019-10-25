40466036249751c350bf62e8f33f55d8d0ee3e67
Christoph Hellwig <hch@lst.de>
2019-10-25 09:57:04 -0700

    xfs: simplify the xfs_iomap_write_direct calling
    Move the EOF alignment and checking for the next allocated extent into
    the callers to avoid the need to pass the byte based offset and count
    as well as looking at the incoming imap.  The added benefit is that
    the caller can unlock the incoming ilock and the function doesn't have
    funny unbalanced locking contexts.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_pnfs.c
+++ fs/xfs/xfs_pnfs.c
@@ -143,22 +143,19 @@ xfs_fs_map_blocks(
 	lock_flags = xfs_ilock_data_map_shared(ip);
 	error = xfs_bmapi_read(ip, offset_fsb, end_fsb - offset_fsb,
 				&imap, &nimaps, bmapi_flags);
-	xfs_iunlock(ip, lock_flags);
-
-	if (error)
-		goto out_unlock;
-
-	if (write &&
+	if (!error && write &&
 	    (!nimaps || imap.br_startblock == HOLESTARTBLOCK)) {
 		ASSERT(imap.br_startblock != DELAYSTARTBLOCK);
 
-		/*
-		 * xfs_iomap_write_direct() expects to take ownership of the
-		 * shared ilock.
-		 */
-		xfs_ilock(ip, XFS_ILOCK_SHARED);
-		error = xfs_iomap_write_direct(ip, offset, length, &imap,
-					       nimaps);
+		if (offset + length > XFS_ISIZE(ip))
+			end_fsb = xfs_iomap_eof_align_last_fsb(ip, end_fsb);
+		else if (nimaps && imap.br_startblock == HOLESTARTBLOCK)
+			end_fsb = min(end_fsb, imap.br_startoff +
+					       imap.br_blockcount);
+		xfs_iunlock(ip, lock_flags);
+
+		error = xfs_iomap_write_direct(ip, offset_fsb,
+				end_fsb - offset_fsb, &imap);
 		if (error)
 			goto out_unlock;
 
@@ -171,6 +168,8 @@ xfs_fs_map_blocks(
 				XFS_PREALLOC_SET | XFS_PREALLOC_SYNC);
 		if (error)
 			goto out_unlock;
+	} else {
+		xfs_iunlock(ip, lock_flags);
 	}
 	xfs_iunlock(ip, XFS_IOLOCK_EXCL);
 
