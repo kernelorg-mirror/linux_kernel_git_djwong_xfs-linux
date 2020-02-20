ff2385c162005906303741d383e79ea72ea81686
Darrick J. Wong <darrick.wong@oracle.com>
2020-02-19 17:02:01 -0800

    xfs: hide most of the incore inode walk interface
    Hide the incore inode walk interface because callers outside of the
    icache code don't need to know about iter_flags and radix tags and other
    implementation details of the incore inode cache.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_qm_syscalls.c
+++ fs/xfs/xfs_qm_syscalls.c
@@ -773,6 +773,5 @@ xfs_qm_dqrele_all_inodes(
 	uint			flags)
 {
 	ASSERT(mp->m_quotainfo);
-	xfs_inode_walk(mp, XFS_INODE_WALK_INEW_WAIT, xfs_dqrele_inode,
-			&flags, XFS_ICI_NO_TAG);
+	xfs_inode_walk(mp, xfs_dqrele_inode, &flags);
 }
