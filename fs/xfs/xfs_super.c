3c03698598dc89a52696f588da6c3f0436a11db5
Darrick J. Wong <darrick.wong@oracle.com>
2020-02-19 17:02:42 -0800

    xfs: define the on-disk format for the metadir feature
    Define the on-disk layout and feature flags for the metadata inode
    directory feature.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -1624,6 +1624,10 @@ xfs_fc_fill_super(
 		}
 	}
 
+	if (xfs_sb_version_hasmetadir(&mp->m_sb))
+		xfs_warn(mp,
+"EXPERIMENTAL metadata directory feature in use. Use at your own risk!");
+
 	if (xfs_sb_version_hasreflink(&mp->m_sb)) {
 		if (mp->m_sb.sb_rblocks) {
 			xfs_alert(mp,
