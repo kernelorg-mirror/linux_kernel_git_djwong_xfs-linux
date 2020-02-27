9ecf1c09564028d33153780da2984e57ec96087c
Darrick J. Wong <darrick.wong@oracle.com>
2020-02-27 14:06:28 -0800

    xfs: mark extended attr corrupt when lookup-by-hash fails
    In xchk_xattr_listent, we attempt to validate the extended attribute
    hash structures by performing a attr lookup by (hashed) name.  If the
    lookup returns ENODATA, that means that the hash information is corrupt.
    The _process_error functions don't catch this, so we have to add that
    explicitly.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/attr.c
+++ fs/xfs/scrub/attr.c
@@ -163,6 +163,11 @@ xchk_xattr_listent(
 	args.valuelen = valuelen;
 
 	error = xfs_attr_get_ilocked(context->dp, &args);
+	if (error == -ENODATA) {
+		/* ENODATA means the hash lookup failed and the attr is bad */
+		xchk_fblock_set_corrupt(sx->sc, XFS_ATTR_FORK, args.blkno);
+		goto fail_xref;
+	}
 	if (!xchk_fblock_process_error(sx->sc, XFS_ATTR_FORK, args.blkno,
 			&error))
 		goto fail_xref;
