967c77d10b3e5b19d5750ae2b4952064e82965a5
Darrick J. Wong <darrick.wong@oracle.com>
2020-03-17 14:05:24 -0700

    xfs: online repair of realtime bitmaps
    Rebuild the realtime bitmap from the realtime rmap btree.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/repair.h
+++ fs/xfs/scrub/repair.h
@@ -100,8 +100,10 @@ int xrep_quota(struct xfs_scrub *sc);
 #endif /* CONFIG_XFS_QUOTA */
 #ifdef CONFIG_XFS_RT
 int xrep_rtsummary(struct xfs_scrub *sc);
+int xrep_rtbitmap(struct xfs_scrub *sc);
 #else
 # define xrep_rtsummary			xrep_notsupported
+# define xrep_rtbitmap			xrep_notsupported
 #endif /* CONFIG_XFS_RT */
 
 struct xrep_newbt_resv {
@@ -223,6 +225,7 @@ xrep_rmapbt_setup(
 #define xrep_fscounters			xrep_notsupported
 #define xrep_quota			xrep_notsupported
 #define xrep_rtsummary			xrep_notsupported
+#define xrep_rtbitmap			xrep_notsupported
 
 #endif /* CONFIG_XFS_ONLINE_REPAIR */
 
