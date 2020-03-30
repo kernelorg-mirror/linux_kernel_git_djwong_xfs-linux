9b520b559a5955733299e77823f226ca5ca9b791
Darrick J. Wong <darrick.wong@oracle.com>
2020-03-30 13:38:28 -0700

    xfs: add a ->swap_file_range handler
    Add a function to handle range swap requests from the vfs.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_swaprange.h
+++ fs/xfs/xfs_swaprange.h
@@ -15,5 +15,9 @@ void xfs_swap_range_iunlock(struct xfs_inode *ip1, struct xfs_inode *ip2);
 
 int xfs_swap_range_estimate(const struct xfs_swapext_req *req,
 		struct xfs_swapext_res *res);
+int xfs_swap_range_prep(struct file *file1, struct file *file2,
+		struct file_swap_range *fsr);
+int xfs_swap_range(struct xfs_inode *ip1, struct xfs_inode *ip2,
+		const struct file_swap_range *fsr);
 
 #endif /* __XFS_SWAPRANGE_H__ */
