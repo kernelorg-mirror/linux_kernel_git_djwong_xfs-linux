1fc008c26609e56f42ea200bc935eaa6d1435257
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:19:17 -0700

    xfs: decide if inode needs inactivation
    Add a predicate function to decide if an inode needs (deferred)
    inactivation.  Any file that has been unlinked or has speculative
    preallocations either for post-EOF writes or for CoW qualifies.
    This function will also be used by the upcoming deferred inactivation
    patch.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_inode.h
+++ fs/xfs/xfs_inode.h
@@ -495,6 +495,7 @@ extern struct kmem_zone	*xfs_inode_zone;
 #define XFS_DEFAULT_COWEXTSZ_HINT 32
 
 int xfs_has_eofblocks(struct xfs_inode *ip, bool *has);
+bool xfs_inode_needs_inactivation(struct xfs_inode *ip);
 
 int xfs_iunlink_init(struct xfs_perag *pag);
 void xfs_iunlink_destroy(struct xfs_perag *pag);
