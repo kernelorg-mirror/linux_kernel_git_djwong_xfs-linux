bd7852b1d6a745a4eed21862afc4e4db28a231f9
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:19:08 -0700

    xfs: implement live quotacheck inode scan
    Create a new trio of scrub functions to check quota counters.  While the
    dquots themselves are filesystem metadata and should be checked early,
    the dquot counter values are computed from other metadata and are
    therefore summary counters.  We don't plug these into the scrub dispatch
    just yet, because we still need to be able to watch quota updates while
    doing our scan.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/scrub.h
+++ fs/xfs/scrub/scrub.h
@@ -72,6 +72,12 @@ struct xfs_scrub {
 	/* Kernel memory buffer used by scrubbers; freed at teardown. */
 	void				*buf;
 
+	/*
+	 * Clean up resources pointed owned by the buffer.  This function must
+	 * not free the buffer itself.
+	 */
+	void				(*buf_cleanup)(void *buf);
+
 	/* xfile used by the scrubbers; freed at teardown. */
 	struct xfile			*xfile;
 
@@ -136,9 +142,15 @@ xchk_rtsummary(struct xfs_scrub *sc)
 #endif
 #ifdef CONFIG_XFS_QUOTA
 int xchk_quota(struct xfs_scrub *sc);
+int xchk_quotacheck(struct xfs_scrub *sc);
 #else
 static inline int
 xchk_quota(struct xfs_scrub *sc)
+{
+	return -ENOENT;
+}
+static inline int
+xchk_quotacheck(struct xfs_scrub *sc)
 {
 	return -ENOENT;
 }
