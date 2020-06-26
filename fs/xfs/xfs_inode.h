4d981a68a200af5b5f88f1774a5793cbc59ad081
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:19:40 -0700

    xfs: hoist xfs_iunlink to libxfs
    Move xfs_iunlink and xfs_iunlink_remove to libxfs.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_inode.h
+++ fs/xfs/xfs_inode.h
@@ -498,10 +498,6 @@ bool xfs_inode_needs_inactivation(struct xfs_inode *ip);
 void xfs_inode_inactivation_prep(struct xfs_inode *ip);
 void xfs_inode_inactivation_cleanup(struct xfs_inode *ip);
 
-int xfs_iunlink_init(struct xfs_perag *pag);
-void xfs_iunlink_destroy(struct xfs_perag *pag);
-int xfs_iunlink(struct xfs_trans *tp, struct xfs_inode *ip);
-
 void xfs_end_io(struct work_struct *work);
 
 int xfs_ilock2_io_mmap(struct xfs_inode *ip1, struct xfs_inode *ip2);
