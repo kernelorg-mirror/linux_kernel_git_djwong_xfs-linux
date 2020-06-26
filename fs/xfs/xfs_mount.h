1cd44ffbd557332cf106b8542c5c5cbfed37d2fa
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:19:52 -0700

    xfs: add realtime reverse map inode to superblock
    Add a metadir path to select the realtime rmap btree inode and load
    it at mount time.  The rtrmapbt inode will have a unique extent format
    code, which means that we also have to update the inode validation and
    flush routines to look for it.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -76,6 +76,7 @@ typedef struct xfs_mount {
 	struct xlog		*m_log;		/* log specific stuff */
 	struct xfs_inode	*m_rbmip;	/* pointer to bitmap inode */
 	struct xfs_inode	*m_rsumip;	/* pointer to summary inode */
+	struct xfs_inode	*m_rrmapip;	/* pointer to rmap inode */
 	struct xfs_inode	*m_rootip;	/* pointer to root directory */
 	struct xfs_inode	*m_metadirip;	/* metadata inode directory */
 	struct xfs_quotainfo	*m_quotainfo;	/* disk quota information */
