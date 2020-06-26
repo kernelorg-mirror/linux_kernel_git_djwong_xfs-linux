2ad897fb2776a30776bce0eb6b5e1aa40c2d54f0
Darrick J. Wong <darrick.wong@oracle.com>
2020-06-25 18:19:14 -0700

    xfs: flush speculative space allocations when we run out of quota
    If a fs modification (creation, file write, reflink, etc.) is unable to
    reserve enough quota to handle the modification, try clearing whatever
    space the filesystem might have been hanging onto in the hopes of
    speeding up the filesystem.  The flushing behavior will become
    particularly important when we add deferred inode inactivation because
    that will increase the amount of space that isn't actively tied to user
    data.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_trace.h
+++ fs/xfs/xfs_trace.h
@@ -37,6 +37,7 @@ struct xfs_trans_res;
 struct xfs_inobt_rec_incore;
 union xfs_btree_ptr;
 struct xfs_dqtrx;
+struct xfs_eofblocks;
 
 #define XFS_ATTR_FILTER_FLAGS \
 	{ XFS_ATTR_ROOT,	"ROOT" }, \
@@ -3870,6 +3871,45 @@ DEFINE_EVENT(xfs_timestamp_range_class, name, \
 	TP_ARGS(mp, min, max))
 DEFINE_TIMESTAMP_RANGE_EVENT(xfs_inode_timestamp_range);
 DEFINE_TIMESTAMP_RANGE_EVENT(xfs_quota_expiry_range);
+
+DECLARE_EVENT_CLASS(xfs_eofblocks_class,
+	TP_PROTO(struct xfs_mount *mp, struct xfs_eofblocks *eofb,
+		 unsigned long caller_ip),
+	TP_ARGS(mp, eofb, caller_ip),
+	TP_STRUCT__entry(
+		__field(dev_t, dev)
+		__field(__u32, flags)
+		__field(uint32_t, uid)
+		__field(uint32_t, gid)
+		__field(prid_t, prid)
+		__field(__u64, min_file_size)
+		__field(unsigned long, caller_ip)
+	),
+	TP_fast_assign(
+		__entry->dev = mp->m_super->s_dev;
+                __entry->flags = eofb->eof_flags;
+                __entry->uid = from_kuid(mp->m_super->s_user_ns, eofb->eof_uid);
+                __entry->gid = from_kgid(mp->m_super->s_user_ns, eofb->eof_gid);
+                __entry->prid = eofb->eof_prid;
+                __entry->min_file_size = eofb->eof_min_file_size;
+		__entry->caller_ip = caller_ip;
+	),
+	TP_printk("dev %d:%d flags 0x%x uid %u gid %u prid %u minsize %llu caller %pS",
+		  MAJOR(__entry->dev), MINOR(__entry->dev),
+                  __entry->flags,
+                  __entry->uid,
+                  __entry->gid,
+                  __entry->prid,
+                  __entry->min_file_size,
+		  (char *)__entry->caller_ip)
+);
+#define DEFINE_EOFBLOCKS_EVENT(name)	\
+DEFINE_EVENT(xfs_eofblocks_class, name,	\
+	TP_PROTO(struct xfs_mount *mp, struct xfs_eofblocks *eofb, \
+		 unsigned long caller_ip), \
+	TP_ARGS(mp, eofb, caller_ip))
+DEFINE_EOFBLOCKS_EVENT(xfs_ioc_free_eofblocks);
+DEFINE_EOFBLOCKS_EVENT(xfs_inode_free_quota_blocks);
 
 #endif /* _TRACE_XFS_H */
 
