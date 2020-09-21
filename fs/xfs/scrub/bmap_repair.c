7e8abb18bf5d6dd5231ffef90c382edf88e06264
Darrick J. Wong <darrick.wong@oracle.com>
2020-09-21 14:27:49 -0700

    xfs: replace shouty XFS_BM{BT,DR} macros
    Replace all the shouty bmap btree and bmap disk root macros with actual
    functions, and fix a type handling error in the xattr code that the
    macros previously didn't care about.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/bmap_repair.c
+++ fs/xfs/scrub/bmap_repair.c
@@ -259,7 +259,7 @@ xrep_bmap_iroot_size(
 	unsigned int		nr_this_level,
 	void			*priv)
 {
-	return XFS_BMAP_BROOT_SPACE_CALC(cur->bc_mp, nr_this_level);
+	return xfs_bmap_broot_space_calc(cur->bc_mp, nr_this_level);
 }
 
 /* Update the inode counters. */
@@ -404,7 +404,7 @@ xrep_bmap_build_new_fork(
 	 * so that we can roll the transaction while holding the inode locked.
 	 */
 	ifake->if_fork->if_nextents = xfbma_length(rb->bmap_records);
-	if (XFS_BMDR_SPACE_CALC(ifake->if_fork->if_nextents) <=
+	if (xfs_bmdr_space_calc(ifake->if_fork->if_nextents) <=
 	    XFS_DFORK_SIZE(&sc->ip->i_d, sc->mp, rb->whichfork)) {
 		ifake->if_fork->if_format = XFS_DINODE_FMT_EXTENTS;
 		error = xrep_bmap_extents_load(rb, bmap_cur);
