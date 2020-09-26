3a746cc1cd10c90548e1bcbeaec467a15f452478
Darrick J. Wong <darrick.wong@oracle.com>
2020-09-25 17:39:51 -0700

    xfs: fix an incore inode UAF in xfs_bui_recover
    In xfs_bui_item_recover, there exists a use-after-free bug with regards
    to the inode that is involved in the bmap replay operation.  If the
    mapping operation does not complete, we call xfs_bmap_unmap_extent to
    create a deferred op to finish the unmapping work, and we retain a
    pointer to the incore inode.
    
    Unfortunately, the very next thing we do is commit the transaction and
    drop the inode.  If reclaim tears down the inode before we try to finish
    the defer ops, we dereference garbage and blow up.  Therefore, create a
    way to join inodes to the defer ops freezer so that we can maintain the
    xfs_inode reference until we're done with the inode.
    
    Note: This imposes the requirement that there be enough memory to keep
    every incore inode in memory throughout recovery.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/libxfs/xfs_log_recover.h
+++ fs/xfs/libxfs/xfs_log_recover.h
@@ -124,7 +124,14 @@ bool xlog_is_buffer_cancelled(struct xlog *log, xfs_daddr_t blkno, uint len);
 
 void xlog_recover_release_intent(struct xlog *log, unsigned short intent_type,
 		uint64_t intent_id);
-int xlog_recover_trans_commit(struct xfs_trans *tp,
-		struct list_head *capture_list);
+int xlog_recover_trans_commit_inodes(struct xfs_trans *tp,
+		struct list_head *capture_list, struct xfs_inode *ip1,
+		struct xfs_inode *ip2);
+
+static inline int
+xlog_recover_trans_commit(struct xfs_trans *tp, struct list_head *capture_list)
+{
+	return xlog_recover_trans_commit_inodes(tp, capture_list, NULL, NULL);
+}
 
 #endif	/* __XFS_LOG_RECOVER_H__ */
