3d46c44ae16bada5457adf9baffd4d19a611e6eb
Darrick J. Wong <darrick.wong@oracle.com>
2020-10-25 17:14:51 -0700

    xfs: deferred inode inactivation
    Instead of calling xfs_inactive directly from xfs_fs_destroy_inode,
    defer the inactivation phase to a separate workqueue.  With this we
    avoid blocking memory reclaim on filesystem metadata updates that are
    necessary to free an in-core inode, such as post-eof block freeing, COW
    staging extent freeing, and truncating and freeing unlinked inodes.  Now
    that work is deferred to a workqueue where we can do the freeing in
    batches.
    
    We introduce two new inode flags -- NEEDS_INACTIVE and INACTIVATING.
    The first flag helps our worker find inodes needing inactivation, and
    the second flag marks inodes that are in the process of being
    inactivated.  A concurrent xfs_iget on the inode can still resurrect the
    inode by clearing NEEDS_INACTIVE (or bailing if INACTIVATING is set).
    
    Unfortunately, deferring the inactivation has one huge downside --
    eventual consistency.  Since all the freeing is deferred to a worker
    thread, one can rm a file but the space doesn't come back immediately.
    This can cause some odd side effects with quota accounting and statfs,
    so we also force inactivation scans in order to maintain the existing
    behaviors, at least outwardly.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/quotacheck.c
+++ fs/xfs/scrub/quotacheck.c
@@ -567,6 +567,13 @@ xqcheck_iwalk(
 				return error;
 			break;
 		case -ENOENT:
+			/*¬
+			 * It's possible that this inode has lost all of its
+			 * links but hasn't yet been inactivated.  Try to push
+			 * it towards inactivation.
+			 */
+			xfs_inactive_inodes(xqc->sc->mp, NULL);
+			/* fall through */
 		case -EINVAL:
 			/*
 			 * We thought the inode was allocated, but iget failed
