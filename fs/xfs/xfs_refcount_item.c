3e2aa8713d51d29106d23673ca2bdea48f363d5b
Darrick J. Wong <darrick.wong@oracle.com>
2020-10-25 17:16:41 -0700

    xfs: add a realtime flag to the refcount update log redo items
    Extend the refcount update (CUI) log items with a new realtime flag that
    indicates that the updates apply against the realtime refcountbt.  We'll
    wire up the actual refcount code later.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_refcount_item.c
+++ fs/xfs/xfs_refcount_item.c
@@ -245,12 +245,13 @@ xfs_trans_log_finish_refcount_update(
 	xfs_filblks_t			blockcount,
 	xfs_fsblock_t			*new_fsb,
 	xfs_filblks_t			*new_len,
+	bool				is_rt,
 	struct xfs_btree_cur		**pcur)
 {
 	int				error;
 
 	error = xfs_refcount_finish_one(tp, type, startblock,
-			blockcount, new_fsb, new_len, pcur);
+			blockcount, new_fsb, new_len, is_rt, pcur);
 
 	/*
 	 * Mark the transaction dirty, even on error. This ensures the
@@ -275,18 +276,27 @@ xfs_refcount_update_diff_items(
 	struct xfs_mount		*mp = priv;
 	struct xfs_refcount_intent	*ra;
 	struct xfs_refcount_intent	*rb;
+	xfs_agnumber_t			a_ag, b_ag;
 
 	ra = container_of(a, struct xfs_refcount_intent, ri_list);
 	rb = container_of(b, struct xfs_refcount_intent, ri_list);
-	return  XFS_FSB_TO_AGNO(mp, ra->ri_startblock) -
-		XFS_FSB_TO_AGNO(mp, rb->ri_startblock);
+	if (ra->ri_realtime)
+		a_ag = NULLAGNUMBER;
+	else
+		a_ag = XFS_FSB_TO_AGNO(mp, ra->ri_startblock);
+	if (rb->ri_realtime)
+		b_ag = NULLAGNUMBER;
+	else
+		b_ag = XFS_FSB_TO_AGNO(mp, rb->ri_startblock);
+	return a_ag - b_ag;
 }
 
 /* Set the phys extent flags for this reverse mapping. */
 static void
 xfs_trans_set_refcount_flags(
 	struct xfs_phys_extent		*refc,
-	enum xfs_refcount_intent_type	type)
+	enum xfs_refcount_intent_type	type,
+	bool				is_rt)
 {
 	refc->pe_flags = 0;
 	switch (type) {
@@ -299,6 +309,9 @@ xfs_trans_set_refcount_flags(
 	default:
 		ASSERT(0);
 	}
+
+	if (is_rt)
+		refc->pe_flags |= XFS_REFCOUNT_EXTENT_REALTIME;
 }
 
 /* Log refcount updates in the intent item. */
@@ -324,7 +337,7 @@ xfs_refcount_update_log_item(
 	ext = &cuip->cui_format.cui_extents[next_extent];
 	ext->pe_startblock = refc->ri_startblock;
 	ext->pe_len = refc->ri_blockcount;
-	xfs_trans_set_refcount_flags(ext, refc->ri_type);
+	xfs_trans_set_refcount_flags(ext, refc->ri_type, refc->ri_realtime);
 }
 
 static struct xfs_log_item *
@@ -374,7 +387,7 @@ xfs_refcount_update_finish_item(
 	refc = container_of(item, struct xfs_refcount_intent, ri_list);
 	error = xfs_trans_log_finish_refcount_update(tp, CUD_ITEM(done),
 			refc->ri_type, refc->ri_startblock, refc->ri_blockcount,
-			&new_fsb, &new_aglen, state);
+			&new_fsb, &new_aglen, refc->ri_realtime, state);
 
 	/* Did we run out of reservation?  Requeue what we didn't finish. */
 	if (!error && new_aglen > 0) {
@@ -462,6 +475,7 @@ xfs_cui_item_recover(
 	xfs_filblks_t			new_len;
 	unsigned int			refc_type;
 	bool				requeue_only = false;
+	bool				is_rt;
 	enum xfs_refcount_intent_type	type;
 	int				i;
 	int				error = 0;
@@ -503,6 +517,7 @@ xfs_cui_item_recover(
 	for (i = 0; i < cuip->cui_format.cui_nextents; i++) {
 		refc = &cuip->cui_format.cui_extents[i];
 		refc_type = refc->pe_flags & XFS_REFCOUNT_EXTENT_TYPE_MASK;
+		is_rt = refc->pe_flags & XFS_REFCOUNT_EXTENT_REALTIME;
 		switch (refc_type) {
 		case XFS_REFCOUNT_INCREASE:
 		case XFS_REFCOUNT_DECREASE:
@@ -521,7 +536,7 @@ xfs_cui_item_recover(
 		} else
 			error = xfs_trans_log_finish_refcount_update(tp, cudp,
 				type, refc->pe_startblock, refc->pe_len,
-				&new_fsb, &new_len, &rcur);
+				&new_fsb, &new_len, is_rt, &rcur);
 		if (error)
 			goto abort_error;
 
@@ -531,20 +546,20 @@ xfs_cui_item_recover(
 			irec.br_blockcount = new_len;
 			switch (type) {
 			case XFS_REFCOUNT_INCREASE:
-				xfs_refcount_increase_extent(tp, &irec);
+				xfs_refcount_increase_extent(tp, &irec, is_rt);
 				break;
 			case XFS_REFCOUNT_DECREASE:
-				xfs_refcount_decrease_extent(tp, &irec);
+				xfs_refcount_decrease_extent(tp, &irec, is_rt);
 				break;
 			case XFS_REFCOUNT_ALLOC_COW:
 				xfs_refcount_alloc_cow_extent(tp,
 						irec.br_startblock,
-						irec.br_blockcount);
+						irec.br_blockcount, is_rt);
 				break;
 			case XFS_REFCOUNT_FREE_COW:
 				xfs_refcount_free_cow_extent(tp,
 						irec.br_startblock,
-						irec.br_blockcount);
+						irec.br_blockcount, is_rt);
 				break;
 			default:
 				ASSERT(0);
