bed71ae5aec220bf3ddac789dc097336147be583
Darrick J. Wong <darrick.wong@oracle.com>
2020-10-25 17:15:50 -0700

    xfs: online repair of directories
    If a directory looks like it's in bad shape, try to sift through the
    rubble to find whatever directory entries we can, scan the directory
    tree for the parent (if needed), stage the new directory contents in a
    temporary file and use the atomic extent swapping mechanism to commit
    the results in bulk.  As a side effect of this patch, directory
    inactivation will be able to purge any leftover dir blocks.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/scrub/dir.c
+++ fs/xfs/scrub/dir.c
@@ -18,6 +18,7 @@
 #include "scrub/scrub.h"
 #include "scrub/common.h"
 #include "scrub/dabtree.h"
+#include "scrub/repair.h"
 
 /* Set us up to scrub directories. */
 int
@@ -25,7 +26,32 @@ xchk_setup_directory(
 	struct xfs_scrub	*sc,
 	struct xfs_inode	*ip)
 {
-	return xchk_setup_inode_contents(sc, ip, 0);
+	unsigned int		sz;
+	int			error;
+
+	error = xrep_setup_tempfile(sc, S_IFDIR);
+	if (error)
+		return error;
+
+	if (sc->flags & XCHK_TRY_HARDER) {
+		error = xchk_fs_freeze(sc);
+		if (error)
+			return error;
+	}
+
+	error = xchk_setup_inode_contents(sc, ip, 0);
+	if (error)
+		return error;
+
+	if (!(sc->sm->sm_flags & XFS_SCRUB_IFLAG_REPAIR))
+		return 0;
+
+	sz = max_t(unsigned int, MAXNAMELEN + 1, sizeof(struct xfs_da_args));
+	sc->buf = kmem_alloc_large(sz, 0);
+	if (!sc->buf)
+		return -ENOMEM;
+
+	return 0;
 }
 
 /* Directories */
