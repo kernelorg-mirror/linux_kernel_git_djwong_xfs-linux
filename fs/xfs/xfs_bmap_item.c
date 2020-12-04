5a6b9401f4b6314be6646b47eff8b4ac4015a12a
Darrick J. Wong <darrick.wong@oracle.com>
2020-12-04 13:41:36 -0800

    xfs: validate feature support when recovering rmap/refcount/bmap intents
    The bmap, rmap, and refcount log intent items were added to support the
    rmap and reflink features.  Because these features come with changes to
    the ondisk format, the log items aren't tied to a log incompat flag.
    
    However, the log recovery routines don't actually check for those
    feature flags.  The kernel has no business replayng an intent item for a
    feature that isn't enabled, so check that as part of recovered log item
    validation.  (Note that kernels pre-dating rmap and reflink will fail
    the mount on the unknown log item type code.)
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/xfs_bmap_item.c
+++ fs/xfs/xfs_bmap_item.c
@@ -425,6 +425,10 @@ xfs_bui_validate(
 {
 	struct xfs_map_extent		*bmap;
 
+	if (!xfs_sb_version_hasrmapbt(&mp->m_sb) &&
+	    !xfs_sb_version_hasreflink(&mp->m_sb))
+		return false;
+
 	/* Only one mapping operation per BUI... */
 	if (buip->bui_format.bui_nextents != XFS_BUI_MAX_FAST_EXTENTS)
 		return false;
