705064db4bfd4fcdee0ae3229c74bc2dd703d441
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:59 -0800

    xfs: wire up rmap map and unmap to the realtime rmapbt
    Connect the map and unmap reverse-mapping operations to the realtime
    rmapbt via the deferred operation callbacks.  This enables us to
    perform rmap operations against the correct btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -1476,6 +1476,12 @@ xfs_rtlock(
 		if (tp)
 			xfs_trans_ijoin(tp, mp->m_rsumip, XFS_ILOCK_EXCL);
 	}
+
+	if ((lock_flags & XFS_RTLOCK_RMAP) && mp->m_rrmapip) {
+		xfs_ilock(mp->m_rrmapip, XFS_ILOCK_EXCL);
+		if (tp)
+			xfs_trans_ijoin(tp, mp->m_rrmapip, XFS_ILOCK_EXCL);
+	}
 }
 
 /* Unlock the realtime metadata inodes. */
@@ -1490,4 +1496,7 @@ xfs_rtunlock(
 		xfs_iunlock(mp->m_rsumip, XFS_ILOCK_EXCL);
 		xfs_iunlock(mp->m_rbmip, XFS_ILOCK_EXCL);
 	}
+
+	if ((lock_flags & XFS_RTLOCK_RMAP) && mp->m_rrmapip)
+		xfs_iunlock(mp->m_rrmapip, XFS_ILOCK_EXCL);
 }
