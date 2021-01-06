c07c05459ae1ea5162a905d77e4a4c388748d210
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:48 -0800

    xfs: compute the maximum height of the rmap btree when reflink enabled
    Instead of assuming that the hardcoded XFS_BTREE_MAXLEVELS value is big
    enough to handle the maximally tall rmap btree when all blocks are in
    use and maximally shared, let's compute the maximum height assuming the
    rmapbt consumes as many blocks as possible.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_mount.c
+++ fs/xfs/xfs_mount.c
@@ -796,7 +796,7 @@ xfs_mountfs(
 	xfs_bmap_compute_maxlevels(mp, XFS_DATA_FORK);
 	xfs_bmap_compute_maxlevels(mp, XFS_ATTR_FORK);
 	xfs_ialloc_setup_geometry(mp);
-	xfs_rmapbt_compute_maxlevels(mp);
+	mp->m_rmap_maxlevels = xfs_rmapbt_compute_maxlevels(mp);
 	xfs_refcountbt_compute_maxlevels(mp);
 
 	/*
