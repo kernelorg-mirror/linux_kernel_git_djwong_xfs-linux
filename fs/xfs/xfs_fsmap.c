edc6ec76f74a87d0779ab1367a45a26a5a70ac04
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:50 -0800

    xfs: refactor realtime inode locking
    Refactor realtime metadata inode locking so that we can get some sense
    here.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_fsmap.c
+++ fs/xfs/xfs_fsmap.c
@@ -524,7 +524,7 @@ xfs_getfsmap_rtdev_rtbitmap_query(
 	struct xfs_mount		*mp = tp->t_mountp;
 	int				error;
 
-	xfs_ilock(mp->m_rbmip, XFS_ILOCK_SHARED);
+	xfs_rtlock(NULL, mp, XFS_RTLOCK_ALL);
 
 	/*
 	 * Set up query parameters to return free extents covering the range we
@@ -553,7 +553,7 @@ xfs_getfsmap_rtdev_rtbitmap_query(
 	if (error)
 		goto err;
 err:
-	xfs_iunlock(mp->m_rbmip, XFS_ILOCK_SHARED);
+	xfs_rtunlock(mp, XFS_RTLOCK_ALL);
 	return error;
 }
 
