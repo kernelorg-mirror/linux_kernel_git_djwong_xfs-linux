9c166522cd6738de6604575c55b8b012f03cb919
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:46:03 -0800

    xfs: online repair of realtime file bmaps
    Repair the block mappings of realtime files.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.h
+++ fs/xfs/scrub/repair.h
@@ -83,6 +83,15 @@ int xrep_rmapbt_setup(struct xfs_scrub *sc);
 int xrep_xattr_reset_fork(struct xfs_scrub *sc, struct xfs_inode *ip);
 
 void xrep_ag_btcur_init(struct xfs_scrub *sc, struct xchk_ag *sa);
+void xrep_rt_btcur_init(struct xfs_scrub *sc, struct xchk_rt *sr);
+
+#ifdef CONFIG_XFS_RT
+int xrep_require_rtext_inuse(struct xfs_scrub *sc, xfs_rtblock_t rtbno,
+		xfs_filblks_t len);
+#else
+# define xrep_require_rtext_inuse(sc, rtbno, len)	(-ENOSYS)
+#endif
+bool xrep_is_rtmeta_ino(struct xfs_scrub *sc, xfs_ino_t ino);
 
 static inline int
 xrep_ag_init(
