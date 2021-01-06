7002a4dbb32d35229c26b84b8bca5c7f104dc7b7
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:47:01 -0800

    xfs: add realtime reverse map inode to metadata directory
    Add a metadir path to select the realtime refcount btree inode and load
    it at mount time.  The rtrefcountbt inode will have a unique extent format
    code, which means that we also have to update the inode validation and
    flush routines to look for it.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -1343,8 +1343,29 @@ xfs_rtmount_inodes(
 		}
 	}
 
+	/* If we have reflink and a realtime device, load the refcount btree. */
+	if (xfs_sb_version_hasrtreflink(&mp->m_sb)) {
+		error = xfs_imeta_lookup(mp, &XFS_IMETA_RTREFCOUNTBT, &ino);
+		if (error)
+			goto out_rrmap;
+
+		error = xfs_imeta_iget(mp, ino, XFS_DIR3_FT_REG_FILE,
+				&mp->m_rrefcountip);
+		if (error)
+			goto out_rrmap;
+
+		if (XFS_IS_CORRUPT(mp,
+				   mp->m_rrefcountip->i_df.if_format !=
+				   XFS_DINODE_FMT_REFCOUNT)) {
+			error = -EFSCORRUPTED;
+			goto out_rrefcount;
+		}
+	}
+
 	xfs_alloc_rsum_cache(mp, sbp->sb_rbmblocks);
 	return 0;
+out_rrefcount:
+	xfs_imeta_irele(mp->m_rrefcountip);
 out_rrmap:
 	xfs_imeta_irele(mp->m_rrmapip);
 out_rsum:
@@ -1375,6 +1396,12 @@ xfs_rtmount_dqattach(
 			return error;
 	}
 
+	if (xfs_sb_version_hasrtreflink(&mp->m_sb)) {
+		error = xfs_qm_dqattach(mp->m_rrefcountip);
+		if (error)
+			return error;
+	}
+
 	return 0;
 }
 
@@ -1383,6 +1410,8 @@ xfs_rtunmount_inodes(
 	struct xfs_mount	*mp)
 {
 	kmem_free(mp->m_rsum_cache);
+	if (mp->m_rrefcountip)
+		xfs_imeta_irele(mp->m_rrefcountip);
 	if (mp->m_rrmapip)
 		xfs_imeta_irele(mp->m_rrmapip);
 	if (mp->m_rbmip)
