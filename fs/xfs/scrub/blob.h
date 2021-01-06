af000cdaf517a3c0512c859ddd5265d8d5fc0a2e
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:16 -0800

    xfs: create a blob array data structure
    Create a simple 'blob array' data structure for storage of arbitrarily
    sized metadata objects that will be used to reconstruct metadata.  For
    the intended usage (temporarily storing extended attribute names and
    values) we only have to support storing objects and retrieving them.
    Use the xfile abstraction to store the attribute information in memory
    that can be swapped out.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

---
+++ fs/xfs/scrub/blob.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2021 Oracle.  All Rights Reserved.
+ * Author: Darrick J. Wong <djwong@kernel.org>
+ */
+#ifndef __XFS_SCRUB_BLOB_H__
+#define __XFS_SCRUB_BLOB_H__
+
+struct xblob {
+	struct xfile	*xfile;
+	loff_t		last_offset;
+};
+
+typedef loff_t		xblob_cookie;
+
+struct xblob *xblob_init(const char *descr);
+void xblob_destroy(struct xblob *blob);
+int xblob_get(struct xblob *blob, xblob_cookie cookie, void *ptr,
+		uint32_t size);
+int xblob_put(struct xblob *blob, xblob_cookie *cookie, void *ptr,
+		uint32_t size);
+int xblob_free(struct xblob *blob, xblob_cookie cookie);
+
+#endif /* __XFS_SCRUB_BLOB_H__ */
