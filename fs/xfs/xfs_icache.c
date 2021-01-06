08349331965f8b674891c60fe508206705306636
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:43:35 -0800

    xfs: implement live quotacheck inode scan
    Create a new trio of scrub functions to check quota counters.  While the
    dquots themselves are filesystem metadata and should be checked early,
    the dquot counter values are computed from other metadata and are
    therefore summary counters.  We don't plug these into the scrub dispatch
    just yet, because we still need to be able to watch quota updates while
    doing our scan.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_icache.c
+++ fs/xfs/xfs_icache.c
@@ -2365,6 +2365,22 @@ xfs_inodegc_flush(
 		flush_delayed_work(&pag->pag_inodegc_work);
 }
 
+/* Flush inactivation work for all inodes in the same AGs as this inode. */
+void
+xfs_inodegc_flush_ino(
+	struct xfs_mount	*mp,
+	xfs_ino_t		ino)
+{
+	struct xfs_perag	*pag;
+
+	pag = xfs_perag_get(mp, XFS_INO_TO_AGNO(mp, ino));
+	if (radix_tree_tagged(&pag->pag_ici_root, XFS_ICI_INODEGC_TAG)) {
+		trace_xfs_inodegc_queue(pag, 0);
+		flush_delayed_work(&pag->pag_inodegc_work);
+	}
+	xfs_perag_put(pag);
+}
+
 /* Disable the inode inactivation background worker and wait for it to stop. */
 void
 xfs_inodegc_stop(
