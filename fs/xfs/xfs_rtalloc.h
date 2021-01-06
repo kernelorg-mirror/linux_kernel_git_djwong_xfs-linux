dde4d9578f642457d70c58f7216177e13f461e0e
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:59 -0800

    xfs: wire up rmap map and unmap to the realtime rmapbt
    Connect the map and unmap reverse-mapping operations to the realtime
    rmapbt via the deferred operation callbacks.  This enables us to
    perform rmap operations against the correct btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.h
+++ fs/xfs/xfs_rtalloc.h
@@ -147,7 +147,9 @@ int xfs_rtfile_convert_unwritten(struct xfs_inode *ip, loff_t pos,
 		uint64_t len);
 
 #define XFS_RTLOCK_ALLOC	(1 << 0) /* rt allocation */
-#define XFS_RTLOCK_ALL		(XFS_RTLOCK_ALLOC)
+#define XFS_RTLOCK_RMAP		(1 << 1) /* rmap operations */
+#define XFS_RTLOCK_ALL		(XFS_RTLOCK_ALLOC | \
+				 XFS_RTLOCK_RMAP)
 
 void xfs_rtlock(struct xfs_trans *tp, struct xfs_mount *mp,
 		unsigned int rtlock_flags);
