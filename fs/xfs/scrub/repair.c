ca5209e0c387bf322a727a3e0804e8c0578d84c1
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:23 -0800

    xfs: push xfs_ialloc_args creation out of xfs_dir_ialloc
    Move the initialization of the xfs_ialloc_args structure out of
    xfs_dir_ialloc into its callers' callers so that we can set the new
    inode's parameters in one place and pass it through instead of open
    coding the new uid/gid/prid all over the code.  This also prepares us
    for moving xfs_dir_ialloc and xfs_create to libxfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.c
+++ fs/xfs/scrub/repair.c
@@ -1581,6 +1581,10 @@ xrep_setup_tempfile(
 	struct xfs_scrub	*sc,
 	uint16_t		mode)
 {
+	struct xfs_ialloc_args	args = {
+		.pip		= sc->mp->m_rootip,
+		.nlink 		= 0,
+	};
 	struct xfs_mount	*mp = sc->mp;
 	struct xfs_trans	*tp = NULL;
 	struct xfs_dquot	*udqp = NULL;
@@ -1591,6 +1595,8 @@ xrep_setup_tempfile(
 	bool			is_dir = S_ISDIR(mode);
 	bool			use_atomic;
 	int			error;
+
+	xfs_ialloc_internal_args(&args, mode);
 
 	if (!(sc->sm->sm_flags & XFS_SCRUB_IFLAG_REPAIR))
 		return 0;
@@ -1612,9 +1618,8 @@ xrep_setup_tempfile(
 	 * inode should be completely root owned so that we don't fail due to
 	 * quota limits.
 	 */
-	error = xfs_qm_vop_dqalloc(sc->mp->m_rootip, GLOBAL_ROOT_UID,
-			GLOBAL_ROOT_GID, 0, XFS_QMOPT_QUOTALL, &udqp, &gdqp,
-			&pdqp);
+	error = xfs_qm_vop_dqalloc(sc->mp->m_rootip, args.uid, args.gid,
+			args.prid, XFS_QMOPT_QUOTALL, &udqp, &gdqp, &pdqp);
 	if (error)
 		return error;
 
@@ -1632,15 +1637,9 @@ xrep_setup_tempfile(
 		goto out_release_dquots;
 
 	/* Allocate inode, set up directory. */
-	error = xfs_dir_ialloc(&init_user_ns, &tp, sc->mp->m_rootip, mode, 0,
-			0, 0, &sc->tempip);
+	error = xfs_dir_ialloc(&tp, &args, &sc->tempip);
 	if (error)
 		goto out_trans_cancel;
-
-	/* Change the ownership of the inode to root. */
-	VFS_I(sc->tempip)->i_uid = GLOBAL_ROOT_UID;
-	VFS_I(sc->tempip)->i_gid = GLOBAL_ROOT_GID;
-	xfs_trans_log_inode(tp, sc->tempip, XFS_ILOG_CORE);
 
 	/*
 	 * Mark our temporary file as private so that LSMs and the ACL code
