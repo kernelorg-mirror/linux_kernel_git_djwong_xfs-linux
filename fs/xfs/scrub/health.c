f4a2a41dac1967a5800c34d9576ee1c195095ef3
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:47:08 -0800

    xfs: scrub the realtime refcount btree
    Add code to scrub realtime refcount btrees.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/health.c
+++ fs/xfs/scrub/health.c
@@ -108,6 +108,7 @@ static const struct xchk_health_map type_to_health_flag[XFS_SCRUB_TYPE_NR] = {
 	[XFS_SCRUB_TYPE_FSCOUNTERS]	= { XHG_FS,  XFS_SICK_FS_COUNTERS },
 	[XFS_SCRUB_TYPE_QUOTACHECK]	= { XHG_FS,  XFS_SICK_FS_QUOTACHECK },
 	[XFS_SCRUB_TYPE_RTRMAPBT]	= { XHG_RT,  XFS_SICK_RT_RMAPBT },
+	[XFS_SCRUB_TYPE_RTREFCBT]	= { XHG_RT,  XFS_SICK_RT_REFCNTBT },
 };
 
 /* Return the health status mask for this scrub type. */
--- fs/xfs/scrub/scrub.h
+++ fs/xfs/scrub/scrub.h
@@ -65,6 +65,7 @@ struct xchk_rt {
 	bool			locked;
 
 	struct xfs_btree_cur	*rmap_cur;
+	struct xfs_btree_cur	*refc_cur;
 };
 
 struct xfs_scrub {
@@ -163,6 +164,7 @@ int xchk_parent(struct xfs_scrub *sc);
 int xchk_rtbitmap(struct xfs_scrub *sc);
 int xchk_rtsummary(struct xfs_scrub *sc);
 int xchk_rtrmapbt(struct xfs_scrub *sc);
+int xchk_rtrefcountbt(struct xfs_scrub *sc);
 #else
 static inline int
 xchk_rtbitmap(struct xfs_scrub *sc)
@@ -176,6 +178,11 @@ xchk_rtsummary(struct xfs_scrub *sc)
 }
 static inline int
 xchk_rtrmapbt(struct xfs_scrub *sc)
+{
+	return -ENOENT;
+}
+static inline int
+xchk_rtrefcountbt(struct xfs_scrub *sc)
 {
 	return -ENOENT;
 }
