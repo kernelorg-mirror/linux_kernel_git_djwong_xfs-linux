0f9fbdd81875b23479769538e767e70662d74738
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:49 -0800

    xfs: refactor realtime scrubbing context management
    Create a pair of helpers to deal with setting up the necessary incore
    context to check metadata records against the realtime metadata.  Right
    now this is limited to locking the realtime bitmap and summary inodes,
    but as we add rmap and reflink to the realtime device this will grow to
    include btree cursors.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/common.h
+++ fs/xfs/scrub/common.h
@@ -121,6 +121,8 @@ int xchk_setup_fscounters(struct xfs_scrub *sc);
 void xchk_ag_free(struct xfs_scrub *sc, struct xchk_ag *sa);
 int xchk_ag_init(struct xfs_scrub *sc, xfs_agnumber_t agno,
 		struct xchk_ag *sa);
+void xchk_rt_init(struct xfs_scrub *sc, struct xchk_rt *sr);
+void xchk_rt_unlock(struct xfs_scrub *sc, struct xchk_rt *sr);
 void xchk_perag_get(struct xfs_mount *mp, struct xchk_ag *sa);
 int xchk_ag_read_headers(struct xfs_scrub *sc, xfs_agnumber_t agno,
 		struct xchk_ag *sa);
