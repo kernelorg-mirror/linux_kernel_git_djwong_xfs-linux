3c6490b7e4954f136300b1250ed14d1e7bb38855
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:50 -0800

    xfs: refactor realtime inode locking
    Refactor realtime metadata inode locking so that we can get some sense
    here.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/common.c
+++ fs/xfs/scrub/common.c
@@ -27,6 +27,7 @@
 #include "xfs_trans_priv.h"
 #include "xfs_attr.h"
 #include "xfs_reflink.h"
+#include "xfs_rtalloc.h"
 #include "scrub/scrub.h"
 #include "scrub/common.h"
 #include "scrub/trace.h"
@@ -608,8 +609,7 @@ xchk_rt_init(
 	struct xfs_scrub	*sc,
 	struct xchk_rt		*sr)
 {
-	xfs_ilock(sc->mp->m_rbmip, XFS_ILOCK_EXCL | XFS_ILOCK_RTBITMAP);
-	xfs_ilock(sc->mp->m_rsumip, XFS_ILOCK_EXCL | XFS_ILOCK_RTSUM);
+	xfs_rtlock(NULL, sc->mp, XFS_RTLOCK_ALL);
 	sr->locked = true;
 }
 
@@ -625,8 +625,7 @@ xchk_rt_unlock(
 	if (!sr->locked)
 		return;
 
-	xfs_iunlock(sc->mp->m_rsumip, XFS_ILOCK_EXCL);
-	xfs_iunlock(sc->mp->m_rbmip, XFS_ILOCK_EXCL);
+	xfs_rtunlock(sc->mp, XFS_RTLOCK_ALL);
 	sr->locked = false;
 }
 
