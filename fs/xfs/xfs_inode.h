f1a1acbd8ef9854fab5aba088c1c7086f6f8f027
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:20 -0800

    xfs: hoist extent size helpers to libxfs
    Move the extent size helpers to xfs_bmap.c in libxfs since they're used
    there already.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_inode.h
+++ fs/xfs/xfs_inode.h
@@ -193,6 +193,8 @@ static inline bool xfs_is_metadata_inode(struct xfs_inode *ip)
 		xfs_is_quota_inode(&mp->m_sb, ip->i_ino);
 }
 
+bool xfs_is_always_cow_inode(struct xfs_inode *ip);
+
 /*
  * Check if an inode has any data in the COW fork.  This might be often false
  * even for inodes with the reflink flag when there is no pending COW operation.
@@ -422,9 +424,6 @@ void		xfs_iunpin_wait(xfs_inode_t *);
 int		xfs_iflush_cluster(struct xfs_buf *);
 void		xfs_lock_two_inodes(struct xfs_inode *ip0, uint ip0_mode,
 				struct xfs_inode *ip1, uint ip1_mode);
-
-xfs_extlen_t	xfs_get_extsz_hint(struct xfs_inode *ip);
-xfs_extlen_t	xfs_get_cowextsz_hint(struct xfs_inode *ip);
 
 int xfs_dir_ialloc(struct xfs_trans **tpp, struct xfs_inode *dp, umode_t mode,
 		   xfs_nlink_t nlink, dev_t dev, prid_t prid,
