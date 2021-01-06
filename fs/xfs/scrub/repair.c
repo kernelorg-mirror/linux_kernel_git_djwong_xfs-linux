f73c28f86c0c394ec627852bf357f1aa26a3dadc
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:46:03 -0800

    xfs: online repair of realtime file bmaps
    Repair the block mappings of realtime files.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.c
+++ fs/xfs/scrub/repair.c
@@ -38,6 +38,8 @@
 #include "xfs_swapext.h"
 #include "xfs_xchgrange.h"
 #include "xfs_icache.h"
+#include "xfs_rtrmap_btree.h"
+#include "xfs_rtalloc.h"
 #include "scrub/scrub.h"
 #include "scrub/common.h"
 #include "scrub/trace.h"
@@ -1459,6 +1461,20 @@ xrep_ag_btcur_init(
 				sa->agf_bp, agno);
 }
 
+/* Initialize all the btree cursors for a RT repair. */
+void
+xrep_rt_btcur_init(
+	struct xfs_scrub	*sc,
+	struct xchk_rt		*sr)
+{
+	struct xfs_mount	*mp = sc->mp;
+
+	if (sc->sm->sm_type != XFS_SCRUB_TYPE_RTRMAPBT &&
+	    xfs_sb_version_hasrtrmapbt(&mp->m_sb))
+		sr->rmap_cur = xfs_rtrmapbt_init_cursor(mp, sc->tp,
+				mp->m_rrmapip);
+}
+
 /* Reinitialize the per-AG block reservation for the AG we just fixed. */
 int
 xrep_reset_perag_resv(
@@ -2238,4 +2254,45 @@ xrep_swapext_prep(
 	}
 
 	return xfs_swapext_estimate_overhead(req, res);
+}
+
+#ifdef CONFIG_XFS_RT
+/* Ensure that all rt blocks in the given range are not marked free. */
+int
+xrep_require_rtext_inuse(
+	struct xfs_scrub	*sc,
+	xfs_rtblock_t		rtbno,
+	xfs_filblks_t		len)
+{
+	struct xfs_mount	*mp = sc->mp;
+	xfs_rtblock_t		startext;
+	xfs_rtblock_t		endext;
+	xfs_rtblock_t		extcount;
+	uint32_t		mod;
+	bool			is_free = false;
+	int			error;
+
+	/* Round the starting rt extent down and the end rt extent up. */
+	startext = div_u64_rem(rtbno, mp->m_sb.sb_rextsize, &mod);
+	endext = div_u64_rem(rtbno + len - 1, mp->m_sb.sb_rextsize, &mod);
+
+	extcount = endext - startext + 1;
+	error = xfs_rtalloc_extent_is_free(mp, sc->tp, startext, extcount,
+			&is_free);
+	if (error)
+		return error;
+
+	return is_free ? -EFSCORRUPTED : 0;
+}
+#endif
+
+/* Are we looking at a realtime metadata inode? */
+bool
+xrep_is_rtmeta_ino(
+	struct xfs_scrub	*sc,
+	xfs_ino_t		ino)
+{
+	return ino == sc->mp->m_rbmip->i_ino ||
+	       ino == sc->mp->m_rsumip->i_ino ||
+	       ino == sc->mp->m_rrmapip->i_ino;
 }
