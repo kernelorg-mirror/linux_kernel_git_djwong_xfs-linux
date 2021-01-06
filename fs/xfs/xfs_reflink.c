d1d71eb1434da7ce06e24678ad744447c63549e5
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:47:06 -0800

    xfs: refcover CoW leftovers in the realtime volume
    Scan the realtime refcount tree at mount time to get rid of leftover
    CoW staging extents.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_reflink.c
+++ fs/xfs/xfs_reflink.c
@@ -844,8 +844,13 @@ xfs_reflink_recover_cow(
 		if (error)
 			break;
 	}
+	if (error)
+		return error;
 
-	return error;
+	if (xfs_sb_version_hasrealtime(&mp->m_sb))
+		return xfs_refcount_recover_cow_leftovers(mp, NULL);
+
+	return 0;
 }
 
 /*
