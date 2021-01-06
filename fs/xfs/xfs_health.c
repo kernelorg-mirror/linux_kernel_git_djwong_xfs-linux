d21181633bdf0c7f9c866f2449cc50fc11a790e8
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:46:05 -0800

    xfs: report realtime rmap btree corruption errors to the health system
    Whenever we encounter corrupt realtime rmap btree blocks, we should
    report that to the health monitoring system for later reporting.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_health.c
+++ fs/xfs/xfs_health.c
@@ -510,6 +510,9 @@ xfs_btree_mark_sick(
 	case XFS_BTNUM_BMAP:
 		xfs_bmap_mark_sick(cur->bc_ino.ip, cur->bc_ino.whichfork);
 		return;
+	case XFS_BTNUM_RTRMAP:
+		xfs_rt_mark_sick(cur->bc_mp, XFS_SICK_RT_RMAPBT);
+		return;
 	case XFS_BTNUM_BNO:
 		mask = XFS_SICK_AG_BNOBT;
 		break;
