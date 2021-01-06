5c014bcf2a5d91fa7f589bff913c5a16daca442c
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:21 -0800

    xfs: hoist project id get/set functions
    Move the project id get and set functions into libxfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_inode.h
+++ fs/xfs/xfs_inode.h
@@ -172,15 +172,6 @@ xfs_iflags_test_and_set(xfs_inode_t *ip, unsigned short flags)
 	return ret;
 }
 
-static inline prid_t
-xfs_get_initial_prid(struct xfs_inode *dp)
-{
-	if (dp->i_d.di_flags & XFS_DIFLAG_PROJINHERIT)
-		return dp->i_d.di_projid;
-
-	return XFS_PROJID_DEFAULT;
-}
-
 static inline bool xfs_is_reflink_inode(struct xfs_inode *ip)
 {
 	return ip->i_d.di_flags2 & XFS_DIFLAG2_REFLINK;
