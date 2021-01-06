9ee660b6de90d061e43e2039b05238a8ac2a517a
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:47 -0800

    xfs: dynamically allocate cursors based on maxlevels
    Replace the statically-sized btree cursor zone with dynamically sized
    allocations so that we can reduce the memory overhead for per-AG bt
    cursors while handling very tall btrees for rt metadata.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -2034,17 +2034,11 @@ xfs_init_zones(void)
 	if (!xfs_bmap_free_item_zone)
 		goto out_destroy_log_ticket_zone;
 
-	xfs_btree_cur_zone = kmem_cache_create("xfs_btree_cur",
-				xfs_btree_cur_sizeof(XFS_BTREE_MAXLEVELS),
-					       0, 0, NULL);
-	if (!xfs_btree_cur_zone)
-		goto out_destroy_bmap_free_item_zone;
-
 	xfs_da_state_zone = kmem_cache_create("xfs_da_state",
 					      sizeof(struct xfs_da_state),
 					      0, 0, NULL);
 	if (!xfs_da_state_zone)
-		goto out_destroy_btree_cur_zone;
+		goto out_destroy_bmap_free_item_zone;
 
 	xfs_ifork_zone = kmem_cache_create("xfs_ifork",
 					   sizeof(struct xfs_ifork),
@@ -2190,8 +2184,6 @@ xfs_init_zones(void)
 	kmem_cache_destroy(xfs_ifork_zone);
  out_destroy_da_state_zone:
 	kmem_cache_destroy(xfs_da_state_zone);
- out_destroy_btree_cur_zone:
-	kmem_cache_destroy(xfs_btree_cur_zone);
  out_destroy_bmap_free_item_zone:
 	kmem_cache_destroy(xfs_bmap_free_item_zone);
  out_destroy_log_ticket_zone:
@@ -2223,7 +2215,6 @@ xfs_destroy_zones(void)
 	kmem_cache_destroy(xfs_trans_zone);
 	kmem_cache_destroy(xfs_ifork_zone);
 	kmem_cache_destroy(xfs_da_state_zone);
-	kmem_cache_destroy(xfs_btree_cur_zone);
 	kmem_cache_destroy(xfs_bmap_free_item_zone);
 	kmem_cache_destroy(xfs_log_ticket_zone);
 }
