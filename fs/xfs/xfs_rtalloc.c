a2c984dff7c33139ada8b64da434efe601ee5b14
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:58 -0800

    xfs: add metadata reservations for realtime rmap btrees
    Reserve some free blocks so that we will always have enough free blocks
    in the data volume to handle expansion of the realtime rmap btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -25,6 +25,7 @@
 #include "xfs_imeta.h"
 #include "xfs_quota.h"
 #include "xfs_error.h"
+#include "xfs_rtrmap_btree.h"
 
 /*
  * Read and return the summary information for a given extent size,
@@ -1296,6 +1297,7 @@ void
 xfs_rt_resv_free(
 	struct xfs_mount	*mp)
 {
+	xfs_imeta_resv_free_inode(mp, mp->m_rrmapip);
 }
 
 /* Reserve space for rt metadata inodes' space expansion. */
@@ -1303,7 +1305,10 @@ int
 xfs_rt_resv_init(
 	struct xfs_mount	*mp)
 {
-	return 0;
+	xfs_filblks_t		ask;
+
+	ask = xfs_rtrmapbt_calc_reserves(mp);
+	return xfs_imeta_resv_init_inode(mp, mp->m_rrmapip, ask);
 }
 
 /*
