6c0b918603bb6aa087e817ec8160cf242e4bf002
Darrick J. Wong <djwong@kernel.org>
2021-01-05 17:45:31 -0800

    xfs: iget for metadata inodes
    Create a xfs_iget_meta function for metadata inodes to ensure that we
    always check that the inobt thinks a metadata inode is in use.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -2620,6 +2620,13 @@ xfs_irele(
 	iput(VFS_I(ip));
 }
 
+void
+xfs_imeta_irele(
+	struct xfs_inode	*ip)
+{
+	xfs_irele(ip);
+}
+
 /*
  * Ensure all commited transactions touching the inode are written to the log.
  */
