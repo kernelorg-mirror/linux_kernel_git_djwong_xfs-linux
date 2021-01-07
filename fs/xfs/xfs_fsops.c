ae9e5ac776fcab78c71697476c930e924d8cb27d
Darrick J. Wong <djwong@kernel.org>
2021-01-06 16:25:40 -0800

    xfs: check that the rtrmapbt maxlevels doesn't increase when growing fs
    The size of filesystem transaction reservations depends on the maximum
    height (maxlevels) of the realtime btrees.  Since we don't want a grow
    operation to increase the reservation size enough that we'll fail the
    minimum log size checks on the next mount, constrain growfs operations
    if they would cause an increase in those maxlevels.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_fsops.c
+++ fs/xfs/xfs_fsops.c
@@ -102,6 +102,9 @@ xfs_growfs_data_private(
 	error = xfs_sb_validate_fsb_count(&mp->m_sb, nb);
 	if (error)
 		return error;
+	error = xfs_growfs_check_rt_maxlevels(mp, nb, mp->m_sb.sb_rblocks);
+	if (error)
+		return error;
 
 	if (nb > mp->m_sb.sb_dblocks) {
 		error = xfs_buf_read_uncached(mp->m_ddev_targp,
