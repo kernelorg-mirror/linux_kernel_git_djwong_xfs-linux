8cc0e00d8453fd82e2ed54570f52010f3c578e60
Darrick J. Wong <djwong@kernel.org>
2021-02-05 10:02:05 -0800

    xfs: attach dquots to rt metadata files when starting quota
    Attach dquots to the realtime metadata files when starting up quotas,
    since the resources used by them are charged to the root dquot.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_mount.c
+++ fs/xfs/xfs_mount.c
@@ -1082,7 +1082,9 @@ xfs_mountfs(
 		ASSERT(mp->m_qflags == 0);
 		mp->m_qflags = quotaflags;
 
-		xfs_qm_mount_quotas(mp);
+		error = xfs_qm_mount_quotas(mp);
+		if (error)
+			goto out_rtunmount;
 	}
 
 	/*
