722d69e35e45ef2dc11e86b43dd7991ab7f879a8
Darrick J. Wong <djwong@kernel.org>
2021-02-16 09:41:08 -0800

    xfs: introduce vectored scrub mode
    Introduce a variant on XFS_SCRUB_METADATA that allows for vectored mode.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/xfs_scrub.h
+++ fs/xfs/scrub/xfs_scrub.h
@@ -8,8 +8,10 @@
 
 #ifndef CONFIG_XFS_ONLINE_SCRUB
 # define xfs_scrub_metadata(ip, sm)	(-ENOTTY)
+# define xfs_scrubv_metadata(ip, vhead)	(-ENOTTY)
 #else
 int xfs_scrub_metadata(struct xfs_inode *ip, struct xfs_scrub_metadata *sm);
+int xfs_scrubv_metadata(struct xfs_inode *ip, struct xfs_scrub_vec_head *vhead);
 #endif /* CONFIG_XFS_ONLINE_SCRUB */
 
 #endif	/* __XFS_SCRUB_H__ */
