5fc557f566b4196e3c3c77271840ba84bf158d3b
Darrick J. Wong <djwong@kernel.org>
2021-02-20 08:18:20 -0800

    xfs: experiment with dontcache when scanning inodes
    Add some experimental flags to drop inodes from the cache after a scan.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/rmap_repair.c
+++ fs/xfs/scrub/rmap_repair.c
@@ -614,7 +614,7 @@ xrep_rmap_scan_inode(
 	int				error;
 
 	/* Grab inode and lock it so we can scan it. */
-	error = xfs_iget(mp, rr->sc->tp, ino, XFS_IGET_DONTCACHE, 0, &ip);
+	error = xfs_iget(mp, rr->sc->tp, ino, 0, 0, &ip);
 	if (error == -ENOENT)
 		xfs_emerg(mp, "%s: iget of ino 0x%llx returned ENOENT?!",
 				__func__, ino);
@@ -637,7 +637,7 @@ xrep_rmap_scan_inode(
 
 out_unlock:
 	xfs_iunlock(ip, lock_mode);
-	xfs_irele(ip);
+	xchk_irele(rr->sc, ip);
 	return error;
 }
 
