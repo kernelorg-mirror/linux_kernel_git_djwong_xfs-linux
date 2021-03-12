6cd15c1d36bb76dd858be6f34038ea871916c14a
Darrick J. Wong <djwong@kernel.org>
2021-03-12 13:54:56 -0800

    xfs: repair inodes that have a refcount btree in the data fork
    Plumb knowledge of refcount btrees into the inode core repair code.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/inode_repair.c
+++ fs/xfs/scrub/inode_repair.c
@@ -33,6 +33,7 @@
 #include "xfs_quota_defs.h"
 #include "xfs_attr_leaf.h"
 #include "libxfs/xfs_rtrmap_btree.h"
+#include "libxfs/xfs_rtrefcount_btree.h"
 #include "xfs_log_priv.h"
 #include "scrub/xfs_scrub.h"
 #include "scrub/scrub.h"
@@ -880,6 +881,7 @@ xrep_dinode_ensure_forkoff(
 {
 	struct xfs_bmdr_block		*bmdr;
 	struct xfs_rtrmap_root		*rmdr;
+	struct xfs_rtrefcount_root	*rcdr;
 	size_t				bmdr_minsz = xfs_bmdr_space_calc(1);
 	unsigned int			lit_sz = XFS_LITINO(sc->mp);
 	unsigned int			afork_min, dfork_min;
@@ -987,6 +989,10 @@ xrep_dinode_ensure_forkoff(
 	case XFS_DINODE_FMT_RMAP:
 		rmdr = XFS_DFORK_PTR(dip, XFS_DATA_FORK);
 		dfork_min = xfs_rtrmap_broot_space(sc->mp, rmdr);
+		break;
+	case XFS_DINODE_FMT_REFCOUNT:
+		rcdr = XFS_DFORK_PTR(dip, XFS_DATA_FORK);
+		dfork_min = xfs_rtrefcount_broot_space(sc->mp, rcdr);
 		break;
 	default:
 		dfork_min = 0;
