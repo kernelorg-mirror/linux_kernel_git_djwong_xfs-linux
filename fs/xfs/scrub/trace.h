a5f918020ff3c41ff07313f102abc47583afee37
Darrick J. Wong <djwong@kernel.org>
2021-03-17 16:31:31 -0700

    xfs: teach repair to fix file nlinks
    Fix the nlinks now too.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/trace.h
+++ fs/xfs/scrub/trace.h
@@ -1851,6 +1851,9 @@ TRACE_EVENT(xrep_rtrefc_found,
 		  __entry->refcount)
 )
 
+DEFINE_SCRUB_NLINK_DIFF_EVENT(xrep_nlinks_commit_inode);
+DEFINE_SCRUB_NLINK_DIFF_EVENT(xrep_nlinks_unfixable_inode);
+
 #endif /* IS_ENABLED(CONFIG_XFS_ONLINE_REPAIR) */
 
 #endif /* _TRACE_XFS_SCRUB_TRACE_H */
