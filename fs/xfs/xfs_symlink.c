8e45025becb250087a4bb20e82f549a9297ec15f
Darrick J. Wong <djwong@kernel.org>
2021-03-17 09:27:17 -0700

    xfs: teach scrub to check file nlinks
    Copy-pasta the online quotacheck code to check inode link counts too.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_symlink.c
+++ fs/xfs/xfs_symlink.c
@@ -331,6 +331,7 @@ xfs_symlink(
 		goto out_trans_cancel;
 	xfs_trans_ichgtime(tp, dp, XFS_ICHGTIME_MOD | XFS_ICHGTIME_CHG);
 	xfs_trans_log_inode(tp, dp, XFS_ILOG_CORE);
+	xfs_inode_nlink_delta(dp, ip, 1);
 
 	/*
 	 * If this is a synchronous mount, make sure that the
