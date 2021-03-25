5799c2fba9827f834f6cc1cd337317453cd80efc
Darrick J. Wong <djwong@kernel.org>
2021-03-25 13:37:03 -0700

    xfs: create a polled function to force inode inactivation
    Create a polled version of xfs_inactive_force so that we can force
    inactivation while holding a lock (usually the umount lock) without
    tripping over the softlockup timer.  This is for callers that hold vfs
    locks while calling inactivation, which is currently unmount, iunlink
    processing during mount, and rw->ro remount.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -1782,7 +1782,7 @@ xfs_remount_ro(
 	 * Since this can involve finobt updates, do it now before we lose the
 	 * per-AG space reservations to guarantee that we won't fail there.
 	 */
-	xfs_inodegc_flush(mp);
+	xfs_inodegc_flush_poll(mp);
 
 	/* Free the per-AG metadata reservation pool. */
 	error = xfs_fs_unreserve_ag_blocks(mp);
@@ -1908,6 +1908,7 @@ static int xfs_init_fs_context(
 	INIT_WORK(&mp->m_flush_inodes_work, xfs_flush_inodes_worker);
 	INIT_DELAYED_WORK(&mp->m_reclaim_work, xfs_reclaim_worker);
 	mp->m_kobj.kobject.kset = xfs_kset;
+	init_waitqueue_head(&mp->m_inactive_wait);
 	/*
 	 * We don't create the finobt per-ag space reservation until after log
 	 * recovery, so we must set this to true so that an ifree transaction
