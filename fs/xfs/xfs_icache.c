53cccdd7f7a3ffa028a53bb36f8badba82123f28
Darrick J. Wong <djwong@kernel.org>
2021-03-26 13:32:29 -0700

    xfs: expose sysfs knob to control inode inactivation delay
    Allow administrators to control the length that we defer inode
    inactivation.  By default we'll set the delay to 2 seconds, as an
    arbitrary choice between allowing for some batching of a deltree
    operation, and not letting too many inodes pile up in memory.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_icache.c
+++ fs/xfs/xfs_icache.c
@@ -308,7 +308,8 @@ xfs_inodegc_queue(
 
 	rcu_read_lock();
 	if (radix_tree_tagged(&mp->m_perag_tree, XFS_ICI_INODEGC_TAG))
-		queue_delayed_work(mp->m_gc_workqueue, &mp->m_inodegc_work, 0);
+		queue_delayed_work(mp->m_gc_workqueue, &mp->m_inodegc_work,
+				msecs_to_jiffies(xfs_inodegc_centisecs * 10));
 	rcu_read_unlock();
 }
 
