41d98d3cb89ab6bd629314725bae7f0813eac52b
Darrick J. Wong <djwong@kernel.org>
2021-04-07 14:31:31 -0700

    xfs: deferred inode inactivation
    Instead of calling xfs_inactive directly from xfs_fs_destroy_inode,
    defer the inactivation phase to a separate workqueue.  With this we
    avoid blocking memory reclaim on filesystem metadata updates that are
    necessary to free an in-core inode, such as post-eof block freeing, COW
    staging extent freeing, and truncating and freeing unlinked inodes.  Now
    that work is deferred to a workqueue where we can do the freeing in
    batches.
    
    We introduce two new inode flags -- NEEDS_INACTIVE and INACTIVATING.
    The first flag helps our worker find inodes needing inactivation, and
    the second flag marks inodes that are in the process of being
    inactivated.  A concurrent xfs_iget on the inode can still resurrect the
    inode by clearing NEEDS_INACTIVE (or bailing if INACTIVATING is set).
    
    Unfortunately, deferring the inactivation has one huge downside --
    eventual consistency.  Since all the freeing is deferred to a worker
    thread, one can rm a file but the space doesn't come back immediately.
    This can cause some odd side effects with quota accounting and statfs,
    so we also force inactivation scans in order to maintain the existing
    behaviors, at least outwardly.
    
    For this patch we'll set the delay to zero to mimic the old timing as
    much as possible; in the next patch we'll play with different delay
    settings.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/common.c
+++ fs/xfs/scrub/common.c
@@ -883,6 +883,7 @@ xchk_stop_reaping(
 {
 	sc->flags |= XCHK_REAPING_DISABLED;
 	xfs_blockgc_stop(sc->mp);
+	xfs_inodegc_stop(sc->mp);
 }
 
 /* Restart background reaping of resources. */
@@ -890,6 +891,7 @@ void
 xchk_start_reaping(
 	struct xfs_scrub	*sc)
 {
+	xfs_inodegc_start(sc->mp);
 	xfs_blockgc_start(sc->mp);
 	sc->flags &= ~XCHK_REAPING_DISABLED;
 }
