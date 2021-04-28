14a6d3eeae31ae6ff34e993775ce6569a76c1073
Darrick J. Wong <djwong@kernel.org>
2021-04-27 17:11:50 -0700

    xfs: allow inode-based btrees to reserve space in the data device
    Create a new space reservation scheme so that btree metadata for the
    realtime volume can reserve space in the data device to avoid space
    underruns.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_fsops.c
+++ fs/xfs/xfs_fsops.c
@@ -19,6 +19,7 @@
 #include "xfs_log.h"
 #include "xfs_ag.h"
 #include "xfs_ag_resv.h"
+#include "xfs_rtalloc.h"
 
 /*
  * Write new AG headers to disk. Non-transactional, but need to be
@@ -585,6 +586,20 @@ xfs_fs_reserve_ag_blocks(
 		xfs_warn(mp,
 	"Error %d reserving per-AG metadata reserve pool.", error);
 		xfs_force_shutdown(mp, SHUTDOWN_CORRUPT_INCORE);
+		return error;
+	}
+
+	if (xfs_sb_version_hasrealtime(&mp->m_sb)) {
+		int err2 = xfs_rt_resv_init(mp);
+
+		if (err2 && err2 != -ENOSPC) {
+			xfs_warn(mp,
+		"Error %d reserving realtime metadata reserve pool.", err2);
+			xfs_force_shutdown(mp, SHUTDOWN_CORRUPT_INCORE);
+		}
+
+		if (err2 && !error)
+			error = err2;
 	}
 
 	return error;
@@ -599,6 +614,9 @@ xfs_fs_unreserve_ag_blocks(
 {
 	xfs_agnumber_t		agno;
 	struct xfs_perag	*pag;
+
+	if (xfs_sb_version_hasrealtime(&mp->m_sb))
+		xfs_rt_resv_free(mp);
 
 	for_each_perag(mp, agno, pag)
 		xfs_ag_resv_free(pag);
