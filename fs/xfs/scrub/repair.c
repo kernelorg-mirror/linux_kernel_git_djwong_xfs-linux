59bc9f475f389a32d6a01ae8c1ad80af881dbd24
Darrick J. Wong <djwong@kernel.org>
2021-04-27 20:26:30 -0700

    xfs: simplify xfs_ag_resv_free signature
    It's not possible to fail at increasing fdblocks, so get rid of all the
    error returns here.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.c
+++ fs/xfs/scrub/repair.c
@@ -1462,8 +1462,6 @@ int
 xrep_reset_perag_resv(
 	struct xfs_scrub	*sc)
 {
-	int			error;
-
 	if (!(sc->flags & XREP_RESET_PERAG_RESV))
 		return 0;
 
@@ -1472,12 +1470,8 @@ xrep_reset_perag_resv(
 	ASSERT(sc->tp);
 
 	sc->flags &= ~XREP_RESET_PERAG_RESV;
-	error = xfs_ag_resv_free(sc->sa.pag);
-	if (error)
-		goto out;
-	error = xfs_ag_resv_init(sc->sa.pag, sc->tp);
-out:
-	return error;
+	xfs_ag_resv_free(sc->sa.pag);
+	return xfs_ag_resv_init(sc->sa.pag, sc->tp);
 }
 
 /*
