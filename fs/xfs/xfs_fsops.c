c7eff8f94bea577eb69580863db572b46dad6019
Darrick J. Wong <djwong@kernel.org>
2021-04-27 20:26:30 -0700

    xfs: simplify xfs_ag_resv_free signature
    It's not possible to fail at increasing fdblocks, so get rid of all the
    error returns here.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_fsops.c
+++ fs/xfs/xfs_fsops.c
@@ -200,11 +200,10 @@ xfs_growfs_data_private(
 			struct xfs_perag	*pag;
 
 			pag = xfs_perag_get(mp, id.agno);
-			error = xfs_ag_resv_free(pag);
+			xfs_ag_resv_free(pag);
 			xfs_perag_put(pag);
-			if (error)
-				return error;
 		}
+
 		/*
 		 * Reserve AG metadata blocks. ENOSPC here does not mean there
 		 * was a growfs failure, just that there still isn't space for
@@ -592,22 +591,10 @@ xfs_fs_reserve_ag_blocks(
 /*
  * Free space reserved for per-AG metadata.
  */
-int
+void
 xfs_fs_unreserve_ag_blocks(
 	struct xfs_mount	*mp)
 {
-	int			error = 0;
-	int			err2;
-
-	for_each_perag(mp, iter) {
-		err2 = xfs_ag_resv_free(iter.pag);
-		if (err2 && !error)
-			error = err2;
-	}
-
-	if (error)
-		xfs_warn(mp,
-	"Error %d freeing per-AG metadata reserve pool.", error);
-
-	return error;
+	for_each_perag(mp, iter)
+		xfs_ag_resv_free(iter.pag);
 }
