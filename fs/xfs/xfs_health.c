ceb7e7d3cc5a3677e2b5a1c6d187ed236a5e6a3f
Darrick J. Wong <djwong@kernel.org>
2021-05-09 16:23:02 -0700

    xfs: drop IDONTCACHE on inodes when we mark them sick
    When we decide to mark an inode sick, clear the DONTCACHE flag so that
    the incore inode will be kept around until memory pressure forces it out
    of memory.  This increases the chances that the sick status will be
    caught by someone compiling a health report later on.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_health.c
+++ fs/xfs/xfs_health.c
@@ -231,6 +231,11 @@ xfs_inode_mark_sick(
 	ip->i_sick |= mask;
 	ip->i_checked |= mask;
 	spin_unlock(&ip->i_flags_lock);
+
+	/* Keep this inode around so we don't lose the sickness report. */
+	spin_lock(&VFS_I(ip)->i_lock);
+	VFS_I(ip)->i_state &= ~I_DONTCACHE;
+	spin_unlock(&VFS_I(ip)->i_lock);
 }
 
 /* Mark parts of an inode healed. */
