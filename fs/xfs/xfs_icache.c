86e5e22deeefacf66375a7003f37f69832ffaa9b
Darrick J. Wong <djwong@kernel.org>
2021-05-31 11:32:06 -0700

    xfs: force inode inactivation and retry fs writes when there isn't space
    Any time we try to modify a file's contents and it fails due to ENOSPC
    or EDQUOT, force inode inactivation work to try to free space.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_icache.c
+++ fs/xfs/xfs_icache.c
@@ -1632,9 +1632,15 @@ xfs_blockgc_free_space(
 	struct xfs_mount	*mp,
 	struct xfs_eofblocks	*eofb)
 {
+	int			error;
+
 	trace_xfs_blockgc_free_space(mp, eofb, _RET_IP_);
 
-	return xfs_inode_walk(mp, XFS_ICWALK_BLOCKGC, eofb);
+	error = xfs_inode_walk(mp, XFS_ICWALK_BLOCKGC, eofb);
+	if (error)
+		return error;
+
+	return xfs_inodegc_free_space(mp, eofb);
 }
 
 /*
