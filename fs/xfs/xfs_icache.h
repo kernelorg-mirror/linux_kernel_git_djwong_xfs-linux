ed4b3fc44278c4413c74c93570161af0728dde8d
Darrick J. Wong <djwong@kernel.org>
2021-05-31 11:32:07 -0700

    xfs: create a polled function to force inode inactivation
    Create a polled version of xfs_inactive_force so that we can force
    inactivation while holding a lock (usually the umount lock) without
    tripping over the softlockup timer.  This is for callers that hold vfs
    locks while calling inactivation, which is currently unmount, iunlink
    processing during mount, and rw->ro remount.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_icache.h
+++ fs/xfs/xfs_icache.h
@@ -84,6 +84,7 @@ void xfs_blockgc_start(struct xfs_mount *mp);
 
 void xfs_inodegc_worker(struct work_struct *work);
 void xfs_inodegc_flush(struct xfs_mount *mp);
+void xfs_inodegc_flush_poll(struct xfs_mount *mp);
 void xfs_inodegc_stop(struct xfs_mount *mp);
 void xfs_inodegc_start(struct xfs_mount *mp);
 int xfs_inodegc_free_space(struct xfs_mount *mp, struct xfs_icwalk *icw);
