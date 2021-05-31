5ab4370b74964c8220932cdda7ff1633aea4a12e
Darrick J. Wong <djwong@kernel.org>
2021-05-31 11:32:08 -0700

    xfs: don't run speculative preallocation gc when fs is frozen
    Now that we have the infrastructure to switch background workers on and
    off at will, fix the block gc worker code so that we don't actually run
    the worker when the filesystem is frozen, same as we do for deferred
    inactivation.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -269,6 +269,9 @@ typedef struct xfs_mount {
 						   inode inactivation worker? */
 #define XFS_OPFLAG_INACTIVATE_NOW_BIT	(1)	/* force foreground inactivation
 						   of unlinked inodes */
+#define XFS_OPFLAG_BLOCKGC_RUNNING_BIT	(2)	/* are we allowed to start the
+						   speculative preallocation gc
+						   worker? */
 
 /*
  * Max and min values for mount-option defined I/O
