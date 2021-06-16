028c5ae42fb7471df7ce18d583b454b55a786936
Darrick J. Wong <djwong@kernel.org>
2021-06-15 17:07:15 -0700

    xfs: fallocate free space into a file
    Add a new fallocate mode to map free physical space into a file, at the
    same file offset as if the file were a sparse image of the physical
    device backing the filesystem.  The intent here is to use this to
    prototype a free space defragmentation tool.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.h
+++ fs/xfs/xfs_rtalloc.h
@@ -159,8 +159,15 @@ int xfs_rtfile_convert_unwritten(struct xfs_inode *ip, loff_t pos,
 void xfs_rtlock(struct xfs_trans *tp, struct xfs_mount *mp,
 		unsigned int rtlock_flags);
 void xfs_rtunlock(struct xfs_mount *mp, unsigned int lock_flags);
+int xfs_rtalloc_find_freesp(struct xfs_mount *mp, xfs_rtblock_t *rtx,
+		xfs_rtblock_t end_rtx, xfs_extlen_t *len_rtx);
 #else
-# define xfs_rtallocate_extent(t,b,min,max,l,f,p,rb)    (ENOSYS)
+int xfs_rtallocate_extent(struct xfs_trans *tp, xfs_rtblock_t bno,
+		xfs_extlen_t minlen, xfs_extlen_t maxlen, xfs_extlen_t *len,
+		int wasdel, xfs_extlen_t prod, xfs_rtblock_t *rtblock)
+{
+	return ENOSYS;
+}
 # define xfs_rtfree_extent(t,b,l)                       (ENOSYS)
 # define xfs_rtfree_blocks(t,rb,rl)			(ENOSYS)
 # define xfs_rtpick_extent(m,t,l,rb)                    (ENOSYS)
@@ -189,6 +196,7 @@ xfs_rtmount_init(
 # define xfs_rtfile_convert_unwritten(ip, pos, len)	(0)
 # define xfs_rtlock(tp, mp, lock_flags)	do { } while (0)
 # define xfs_rtunlock(mp, lock_flags)	do { } while (0)
+# define xfs_rtalloc_find_freesp(mp, rtx, end_rtx, len_rtx)	(-ENOSYS)
 #endif	/* CONFIG_XFS_RT */
 
 #endif	/* __XFS_RTALLOC_H__ */
