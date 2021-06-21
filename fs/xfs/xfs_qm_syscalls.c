318d1134da247feb7122ca3fdf7e210f9e53fcff
Darrick J. Wong <djwong@kernel.org>
2021-06-21 10:35:44 -0700

    xfs: flush inode inactivation work when compiling usage statistics
    Users have come to expect that the space accounting information in
    statfs and getquota reports are fairly accurate.  Now that we inactivate
    inodes from a background queue, these numbers can be thrown off by
    whatever resources are singly-owned by the inodes in the queue.  Flush
    the pending inactivations when userspace asks for a space usage report.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_qm_syscalls.c
+++ fs/xfs/xfs_qm_syscalls.c
@@ -698,6 +698,10 @@ xfs_qm_scall_getquota(
 	struct xfs_dquot	*dqp;
 	int			error;
 
+	/* Flush inodegc work at the start of a quota reporting scan. */
+	if (id == 0)
+		xfs_inodegc_flush(mp);
+
 	/*
 	 * Try to get the dquot. We don't want it allocated on disk, so don't
 	 * set doalloc. If it doesn't exist, we'll get ENOENT back.
@@ -735,6 +739,10 @@ xfs_qm_scall_getquota_next(
 {
 	struct xfs_dquot	*dqp;
 	int			error;
+
+	/* Flush inodegc work at the start of a quota reporting scan. */
+	if (*id == 0)
+		xfs_inodegc_flush(mp);
 
 	error = xfs_qm_dqget_next(mp, *id, type, &dqp);
 	if (error)
