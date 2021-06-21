8fe5d0965a736754b08299c571a6ca7ab6e444ce
Darrick J. Wong <djwong@kernel.org>
2021-06-21 10:34:16 -0700

    xfs: attach dquots earlier in xfs_inactive()
    Move the dqattach further up in xfs_inactive.  In theory we should
    always have dquots attached if there are CoW blocks, but this makes the
    usage pattern more consistent with the rest of xfs (attach dquots, then
    start making changes).
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Brian Foster <bfoster@redhat.com>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -1664,7 +1664,7 @@ xfs_inactive_ifree(
  */
 void
 xfs_inactive(
-	xfs_inode_t	*ip)
+	struct xfs_inode	*ip)
 {
 	struct xfs_mount	*mp;
 	int			error;
@@ -1688,6 +1688,11 @@ xfs_inactive(
 
 	/* Metadata inodes require explicit resource cleanup. */
 	if (xfs_is_metadata_inode(ip))
+		goto out;
+
+	/* Ensure dquots are attached prior to making changes to this file. */
+	error = xfs_qm_dqattach(ip);
+	if (error)
 		goto out;
 
 	/* Try to clean out the cow blocks if there are any. */
@@ -1714,10 +1719,6 @@ xfs_inactive(
 	    (ip->i_disk_size != 0 || XFS_ISIZE(ip) != 0 ||
 	     ip->i_df.if_nextents > 0 || ip->i_delayed_blks > 0))
 		truncate = 1;
-
-	error = xfs_qm_dqattach(ip);
-	if (error)
-		goto out;
 
 	if (S_ISLNK(VFS_I(ip)->i_mode))
 		error = xfs_inactive_symlink(ip);
