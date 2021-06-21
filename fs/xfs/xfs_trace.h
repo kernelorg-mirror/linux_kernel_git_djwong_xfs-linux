acb1cc6d682971440bbf07aeeb284d7d8be158b0
Darrick J. Wong <djwong@kernel.org>
2021-06-21 10:35:41 -0700

    xfs: queue inodegc worker immediately when memory is tight
    If there is enough memory pressure that we're scheduling inodes for
    inactivation from a shrinker, queue the inactivation worker immediately
    to try to facilitate reclaming inodes.  This patch prepares us for
    adding a configurable inodegc delay in the next patch.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_trace.h
+++ fs/xfs/xfs_trace.h
@@ -192,6 +192,7 @@ DEFINE_FS_EVENT(xfs_inodegc_stop);
 DEFINE_FS_EVENT(xfs_inodegc_worker);
 DEFINE_FS_EVENT(xfs_inodegc_throttled);
 DEFINE_FS_EVENT(xfs_fs_sync_fs);
+DEFINE_FS_EVENT(xfs_inodegc_delay_mempressure);
 
 TRACE_EVENT(xfs_inodegc_requeue_mempressure,
 	TP_PROTO(struct xfs_mount *mp, unsigned long nr, void *caller_ip),
