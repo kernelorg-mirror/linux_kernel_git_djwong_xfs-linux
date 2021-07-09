11eb88af802fc12275b1adf29fb4123e93ebbae8
Darrick J. Wong <djwong@kernel.org>
2021-07-09 11:05:52 -0700

    xfs: add a ->xchg_file_range handler
    Add a function to handle file range exchange requests from the vfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_xchgrange.h
+++ fs/xfs/xfs_xchgrange.h
@@ -15,5 +15,20 @@ void xfs_xchg_range_iunlock(struct xfs_inode *ip1, struct xfs_inode *ip2);
 
 int xfs_xchg_range_estimate(const struct xfs_swapext_req *req,
 		struct xfs_swapext_res *res);
+int xfs_xchg_range_prep(struct file *file1, struct file *file2,
+		struct file_xchg_range *fxr);
+
+int xfs_xchg_range_grab_log_assist(struct xfs_mount *mp, bool force,
+		bool *enabled);
+void xfs_xchg_range_rele_log_assist(struct xfs_mount *mp);
+
+/* Update ip1's change and mod time. */
+#define XFS_XCHG_RANGE_UPD_CMTIME1	(1 << 0)
+
+/* Update ip2's change and mod time. */
+#define XFS_XCHG_RANGE_UPD_CMTIME2	(1 << 1)
+
+int xfs_xchg_range(struct xfs_inode *ip1, struct xfs_inode *ip2,
+		const struct file_xchg_range *fxr, unsigned int xchg_flags);
 
 #endif /* __XFS_XCHGRANGE_H__ */
