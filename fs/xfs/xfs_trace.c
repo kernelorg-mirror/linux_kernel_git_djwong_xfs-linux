167632d71d3da5ba88a58964673776405ca5ba5b
Darrick J. Wong <djwong@kernel.org>
2021-07-09 11:05:52 -0700

    xfs: add a ->xchg_file_range handler
    Add a function to handle file range exchange requests from the vfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_trace.c
+++ fs/xfs/xfs_trace.c
@@ -34,6 +34,7 @@
 #include "xfs_ag_resv.h"
 #include "xfs_bmap.h"
 #include "xfs_swapext.h"
+#include "xfs_xchgrange.h"
 
 /*
  * We include this last to have the helpers above available for the trace
