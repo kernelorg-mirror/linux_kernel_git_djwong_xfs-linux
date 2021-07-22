bf780787d41eaea40809f362100701e9449bc9b4
Darrick J. Wong <djwong@kernel.org>
2021-07-22 13:18:17 -0700

    xfs: use background worker pool when transactions can't get free space
    In xfs_trans_alloc, if the block reservation call returns ENOSPC, we
    call xfs_blockgc_free_space with a NULL icwalk structure to try to free
    space.  Each frontend thread that encounters this situation starts its
    own walk of the inode cache to see if it can find anything, which is
    wasteful since we don't have any additional selection criteria.  For
    this one common case, create a function that reschedules all pending
    background work immediately and flushes the workqueue so that the scan
    can run in parallel.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_trace.h
+++ fs/xfs/xfs_trace.h
@@ -217,6 +217,7 @@ DEFINE_FS_EVENT(xfs_fs_sync_fs);
 DEFINE_FS_EVENT(xfs_blockgc_start);
 DEFINE_FS_EVENT(xfs_blockgc_stop);
 DEFINE_FS_EVENT(xfs_blockgc_worker);
+DEFINE_FS_EVENT(xfs_blockgc_flush_all);
 
 TRACE_EVENT(xfs_inodegc_shrinker_scan,
 	TP_PROTO(struct xfs_mount *mp, struct shrink_control *sc,
