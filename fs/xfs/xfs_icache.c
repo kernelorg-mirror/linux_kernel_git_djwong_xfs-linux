b44d9ad45373e1205082d932ab00ae51f06ac629
Darrick J. Wong <djwong@kernel.org>
2021-07-22 13:18:12 -0700

    xfs: reduce inactivation delay when realtime extents are tight
    Implement the same scaling down of inodegc delays when we're tight on
    realtime extents that we do for free blocks on the data device.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_icache.c
+++ fs/xfs/xfs_icache.c
@@ -1904,6 +1904,27 @@ xfs_inodegc_start(
 	}
 }
 
+#ifdef CONFIG_XFS_RT
+static inline bool
+xfs_inodegc_want_queue_rt_file(
+	struct xfs_inode	*ip)
+{
+	struct xfs_mount	*mp = ip->i_mount;
+	uint64_t		freertx;
+
+	if (!XFS_IS_REALTIME_INODE(ip))
+		return false;
+
+	spin_lock(&mp->m_sb_lock);
+	freertx = mp->m_sb.sb_frextents;
+	spin_unlock(&mp->m_sb_lock);
+
+	return freertx < mp->m_low_rtexts[XFS_LOWSP_5_PCNT];
+}
+#else
+# define xfs_inodegc_want_queue_rt_file(ip)	(false)
+#endif /* CONFIG_XFS_RT */
+
 /*
  * Schedule the inactivation worker when:
  *
@@ -1928,6 +1949,9 @@ xfs_inodegc_want_queue_work(
 	if (__percpu_counter_compare(&mp->m_fdblocks,
 				mp->m_low_space[XFS_LOWSP_5_PCNT],
 				XFS_FDBLOCKS_BATCH) < 0)
+		return true;
+
+	if (xfs_inodegc_want_queue_rt_file(ip))
 		return true;
 
 	if (xfs_inode_near_dquot_enforcement(ip, XFS_DQTYPE_USER))
