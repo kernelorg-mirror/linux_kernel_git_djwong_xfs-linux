d46903a0794a14003eb29cf6159232ec62ee5d8f
Darrick J. Wong <djwong@kernel.org>
2021-08-04 14:27:32 -0700

    xfs: throttle inode inactivation queuing on memory reclaim
    Now that we defer inode inactivation, we've decoupled the process of
    unlinking or closing an inode from the process of inactivating it.  In
    theory this should lead to better throughput since we now inactivate the
    queued inodes in batches instead of one at a time.
    
    Unfortunately, one of the primary risks with this decoupling is the loss
    of rate control feedback between the frontend and background threads.
    In other words, a rm -rf /* thread can run the system out of memory if
    it can queue inodes for inactivation and jump to a new CPU faster than
    the background threads can actually clear the deferred work.  The
    workers can get scheduled off the CPU if they have to do IO, etc.
    
    To solve this problem, we configure a shrinker so that it will activate
    the /second/ time the shrinkers are called.  The custom shrinker will
    queue all percpu deferred inactivation workers immediately and set a
    flag to force frontend callers who are releasing a vfs inode to wait for
    the inactivation workers.
    
    On my test VM with 560M of RAM and a 2TB filesystem, this seems to solve
    most of the OOMing problem when deleting 10 million inodes.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -63,6 +63,7 @@ struct xfs_inodegc {
 	struct llist_head	list;
 	struct work_struct	work;
 	unsigned int		items;
+	unsigned int		shrinker_hits;
 };
 
 /*
@@ -208,6 +209,8 @@ typedef struct xfs_mount {
 	xfs_agnumber_t		m_agirotor;	/* last ag dir inode alloced */
 	spinlock_t		m_agirotor_lock;/* .. and lock protecting it */
 
+	/* Memory shrinker to throttle and reprioritize inodegc */
+	struct shrinker		m_inodegc_shrinker;
 	/*
 	 * Workqueue item so that we can coalesce multiple inode flush attempts
 	 * into a single flush.
