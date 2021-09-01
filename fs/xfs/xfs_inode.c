ed1cc68fe3498e97e7df42f12964c52780f2b7ba
Darrick J. Wong <djwong@kernel.org>
2021-09-01 10:46:57 -0700

    xfs: create a new helper to return a file's allocation unit
    Create a new helper function to calculate the fundamental allocation
    unit (i.e. the smallest unit of space we can allocate) of a file.
    Things are going to get hairy with range-exchange on the realtime
    device, so prepare for this now.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -3862,3 +3862,16 @@ xfs_inode_count_blocks(
 	xfs_bmap_count_leaves(ifp, rblocks);
 	*dblocks = ip->i_nblocks - *rblocks;
 }
+
+/* Returns the size of fundamental allocation unit for a file, in bytes. */
+unsigned int
+xfs_inode_alloc_unitsize(
+	struct xfs_inode	*ip)
+{
+	unsigned int		blocks = 1;
+
+	if (XFS_IS_REALTIME_INODE(ip))
+		blocks = ip->i_mount->m_sb.sb_rextsize;
+
+	return XFS_FSB_TO_B(ip->i_mount, blocks);
+}
