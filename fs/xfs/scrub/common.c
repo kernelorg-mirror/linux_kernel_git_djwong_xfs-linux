822746d21031997532ecb6d3526e4ee7cfe112a8
Darrick J. Wong <djwong@kernel.org>
2021-09-01 11:17:56 -0700

    xfs: scrub the realtime rmapbt
    Check the realtime reverse mapping btree against the rtbitmap, and
    modify the rtbitmap scrub to check against the rtrmapbt.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/common.c
+++ fs/xfs/scrub/common.c
@@ -29,6 +29,7 @@
 #include "xfs_ag.h"
 #include "xfs_error.h"
 #include "xfs_rtalloc.h"
+#include "xfs_rtrmap_btree.h"
 #include "scrub/scrub.h"
 #include "scrub/common.h"
 #include "scrub/trace.h"
@@ -599,6 +600,16 @@ xchk_ag_init(
 	return 0;
 }
 
+/* Lock everything we need to work on realtime metadata. */
+void
+xchk_rt_lock(
+	struct xfs_scrub	*sc,
+	struct xchk_rt		*sr)
+{
+	xfs_rtlock(NULL, sc->mp, XFS_RTLOCK_ALL);
+	sr->locked = true;
+}
+
 /*
  * For scrubbing a realtime file, grab all the in-core resources we'll need to
  * check the realtime metadata, which means taking the ILOCK of the realtime
@@ -610,8 +621,28 @@ xchk_rt_init(
 	struct xfs_scrub	*sc,
 	struct xchk_rt		*sr)
 {
-	xfs_rtlock(NULL, sc->mp, XFS_RTLOCK_ALL);
-	sr->locked = true;
+	struct xfs_mount	*mp = sc->mp;
+
+	xchk_rt_lock(sc, sr);
+
+	if (xfs_has_rtrmapbt(mp))
+		sr->rmap_cur = xfs_rtrmapbt_init_cursor(mp, sc->tp,
+				mp->m_rrmapip);
+}
+
+/*
+ * Free all the btree cursors and other incore data relating to the realtime
+ * volume.  This has to be done /before/ committing (or cancelling) the scrub
+ * transaction.
+ */
+void
+xchk_rt_btcur_free(
+	struct xchk_rt		*sr)
+{
+	if (sr->rmap_cur)
+		xfs_btree_del_cursor(sr->rmap_cur, XFS_BTREE_ERROR);
+
+	sr->rmap_cur = NULL;
 }
 
 /*
