ffe7e7772e191c6f9ebbd4ee383fe8e01df3132b
Darrick J. Wong <djwong@kernel.org>
2021-09-01 11:15:55 -0700

    xfs: refactor realtime inode locking
    Refactor realtime metadata inode locking so that we can get some sense
    here.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/fscounters.c
+++ fs/xfs/scrub/fscounters.c
@@ -316,7 +316,7 @@ xchk_fscount_check_frextents(
 	}
 
 	fsc->frextents = 0;
-	xfs_ilock(sc->mp->m_rbmip, XFS_ILOCK_EXCL);
+	xfs_rtlock(NULL, sc->mp, XFS_RTLOCK_ALL);
 	error = xfs_rtalloc_query_all(sc->tp, xchk_fscount_add_frextent, fsc);
 	if (error)
 		goto out_unlock;
@@ -335,7 +335,7 @@ xchk_fscount_check_frextents(
 	spin_unlock(&mp->m_sb_lock);
 
 out_unlock:
-	xfs_iunlock(sc->mp->m_rbmip, XFS_ILOCK_EXCL);
+	xfs_rtunlock(sc->mp, XFS_RTLOCK_ALL);
 	return error;
 }
 #else
