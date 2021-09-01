52b4a83ce114626b05fda51fd0b935a5d77a4075
Darrick J. Wong <djwong@kernel.org>
2021-09-01 10:46:48 -0700

    xfs: introduce online scrub freeze
    Introduce a new 'online scrub freeze' that we can use to lock out all
    filesystem modifications and background activity so that we can perform
    global scans in order to rebuild metadata.  This introduces a new IFLAG
    to the scrub ioctl to indicate that userspace is willing to allow a
    freeze.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/trace.h
+++ fs/xfs/scrub/trace.h
@@ -99,7 +99,8 @@ TRACE_DEFINE_ENUM(XFS_SCRUB_TYPE_FSCOUNTERS);
 	{ XFS_SCRUB_OFLAG_XCORRUPT,		"xcorrupt" }, \
 	{ XFS_SCRUB_OFLAG_INCOMPLETE,		"incomplete" }, \
 	{ XFS_SCRUB_OFLAG_WARNING,		"warning" }, \
-	{ XFS_SCRUB_OFLAG_NO_REPAIR_NEEDED,	"norepair" }
+	{ XFS_SCRUB_OFLAG_NO_REPAIR_NEEDED,	"norepair" }, \
+	{ XFS_SCRUB_IFLAG_FREEZE_OK,		"freeze" }
 
 DECLARE_EVENT_CLASS(xchk_class,
 	TP_PROTO(struct xfs_inode *ip, struct xfs_scrub_metadata *sm,
