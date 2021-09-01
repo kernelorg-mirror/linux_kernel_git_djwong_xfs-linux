bda0065738e43e3b057c8a27a76dbc2c69fef634
Darrick J. Wong <djwong@kernel.org>
2021-09-01 11:17:56 -0700

    xfs: scrub the realtime rmapbt
    Check the realtime reverse mapping btree against the rtbitmap, and
    modify the rtbitmap scrub to check against the rtrmapbt.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/bmap.c
+++ fs/xfs/scrub/bmap.c
@@ -321,6 +321,7 @@ xchk_bmap_rt_iextent_xref(
 			irec->br_blockcount);
 
 out_free:
+	xchk_rt_btcur_free(&info->sc->sr);
 	xchk_rt_unlock(info->sc, &info->sc->sr);
 }
 
@@ -756,6 +757,7 @@ xchk_bmap(
 	case XFS_DINODE_FMT_UUID:
 	case XFS_DINODE_FMT_DEV:
 	case XFS_DINODE_FMT_LOCAL:
+	case XFS_DINODE_FMT_RMAP:
 		/* No mappings to check. */
 		goto out;
 	case XFS_DINODE_FMT_EXTENTS:
