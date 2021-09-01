70959c253e1034bcff2bf97b014aa311b28e2c0b
Darrick J. Wong <djwong@kernel.org>
2021-09-01 11:17:56 -0700

    xfs: scrub the realtime rmapbt
    Check the realtime reverse mapping btree against the rtbitmap, and
    modify the rtbitmap scrub to check against the rtrmapbt.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/scrub.h
+++ fs/xfs/scrub/scrub.h
@@ -62,6 +62,8 @@ struct xchk_ag {
 /* Btree cursors for the RT volume. */
 struct xchk_rt {
 	bool			locked;
+
+	struct xfs_btree_cur	*rmap_cur;
 };
 
 struct xfs_scrub {
@@ -159,6 +161,7 @@ int xchk_parent(struct xfs_scrub *sc);
 #ifdef CONFIG_XFS_RT
 int xchk_rtbitmap(struct xfs_scrub *sc);
 int xchk_rtsummary(struct xfs_scrub *sc);
+int xchk_rtrmapbt(struct xfs_scrub *sc);
 #else
 static inline int
 xchk_rtbitmap(struct xfs_scrub *sc)
@@ -167,6 +170,11 @@ xchk_rtbitmap(struct xfs_scrub *sc)
 }
 static inline int
 xchk_rtsummary(struct xfs_scrub *sc)
+{
+	return -ENOENT;
+}
+static inline int
+xchk_rtrmapbt(struct xfs_scrub *sc)
 {
 	return -ENOENT;
 }
