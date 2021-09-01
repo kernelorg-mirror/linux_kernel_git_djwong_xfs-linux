42a2d344f2aa3279ab0e6f02087f1eb3fb7c7065
Darrick J. Wong <djwong@kernel.org>
2021-09-01 10:46:56 -0700

    xfs: support two inodes in the defer capture structure
    Make it so that xfs_defer_ops_capture_and_commit can capture two inodes.
    This will be needed by the atomic extent swap log item so that it can
    recover an operation involving two inodes.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_log_recover.c
+++ fs/xfs/xfs_log_recover.c
@@ -2466,7 +2466,7 @@ xlog_finish_defer_ops(
 {
 	struct xfs_defer_capture *dfc, *next;
 	struct xfs_trans	*tp;
-	struct xfs_inode	*ip;
+	struct xfs_inode	*ip1, *ip2;
 	int			error = 0;
 
 	list_for_each_entry_safe(dfc, next, capture_list, dfc_list) {
@@ -2494,12 +2494,16 @@ xlog_finish_defer_ops(
 		 * from recovering a single intent item.
 		 */
 		list_del_init(&dfc->dfc_list);
-		xfs_defer_ops_continue(dfc, tp, &ip);
+		xfs_defer_ops_continue(dfc, tp, &ip1, &ip2);
 
 		error = xfs_trans_commit(tp);
-		if (ip) {
-			xfs_iunlock(ip, XFS_ILOCK_EXCL);
-			xfs_irele(ip);
+		if (ip1) {
+			xfs_iunlock(ip1, XFS_ILOCK_EXCL);
+			xfs_irele(ip1);
+		}
+		if (ip2) {
+			xfs_iunlock(ip2, XFS_ILOCK_EXCL);
+			xfs_irele(ip2);
 		}
 		if (error)
 			return error;
