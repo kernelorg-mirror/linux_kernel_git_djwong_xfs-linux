ebbf385a912f3d1e5c13d7e33101e8aefb300f15
Darrick J. Wong <djwong@kernel.org>
2021-09-01 10:45:54 -0700

    xfs: repair inode block maps
    Use the reverse-mapping btree information to rebuild an inode block map.
    Update the btree bulk loading code as necessary to support inode rooted
    btrees and fix some bitrot problems.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/bmap.c
+++ fs/xfs/scrub/bmap.c
@@ -29,6 +29,7 @@ int
 xchk_setup_inode_bmap(
 	struct xfs_scrub	*sc)
 {
+	bool			is_repair = false;
 	int			error;
 
 	error = xchk_get_inode(sc);
@@ -36,6 +37,10 @@ xchk_setup_inode_bmap(
 		goto out;
 
 	xchk_ilock(sc, XFS_IOLOCK_EXCL | XFS_MMAPLOCK_EXCL);
+
+#ifdef CONFIG_XFS_REPAIR
+	is_repair = (sc->sm->sm_flags & XFS_SCRUB_IFLAG_REPAIR);
+#endif
 
 	/*
 	 * We don't want any ephemeral data fork updates sitting around
@@ -45,6 +50,14 @@ xchk_setup_inode_bmap(
 	if (S_ISREG(VFS_I(sc->ip)->i_mode) &&
 	    sc->sm->sm_type == XFS_SCRUB_TYPE_BMBTD) {
 		struct address_space	*mapping = VFS_I(sc->ip)->i_mapping;
+
+		/* Break all our leases, we're going to mess with things. */
+		if (is_repair) {
+			error = xfs_break_layouts(VFS_I(sc->ip),
+					&sc->ilock_flags, BREAK_UNMAP);
+			if (error)
+				goto out;
+		}
 
 		inode_dio_wait(VFS_I(sc->ip));
 
@@ -66,6 +79,15 @@ xchk_setup_inode_bmap(
 			error = filemap_fdatawait_keep_errors(mapping);
 		if (error && (error != -ENOSPC && error != -EIO))
 			goto out;
+
+		/* Drop the page cache if we're repairing block mappings. */
+		if (is_repair) {
+			error = invalidate_inode_pages2(
+					VFS_I(sc->ip)->i_mapping);
+			if (error)
+				goto out;
+		}
+
 	}
 
 	/* Got the inode, lock it and we're ready to go. */
