24a0cecfe68f19e1abc7d9879825a860cc63c2f7
Darrick J. Wong <djwong@kernel.org>
2021-09-01 11:17:56 -0700

    xfs: fix scrub tracepoints when inode-rooted btrees are involved
    Fix a couple of minor mistakes in the scrub tracepoints that can
    manifest when inode-rooted btrees are enabled.  The existing code worked
    fine for bmap btrees, but we should tighten the code up to be less
    sloppy.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/trace.c
+++ fs/xfs/scrub/trace.c
@@ -30,7 +30,7 @@ xchk_btree_cur_fsbno(
 		return XFS_DADDR_TO_FSB(cur->bc_mp,
 				xfs_buf_daddr(cur->bc_levels[level].bp));
 	else if (level == cur->bc_nlevels - 1 &&
-		 cur->bc_flags & XFS_BTREE_LONG_PTRS)
+		 (cur->bc_flags & XFS_BTREE_ROOT_IN_INODE))
 		return XFS_INO_TO_FSB(cur->bc_mp, cur->bc_ino.ip->i_ino);
 	if (!(cur->bc_flags & XFS_BTREE_LONG_PTRS))
 		return XFS_AGB_TO_FSB(cur->bc_mp, cur->bc_ag.pag->pag_agno, 0);
