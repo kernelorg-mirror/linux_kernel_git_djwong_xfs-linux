361f5659cc86fc6b4ec733ae0148cc8d1be407d7
Darrick J. Wong <djwong@kernel.org>
2021-09-01 11:15:58 -0700

    xfs: convert "skip_discard" to a proper flags bitset
    Convert the boolean to skip discard on free into a proper flags field so
    that we can add more flags in the next patch.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.c
+++ fs/xfs/scrub/repair.c
@@ -674,8 +674,8 @@ xrep_newbt_destroy_reservation(
 			XFS_FSB_TO_AGBNO(sc->mp, resv->fsbno),
 			resv->len, xnr->oinfo.oi_owner);
 
-	__xfs_free_extent_later(sc->tp, resv->fsbno, resv->len, &xnr->oinfo,
-			true);
+	xfs_free_extent_later(sc->tp, resv->fsbno, resv->len, &xnr->oinfo,
+			XFS_FREE_EXTENT_SKIP_DISCARD);
 
 	return 0;
 }
@@ -1092,7 +1092,8 @@ xrep_agextent_reap(
 		 * Roll the transaction every 100 or so EFIs so that we don't
 		 * exceed the log reservation.
 		 */
-		__xfs_free_extent_later(sc->tp, fsbno, aglen, rs->oinfo, true);
+		xfs_free_extent_later(sc->tp, fsbno, aglen, rs->oinfo,
+				XFS_FREE_EXTENT_SKIP_DISCARD);
 		rs->deferred++;
 		*want_roll = rs->deferred > 100;
 		break;
@@ -1568,8 +1569,8 @@ xrep_bmapi_reap_extent(
 		if (error)
 			goto out_agf;
 
-		__xfs_free_extent_later(sc->tp, imap->br_startblock, len, NULL,
-				true);
+		xfs_free_extent_later(sc->tp, imap->br_startblock, len, NULL,
+				XFS_FREE_EXTENT_SKIP_DISCARD);
 	}
 
 	/* Update the mapping to reflect the work not yet done and exit. */
