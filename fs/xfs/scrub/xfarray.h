e43fbfebf7beae106f3def1cce0f78838e4451c3
Darrick J. Wong <djwong@kernel.org>
2021-09-01 10:40:21 -0700

    xfs: create a big array data structure
    Create a simple 'big array' data structure for storage of fixed-size
    metadata records that will be used to reconstruct a btree index.  For
    repair operations, the most important operations are append, iterate,
    and sort.
    
    Earlier implementations of the big array used linked lists and suffered
    from severe problems -- pinning all records in kernel memory was not a
    good idea and frequently lead to OOM situations; random access was very
    inefficient; and record overhead for the lists was unacceptably high at
    40-60%.
    
    Therefore, the big memory array relies on the 'xfile' abstraction, which
    creates a memfd file and stores the records in page cache pages.  Since
    the memfd is created in tmpfs, the memory pages can be pushed out to
    disk if necessary and we have a built-in usage limit of 50% of physical
    memory.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

---
+++ fs/xfs/scrub/xfarray.h
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2021 Oracle.  All Rights Reserved.
+ * Author: Darrick J. Wong <djwong@kernel.org>
+ */
+#ifndef __XFS_SCRUB_XFARRAY_H__
+#define __XFS_SCRUB_XFARRAY_H__
+
+struct xfarray {
+	/* Underlying file that backs the array. */
+	struct xfile	*xfile;
+
+	/* Number of array elements. */
+	uint64_t	nr;
+
+	/* Maximum possible array size. */
+	uint64_t	max_nr;
+
+	/* Size of an array element. */
+	size_t		obj_size;
+};
+
+int xfarray_create(struct xfs_mount *mp, const char *descr, size_t obj_size,
+		struct xfarray **arrayp);
+void xfarray_destroy(struct xfarray *array);
+int xfarray_load(struct xfarray *array, uint64_t idx, void *ptr);
+int xfarray_store(struct xfarray *array, uint64_t idx, void *ptr);
+int xfarray_store_anywhere(struct xfarray *array, void *ptr);
+bool xfarray_is_null(struct xfarray *array, void *ptr);
+int xfarray_nullify(struct xfarray *array, uint64_t idx);
+
+/* Append an element to the array. */
+static inline int xfarray_append(struct xfarray *array, void *ptr)
+{
+	return xfarray_store(array, array->nr, ptr);
+}
+
+uint64_t xfarray_length(struct xfarray *array);
+int xfarray_load_next(struct xfarray *array, uint64_t *idx, void *rec);
+
+typedef int (*xfarray_cmp_fn)(const void *a, const void *b);
+
+int xfarray_sort(struct xfarray *array, xfarray_cmp_fn cmp_fn);
+
+#define foreach_xfarray_item(array, i, rec) \
+	for ((i) = 0; (i) < xfarray_length((array)); (i)++) \
+		if (xfarray_load((array), (i), &(rec)) == 0 && \
+		    !xfarray_is_null((array), &(rec)))
+
+#endif /* __XFS_SCRUB_XFARRAY_H__ */
