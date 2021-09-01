20303423bc47b4108fc90a9b1eb326e8f7c29842
Darrick J. Wong <djwong@kernel.org>
2021-09-01 11:15:54 -0700

    xfs: refactor realtime scrubbing context management
    Create a pair of helpers to deal with setting up the necessary incore
    context to check metadata records against the realtime metadata.  Right
    now this is limited to locking the realtime bitmap and summary inodes,
    but as we add rmap and reflink to the realtime device this will grow to
    include btree cursors.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/bmap.c
+++ fs/xfs/scrub/bmap.c
@@ -310,8 +310,18 @@ xchk_bmap_rt_iextent_xref(
 	struct xchk_bmap_info	*info,
 	struct xfs_bmbt_irec	*irec)
 {
+	int			error;
+
+	error = xchk_rt_init(info->sc, &info->sc->sr);
+	if (!xchk_fblock_process_error(info->sc, info->whichfork,
+			irec->br_startoff, &error))
+		goto out_free;
+
 	xchk_xref_is_used_rt_space(info->sc, irec->br_startblock,
 			irec->br_blockcount);
+
+out_free:
+	xchk_rt_unlock(info->sc, &info->sc->sr);
 }
 
 /* Cross-reference a single datadev extent record. */
