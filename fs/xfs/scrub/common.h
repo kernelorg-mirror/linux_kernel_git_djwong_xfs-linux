ffd48eaf9158b6deb40e64ba5667a3d7250678c8
Darrick J. Wong <djwong@kernel.org>
2021-09-01 11:25:40 -0700

    xfs: allow userspace to rebuild metadata structures
    Add a new (superuser-only) flag to the online metadata repair ioctl to
    force it to rebuild structures, even if they're not broken.  We will use
    this to move metadata structures out of the way during a free space
    defragmentation operation.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/common.h
+++ fs/xfs/scrub/common.h
@@ -202,7 +202,8 @@ static inline bool xfs_scrub_needs_repair(struct xfs_scrub_metadata *sm)
 {
 	return sm->sm_flags & (XFS_SCRUB_OFLAG_CORRUPT |
 			       XFS_SCRUB_OFLAG_XCORRUPT |
-			       XFS_SCRUB_OFLAG_PREEN);
+			       XFS_SCRUB_OFLAG_PREEN |
+			       XFS_SCRUB_IFLAG_FORCE_REBUILD);
 }
 
 int xchk_iwalk_find_next(struct xfs_mount *mp, struct xfs_trans *tp,
