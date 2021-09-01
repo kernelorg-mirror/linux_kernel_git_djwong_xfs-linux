9e731f676af95430aa81c0c9dbedfffeed3a2188
Darrick J. Wong <djwong@kernel.org>
2021-09-01 11:15:48 -0700

    xfs: dynamically allocate btree scrub context structure
    Reorganize struct xchk_btree so that we can dynamically size the context
    structure to fit the type of btree cursor that we have.  This will
    enable us to use memory more efficiently once we start adding very tall
    btree types.  Right-size the lastkey array so that we stop wasting the
    first array element.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/btree.h
+++ fs/xfs/scrub/btree.h
@@ -29,6 +29,11 @@ typedef int (*xchk_btree_rec_fn)(
 	struct xchk_btree		*bs,
 	const union xfs_btree_rec	*rec);
 
+struct xchk_btree_levels {
+	union xfs_btree_key		lastkey;
+	bool				has_lastkey;
+};
+
 struct xchk_btree {
 	/* caller-provided scrub state */
 	struct xfs_scrub		*sc;
@@ -39,9 +44,18 @@ struct xchk_btree {
 
 	/* internal scrub state */
 	union xfs_btree_rec		lastrec;
-	union xfs_btree_key		lastkey[XFS_BTREE_MAXLEVELS];
 	struct list_head		to_check;
+
+	/* this element must come last! */
+	union xfs_btree_key		lastkey[];
 };
+
+static inline size_t
+xchk_btree_sizeof(unsigned int nr_levels)
+{
+	return struct_size((struct xchk_btree *)NULL, lastkey, nr_levels);
+}
+
 int xchk_btree(struct xfs_scrub *sc, struct xfs_btree_cur *cur,
 		xchk_btree_rec_fn scrub_fn, const struct xfs_owner_info *oinfo,
 		void *private);
