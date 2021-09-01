125add7256748d04cff7198d3b5ed735740f1e5f
Darrick J. Wong <djwong@kernel.org>
2021-09-01 11:16:06 -0700

    xfs: define the on-disk realtime rmap btree format
    Start filling out the rtrmap btree implementation. Start with the
    on-disk btree format; add everything needed to read, write and
    manipulate rmap btree blocks. This prepares the way for connecting the
    btree operations implementation.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -44,6 +44,7 @@
 #include "xfs_bmap_btree.h"
 #include "xfs_rmap_btree.h"
 #include "xfs_refcount_btree.h"
+#include "xfs_rtrmap_btree.h"
 
 #include <linux/magic.h>
 #include <linux/fs_context.h>
@@ -2026,6 +2027,9 @@ xfs_init_btree_caches(void)
 	if (error)
 		return error;
 	error = xfs_refcountbt_create_cursor_cache();
+	if (error)
+		return error;
+	error = xfs_rtrmapbt_create_cursor_cache();
 	if (error)
 		return error;
 
