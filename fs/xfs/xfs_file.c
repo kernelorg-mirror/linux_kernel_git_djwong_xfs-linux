5b1b7068fc8f653d38cb59fe1448475ca7e995ca
Darrick J. Wong <djwong@kernel.org>
2021-09-01 11:25:43 -0700

    xfs: implement FALLOC_FL_ZEROINIT_RANGE
    Implement this new fallocate mode so that persistent memory users can,
    upon receipt of a pmem poison notification, cause the pmem to be
    reinitialized to a known value (zero) and clear any hardware poison
    state that might be lurking.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_file.c
+++ fs/xfs/xfs_file.c
@@ -987,6 +987,7 @@ xfs_break_layouts(
 #define	XFS_FALLOC_FL_SUPPORTED						\
 		(FALLOC_FL_KEEP_SIZE | FALLOC_FL_PUNCH_HOLE |		\
 		 FALLOC_FL_COLLAPSE_RANGE | FALLOC_FL_ZERO_RANGE |	\
+		 FALLOC_FL_ZEROINIT_RANGE |				\
 		 FALLOC_FL_INSERT_RANGE | FALLOC_FL_UNSHARE_RANGE |	\
 		 FALLOC_FL_MAP_FREE_SPACE)
 
@@ -1039,13 +1040,17 @@ xfs_file_fallocate(
 	 * handled at the right time by xfs_prepare_shift().
 	 */
 	if (mode & (FALLOC_FL_PUNCH_HOLE | FALLOC_FL_ZERO_RANGE |
-		    FALLOC_FL_COLLAPSE_RANGE)) {
+		    FALLOC_FL_COLLAPSE_RANGE | FALLOC_FL_ZEROINIT_RANGE)) {
 		error = xfs_flush_unmap_range(ip, offset, len);
 		if (error)
 			goto out_unlock;
 	}
 
-	if (mode & FALLOC_FL_PUNCH_HOLE) {
+	if (mode & FALLOC_FL_ZEROINIT_RANGE) {
+		error = xfs_file_zeroinit_space(ip, offset, len);
+		if (error)
+			goto out_unlock;
+	} else if (mode & FALLOC_FL_PUNCH_HOLE) {
 		/* Unshare around the region to punch, if needed. */
 		if (xfs_inode_needs_cow_around(ip)) {
 			error = xfs_file_cow_around(ip, offset, len);
