69a112b93776e28278ba7a74089efb7a8a962eb4
Darrick J. Wong <djwong@kernel.org>
2021-09-03 14:07:18 -0700

    dax: prepare pmem for use by zero-initializing contents and clearing poisons
    Our current "advice" to people using persistent memory and FSDAX who
    wish to recover upon receipt of a media error (aka 'hwpoison') event
    from ACPI is to punch-hole that part of the file and then pwrite it,
    which will magically cause the pmem to be reinitialized and the poison
    to be cleared.
    
    Punching doesn't make any sense at all -- the (re)allocation on pwrite
    does not permit the caller to specify where to find blocks, which means
    that we might not get the same pmem back.  This pushes the user farther
    away from the goal of reinitializing poisoned memory and leads to
    complaints about unnecessary file fragmentation.
    
    AFAICT, the only reason why the "punch and write" dance works at all is
    that the XFS and ext4 currently call blkdev_issue_zeroout when
    allocating pmem ahead of a write call.  Even a regular overwrite won't
    clear the poison, because dax_direct_access is smart enough to bail out
    on poisoned pmem, but not smart enough to clear it.  To be fair, that
    function maps pages and has no idea what kinds of reads and writes the
    caller might want to perform.
    
    Therefore, create a dax_zeroinit_range function that filesystems can to
    reset the pmem contents to zero and clear hardware media error flags.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/linux/dax.h
+++ include/linux/dax.h
@@ -152,6 +152,8 @@ struct page *dax_layout_busy_page(struct address_space *mapping);
 struct page *dax_layout_busy_page_range(struct address_space *mapping, loff_t start, loff_t end);
 dax_entry_t dax_lock_page(struct page *page);
 void dax_unlock_page(struct page *page, dax_entry_t cookie);
+int dax_zeroinit_range(struct inode *inode, loff_t pos, u64 len,
+			const struct iomap_ops *ops);
 #else
 static inline bool bdev_dax_supported(struct block_device *bdev,
 		int blocksize)
@@ -200,6 +202,11 @@ static inline dax_entry_t dax_lock_page(struct page *page)
 
 static inline void dax_unlock_page(struct page *page, dax_entry_t cookie)
 {
+}
+static inline int dax_zeroinit_range(struct inode *inode, loff_t pos, u64 len,
+		const struct iomap_ops *ops)
+{
+	return -EOPNOTSUPP;
 }
 #endif
 
