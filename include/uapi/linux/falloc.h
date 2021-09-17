4586e51498f80160a8f7e2ae29e24516f78d9dab
Darrick J. Wong <djwong@kernel.org>
2021-09-17 16:27:51 -0700

    vfs: add a zero-initialization mode to fallocate
    Add a new mode to fallocate to zero-initialize all the storage backing a
    file.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/uapi/linux/falloc.h
+++ include/uapi/linux/falloc.h
@@ -77,4 +77,13 @@
  */
 #define FALLOC_FL_UNSHARE_RANGE		0x40
 
+/*
+ * FALLOC_FL_ZEROINIT_RANGE is used to reinitialize storage backing a file by
+ * writing zeros to it.  Subsequent read and writes should not fail due to any
+ * previous media errors.  Blocks must be not be shared or require copy on
+ * write.  Holes and unwritten extents are left untouched.  This mode must be
+ * used with FALLOC_FL_KEEP_SIZE.
+ */
+#define FALLOC_FL_ZEROINIT_RANGE	0x80
+
 #endif /* _UAPI_FALLOC_H_ */
