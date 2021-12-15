8d6b2944fec4da61f47a01da7f904775bae9b922
Darrick J. Wong <djwong@kernel.org>
2021-12-15 12:07:41 -0800

    xfs: don't expose internal symlink metadata buffers to the vfs
    Ian Kent reported that for inline symlinks, it's possible for
    vfs_readlink to hang on to the target buffer returned by
    _vn_get_link_inline long after it's been freed by xfs inode reclaim.
    This is a layering violation -- we should never expose XFS internals to
    the VFS.
    
    When the symlink has a remote target, we allocate a separate buffer,
    copy the internal information, and let the VFS manage the new buffer's
    lifetime.  Let's adapt the inline code paths to do this too.  It's
    less efficient, but fixes the layering violation and avoids the need to
    adapt the if_data lifetime to rcu rules.  Clearly I don't care about
    readlink benchmarks.
    
    As a side note, this fixes the minor locking violation where we can
    access the inode data fork without taking any locks; proper locking (and
    eliminating the possibility of having to switch inode_operations on a
    live inode) is essential to online repair coordinating repairs
    correctly.
    
    Reported-by: Ian Kent <raven@themaw.net>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_symlink.c
+++ fs/xfs/xfs_symlink.c
@@ -22,6 +22,7 @@
 #include "xfs_trace.h"
 #include "xfs_trans.h"
 #include "xfs_ialloc.h"
+#include "xfs_error.h"
 
 /* ----- Kernel only functions below ----- */
 int
@@ -101,11 +102,9 @@ xfs_readlink(
 {
 	struct xfs_mount *mp = ip->i_mount;
 	xfs_fsize_t	pathlen;
-	int		error = 0;
+	int		error = -EFSCORRUPTED;
 
 	trace_xfs_readlink(ip);
-
-	ASSERT(ip->i_df.if_format != XFS_DINODE_FMT_LOCAL);
 
 	if (xfs_is_shutdown(mp))
 		return -EIO;
@@ -121,12 +120,22 @@ xfs_readlink(
 			 __func__, (unsigned long long) ip->i_ino,
 			 (long long) pathlen);
 		ASSERT(0);
-		error = -EFSCORRUPTED;
 		goto out;
 	}
 
+	if (ip->i_df.if_format == XFS_DINODE_FMT_LOCAL) {
+		/*
+		 * The VFS crashes on a NULL pointer, so return -EFSCORRUPTED if
+		 * if_data is junk.
+		 */
+		if (XFS_IS_CORRUPT(ip->i_mount, !ip->i_df.if_u1.if_data))
+			goto out;
 
-	error = xfs_readlink_bmap_ilocked(ip, link);
+		memcpy(link, ip->i_df.if_u1.if_data, ip->i_disk_size + 1);
+		error = 0;
+	} else {
+		error = xfs_readlink_bmap_ilocked(ip, link);
+	}
 
  out:
 	xfs_iunlock(ip, XFS_ILOCK_SHARED);
