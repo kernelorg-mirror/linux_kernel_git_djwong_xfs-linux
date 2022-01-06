f7cf1eb854ae03e57d224ceb57aa06890a29d330
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:28:29 -0800

    xfs: move files to orphanage instead of letting nlinks drop to zero
    If we encounter an inode with a nonzero link count but zero observed
    links, move it to the orphanage.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.h
+++ fs/xfs/scrub/repair.h
@@ -82,6 +82,7 @@ int xrep_setup_rtsummary(struct xfs_scrub *sc, unsigned int *resblks,
 int xrep_setup_xattr(struct xfs_scrub *sc);
 int xrep_setup_directory(struct xfs_scrub *sc);
 int xrep_setup_parent(struct xfs_scrub *sc);
+int xrep_setup_nlinks(struct xfs_scrub *sc, unsigned int *buf_bytes);
 
 int xrep_xattr_reset_fork(struct xfs_scrub *sc);
 
@@ -271,6 +272,11 @@ xrep_setup_rtsummary(
 	struct xfs_scrub	*sc,
 	unsigned int		*whatever,
 	size_t			*dontcare)
+{
+	return 0;
+}
+
+static inline int xrep_setup_nlinks(struct xfs_scrub *sc, unsigned int *x)
 {
 	return 0;
 }
