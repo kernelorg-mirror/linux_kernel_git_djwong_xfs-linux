0a7f5272defb1454e174a2697bd7e3da5460479c
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:27:50 -0800

    xfs: repair dquots based on live quotacheck results
    Use the shadow quota counters that live quotacheck creates to reset the
    incore dquot counters.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.h
+++ fs/xfs/scrub/repair.h
@@ -107,8 +107,10 @@ int xrep_bmap_attr(struct xfs_scrub *sc);
 
 #ifdef CONFIG_XFS_QUOTA
 int xrep_quota(struct xfs_scrub *sc);
+int xrep_quotacheck(struct xfs_scrub *sc);
 #else
 # define xrep_quota			xrep_notsupported
+# define xrep_quotacheck		xrep_notsupported
 #endif /* CONFIG_XFS_QUOTA */
 
 struct xrep_newbt_resv {
@@ -226,6 +228,7 @@ xrep_setup_nothing(
 #define xrep_bmap_data			xrep_notsupported
 #define xrep_bmap_attr			xrep_notsupported
 #define xrep_quota			xrep_notsupported
+#define xrep_quotacheck			xrep_notsupported
 
 #endif /* CONFIG_XFS_ONLINE_REPAIR */
 
