98b9b1a3c78f549269d99b0d4a89e0a2c9e28e9b
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:29:16 -0800

    xfs: allow queued realtime intents to drain before scrubbing
    When a writer thread executes a chain of log intent items for the
    realtime volume, the ILOCKs taken during each step are for each rt
    metadata file, not the entire rt volume itself.  Although scrub takes
    all rt metadata ILOCKs, this isn't sufficient to guard against scrub
    checking the rt volume while that writer thread is in the middle of
    finishing a chain because there's no higher level locking primitive
    guarding the realtime volume.
    
    When there's a collision, cross-referencing between data structures
    (e.g. rtrmapbt and rtrefcountbt) yields false corruption events; if
    repair is running, this results in incorrect repairs, which is
    catastrophic.
    
    Fix this by adding to the mount structure the same drain that we use to
    protect scrub against concurrent AG updates, but this time for the
    realtime volume.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_mount.c
+++ fs/xfs/xfs_mount.c
@@ -1490,6 +1490,28 @@ static inline int xfs_drain_wait(struct xfs_drain *dr)
 	return error;
 }
 
+#ifdef CONFIG_XFS_RT
+# define xfs_rt_drain_bump(dr)	xfs_drain_bump(dr)
+# define xfs_rt_drain_drop(dr)	xfs_drain_drop(dr)
+
+/*
+ * Wait for the pending intent count for realtime metadata to hit zero.
+ * Callers must not hold any rt metadata inode locks.
+ */
+int
+xfs_rt_drain_intents(
+	struct xfs_mount	*mp)
+{
+	trace_xfs_rt_wait_intents(mp, __return_address);
+	return xfs_drain_wait(&mp->m_rt_intents);
+}
+#else
+# define trace_xfs_rt_bump_intents(...)
+# define trace_xfs_rt_drop_intents(...)
+# define xfs_rt_drain_bump(dr)
+# define xfs_rt_drain_drop(dr)
+#endif /* CONFIG_XFS_RT */
+
 /* Add an item to the pending count. */
 void
 xfs_fs_bump_intents(
@@ -1499,8 +1521,11 @@ xfs_fs_bump_intents(
 {
 	struct xfs_perag	*pag;
 
-	if (isrt)
+	if (isrt) {
+		trace_xfs_rt_bump_intents(mp, __return_address);
+		xfs_rt_drain_bump(&mp->m_rt_intents);
 		return;
+	}
 
 	pag = xfs_perag_get(mp, XFS_FSB_TO_AGNO(mp, fsb));
 	trace_xfs_perag_bump_intents(pag, __return_address);
@@ -1517,8 +1542,11 @@ xfs_fs_drop_intents(
 {
 	struct xfs_perag	*pag;
 
-	if (isrt)
+	if (isrt) {
+		trace_xfs_rt_drop_intents(mp, __return_address);
+		xfs_rt_drain_drop(&mp->m_rt_intents);
 		return;
+	}
 
 	pag = xfs_perag_get(mp, XFS_FSB_TO_AGNO(mp, fsb));
 	trace_xfs_perag_drop_intents(pag, __return_address);
