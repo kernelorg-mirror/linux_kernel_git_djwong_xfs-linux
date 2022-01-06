750dc07c042c4cd5d22b2f24dc11e04d412bf74e
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:28:58 -0800

    xfs: remove XFS_ILOCK_RT*
    Now that we've centralized the realtime metadata locking routines, get
    rid of the ILOCK subclasses since we now use explicit lockdep classes.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -1589,11 +1589,11 @@ xfs_rtlock(
 	ASSERT(!(lock_flags & ~XFS_RTLOCK_ALL));
 
 	if (lock_flags & XFS_RTLOCK_ALLOC) {
-		xfs_ilock(mp->m_rbmip, XFS_ILOCK_EXCL | XFS_ILOCK_RTBITMAP);
+		xfs_ilock(mp->m_rbmip, XFS_ILOCK_EXCL);
 		if (tp)
 			xfs_trans_ijoin(tp, mp->m_rbmip, XFS_ILOCK_EXCL);
 
-		xfs_ilock(mp->m_rsumip, XFS_ILOCK_EXCL | XFS_ILOCK_RTSUM);
+		xfs_ilock(mp->m_rsumip, XFS_ILOCK_EXCL);
 		if (tp)
 			xfs_trans_ijoin(tp, mp->m_rsumip, XFS_ILOCK_EXCL);
 	}
@@ -1608,7 +1608,7 @@ xfs_rtunlock(
 	ASSERT(!(lock_flags & ~XFS_RTLOCK_ALL));
 
 	if (lock_flags & XFS_RTLOCK_ALLOC) {
-		xfs_iunlock(mp->m_rsumip, XFS_ILOCK_EXCL | XFS_ILOCK_RTSUM);
-		xfs_iunlock(mp->m_rbmip, XFS_ILOCK_EXCL | XFS_ILOCK_RTBITMAP);
+		xfs_iunlock(mp->m_rsumip, XFS_ILOCK_EXCL);
+		xfs_iunlock(mp->m_rbmip, XFS_ILOCK_EXCL);
 	}
 }
