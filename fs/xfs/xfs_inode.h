ad8168313947025921eedfcc9caeb800f6a64e46
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:28:36 -0800

    xfs: hoist xfs_{bump,drop}link to libxfs
    Move xfs_bumplink and xfs_droplink to libxfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_inode.h
+++ fs/xfs/xfs_inode.h
@@ -510,7 +510,6 @@ void xfs_end_io(struct work_struct *work);
 
 int xfs_ilock2_io_mmap(struct xfs_inode *ip1, struct xfs_inode *ip2);
 void xfs_iunlock2_io_mmap(struct xfs_inode *ip1, struct xfs_inode *ip2);
-void xfs_bumplink(struct xfs_trans *tp, struct xfs_inode *ip);
 
 void xfs_inode_count_blocks(struct xfs_trans *tp, struct xfs_inode *ip,
 		xfs_filblks_t *dblocks, xfs_filblks_t *rblocks);
