e4ae06bafa2e149e6eebf10a9f706d536b4e1286
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:28:57 -0800

    xfs: refactor realtime inode locking
    Refactor realtime metadata inode locking so that we can get some sense
    here.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -1056,10 +1056,10 @@ xfs_growfs_rt(
 		if (error)
 			break;
 		/*
-		 * Lock out other callers by grabbing the bitmap inode lock.
+		 * Lock out other callers by grabbing the bitmap and summary
+		 * inode locks and joining them to the transaction.
 		 */
-		xfs_ilock(mp->m_rbmip, XFS_ILOCK_EXCL | XFS_ILOCK_RTBITMAP);
-		xfs_trans_ijoin(tp, mp->m_rbmip, XFS_ILOCK_EXCL);
+		xfs_rtlock(tp, mp, XFS_RTLOCK_ALLOC);
 		/*
 		 * Update the bitmap inode's size ondisk and incore.  We need
 		 * to update the incore size so that inode inactivation won't
@@ -1069,11 +1069,6 @@ xfs_growfs_rt(
 			nsbp->sb_rbmblocks * nsbp->sb_blocksize;
 		i_size_write(VFS_I(mp->m_rbmip), mp->m_rbmip->i_disk_size);
 		xfs_trans_log_inode(tp, mp->m_rbmip, XFS_ILOG_CORE);
-		/*
-		 * Get the summary inode into the transaction.
-		 */
-		xfs_ilock(mp->m_rsumip, XFS_ILOCK_EXCL | XFS_ILOCK_RTSUM);
-		xfs_trans_ijoin(tp, mp->m_rsumip, XFS_ILOCK_EXCL);
 		/*
 		 * Update the summary inode's size.  We need to update the
 		 * incore size so that inode inactivation won't punch what it
@@ -1313,10 +1308,10 @@ xfs_rtalloc_reinit_frextents(
 	uint64_t		val = 0;
 	int			error;
 
-	xfs_ilock(mp->m_rbmip, XFS_ILOCK_EXCL);
+	xfs_rtlock(NULL, mp, XFS_RTLOCK_ALLOC);
 	error = xfs_rtalloc_query_all(mp, NULL, xfs_rtalloc_count_frextent,
 			&val);
-	xfs_iunlock(mp->m_rbmip, XFS_ILOCK_EXCL);
+	xfs_rtunlock(mp, XFS_RTLOCK_ALLOC);
 	if (error)
 		return error;
 
@@ -1578,4 +1573,42 @@ xfs_rtfile_convert_unwritten(
 err:
 	xfs_trans_cancel(tp);
 	return ret;
+}
+
+/*
+ * Lock the metadata inodes for the realtime volume.  If a transaction is
+ * given, the rt metadata inode will be joined to the transaction and the lock
+ * will be released on transaction commit.
+ */
+void
+xfs_rtlock(
+	struct xfs_trans	*tp,
+	struct xfs_mount	*mp,
+	unsigned int		lock_flags)
+{
+	ASSERT(!(lock_flags & ~XFS_RTLOCK_ALL));
+
+	if (lock_flags & XFS_RTLOCK_ALLOC) {
+		xfs_ilock(mp->m_rbmip, XFS_ILOCK_EXCL | XFS_ILOCK_RTBITMAP);
+		if (tp)
+			xfs_trans_ijoin(tp, mp->m_rbmip, XFS_ILOCK_EXCL);
+
+		xfs_ilock(mp->m_rsumip, XFS_ILOCK_EXCL | XFS_ILOCK_RTSUM);
+		if (tp)
+			xfs_trans_ijoin(tp, mp->m_rsumip, XFS_ILOCK_EXCL);
+	}
+}
+
+/* Unlock the realtime metadata inodes. */
+void
+xfs_rtunlock(
+	struct xfs_mount	*mp,
+	unsigned int		lock_flags)
+{
+	ASSERT(!(lock_flags & ~XFS_RTLOCK_ALL));
+
+	if (lock_flags & XFS_RTLOCK_ALLOC) {
+		xfs_iunlock(mp->m_rsumip, XFS_ILOCK_EXCL | XFS_ILOCK_RTSUM);
+		xfs_iunlock(mp->m_rbmip, XFS_ILOCK_EXCL | XFS_ILOCK_RTBITMAP);
+	}
 }
