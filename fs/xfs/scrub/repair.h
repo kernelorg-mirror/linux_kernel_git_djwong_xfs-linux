2767664bbb3bd27a2b6b757c6158601ca7f83584
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:28:29 -0800

    xfs: online repair of symbolic links
    If a symbolic link target looks bad, try to sift through the rubble to
    find as much of the target buffer that we can, and stage a new target
    (short or remote format as needed) in a temporary file and use the
    atomic extent swapping mechanism to commit the results.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.h
+++ fs/xfs/scrub/repair.h
@@ -81,6 +81,7 @@ int xrep_setup_xattr(struct xfs_scrub *sc);
 int xrep_setup_directory(struct xfs_scrub *sc);
 int xrep_setup_parent(struct xfs_scrub *sc);
 int xrep_setup_nlinks(struct xfs_scrub *sc, unsigned int *buf_bytes);
+int xrep_setup_symlink(struct xfs_scrub *sc, unsigned int *resblks);
 
 int xrep_xattr_reset_fork(struct xfs_scrub *sc, struct xfs_inode *ip);
 
@@ -134,6 +135,7 @@ int xrep_fscounters(struct xfs_scrub *sc);
 int xrep_xattr(struct xfs_scrub *sc);
 int xrep_directory(struct xfs_scrub *sc);
 int xrep_parent(struct xfs_scrub *sc);
+int xrep_symlink(struct xfs_scrub *sc);
 
 #ifdef CONFIG_XFS_QUOTA
 int xrep_quota(struct xfs_scrub *sc);
@@ -264,6 +266,7 @@ xrep_setup_rtsummary(struct xfs_scrub *sc, unsigned int *whatever)
 }
 
 #define xrep_setup_nlinks		xrep_setup_rtsummary
+#define xrep_setup_symlink		xrep_setup_rtsummary
 
 #define xrep_revalidate_allocbt		(NULL)
 #define xrep_revalidate_iallocbt	(NULL)
@@ -288,6 +291,7 @@ xrep_setup_rtsummary(struct xfs_scrub *sc, unsigned int *whatever)
 #define xrep_xattr			xrep_notsupported
 #define xrep_directory			xrep_notsupported
 #define xrep_parent			xrep_notsupported
+#define xrep_symlink			xrep_notsupported
 
 #endif /* CONFIG_XFS_ONLINE_REPAIR */
 
