5bffc34b8ce1c2de2fae2375c2bf275f99d085e7
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:28:04 -0800

    xfs: repair the rmapbt
    Rebuild the reverse mapping btree from all primary metadata.  This first
    patch establishes the bare mechanics of finding records and putting
    together a new ondisk tree; more complex pieces are needed to make it
    work properly.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/common.h
+++ fs/xfs/scrub/common.h
@@ -133,6 +133,7 @@ int xchk_setup_nlinks(struct xfs_scrub *sc);
 void xchk_ag_free(struct xfs_scrub *sc, struct xchk_ag *sa);
 int xchk_ag_init(struct xfs_scrub *sc, xfs_agnumber_t agno,
 		struct xchk_ag *sa);
+int xchk_perag_lock(struct xfs_scrub *sc);
 
 /*
  * Grab all AG resources, treating the inability to grab the perag structure as
