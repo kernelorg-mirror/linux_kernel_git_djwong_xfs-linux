0c6915410fd64c19345b4bc617a909fa4ba91a19
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:28:27 -0800

    xfs: online repair of parent pointers
    Teach the online repair code to fix directory '..' entries (aka
    directory parent pointers).  Since this requires us to know how to scan
    every dirent in every directory on the filesystem, we can reuse the
    parent scanner components to validate (or find!) the correct parent
    entry when rebuilding directories too.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

---
+++ fs/xfs/scrub/parent.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2022 Oracle.  All Rights Reserved.
+ * Author: Darrick J. Wong <djwong@kernel.org>
+ */
+#ifndef __XFS_SCRUB_PARENT_H__
+#define __XFS_SCRUB_PARENT_H__
+
+int xchk_parent_lock_two_dirs(struct xfs_scrub *sc, struct xfs_inode *dp);
+
+int xrep_parent_confirm(struct xfs_scrub *sc, xfs_ino_t *parent_ino);
+int xrep_parent_scan(struct xfs_scrub *sc, xfs_ino_t *parent_ino);
+
+#endif /* __XFS_SCRUB_PARENT_H__ */
