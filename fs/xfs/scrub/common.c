6bc32276f8c1b45d7b10ed78423eb9ffa8141518
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:28:23 -0800

    xfs: create temporary files and directories for online repair
    Teach the online repair code how to create temporary files or
    directories.  These temporary files can be used to stage reconstructed
    information until we're ready to perform an atomic extent swap to commit
    the new metadata.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/common.c
+++ fs/xfs/scrub/common.c
@@ -34,6 +34,7 @@
 #include "scrub/trace.h"
 #include "scrub/repair.h"
 #include "scrub/health.h"
+#include "scrub/tempfile.h"
 
 /* Common code for the metadata scrubbers. */
 
@@ -1029,7 +1030,10 @@ xchk_setup_inode_contents(
 	if (error)
 		return error;
 
-	/* Got the inode, lock it and we're ready to go. */
+	/*
+	 * Prepare to scrub the file contents by locking out IO and page
+	 * faults.
+	 */
 	xchk_ilock(sc, XFS_IOLOCK_EXCL | XFS_MMAPLOCK_EXCL);
 	error = xchk_trans_alloc(sc, resblks);
 	if (error)
