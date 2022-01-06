97da47ebade8e7acb92c2ab7736970e97cd2b40b
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:29:55 -0800

    xfs: whine to dmesg when we encounter errors
    Forward everything scrub whines about to dmesg.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/inode.c
+++ fs/xfs/scrub/inode.c
@@ -103,6 +103,12 @@ retry:
 		break;
 	default:
 		/* Treat everything else as an operational error. */
+		xchk_whine(mp, "type %d agno 0x%x agbno 0x%x error %d ret_ip %pS",
+				sc->sm->sm_type,
+				XFS_INO_TO_AGNO(mp, sc->sm->sm_ino),
+				XFS_INO_TO_AGBNO(mp, sc->sm->sm_ino),
+				error,
+				__return_address);
 		trace_xchk_op_error(sc, agno,
 				XFS_INO_TO_AGBNO(mp, sc->sm->sm_ino),
 				error, __return_address);
