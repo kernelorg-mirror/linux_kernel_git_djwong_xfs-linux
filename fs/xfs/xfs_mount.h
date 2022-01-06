a76caf5b36c9872c50c4ab64ffffd908d46b810d
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:28:21 -0800

    xfs: make atomic extent swapping support realtime files
    Now that bmap items support the realtime device, we can add the
    necessary pieces to the atomic extent swapping code to support such
    things.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -492,8 +492,7 @@ __XFS_LOG_FEAT(atomicswap, ATOMIC_SWAP)
  */
 static inline bool xfs_can_logswap(struct xfs_mount *mp)
 {
-	return (xfs_has_reflink(mp) || xfs_has_rmapbt(mp)) &&
-	       !xfs_has_realtime(mp);
+	return xfs_has_reflink(mp) || xfs_has_rmapbt(mp);
 }
 
 /*
