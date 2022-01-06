cc0956bdf3ccc7c466e8412b5c4863958ee7d7a7
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:28:00 -0800

    xfs: repair summary counters
    Use the same summary counter calculation infrastructure to generate new
    values for the in-core summary counters.   The difference between the
    scrubber and the repairer is that the repairer will freeze the fs during
    setup, which means that the values should match exactly.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.h
+++ fs/xfs/scrub/repair.h
@@ -105,6 +105,7 @@ int xrep_inode(struct xfs_scrub *sc);
 int xrep_bmap_data(struct xfs_scrub *sc);
 int xrep_bmap_attr(struct xfs_scrub *sc);
 int xrep_nlinks(struct xfs_scrub *sc);
+int xrep_fscounters(struct xfs_scrub *sc);
 
 #ifdef CONFIG_XFS_QUOTA
 int xrep_quota(struct xfs_scrub *sc);
@@ -231,6 +232,7 @@ xrep_setup_nothing(
 #define xrep_quota			xrep_notsupported
 #define xrep_quotacheck			xrep_notsupported
 #define xrep_nlinks			xrep_notsupported
+#define xrep_fscounters			xrep_notsupported
 
 #endif /* CONFIG_XFS_ONLINE_REPAIR */
 
