a7c4ff5394d304e6747bfccbcd3be135ff3f89bd
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:28:56 -0800

    xfs: refactor realtime scrubbing context management
    Create a pair of helpers to deal with setting up the necessary incore
    context to check metadata records against the realtime metadata.  Right
    now this is limited to locking the realtime bitmap and summary inodes,
    but as we add rmap and reflink to the realtime device this will grow to
    include btree cursors.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/rtbitmap.c
+++ fs/xfs/scrub/rtbitmap.c
@@ -32,8 +32,7 @@ xchk_setup_rtbitmap(
 	if (error)
 		return error;
 
-	xchk_ilock(sc, XFS_ILOCK_EXCL | XFS_ILOCK_RTBITMAP);
-	return 0;
+	return xchk_rt_init(sc, &sc->sr);
 }
 
 /* Realtime bitmap. */
@@ -144,13 +143,10 @@ xchk_xref_is_used_rt_space(
 	do_div(startext, sc->mp->m_sb.sb_rextsize);
 	do_div(endext, sc->mp->m_sb.sb_rextsize);
 	extcount = endext - startext + 1;
-	xfs_ilock(sc->mp->m_rbmip, XFS_ILOCK_SHARED | XFS_ILOCK_RTBITMAP);
 	error = xfs_rtalloc_extent_is_free(sc->mp, sc->tp, startext, extcount,
 			&is_free);
 	if (!xchk_should_check_xref(sc, &error, NULL))
-		goto out_unlock;
+		return;
 	if (is_free)
 		xchk_ino_xref_set_corrupt(sc, sc->mp->m_rbmip->i_ino);
-out_unlock:
-	xfs_iunlock(sc->mp->m_rbmip, XFS_ILOCK_SHARED | XFS_ILOCK_RTBITMAP);
 }
