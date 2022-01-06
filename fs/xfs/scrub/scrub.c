eaba91b7a7c5f5495e24b763da871b216fd4d3f9
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:28:28 -0800

    xfs: move orphan files to the orphanage
    If we can't find a parent for a file, move it to the orphanage.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/scrub.c
+++ fs/xfs/scrub/scrub.c
@@ -28,6 +28,7 @@
 #include "scrub/health.h"
 #include "scrub/xfile.h"
 #include "scrub/tempfile.h"
+#include "scrub/orphanage.h"
 
 /*
  * Online Scrub and Repair
@@ -195,6 +196,7 @@ xchk_teardown(
 		sc->buf = NULL;
 	}
 	xrep_tempfile_rele(sc);
+	xrep_orphanage_rele(sc);
 	return error;
 }
 
