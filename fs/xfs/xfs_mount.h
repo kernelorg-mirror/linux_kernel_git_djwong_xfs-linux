e7d350a198db2fd6bb80b929b0882647a7f7a9d5
Darrick J. Wong <djwong@kernel.org>
2022-01-06 14:29:22 -0800

    xfs: hook live realtime rmap operations during a repair operation
    Hook the regular realtime rmap code when an rtrmapbt repair operation is
    running so that we can unlock the AGF buffer to scan the filesystem and
    keep the in-memory btree up to date during the scan.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -332,6 +332,9 @@ typedef struct xfs_mount {
 	 * inconsistencies.
 	 */
 	struct xfs_drain	m_rt_intents;
+
+	/* Hook to feed rt rmapbt updates to an active online repair. */
+	struct xfs_hook_chain	m_rtrmap_update_hooks;
 #endif /* CONFIG_XFS_RT */
 } xfs_mount_t;
 
