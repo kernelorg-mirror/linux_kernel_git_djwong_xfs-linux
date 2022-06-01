71b41765381c99df89948bc7a106570d6386df30
Darrick J. Wong <djwong@kernel.org>
2022-06-01 14:29:28 -0700

    xfs: wrap inode creation dqalloc calls
    Create a helper that calls dqalloc to allocate and grab a reference to
    dquots for the user, group, and project ids listed in an icreate
    structure.  This simplifies the creat-related dqalloc callsites
    scattered around the code base.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_inode.h
+++ fs/xfs/xfs_inode.h
@@ -551,5 +551,8 @@ void xfs_nlink_hook_del(struct xfs_mount *mp, struct xfs_nlink_hook *hook);
 void xfs_icreate_args_inherit(struct xfs_icreate_args *args, struct xfs_inode *dp,
 		struct user_namespace *mnt_userns, umode_t mode);
 void xfs_icreate_args_rootfile(struct xfs_icreate_args *args, umode_t mode);
+int xfs_icreate_dqalloc(const struct xfs_icreate_args *args,
+		struct xfs_dquot **udqpp, struct xfs_dquot **gdqpp,
+		struct xfs_dquot **pdqpp);
 
 #endif	/* __XFS_INODE_H__ */
