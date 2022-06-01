1e08c03b29bc842b877cf9d5d8aaade485b6ed48
Darrick J. Wong <djwong@kernel.org>
2022-06-01 14:28:22 -0700

    xfs: ignore stale buffers when scanning the buffer cache
    After an online repair, we need to invalidate buffers representing the
    blocks from the old metadata that we're replacing.  It's possible that
    parts of a tree that were previously cached in memory are no longer
    accessible due to media failure or other corruption on interior nodes,
    so repair figures out the old blocks from the reverse mapping data and
    scans the buffer cache directly.
    
    Unfortunately, the current buffer cache code triggers asserts if the
    rhashtable lookup finds a non-stale buffer of a different length than
    the key we searched for.  For regular operation this is desirable, but
    for this repair procedure, we don't care since we're going to forcibly
    stale the buffer anyway.  Add an internal lookup flag to avoid the
    assert.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.c
+++ fs/xfs/scrub/repair.c
@@ -525,7 +525,7 @@ xrep_block_reap_binval(
 	 */
 	bp = xfs_buf_incore(sc->mp->m_ddev_targp,
 			XFS_FSB_TO_DADDR(sc->mp, fsbno),
-			XFS_FSB_TO_BB(sc->mp, 1), 0);
+			XFS_FSB_TO_BB(sc->mp, 1), XBF_BCACHE_SCAN);
 	if (!bp)
 		return;
 
