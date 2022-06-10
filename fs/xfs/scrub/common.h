ad262136764c810b3c2ca6dc9e668436d232e531
Darrick J. Wong <djwong@kernel.org>
2022-06-10 15:11:03 -0700

    xfs: remove XCHK_REAPING_DISABLED from scrub
    Nobody uses this code anymore, so get rid of it.  It was racy with
    regards to freezes and remounts anyway.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/common.h
+++ fs/xfs/scrub/common.h
@@ -211,8 +211,6 @@ static inline bool xchk_could_repair(const struct xfs_scrub *sc)
 #endif /* CONFIG_XFS_ONLINE_REPAIR */
 
 int xchk_metadata_inode_forks(struct xfs_scrub *sc);
-void xchk_stop_reaping(struct xfs_scrub *sc);
-void xchk_start_reaping(struct xfs_scrub *sc);
 
 void xchk_fshooks_enable(struct xfs_scrub *sc, unsigned int scrub_fshooks);
 
