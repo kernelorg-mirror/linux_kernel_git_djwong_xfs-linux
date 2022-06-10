966a018c0a7b1bffffa7795b99835abfe46ac80d
Darrick J. Wong <djwong@kernel.org>
2022-06-10 15:10:58 -0700

    xfs: track file link count updates during live nlinks fsck
    Create the necessary hooks in the file create/unlink/rename code so that
    our live nlink scrub code can stay up to date with the rest of the
    filesystem.  This will be the means to keep our shadow link count
    information up to date while the scan runs in real time.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/scrub.h
+++ fs/xfs/scrub/scrub.h
@@ -121,11 +121,13 @@ struct xfs_scrub {
 #define XCHK_REAPING_DISABLED	(1 << 1)  /* background block reaping paused */
 #define XCHK_FSHOOKS_DRAIN	(1 << 2)  /* defer ops draining enabled */
 #define XCHK_FSHOOKS_QUOTA	(1 << 3)  /* quota live update enabled */
+#define XCHK_FSHOOKS_NLINKS	(1 << 4)  /* link count live update enabled */
 #define XREP_RESET_PERAG_RESV	(1 << 30) /* must reset AG space reservation */
 #define XREP_ALREADY_FIXED	(1 << 31) /* checking our repair work */
 
 #define XCHK_FSHOOKS_ALL	(XCHK_FSHOOKS_DRAIN | \
-				 XCHK_FSHOOKS_QUOTA)
+				 XCHK_FSHOOKS_QUOTA | \
+				 XCHK_FSHOOKS_NLINKS)
 
 /* Metadata scrubbers */
 int xchk_tester(struct xfs_scrub *sc);
