a4b9c7a9bec59959fc32db9cb202194a68ecf791
Darrick J. Wong <djwong@kernel.org>
2022-06-27 14:06:10 -0700

    xfs: disable large folios except for developers
    Disable multi-page folios by default until we figure out why data
    corruption keeps happening.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_icache.c
+++ fs/xfs/xfs_icache.c
@@ -88,7 +88,11 @@ xfs_inode_alloc(
 	/* VFS doesn't initialise i_mode or i_state! */
 	VFS_I(ip)->i_mode = 0;
 	VFS_I(ip)->i_state = 0;
+#ifdef CONFIG_XFS_LARGE_FOLIOS
 	mapping_set_large_folios(VFS_I(ip)->i_mapping);
+#else
+	__clear_bit(AS_LARGE_FOLIO_SUPPORT, &VFS_I(ip)->i_mapping->flags);
+#endif
 
 	XFS_STATS_INC(mp, vn_active);
 	ASSERT(atomic_read(&ip->i_pincount) == 0);
@@ -323,7 +327,11 @@ xfs_reinit_inode(
 	inode->i_rdev = dev;
 	inode->i_uid = uid;
 	inode->i_gid = gid;
+#ifdef CONFIG_XFS_LARGE_FOLIOS
 	mapping_set_large_folios(inode->i_mapping);
+#else
+	__clear_bit(AS_LARGE_FOLIO_SUPPORT, &inode->i_mapping->flags);
+#endif
 	return error;
 }
 
