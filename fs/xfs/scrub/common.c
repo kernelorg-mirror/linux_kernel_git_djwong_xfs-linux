7563b223b0a267814a97596f27fb4e74cfbfbcfc
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:06:34 -0700

    xfs: hook live rmap operations during a repair operation
    Hook the regular rmap code when an rmapbt repair operation is running so
    that we can unlock the AGF buffer to scan the filesystem and keep the
    in-memory btree up to date during the scan.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/common.c
+++ fs/xfs/scrub/common.c
@@ -1256,5 +1256,8 @@ xchk_fshooks_enable(
 	if (scrub_fshooks & XCHK_FSHOOKS_NLINKS)
 		xfs_nlink_hook_enable();
 
+	if (scrub_fshooks & XCHK_FSHOOKS_RMAP)
+		xfs_rmap_hook_enable();
+
 	sc->flags |= scrub_fshooks;
 }
