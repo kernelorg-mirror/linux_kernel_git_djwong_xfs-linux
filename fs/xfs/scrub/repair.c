e74e23fc2fa44f2885871f226ff6384d6990be8d
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:05:44 -0700

    xfs: don't return -EFSCORRUPTED from repair when resources cannot be grabbed
    If we tried to repair something but the repair failed with -EDEADLOCK or
    -EAGAIN, that means that the repair function couldn't grab some resource
    it needed and wants us to try again.  If we try again (with TRY_HARDER)
    but still can't do it, exit back to userspace, since xfs_scrub_metadata
    requires xrep_attempt to return -EAGAIN.
    
    This makes the return value diagnostics look less weird, and fixes a
    wart that remains from very early in the repair implementation.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.c
+++ fs/xfs/scrub/repair.c
@@ -69,9 +69,9 @@ xrep_attempt(
 		/*
 		 * We tried harder but still couldn't grab all the resources
 		 * we needed to fix it.  The corruption has not been fixed,
-		 * so report back to userspace.
+		 * so exit to userspace.
 		 */
-		return -EFSCORRUPTED;
+		return 0;
 	case -EAGAIN:
 		/* Repair functions should return EDEADLOCK, not EAGAIN. */
 		ASSERT(0);
