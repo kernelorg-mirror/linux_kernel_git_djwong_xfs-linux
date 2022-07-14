2f9d935e62a3fb0d42347bd18d8cf1874bcab53b
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:06:38 -0700

    xfs: ignore realtime intents for s to drain before scrubbing
    Provide a way to mask off the scrub intent counter bump and drop
    operations when we deal with realtime extents.  We'll need this
    functionality when we gain the need to create deferred rmap and refcount
    update chains, but for now we can ignore anything touching rt.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.c
+++ fs/xfs/scrub/repair.c
@@ -522,16 +522,16 @@ xrep_newbt_schedule_autoreap(
 
 	INIT_LIST_HEAD(&efi_item.xefi_list);
 	list_add(&efi_item.xefi_list, &items);
-	xfs_fs_bump_intents(xnr->sc->mp, resv->fsbno);
+	xfs_fs_bump_intents(xnr->sc->mp, false, resv->fsbno);
 	lip = xfs_extent_free_defer_type.create_intent(xnr->sc->tp,
 			&items, 1, false);
 	if (!lip) {
 		ASSERT(0);
-		xfs_fs_drop_intents(xnr->sc->mp, resv->fsbno);
+		xfs_fs_drop_intents(xnr->sc->mp, false, resv->fsbno);
 		return -EFSCORRUPTED;
 	}
 	if (IS_ERR(lip)) {
-		xfs_fs_drop_intents(xnr->sc->mp, resv->fsbno);
+		xfs_fs_drop_intents(xnr->sc->mp, false, resv->fsbno);
 		return PTR_ERR(lip);
 	}
 
@@ -745,7 +745,7 @@ xrep_newbt_free_resv(
 	 */
 	list_for_each_entry_safe(resv, n, &xnr->resv_list, list) {
 		xrep_newbt_cancel_autoreap(resv);
-		xfs_fs_drop_intents(sc->mp, resv->fsbno);
+		xfs_fs_drop_intents(sc->mp, false, resv->fsbno);
 		list_del(&resv->list);
 		kfree(resv);
 	}
@@ -780,7 +780,7 @@ xrep_newbt_cancel_resv(
 			&xnr->oinfo, true);
 
 	/* Drop the intent drain after we commit the new item. */
-	xfs_fs_drop_intents(sc->mp, resv->fsbno);
+	xfs_fs_drop_intents(sc->mp, false, resv->fsbno);
 }
 
 /*
@@ -877,7 +877,7 @@ xrep_newbt_destroy_resv(
 	 * original fsbno from the reservation because destroying the
 	 * reservation consumes resv->fsbno.
 	 */
-	xfs_fs_drop_intents(sc->mp, fsbno);
+	xfs_fs_drop_intents(sc->mp, false, fsbno);
 }
 
 /* Free all the accounting info and disk space we reserved for a new btree. */
