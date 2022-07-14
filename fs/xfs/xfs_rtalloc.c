23ae0faa2e31dde66301a5047f9dbf2929caa789
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:15:24 -0700

    xfs: remove XFS_ILOCK_RT*
    Now that we've centralized the realtime metadata locking routines, get
    rid of the ILOCK subclasses since we now use explicit lockdep classes.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -1379,8 +1379,7 @@ __xfs_rt_iget(
  */
 static inline int
 xfs_rtmount_iread_extents(
-	struct xfs_inode	*ip,
-	unsigned int		lock_class)
+	struct xfs_inode	*ip)
 {
 	struct xfs_trans	*tp;
 	int			error;
@@ -1389,7 +1388,7 @@ xfs_rtmount_iread_extents(
 	if (error)
 		return error;
 
-	xfs_ilock(ip, XFS_ILOCK_EXCL | lock_class);
+	xfs_ilock(ip, XFS_ILOCK_EXCL);
 
 	error = xfs_iread_extents(tp, ip, XFS_DATA_FORK);
 	if (error)
@@ -1402,7 +1401,7 @@ xfs_rtmount_iread_extents(
 	}
 
 out_unlock:
-	xfs_iunlock(ip, XFS_ILOCK_EXCL | lock_class);
+	xfs_iunlock(ip, XFS_ILOCK_EXCL);
 	xfs_trans_cancel(tp);
 	return error;
 }
@@ -1427,7 +1426,7 @@ xfs_rtmount_inodes(
 		return error;
 	ASSERT(mp->m_rbmip != NULL);
 
-	error = xfs_rtmount_iread_extents(mp->m_rbmip, XFS_ILOCK_RTBITMAP);
+	error = xfs_rtmount_iread_extents(mp->m_rbmip);
 	if (error)
 		goto out_rele_bitmap;
 
@@ -1439,7 +1438,7 @@ xfs_rtmount_inodes(
 		goto out_rele_bitmap;
 	ASSERT(mp->m_rsumip != NULL);
 
-	error = xfs_rtmount_iread_extents(mp->m_rsumip, XFS_ILOCK_RTSUM);
+	error = xfs_rtmount_iread_extents(mp->m_rsumip);
 	if (error)
 		goto out_rele_summary;
 
