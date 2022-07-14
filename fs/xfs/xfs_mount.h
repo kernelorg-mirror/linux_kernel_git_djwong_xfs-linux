e4a6501f36c104c409dc23a4d3878437e3ed83f3
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:06:38 -0700

    xfs: ignore intents targetting file data on the realtime device
    Provide a way to mask off the scrub intent counter bump and drop
    operations when we deal with realtime extents.  We'll need this
    functionality when we gain the need to create deferred rmap and refcount
    update chains, but for now this API change just makes it so that we can
    ignore anything touching rt.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -139,8 +139,8 @@ bool xfs_ag_intents_busy(struct xfs_perag *pag);
 void xfs_drain_wait_disable(void);
 void xfs_drain_wait_enable(void);
 
-void xfs_fs_bump_intents(struct xfs_mount *mp, xfs_fsblock_t fsb);
-void xfs_fs_drop_intents(struct xfs_mount *mp, xfs_fsblock_t fsb);
+void xfs_fs_bump_intents(struct xfs_mount *mp, bool isrt, xfs_fsblock_t fsb);
+void xfs_fs_drop_intents(struct xfs_mount *mp, bool isrt, xfs_fsblock_t fsb);
 
 /*
  * Use a large batch value for the drain counter so that writer threads
@@ -171,12 +171,12 @@ static inline void xfs_drain_free(struct xfs_drain *dr)
 struct xfs_drain { /* empty */ };
 
 static inline void
-xfs_fs_bump_intents(struct xfs_mount *mp, xfs_fsblock_t fsb)
+xfs_fs_bump_intents(struct xfs_mount *mp, bool isrt, xfs_fsblock_t fsb)
 {
 }
 
 static inline void
-xfs_fs_drop_intents(struct xfs_mount *mp, xfs_fsblock_t fsb)
+xfs_fs_drop_intents(struct xfs_mount *mp, bool isrt, xfs_fsblock_t fsb)
 {
 }
 # define xfs_drain_init(dr)	(0)
