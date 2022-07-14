ee92792ee88a100f5b99174a132b96415f8841aa
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:15:34 -0700

    xfs: add a realtime flag to the rmap update log redo items
    Extend the rmap update (RUI) log items with a new realtime flag that
    indicates that the updates apply against the realtime rmapbt.  We'll
    wire up the actual rmap code later.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/alloc_repair.c
+++ fs/xfs/scrub/alloc_repair.c
@@ -525,7 +525,7 @@ xrep_abt_dispose_one(
 
 	/* Add a deferred rmap for each extent we used. */
 	if (resv->used > 0)
-		xfs_rmap_alloc_extent(sc->tp, fsbno, resv->used,
+		xfs_rmap_alloc_extent(sc->tp, false, fsbno, resv->used,
 				XFS_RMAP_OWN_AG);
 
 	/*
