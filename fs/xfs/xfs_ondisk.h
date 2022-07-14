1c2ce18e376a08baa8426adbf9d9075b7b2af305
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:15:55 -0700

    xfs: define the on-disk realtime refcount btree format
    Start filling out the rtrefcount btree implementation. Start with the
    on-disk btree format; add everything needed to read, write and
    manipulate refcount btree blocks. This prepares the way for connecting
    the btree operations implementation.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_ondisk.h
+++ fs/xfs/xfs_ondisk.h
@@ -47,6 +47,8 @@ xfs_check_ondisk_structs(void)
 	XFS_CHECK_STRUCT_SIZE(struct xfs_rmap_rec,		24);
 	XFS_CHECK_STRUCT_SIZE(xfs_timestamp_t,			8);
 	XFS_CHECK_STRUCT_SIZE(struct xfs_legacy_timestamp,	8);
+	XFS_CHECK_STRUCT_SIZE(struct xfs_rtrefcount_key,	4);
+	XFS_CHECK_STRUCT_SIZE(struct xfs_rtrefcount_rec,	12);
 	XFS_CHECK_STRUCT_SIZE(struct xfs_rtrmap_key,		20);
 	XFS_CHECK_STRUCT_SIZE(struct xfs_rtrmap_rec,		24);
 	XFS_CHECK_STRUCT_SIZE(struct xfs_rtrmap_root,		4);
@@ -58,6 +60,7 @@ xfs_check_ondisk_structs(void)
 	XFS_CHECK_STRUCT_SIZE(xfs_rmap_ptr_t,			4);
 	XFS_CHECK_STRUCT_SIZE(struct xfs_rtsb,			104);
 	XFS_CHECK_STRUCT_SIZE(xfs_rtrmap_ptr_t,			8);
+	XFS_CHECK_STRUCT_SIZE(xfs_rtrefcount_ptr_t,		8);
 
 	/* dir/attr trees */
 	XFS_CHECK_STRUCT_SIZE(struct xfs_attr3_leaf_hdr,	80);
