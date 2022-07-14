fc211578ea83f9c0400c14397e05c3c1177ddaa7
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:06:56 -0700

    xfs: online repair of parent pointers
    Teach the online repair code to fix directory '..' entries (aka
    directory parent pointers).  Since this requires us to know how to scan
    every dirent in every directory on the filesystem, we can reuse the
    parent scanner components to validate (or find!) the correct parent
    entry when rebuilding directories too.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.h
+++ fs/xfs/scrub/repair.h
@@ -122,6 +122,7 @@ int xrep_nlinks(struct xfs_scrub *sc);
 int xrep_fscounters(struct xfs_scrub *sc);
 int xrep_xattr(struct xfs_scrub *sc);
 int xrep_directory(struct xfs_scrub *sc);
+int xrep_parent(struct xfs_scrub *sc);
 
 #ifdef CONFIG_XFS_RT
 int xrep_rtbitmap(struct xfs_scrub *sc);
@@ -234,6 +235,7 @@ xrep_setup_rtsummary(
 #define xrep_rtsummary			xrep_notsupported
 #define xrep_xattr			xrep_notsupported
 #define xrep_directory			xrep_notsupported
+#define xrep_parent			xrep_notsupported
 
 #endif /* CONFIG_XFS_ONLINE_REPAIR */
 
