c87a92e145657f0f76f4c742b9f06458827d45ee
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:05:37 -0700

    xfs: minimize overhead of drain wakeups by using jump labels
    To reduce the runtime overhead even further when online fsck isn't
    running, use a static branch key to decide if we call wake_up on the
    drain.  For compilers that support jump labels, the call to wake_up is
    replaced by a nop sled when nobody is waiting for intents to drain.
    
    From my initial microbenchmarking, every transition of the static key
    between the on and off states takes about 22000ns to complete; this is
    paid entirely by the xfs_scrub process.  When the static key is off
    (which it should be when fsck isn't running), the nop sled adds an
    overhead of approximately 0.36ns to runtime code.
    
    For the few compilers that don't support jump labels, runtime code pays
    the cost of calling wake_up on an empty waitqueue, which was observed to
    be about 30ns.  However, most architectures that have sufficient memory
    and CPU capacity to run XFS also support jump labels, so this is not
    much of a worry.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_drain.c
+++ fs/xfs/xfs_drain.c
@@ -12,6 +12,31 @@
 #include "xfs_ag.h"
 #include "xfs_trace.h"
 
+/*
+ * Use a static key here to reduce the overhead of xfs_drain_drop.  If the
+ * compiler supports jump labels, the static branch will be replaced by a nop
+ * sled when there are no xfs_drain_wait callers.  Online fsck is currently
+ * the only caller, so this is a reasonable tradeoff.
+ *
+ * Note: Patching the kernel code requires taking the cpu hotplug lock.  Other
+ * parts of the kernel allocate memory with that lock held, which means that
+ * XFS callers cannot hold any locks that might be used by memory reclaim or
+ * writeback when calling the static_branch_{inc,dec} functions.
+ */
+static DEFINE_STATIC_KEY_FALSE(xfs_drain_waiter_hook);
+
+void
+xfs_drain_wait_disable(void)
+{
+	static_branch_dec(&xfs_drain_waiter_hook);
+}
+
+void
+xfs_drain_wait_enable(void)
+{
+	static_branch_inc(&xfs_drain_waiter_hook);
+}
+
 void
 xfs_drain_init(
 	struct xfs_drain	*dr)
@@ -36,7 +61,7 @@ static inline void xfs_drain_bump(struct xfs_drain *dr)
 static inline void xfs_drain_drop(struct xfs_drain *dr)
 {
 	if (atomic_dec_and_test(&dr->dr_count) &&
-	    wq_has_sleeper(&dr->dr_waiters))
+	    static_branch_unlikely(&xfs_drain_waiter_hook))
 		wake_up(&dr->dr_waiters);
 }
 
