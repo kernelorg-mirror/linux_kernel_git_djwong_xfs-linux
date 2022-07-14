7d04b8cdcad95d15bc0524310c5abee7ad50d931
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:15:46 -0700

    xfs: online repair of realtime bitmaps for a realtime group
    For a given rt group, regenerate the bitmap contents from the group's
    realtime rmap btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/tempswap.h
+++ fs/xfs/scrub/tempswap.h
@@ -13,7 +13,7 @@ struct xrep_tempswap {
 
 int xrep_tempswap_grab_log_assist(struct xfs_scrub *sc);
 int xrep_tempswap_trans_reserve(struct xfs_scrub *sc, int whichfork,
-		struct xrep_tempswap *ti);
+		xfs_fileoff_t off, xfs_filblks_t len, struct xrep_tempswap *ti);
 int xrep_tempswap_trans_alloc(struct xfs_scrub *sc, int whichfork,
 		struct xrep_tempswap *ti);
 
