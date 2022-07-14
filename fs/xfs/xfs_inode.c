388c9565887cfa9b64b3a81db605a7a6b9e1e05a
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:15:12 -0700

    xfs: disable the agi rotor for metadata inodes
    Ideally, we'd put all the metadata inodes in one place if we could, so
    that the metadata all stay reasonably close together instead of
    spreading out over the disk.  Furthermore, if the log is internal we'd
    probably prefer to keep the metadata near the log.  Therefore, disable
    AGI rotoring for metadata inode allocations.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -864,7 +864,7 @@ xfs_create(
 	 * entry pointing to them, but a directory also the "." entry
 	 * pointing to itself.
 	 */
-	error = xfs_dialloc(&tp, dp->i_ino, args->mode, &ino);
+	error = xfs_dialloc(&tp, dp, args->mode, &ino);
 	if (!error)
 		error = xfs_icreate(tp, ino, args, &ip);
 	if (error)
@@ -980,7 +980,7 @@ xfs_create_tmpfile(
 	if (error)
 		goto out_release_dquots;
 
-	error = xfs_dialloc(&tp, dp->i_ino, args->mode, &ino);
+	error = xfs_dialloc(&tp, dp, args->mode, &ino);
 	if (!error)
 		error = xfs_icreate(tp, ino, args, &ip);
 	if (error)
