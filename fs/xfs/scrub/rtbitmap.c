f8451880356773abd2792811580114190db35bd2
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:15:46 -0700

    xfs: online repair of realtime bitmaps for a realtime group
    For a given rt group, regenerate the bitmap contents from the group's
    realtime rmap btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/rtbitmap.c
+++ fs/xfs/scrub/rtbitmap.c
@@ -22,18 +22,34 @@
 #include "scrub/common.h"
 #include "scrub/repair.h"
 #include "scrub/btree.h"
+#include "scrub/repair.h"
 
 /* Set us up with the realtime group metadata locked. */
 int
 xchk_setup_rgbitmap(
 	struct xfs_scrub	*sc)
 {
+	unsigned int		resblks = 0;
+	unsigned int		rtglock_flags = XCHK_RTGLOCK_ALL;
 	int			error;
 
 	if (xchk_need_fshook_drain(sc))
 		xchk_fshooks_enable(sc, XCHK_FSHOOKS_DRAIN);
 
-	error = xchk_trans_alloc(sc, 0);
+	if (xchk_could_repair(sc)) {
+		error = xrep_setup_rgbitmap(sc, &resblks);
+		if (error)
+			return error;
+
+		/*
+		 * We must hold rbmip with ILOCK_EXCL to use the extent swap
+		 * at the end of the repair function.
+		 */
+		rtglock_flags &= ~XFS_RTGLOCK_BITMAP_SHARED;
+		rtglock_flags |= XFS_RTGLOCK_BITMAP;
+	}
+
+	error = xchk_trans_alloc(sc, resblks);
 	if (error)
 		return error;
 
@@ -45,8 +61,7 @@ xchk_setup_rgbitmap(
 	if (error)
 		return error;
 
-	return xchk_rtgroup_init(sc, sc->sm->sm_agno, &sc->sr,
-			XCHK_RTGLOCK_ALL);
+	return xchk_rtgroup_init(sc, sc->sm->sm_agno, &sc->sr, rtglock_flags);
 }
 
 /* Set us up with the realtime metadata locked. */
