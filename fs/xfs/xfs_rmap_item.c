b23467c0cac93fb0464bac81fa93c35b99bb5e2e
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:15:42 -0700

    xfs: allow queued realtime intents to drain before scrubbing
    When a writer thread executes a chain of log intent items for the
    realtime volume, the ILOCKs taken during each step are for each rt
    metadata file, not the entire rt volume itself.  Although scrub takes
    all rt metadata ILOCKs, this isn't sufficient to guard against scrub
    checking the rt volume while that writer thread is in the middle of
    finishing a chain because there's no higher level locking primitive
    guarding the realtime volume.
    
    When there's a collision, cross-referencing between data structures
    (e.g. rtrmapbt and rtrefcountbt) yields false corruption events; if
    repair is running, this results in incorrect repairs, which is
    catastrophic.
    
    Fix this by adding to the mount structure the same drain that we use to
    protect scrub against concurrent AG updates, but this time for the
    realtime volume.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rmap_item.c
+++ fs/xfs/xfs_rmap_item.c
@@ -400,6 +400,7 @@ xfs_rmap_update_get_group(
 
 		rgno = xfs_rtb_to_rgno(mp, ri->ri_bmap.br_startblock);
 		ri->ri_rtg = xfs_rtgroup_get(mp, rgno);
+		xfs_rtgroup_bump_intents(ri->ri_rtg);
 		return;
 	}
 
@@ -414,6 +415,7 @@ xfs_rmap_update_put_group(
 	struct xfs_rmap_intent	*ri)
 {
 	if (ri->ri_realtime) {
+		xfs_rtgroup_drop_intents(ri->ri_rtg);
 		xfs_rtgroup_put(ri->ri_rtg);
 		return;
 	}
