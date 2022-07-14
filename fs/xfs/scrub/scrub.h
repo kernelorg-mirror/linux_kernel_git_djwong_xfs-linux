84f1587dfa01c37d9219dc7042588be1ea8f8d29
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:06:34 -0700

    xfs: hook live rmap operations during a repair operation
    Hook the regular rmap code when an rmapbt repair operation is running so
    that we can unlock the AGF buffer to scan the filesystem and keep the
    in-memory btree up to date during the scan.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/scrub.h
+++ fs/xfs/scrub/scrub.h
@@ -126,12 +126,14 @@ struct xfs_scrub {
 #define XCHK_NEED_DRAIN		(1 << 3)  /* scrub needs to use intent drain */
 #define XCHK_FSHOOKS_QUOTA	(1 << 4)  /* quota live update enabled */
 #define XCHK_FSHOOKS_NLINKS	(1 << 5)  /* link count live update enabled */
+#define XCHK_FSHOOKS_RMAP	(1 << 6)  /* rmapbt live update enabled */
 #define XREP_RESET_PERAG_RESV	(1 << 30) /* must reset AG space reservation */
 #define XREP_ALREADY_FIXED	(1 << 31) /* checking our repair work */
 
 #define XCHK_FSHOOKS_ALL	(XCHK_FSHOOKS_DRAIN | \
 				 XCHK_FSHOOKS_QUOTA | \
-				 XCHK_FSHOOKS_NLINKS)
+				 XCHK_FSHOOKS_NLINKS | \
+				 XCHK_FSHOOKS_RMAP)
 
 /* Metadata scrubbers */
 int xchk_tester(struct xfs_scrub *sc);
