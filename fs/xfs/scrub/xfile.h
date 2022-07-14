c1f1ccf26cef75e96efa55f4ff93d7948af634df
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:06:29 -0700

    xfs: dump xfiles for debugging purposes
    Add a debug function to dump an xfile's contents for debug purposes.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/xfile.h
+++ fs/xfs/scrub/xfile.h
@@ -60,4 +60,6 @@ int xfile_obj_get_page(struct xfile *xf, loff_t offset, unsigned int len,
 int xfile_obj_put_page(struct xfile *xf, loff_t offset, unsigned int len,
 		struct page *page, void *fsdata);
 
+int xfile_dump(struct xfile *xf);
+
 #endif /* __XFS_SCRUB_XFILE_H__ */
