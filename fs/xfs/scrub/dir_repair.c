eabef78e624d8c41b8aecf8212e18b29aef40b42
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:15:14 -0700

    xfs: scrub metadata directories
    Teach online scrub about the metadata directory tree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/dir_repair.c
+++ fs/xfs/scrub/dir_repair.c
@@ -204,6 +204,12 @@ xrep_dir_salvage_entry(
 	if (error)
 		return 0;
 
+	/* Don't mix metadata and regular directory trees. */
+	if (xfs_is_metadata_inode(ip) ^ xfs_is_metadata_inode(rd->sc->ip)) {
+		xchk_irele(sc, ip);
+		return 0;
+	}
+
 	entry.ftype = xfs_mode_to_ftype(VFS_I(ip)->i_mode);
 	xchk_irele(sc, ip);
 
