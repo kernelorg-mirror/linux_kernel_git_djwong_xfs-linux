b43bbb61fe1af784b65e4bf78ce63b707179c3c1
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:15:59 -0700

    xfs: add metadata reservations for realtime refcount btree
    Reserve some free blocks so that we will always have enough free blocks
    in the data volume to handle expansion of the realtime refcount btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -27,6 +27,7 @@
 #include "xfs_quota.h"
 #include "xfs_error.h"
 #include "xfs_rtrmap_btree.h"
+#include "xfs_rtrefcount_btree.h"
 
 /*
  * Realtime metadata files are not quite regular files because userspace can't
@@ -1481,6 +1482,7 @@ void
 xfs_rt_resv_free(
 	struct xfs_mount	*mp)
 {
+	xfs_imeta_resv_free_inode(mp->m_rrefcountip);
 	xfs_imeta_resv_free_inode(mp->m_rrmapip);
 }
 
@@ -1490,9 +1492,15 @@ xfs_rt_resv_init(
 	struct xfs_mount	*mp)
 {
 	xfs_filblks_t		ask;
+	int			error;
 
 	ask = xfs_rtrmapbt_calc_reserves(mp);
-	return xfs_imeta_resv_init_inode(mp->m_rrmapip, ask);
+	error = xfs_imeta_resv_init_inode(mp->m_rrmapip, ask);
+	if (error)
+		return error;
+
+	ask = xfs_rtrefcountbt_calc_reserves(mp);
+	return xfs_imeta_resv_init_inode(mp->m_rrefcountip, ask);
 }
 
 static inline int
