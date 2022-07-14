d6f6414f9494acea0ff3d40af2eba2f1f1db92d3
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:15:57 -0700

    xfs: support recovering refcount intent items targetting realtime extents
    Now that we have reflink on the realtime device, refcount intent items
    have to support remapping extents on the realtime volume.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_refcount_item.c
+++ fs/xfs/xfs_refcount_item.c
@@ -476,6 +476,9 @@ xfs_cui_validate_phys(
 		return false;
 	}
 
+	if (refc->pe_flags & XFS_REFCOUNT_EXTENT_REALTIME)
+		return xfs_verify_rtbext(mp, refc->pe_startblock, refc->pe_len);
+
 	return xfs_verify_fsbext(mp, refc->pe_startblock, refc->pe_len);
 }
 
