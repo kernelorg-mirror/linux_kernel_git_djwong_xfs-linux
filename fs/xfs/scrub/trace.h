78ec828faeba6b04065069df123eb3f6aac93b20
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:06:56 -0700

    xfs: online repair of parent pointers
    Teach the online repair code to fix directory '..' entries (aka
    directory parent pointers).  Since this requires us to know how to scan
    every dirent in every directory on the filesystem, we can reuse the
    parent scanner components to validate (or find!) the correct parent
    entry when rebuilding directories too.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/trace.h
+++ fs/xfs/scrub/trace.h
@@ -2432,6 +2432,7 @@ DEFINE_EVENT(xrep_dir_class, name, \
 	TP_ARGS(dp, parent_ino))
 DEFINE_XREP_DIR_CLASS(xrep_dir_rebuild_tree);
 DEFINE_XREP_DIR_CLASS(xrep_dir_reset_fork);
+DEFINE_XREP_DIR_CLASS(xrep_parent_reset_dir);
 
 DECLARE_EVENT_CLASS(xrep_dirent_class,
 	TP_PROTO(struct xfs_inode *dp, struct xfs_name *name, xfs_ino_t ino),
@@ -2489,6 +2490,7 @@ DEFINE_EVENT(xrep_parent_salvage_class, name, \
 	TP_PROTO(struct xfs_inode *dp, xfs_ino_t ino), \
 	TP_ARGS(dp, ino))
 DEFINE_XREP_PARENT_SALVAGE_CLASS(xrep_dir_salvaged_parent);
+DEFINE_XREP_PARENT_SALVAGE_CLASS(xrep_findparent_dirent);
 
 #endif /* IS_ENABLED(CONFIG_XFS_ONLINE_REPAIR) */
 
