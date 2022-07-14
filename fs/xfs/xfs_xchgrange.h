5358f023c6d2adc9606d4478a4249e59e65801be
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:06:44 -0700

    xfs: create deferred log items for extent swapping
    Now that we've created the skeleton of a log intent item to track and
    restart extent swap operations, add the upper level logic to commit
    intent items and turn them into concrete work recorded in the log.  We
    use the deferred item "multihop" feature that was introduced a few
    patches ago to constrain the number of active swap operations to one per
    thread.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

---
+++ fs/xfs/xfs_xchgrange.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2022 Oracle.  All Rights Reserved.
+ * Author: Darrick J. Wong <djwong@kernel.org>
+ */
+#ifndef __XFS_XCHGRANGE_H__
+#define __XFS_XCHGRANGE_H__
+
+struct xfs_swapext_req;
+
+void xfs_xchg_range_ilock(struct xfs_trans *tp, struct xfs_inode *ip1,
+		struct xfs_inode *ip2);
+void xfs_xchg_range_iunlock(struct xfs_inode *ip1, struct xfs_inode *ip2);
+
+int xfs_xchg_range_estimate(struct xfs_swapext_req *req);
+
+#endif /* __XFS_XCHGRANGE_H__ */
