0c7e5a822eb4cbc0eb7a398cbdb8baf86601cc89
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:16:08 -0700

    xfs: scrub the realtime refcount btree
    Add code to scrub realtime refcount btrees.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/common.h
+++ fs/xfs/scrub/common.h
@@ -94,6 +94,7 @@ int xchk_setup_parent(struct xfs_scrub *sc);
 int xchk_setup_rtbitmap(struct xfs_scrub *sc);
 int xchk_setup_rtsummary(struct xfs_scrub *sc);
 int xchk_setup_rtrmapbt(struct xfs_scrub *sc);
+int xchk_setup_rtrefcountbt(struct xfs_scrub *sc);
 #else
 static inline int
 xchk_setup_rtbitmap(struct xfs_scrub *sc)
@@ -107,6 +108,11 @@ xchk_setup_rtsummary(struct xfs_scrub *sc)
 }
 static inline int
 xchk_setup_rtrmapbt(struct xfs_scrub *sc)
+{
+	return -ENOENT;
+}
+static inline int
+xchk_setup_rtrefcountbt(struct xfs_scrub *sc)
 {
 	return -ENOENT;
 }
