de527c3da8f2abdb67a87d8e58ff599d0b13733c
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:16:12 -0700

    xfs: online repair of the realtime refcount btree
    Port the data device's refcount btree repair code to the realtime
    refcount btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.h
+++ fs/xfs/scrub/repair.h
@@ -108,9 +108,9 @@ int xrep_check_ino_btree_mapping(struct xfs_scrub *sc,
 
 #ifdef CONFIG_XFS_RT
 int xrep_require_rtext_inuse(struct xfs_scrub *sc, xfs_rtblock_t rtbno,
-		xfs_filblks_t len);
+		xfs_filblks_t len, bool must_align);
 #else
-# define xrep_require_rtext_inuse(sc, rtbno, len)	(-ENOSYS)
+# define xrep_require_rtext_inuse(sc, rtbno, len, align)	(-ENOSYS)
 #endif
 bool xrep_is_rtmeta_ino(struct xfs_scrub *sc, xfs_ino_t ino);
 
@@ -172,10 +172,12 @@ int xrep_quotacheck(struct xfs_scrub *sc);
 int xrep_rtsummary(struct xfs_scrub *sc);
 int xrep_rtbitmap(struct xfs_scrub *sc);
 int xrep_rtrmapbt(struct xfs_scrub *sc);
+int xrep_rtrefcountbt(struct xfs_scrub *sc);
 #else
 # define xrep_rtsummary			xrep_notsupported
 # define xrep_rtbitmap			xrep_notsupported
 # define xrep_rtrmapbt			xrep_notsupported
+# define xrep_rtrefcountbt		xrep_notsupported
 #endif /* CONFIG_XFS_RT */
 
 struct xrep_newbt_resv {
@@ -351,6 +353,7 @@ static inline int xrep_setup_rtbitmap(struct xfs_scrub *sc, unsigned int *x)
 #define xrep_symlink			xrep_notsupported
 #define xrep_rtbitmap			xrep_notsupported
 #define xrep_rtrmapbt			xrep_notsupported
+#define xrep_rtrefcountbt		xrep_notsupported
 
 #endif /* CONFIG_XFS_ONLINE_REPAIR */
 
