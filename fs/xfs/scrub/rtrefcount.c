6f27879d3f7ec2cc04b06837c8eba3068ecbb3b6
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:16:12 -0700

    xfs: online repair of the realtime refcount btree
    Port the data device's refcount btree repair code to the realtime
    refcount btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/rtrefcount.c
+++ fs/xfs/scrub/rtrefcount.c
@@ -7,8 +7,10 @@
 #include "xfs_fs.h"
 #include "xfs_shared.h"
 #include "xfs_format.h"
+#include "xfs_log_format.h"
 #include "xfs_trans_resv.h"
 #include "xfs_mount.h"
+#include "xfs_trans.h"
 #include "xfs_btree.h"
 #include "xfs_rmap.h"
 #include "xfs_refcount.h"
@@ -19,6 +21,7 @@
 #include "scrub/scrub.h"
 #include "scrub/common.h"
 #include "scrub/btree.h"
+#include "scrub/repair.h"
 
 /* Set us up with the realtime refcount metadata locked. */
 int
@@ -30,6 +33,12 @@ xchk_setup_rtrefcountbt(
 
 	if (xchk_need_fshook_drain(sc))
 		xchk_fshooks_enable(sc, XCHK_FSHOOKS_DRAIN);
+
+	if (xchk_could_repair(sc)) {
+		error = xrep_setup_rtrefcountbt(sc);
+		if (error)
+			return error;
+	}
 
 	rtg = xfs_rtgroup_get(sc->mp, sc->sm->sm_agno);
 	if (!rtg)
