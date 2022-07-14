4d20a03ac8a2b192db2cba2fff610cee7d35a7e0
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:05:45 -0700

    xfs: don't warn about files that are exactly s_maxbytes long
    We can handle files that are exactly s_maxbytes bytes long; we just
    can't handle anything larger than that.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/inode.c
+++ fs/xfs/scrub/inode.c
@@ -368,7 +368,7 @@ xchk_dinode(
 	 * pagecache can't cache all the blocks in this file due to
 	 * overly large offsets, flag the inode for admin review.
 	 */
-	if (isize >= mp->m_super->s_maxbytes)
+	if (isize > mp->m_super->s_maxbytes)
 		xchk_ino_set_warning(sc, ino);
 
 	/* di_nblocks */
