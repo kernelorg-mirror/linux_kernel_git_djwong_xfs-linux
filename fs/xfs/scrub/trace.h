5fd8a7bbee33b420af2bb2501504a6f262e61a77
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:06:28 -0700

    xfs: remove XCHK_REAPING_DISABLED from scrub
    Nobody uses this code anymore, so get rid of it.  It was racy with
    regards to freezes and remounts anyway.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/trace.h
+++ fs/xfs/scrub/trace.h
@@ -114,7 +114,6 @@ TRACE_DEFINE_ENUM(XFS_SCRUB_TYPE_HEALTHY);
 #define XFS_SCRUB_STATE_STRINGS \
 	{ XCHK_TRY_HARDER,			"try_harder" }, \
 	{ XCHK_HAVE_FREEZE_PROT,		"nofreeze" }, \
-	{ XCHK_REAPING_DISABLED,		"reaping_disabled" }, \
 	{ XCHK_FSHOOKS_DRAIN,			"fshooks_drain" }, \
 	{ XCHK_NEED_DRAIN,			"need_drain" }, \
 	{ XCHK_FSHOOKS_QUOTA,			"fshooks_quota" }, \
