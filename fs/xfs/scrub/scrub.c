54d058b5d4333e26124d10f3efbdb54ce7a25874
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:06:04 -0700

    xfs: wrap ilock/iunlock operations on sc->ip
    Scrub tracks the resources that it's holding onto in the xfs_scrub
    structure.  This includes the inode being checked (if applicable) and
    the inode lock state of that inode.  Replace the open-coded structure
    manipulation with a trivial helper to eliminate sources of error.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/scrub.c
+++ fs/xfs/scrub/scrub.c
@@ -175,7 +175,7 @@ xchk_teardown(
 	}
 	if (sc->ip) {
 		if (sc->ilock_flags)
-			xfs_iunlock(sc->ip, sc->ilock_flags);
+			xchk_iunlock(sc, sc->ilock_flags);
 		xchk_irele(sc, sc->ip);
 		sc->ip = NULL;
 	}
