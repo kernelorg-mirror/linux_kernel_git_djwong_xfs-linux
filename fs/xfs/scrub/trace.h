4d5f19cb51e6f2b69ab9242ae7c6375e0a2e545e
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:06:27 -0700

    xfs: stabilize fs summary counters for online fsck
    If the fscounters scrubber notices incorrect summary counters, it's
    entirely possible that scrub is simply racing with other threads that
    are updating the incore counters.  There isn't a good way to stabilize
    percpu counters or set ourselves up to observe live updates with hooks
    like we do for the quotacheck or nlinks scanners, so we instead choose
    to freeze the filesystem long enough to walk the incore per-AG
    structures.
    
    Past me thought that it was going to be commonplace to have to freeze
    the filesystem to perform some kind of repair and set up a whole
    separate infrastructure to freeze the filesystem in such a way that
    userspace could not unfreeze while we were running.  This involved
    adding a mutex and freeze_super/thaw_super functions and dealing with
    the fact that the VFS freeze/thaw functions can free the VFS superblock
    references on return.
    
    This was all very overwrought, since fscounters turned out to be the
    only user of scrub freezes, and it doesn't require the log to quiesce,
    only the incore superblock counters.  We prevent other threads from
    changing the freeze level by adding a new SB_FREEZE_EXCLUSIVE level.
    
    The end result is that fscounters should be much more efficient.  When
    we're checking a busy system and we can't stabilize the counters, the
    custom freeze will do less work, which should result in less downtime.
    Repair should be similarly speedy, but that's in the next patch.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/trace.h
+++ fs/xfs/scrub/trace.h
@@ -107,6 +107,7 @@ TRACE_DEFINE_ENUM(XFS_SCRUB_TYPE_FSCOUNTERS);
 
 #define XFS_SCRUB_STATE_STRINGS \
 	{ XCHK_TRY_HARDER,			"try_harder" }, \
+	{ XCHK_HAVE_FREEZE_PROT,		"nofreeze" }, \
 	{ XCHK_REAPING_DISABLED,		"reaping_disabled" }, \
 	{ XCHK_FSHOOKS_DRAIN,			"fshooks_drain" }, \
 	{ XCHK_NEED_DRAIN,			"need_drain" }, \
