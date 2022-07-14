606c6aba4e7ee8fc36950b9e9e3df85c042bcb1c
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:06:12 -0700

    xfs: repair inode block maps
    Use the reverse-mapping btree information to rebuild an inode block map.
    Update the btree bulk loading code as necessary to support inode rooted
    btrees and fix some bitrot problems.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.c
+++ fs/xfs/scrub/repair.c
@@ -856,6 +856,32 @@ xrep_reinit_pagi(
 	return 0;
 }
 
+/* Given a reference to a perag structure, load AG headers and cursors. */
+int
+xrep_ag_init(
+	struct xfs_scrub	*sc,
+	struct xfs_perag	*pag,
+	struct xchk_ag		*sa)
+{
+	int			error;
+
+	ASSERT(!sa->pag);
+
+	error = xfs_ialloc_read_agi(pag, sc->tp, &sa->agi_bp);
+	if (error)
+		return error;
+
+	error = xfs_alloc_read_agf(pag, sc->tp, 0, &sa->agf_bp);
+	if (error)
+		return error;
+
+	/* Grab our own reference to the perag structure. */
+	atomic_inc(&pag->pag_ref);
+	sa->pag = pag;
+	xrep_ag_btcur_init(sc, sa);
+	return 0;
+}
+
 /* Reinitialize the per-AG block reservation for the AG we just fixed. */
 int
 xrep_reset_perag_resv(
