4f25712fcf9509a9333bddafb511a14d2184acdf
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:05:56 -0700

    xfs: cross-reference rmap records with ag btrees
    Strengthen the rmap btree record checker a little more by comparing
    OWN_FS and OWN_LOG reverse mappings against the AG headers and internal
    logs, respectively.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/bitmap.h
+++ fs/xfs/scrub/bitmap.h
@@ -38,5 +38,6 @@ int xbitmap_walk_bits(struct xbitmap *bitmap, xbitmap_walk_bits_fn fn,
 		void *priv);
 
 bool xbitmap_empty(struct xbitmap *bitmap);
+bool xbitmap_test(struct xbitmap *bitmap, uint64_t start, uint64_t *len);
 
 #endif	/* __XFS_SCRUB_BITMAP_H__ */
