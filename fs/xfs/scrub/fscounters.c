1ddb0d80eedc91ac89d10cf32375a918b2e1eed4
Darrick J. Wong <djwong@kernel.org>
2022-07-14 11:05:37 -0700

    xfs: minimize overhead of drain wakeups by using jump labels
    To reduce the runtime overhead even further when online fsck isn't
    running, use a static branch key to decide if we call wake_up on the
    drain.  For compilers that support jump labels, the call to wake_up is
    replaced by a nop sled when nobody is waiting for intents to drain.
    
    From my initial microbenchmarking, every transition of the static key
    between the on and off states takes about 22000ns to complete; this is
    paid entirely by the xfs_scrub process.  When the static key is off
    (which it should be when fsck isn't running), the nop sled adds an
    overhead of approximately 0.36ns to runtime code.
    
    For the few compilers that don't support jump labels, runtime code pays
    the cost of calling wake_up on an empty waitqueue, which was observed to
    be about 30ns.  However, most architectures that have sufficient memory
    and CPU capacity to run XFS also support jump labels, so this is not
    much of a worry.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/fscounters.c
+++ fs/xfs/scrub/fscounters.c
@@ -116,6 +116,13 @@ xchk_setup_fscounters(
 	struct xchk_fscounters	*fsc;
 	int			error;
 
+	/*
+	 * If the AGF doesn't track btreeblks, we have to lock the AGF to count
+	 * btree block usage by walking the actual btrees.
+	 */
+	if (!xfs_has_lazysbcount(sc->mp))
+		xchk_fshooks_enable(sc, XCHK_FSHOOKS_DRAIN);
+
 	sc->buf = kzalloc(sizeof(struct xchk_fscounters), XCHK_GFP_FLAGS);
 	if (!sc->buf)
 		return -ENOMEM;
