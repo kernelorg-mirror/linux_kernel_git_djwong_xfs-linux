5b7f451f56d53257a3fbdbd169b2e001531098cd
Darrick J. Wong <djwong@kernel.org>
2022-08-04 15:35:28 -0700

    xfs: repair problems in CoW forks
    Try to repair errors that we see in file CoW forks so that we don't do
    stupid things like remap garbage into a file.  There's not a lot we can
    do with the COW fork -- the ondisk metadata record only that the COW
    staging extents are owned by the refcount btree, which effectively means
    that we can't reconstruct this incore structure from scratch.
    
    Actually, this is even worse -- we can't touch written extents, because
    those map space that are actively under writeback, and there's not much
    to do with delalloc reservations.  Hence we can only detect crosslinked
    unwritten extents and fix them by punching out the problematic parts and
    replacing them with delalloc extents.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.h
+++ fs/xfs/scrub/repair.h
@@ -99,6 +99,7 @@ int xrep_refcountbt(struct xfs_scrub *sc);
 int xrep_inode(struct xfs_scrub *sc);
 int xrep_bmap_data(struct xfs_scrub *sc);
 int xrep_bmap_attr(struct xfs_scrub *sc);
+int xrep_bmap_cow(struct xfs_scrub *sc);
 
 int xrep_reinit_pagf(struct xfs_scrub *sc);
 int xrep_reinit_pagi(struct xfs_scrub *sc);
@@ -159,6 +160,7 @@ xrep_setup_nothing(
 #define xrep_inode			xrep_notsupported
 #define xrep_bmap_data			xrep_notsupported
 #define xrep_bmap_attr			xrep_notsupported
+#define xrep_bmap_cow			xrep_notsupported
 
 #endif /* CONFIG_XFS_ONLINE_REPAIR */
 
