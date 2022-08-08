c083e5f808c0a8790fe82e9c60584fe213108530
Darrick J. Wong <djwong@kernel.org>
2022-08-08 11:52:45 -0700

    xfs: add an explicit owner field to xfs_da_args
    Add an explicit owner field to xfs_da_args, which will make it easier
    for online fsck to set the owner field of the temporary directory and
    xattr structures that it builds to repair damaged metadata.
    
    Note: I hopefully found all the xfs_da_args definitions by looking for
    automatic stack variable declarations and xfs_da_args.dp assignments:
    
    git grep -E '(args.*dp =|struct xfs_da_args[[:space:]]*[a-z0-9][a-z0-9]*)'
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/attr.c
+++ fs/xfs/scrub/attr.c
@@ -172,6 +172,7 @@ xchk_xattr_listent(
 		.hashval		= xfs_da_hashname(name, namelen),
 		.trans			= context->tp,
 		.valuelen		= valuelen,
+		.owner			= context->dp->i_ino,
 	};
 	struct xchk_xattr_buf		*ab;
 	struct xchk_xattr		*sx;
