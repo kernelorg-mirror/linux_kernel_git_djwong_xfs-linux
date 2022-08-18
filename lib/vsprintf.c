24f719160eadc21c8a5fa7f705a265ab118f189b
Darrick J. Wong <djwong@kernel.org>
2022-08-17 17:11:49 -0700

    vsprintf: disable pointer hashing
    Turn off pointer hashing in dmesg, this makes debugging hard.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -804,6 +804,9 @@ int ptr_to_hashval(const void *ptr, unsigned long *hashval_out)
 static char *ptr_to_id(char *buf, char *end, const void *ptr,
 		       struct printf_spec spec)
 {
+#if IS_ENABLED(CONFIG_DISABLE_DMESG_POINTER_HASHING)
+	return pointer_string(buf, end, ptr, spec);
+#else
 	const char *str = sizeof(ptr) == 8 ? "(____ptrval____)" : "(ptrval)";
 	unsigned long hashval;
 	int ret;
@@ -829,6 +832,7 @@ static char *ptr_to_id(char *buf, char *end, const void *ptr,
 	}
 
 	return pointer_string(buf, end, (const void *)hashval, spec);
+#endif
 }
 
 static char *default_pointer(char *buf, char *end, const void *ptr,
