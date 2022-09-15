0b44bda1fc51619b7eb2b78aa2aa309f069d8e48
Darrick J. Wong <djwong@kernel.org>
2022-09-15 10:37:18 -0700

    xfs: convert the rtbitmap macros to static inline functions
    Replace these macros with typechecked helper functions.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/rtbitmap_repair.c
+++ fs/xfs/scrub/rtbitmap_repair.c
@@ -330,8 +330,8 @@ xrep_rgbitmap_load_before(
 	group_rtbno = xfs_rgbno_to_rtb(mp, rtg->rtg_rgno, 0);
 	group_rtx = xfs_rtb_to_rtxt(mp, group_rtbno);
 
-	rb->group_rbmoff = XFS_BITTOBLOCK(mp, group_rtx);
-	rbmoff_rtx = XFS_BLOCKTOBIT(mp, rb->group_rbmoff);
+	rb->group_rbmoff = xfs_rtx_to_rbmblock(mp, group_rtx);
+	rbmoff_rtx = xfs_rbmblock_to_rtx(mp, rb->group_rbmoff);
 
 	trace_xrep_rgbitmap_load(rtg, rb->group_rbmoff, rbmoff_rtx,
 			group_rtx - 1);
@@ -358,7 +358,7 @@ xrep_rgbitmap_load_before(
 	 * of this group.
 	 */
 	wordoff = (rbmoff_rtx >> XFS_NBWORDLOG);
-	wordcnt = XFS_BITTOWORD(mp, group_rtx - rbmoff_rtx);
+	wordcnt = xfs_rtx_to_rbmword(mp, group_rtx - rbmoff_rtx);
 	if (wordcnt > 0) {
 		error = xfile_obj_store(sc->xfile, bp->b_addr,
 				wordcnt * sizeof(xfs_rtword_t),
@@ -436,9 +436,9 @@ xrep_rgbitmap_load_after(
 					rtg->rtg_blockcount - 1);
 	last_group_rtx = xfs_rtb_to_rtxt(mp, last_rtbno);
 
-	last_group_rbmoff = XFS_BITTOBLOCK(mp, last_group_rtx);
+	last_group_rbmoff = xfs_rtx_to_rbmblock(mp, last_group_rtx);
 	rb->group_rbmlen = last_group_rbmoff - rb->group_rbmoff + 1;
-	last_rbmblock_rtx = XFS_BLOCKTOBIT(mp, last_group_rbmoff + 1) - 1;
+	last_rbmblock_rtx = xfs_rbmblock_to_rtx(mp, last_group_rbmoff + 1) - 1;
 
 	trace_xrep_rgbitmap_load(rtg, last_group_rbmoff, last_group_rtx + 1,
 			last_rbmblock_rtx);
@@ -482,7 +482,7 @@ xrep_rgbitmap_load_after(
 	if (error)
 		goto out_rele;
 	ondisk_word = *((xfs_rtword_t *)bp->b_addr +
-					XFS_BITTOWORD(mp, last_group_rtx));
+					xfs_rtx_to_rbmword(mp, last_group_rtx));
 
 	trace_xrep_rgbitmap_load_word(mp, wordoff, bit, ondisk_word,
 			xfile_word, mask);
@@ -498,7 +498,7 @@ xrep_rgbitmap_load_after(
 copy_words:
 	/* Copy as many full words as we can. */
 	wordoff++;
-	wordcnt = XFS_BITTOWORD(mp, last_rbmblock_rtx - last_group_rtx + 1);
+	wordcnt = xfs_rtx_to_rbmword(mp, last_rbmblock_rtx - last_group_rtx + 1);
 	if (wordcnt > 0) {
 		xfs_rtword_t	*p = bp->b_addr;
 
