d67d1afb8a30af91ba1dddf3fc30fb4da7631449
Darrick J. Wong <djwong@kernel.org>
2022-10-06 16:49:28 -0700

    xfs: use i_prev_unlinked to distinguish inodes that are not on the unlinked list
    Alter the definition of i_prev_unlinked slightly to make it more obvious
    when an inode with 0 link count is not part of the iunlink bucket lists
    rooted in the AGI.  An upcoming enhancement to the online AGI fsck code
    will need to make this distinction to check and rebuild the AGI unlinked
    buckets.
    
    This distinction is necessary because it is not sufficient to check
    xfs_inode.i_nlink to decide if an inode is on the unlinked list.
    Updates to i_nlink can happen while holding only ILOCK_EXCL, but updates
    to an inode's position in the AGI unlinked list requires both ILOCK_EXCL
    and the AGI buffer lock.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -2172,6 +2172,7 @@ xfs_iunlink_insert_inode(
 	}
 
 	/* Point the head of the list to point to this inode. */
+	ip->i_prev_unlinked = NULLAGINO;
 	return xfs_iunlink_update_bucket(tp, pag, agibp, bucket_index, agino);
 }
 
@@ -2274,7 +2275,7 @@ xfs_iunlink_remove_inode(
 	}
 
 	ip->i_next_unlinked = NULLAGINO;
-	ip->i_prev_unlinked = NULLAGINO;
+	ip->i_prev_unlinked = 0;
 	return error;
 }
 
