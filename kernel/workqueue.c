e0c58b6eed0645a696212e249085063de2d3e9a9
Darrick J. Wong <djwong@kernel.org>
2022-10-09 09:08:44 -0700

    workqueue: omit "kworker/" from comm
    Skip the "kworker/" part, this gets in the way of knowing which wq we're
    dealing with.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1947,7 +1947,7 @@ static struct worker *create_worker(struct worker_pool *pool)
 		snprintf(id_buf, sizeof(id_buf), "u%d:%d", pool->id, id);
 
 	worker->task = kthread_create_on_node(worker_thread, worker, pool->node,
-					      "kworker/%s", id_buf);
+					      "%s", id_buf);
 	if (IS_ERR(worker->task))
 		goto fail;
 
