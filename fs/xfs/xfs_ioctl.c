b55754c62f7a693de2610930733ec38bff3e5b5c
Darrick J. Wong <djwong@kernel.org>
2022-10-20 16:49:00 -0700

    dax: fix reflink insanity
    Yeahhhh...
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_ioctl.c
+++ fs/xfs/xfs_ioctl.c
@@ -1132,10 +1132,6 @@ xfs_ioctl_setattr_xflags(
 			return -EINVAL;
 	}
 
-	/* Don't allow us to set DAX mode for a reflinked file for now. */
-	if ((fa->fsx_xflags & FS_XFLAG_DAX) && xfs_is_reflink_inode(ip))
-		return -EINVAL;
-
 	/* diflags2 only valid for v3 inodes. */
 	i_flags2 = xfs_flags2diflags2(ip, fa->fsx_xflags);
 	if (i_flags2 && !xfs_has_v3inodes(mp))
