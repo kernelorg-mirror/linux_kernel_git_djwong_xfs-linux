583ad8169bd0169d571a62110f8cd1cc84d2ab55
Darrick J. Wong <djwong@kernel.org>
2023-02-13 15:42:10 -0800

    xfs: create temporary files and directories for online repair
    Teach the online repair code how to create temporary files or
    directories.  These temporary files can be used to stage reconstructed
    information until we're ready to perform an atomic extent swap to commit
    the new metadata.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -48,7 +48,6 @@ struct kmem_cache *xfs_inode_cache;
  */
 #define	XFS_ITRUNC_MAX_EXTENTS	2
 
-STATIC int xfs_iunlink(struct xfs_trans *, struct xfs_inode *);
 STATIC int xfs_iunlink_remove(struct xfs_trans *tp, struct xfs_perag *pag,
 	struct xfs_inode *);
 
@@ -2187,7 +2186,7 @@ xfs_iunlink_insert_inode(
  * We place the on-disk inode on a list in the AGI.  It will be pulled from this
  * list when the inode is freed.
  */
-STATIC int
+int
 xfs_iunlink(
 	struct xfs_trans	*tp,
 	struct xfs_inode	*ip)
