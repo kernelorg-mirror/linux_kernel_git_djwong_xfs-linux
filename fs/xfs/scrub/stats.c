4e14f4fac4f3fe38d26704e16bd0850d02e826f9
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:29 -0800

    xfs: scrub each rtgroup's portion of the rtbitmap separately
    Create a new scrub type code so that userspace can scrub each rtgroup's
    portion of the rtbitmap file separately.  This reduces the long tail
    latency that results from scanning the entire bitmap all at once, and
    prepares us for future patchsets, wherein we'll need to be able to lock
    a specific rtgroup so that we can rebuild that rtgroup's part of the
    rtbitmap contents from the rtgroup's rmap btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/stats.c
+++ fs/xfs/scrub/stats.c
@@ -81,6 +81,7 @@ static const char *name_map[XFS_SCRUB_TYPE_NR] = {
 	[XFS_SCRUB_TYPE_NLINKS]		= "nlinks",
 	[XFS_SCRUB_TYPE_DIRTREE]	= "dirtree",
 	[XFS_SCRUB_TYPE_RGSUPER]	= "rgsuper",
+	[XFS_SCRUB_TYPE_RGBITMAP]	= "rgbitmap",
 };
 
 /* Format the scrub stats into a text buffer, similar to pcp style. */
