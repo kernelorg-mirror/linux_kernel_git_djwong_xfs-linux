3f8a13710fff06cf9873dcaaf25f1c9ccb1032ff
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:07 -0800

    xfs: convert rt extent numbers to xfs_rtxnum_t
    Further disambiguate the xfs_rtblock_t uses by creating a new type,
    xfs_rtxnum_t, to store the position of an extent within the realtime
    section, in units of rtextents.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/rtsummary.c
+++ fs/xfs/scrub/rtsummary.c
@@ -125,7 +125,7 @@ xchk_rtsum_record_free(
 {
 	struct xfs_scrub		*sc = priv;
 	xfs_fileoff_t			rbmoff;
-	xfs_rtblock_t			rtbno;
+	xfs_rtxnum_t			rtbno;
 	xfs_filblks_t			rtlen;
 	xchk_rtsumoff_t			offs;
 	unsigned int			lenlog;
