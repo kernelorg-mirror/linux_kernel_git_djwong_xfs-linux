cd2273a1579eb4241854fa0e8b6f89981f838d3e
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:54:02 -0800

    xfs: introduce new file range exchange ioctl
    Introduce a new ioctl to handle swapping ranges of bytes between files.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/remap_range.c
+++ fs/remap_range.c
@@ -98,8 +98,7 @@ static int generic_remap_checks(struct file *file_in, loff_t pos_in,
 	return 0;
 }
 
-static int remap_verify_area(struct file *file, loff_t pos, loff_t len,
-			     bool write)
+int remap_verify_area(struct file *file, loff_t pos, loff_t len, bool write)
 {
 	if (unlikely(pos < 0 || len < 0))
 		return -EINVAL;
@@ -109,6 +108,7 @@ static int remap_verify_area(struct file *file, loff_t pos, loff_t len,
 
 	return security_file_permission(file, write ? MAY_WRITE : MAY_READ);
 }
+EXPORT_SYMBOL(remap_verify_area);
 
 /*
  * Ensure that we don't remap a partial EOF block in the middle of something
