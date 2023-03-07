aa620212e63bbe8f242c072a058aeb7eb063406e
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:24 -0800

    xfs: allow scrub to hook metadata updates in other writers
    Certain types of filesystem metadata can only be checked by scanning
    every file in the entire filesystem.  Specific examples of this include
    quota counts, file link counts, and reverse mappings of file extents.
    Directory and parent pointer reconstruction may also fall into this
    category.  File scanning is much trickier than scanning AG metadata
    because we have to take inode locks in the same order as the rest of
    [VX]FS, we can't be holding buffer locks when we do that, and scanning
    the whole filesystem takes time.
    
    Earlier versions of the online repair patchset relied heavily on
    fsfreeze as a means to quiesce the filesystem so that we could take
    locks in the proper order without worrying about concurrent updates from
    other writers.  Reviewers of those patches opined that freezing the
    entire fs to check and repair something was not sufficiently better than
    unmounting to run fsck offline.  I don't agree with that 100%, but the
    message was clear: find a way to repair things that minimizes the
    quiet period where nobody can write to the filesystem.
    
    Generally, building btree indexes online can be split into two phases: a
    collection phase where we compute the records that will be put into the
    new btree; and a construction phase, where we construct the physical
    btree blocks and persist them.  While it's simple to hold resource locks
    for the entirety of the two phases to ensure that the new index is
    consistent with the rest of the system, we don't need to hold resource
    locks during the collection phase if we have a means to receive live
    updates of other work going on elsewhere in the system.
    
    The goal of this patch, then, is to enable online fsck to learn about
    metadata updates going on in other threads while it constructs a shadow
    copy of the metadata records to verify or correct the real metadata.  To
    minimize the overhead when online fsck isn't running, we use srcu
    notifiers because they prioritize fast access to the notifier call chain
    (particularly when the chain is empty) at a cost to configuring
    notifiers.  Online fsck should be relatively infrequent, so this is
    acceptable.
    
    The intended usage model is fairly simple.  Code that modifies a
    metadata structure of interest should declare a xfs_hook_chain structure
    in some well defined place, and call xfs_hook_call whenever an update
    happens.  Online fsck code should define a struct notifier_block and use
    xfs_hook_add to attach the block to the chain, along with a function to
    be called.  This function should synchronize with the fsck scanner to
    update whatever in-memory data the scanner is collecting.  When
    finished, xfs_hook_del removes the notifier from the list and waits for
    them all to complete.
    
    On the author's computer, calling an empty srcu notifier chain was
    observed to have an overhead averaging ~40ns with a maximum of 60ns.
    Adding a no-op notifier function increased the average to ~58ns and
    66ns.  When the quotacheck live update notifier is attached, the average
    increases to ~322ns with a max of 372ns to update scrub's in-memory
    observation data, assuming no lock contention.
    
    With jump labels enabled, calls to empty srcu notifier chains are elided
    from the call sites when there are no hooks registered, which means that
    the overhead is 0.36ns when fsck is not running.  For compilers that do
    not support jump labels (all major architectures do), the overhead of a
    no-op notifier call is less bad (on a many-cpu system) than the atomic
    counter ops, so we make the hook switch itself a nop.
    
    Note: This new code is also split out as a separate patch from its
    initial user so that the author can move patches around his tree with
    ease.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

---
+++ fs/xfs/xfs_hooks.h
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2022-2023 Oracle.  All Rights Reserved.
+ * Author: Darrick J. Wong <djwong@kernel.org>
+ */
+#ifndef XFS_HOOKS_H_
+#define XFS_HOOKS_H_
+
+#ifdef CONFIG_XFS_LIVE_HOOKS
+struct xfs_hooks {
+	struct srcu_notifier_head	head;
+};
+#else
+struct xfs_hooks { /* empty */ };
+#endif
+
+/*
+ * If hooks and jump labels are enabled, we use jump labels (aka patching of
+ * the code segment) to avoid the minute overhead of calling an empty notifier
+ * chain when we know there are no callers.  If hooks are enabled without jump
+ * labels, hardwire the predicate to true because calling an empty srcu
+ * notifier chain isn't so expensive.
+ */
+#if defined(CONFIG_JUMP_LABEL) && defined(CONFIG_XFS_LIVE_HOOKS)
+# define DEFINE_STATIC_XFS_HOOK_SWITCH(name) \
+	static DEFINE_STATIC_KEY_FALSE(name)
+# define xfs_hooks_switch_on(name)	static_branch_inc(name)
+# define xfs_hooks_switch_off(name)	static_branch_dec(name)
+# define xfs_hooks_switched_on(name)	static_branch_unlikely(name)
+#elif defined(CONFIG_XFS_LIVE_HOOKS)
+# define DEFINE_STATIC_XFS_HOOK_SWITCH(name)
+# define xfs_hooks_switch_on(name)	((void)0)
+# define xfs_hooks_switch_off(name)	((void)0)
+# define xfs_hooks_switched_on(name)	(true)
+#else
+# define DEFINE_STATIC_XFS_HOOK_SWITCH(name)
+# define xfs_hooks_switch_on(name)	((void)0)
+# define xfs_hooks_switch_off(name)	((void)0)
+# define xfs_hooks_switched_on(name)	(false)
+#endif /* JUMP_LABEL && XFS_LIVE_HOOKS */
+
+#ifdef CONFIG_XFS_LIVE_HOOKS
+struct xfs_hook {
+	/* This must come at the start of the structure. */
+	struct notifier_block		nb;
+};
+
+typedef	int (*xfs_hook_fn_t)(struct xfs_hook *hook, unsigned long action,
+		void *data);
+
+void xfs_hooks_init(struct xfs_hooks *chain);
+int xfs_hooks_add(struct xfs_hooks *chain, struct xfs_hook *hook);
+void xfs_hooks_del(struct xfs_hooks *chain, struct xfs_hook *hook);
+int xfs_hooks_call(struct xfs_hooks *chain, unsigned long action,
+		void *priv);
+
+static inline void xfs_hook_setup(struct xfs_hook *hook, notifier_fn_t fn)
+{
+	hook->nb.notifier_call = fn;
+	hook->nb.priority = 0;
+}
+
+#else
+# define xfs_hooks_init(chain)			((void)0)
+# define xfs_hooks_call(chain, val, priv)	(NOTIFY_DONE)
+#endif
+
+#endif /* XFS_HOOKS_H_ */
