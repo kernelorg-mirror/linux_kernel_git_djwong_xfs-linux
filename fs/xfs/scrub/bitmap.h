b74f7c90011dd747601c7cc5bd66e5d9123003b7
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:56:31 -0800

    xfs: capture realtime CoW staging extents when rebuilding rt rmapbt
    Walk the realtime refcount btree to find the CoW staging extents when
    we're rebuilding the realtime rmap btree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/bitmap.h
+++ fs/xfs/scrub/bitmap.h
@@ -126,6 +126,34 @@ int xagb_bitmap_set_btcur_path(struct xagb_bitmap *bitmap,
 
 uint32_t xagb_bitmap_count_set_regions(struct xagb_bitmap *bitmap);
 
+/* Bitmaps, but for type-checked for xfs_rgblock_t */
+
+struct xrgb_bitmap {
+	struct xbitmap32	rgbitmap;
+};
+
+static inline void xrgb_bitmap_init(struct xrgb_bitmap *bitmap)
+{
+	xbitmap32_init(&bitmap->rgbitmap);
+}
+
+static inline void xrgb_bitmap_destroy(struct xrgb_bitmap *bitmap)
+{
+	xbitmap32_destroy(&bitmap->rgbitmap);
+}
+
+static inline int xrgb_bitmap_set(struct xrgb_bitmap *bitmap,
+		xfs_rgblock_t start, xfs_extlen_t len)
+{
+	return xbitmap32_set(&bitmap->rgbitmap, start, len);
+}
+
+static inline int xrgb_bitmap_walk(struct xrgb_bitmap *bitmap,
+		xbitmap32_walk_fn fn, void *priv)
+{
+	return xbitmap32_walk(&bitmap->rgbitmap, fn, priv);
+}
+
 /* Bitmaps, but for type-checked for xfs_fsblock_t */
 
 struct xfsb_bitmap {
