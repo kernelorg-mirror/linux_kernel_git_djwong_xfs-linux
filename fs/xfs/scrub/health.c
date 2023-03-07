6d861626186d125735af6e4df2df65083dc48066
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:56:00 -0800

    xfs: scrub the realtime rmapbt
    Check the realtime reverse mapping btree against the rtbitmap, and
    modify the rtbitmap scrub to check against the rtrmapbt.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/health.c
+++ fs/xfs/scrub/health.c
@@ -115,6 +115,7 @@ static const struct xchk_health_map type_to_health_flag[XFS_SCRUB_TYPE_NR] = {
 	[XFS_SCRUB_TYPE_DIRTREE]	= { XHG_INO, XFS_SICK_INO_DIRTREE },
 	[XFS_SCRUB_TYPE_METAPATH]	= { XHG_FS,  XFS_SICK_FS_METAPATH },
 	[XFS_SCRUB_TYPE_RGSUPER]	= { XHG_RTGROUP, XFS_SICK_RT_SUPER },
+	[XFS_SCRUB_TYPE_RTRMAPBT]	= { XHG_RTGROUP, XFS_SICK_RT_RMAPBT },
 };
 
 /* Return the health status mask for this scrub type. */
