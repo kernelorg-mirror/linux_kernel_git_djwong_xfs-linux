ae1c058b7acf210ec396d5a5c03c6dbf86c7716a
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:46 -0800

    xfs: simplify the xfs_rmap_{alloc,free}_extent calling conventions
    Simplify the calling conventions by allowing callers to pass a fsbno
    (xfs_fsblock_t) directly into these functions, since we're just going to
    set it in a struct anyway.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/alloc_repair.c
+++ fs/xfs/scrub/alloc_repair.c
@@ -522,9 +522,13 @@ xrep_abt_dispose_one(
 	ASSERT(pag == resv->pag);
 
 	/* Add a deferred rmap for each extent we used. */
-	if (resv->used > 0)
-		xfs_rmap_alloc_extent(sc->tp, pag->pag_agno, resv->agbno,
-				resv->used, XFS_RMAP_OWN_AG);
+	if (resv->used > 0) {
+		xfs_fsblock_t	fsbno;
+
+		fsbno = XFS_AGB_TO_FSB(sc->mp, pag->pag_agno, resv->agbno);
+		xfs_rmap_alloc_extent(sc->tp, fsbno, resv->used,
+				XFS_RMAP_OWN_AG);
+	}
 
 	/*
 	 * For each reserved btree block we didn't use, add it to the free
