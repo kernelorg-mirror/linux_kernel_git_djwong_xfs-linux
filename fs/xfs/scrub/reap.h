ad81c1db68686b28d83fd7d757b76ea334642ce6
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:18 -0800

    xfs: reintroduce reaping of file metadata blocks to xrep_reap_extents
    Reintroduce to xrep_reap_extents the ability to reap extents from any
    AG.  We dropped this before because it was buggy, but in the next patch
    we will gain the ability to reap old bmap btrees, which can have blocks
    in any AG.  To do this, we require that sc->sa is uninitialized, so that
    we can use it to hold all the per-AG context for a given extent.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/reap.h
+++ fs/xfs/scrub/reap.h
@@ -8,5 +8,7 @@
 
 int xrep_reap_agblocks(struct xfs_scrub *sc, struct xagb_bitmap *bitmap,
 		const struct xfs_owner_info *oinfo, enum xfs_ag_resv_type type);
+int xrep_reap_fsblocks(struct xfs_scrub *sc, struct xfsb_bitmap *bitmap,
+		const struct xfs_owner_info *oinfo);
 
 #endif /* __XFS_SCRUB_REAP_H__ */
