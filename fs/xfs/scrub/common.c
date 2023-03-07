c1c8269d3c22e3086258cb9689ba49b73a7606ca
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:47 -0800

    xfs: hook live rmap operations during a repair operation
    Hook the regular rmap code when an rmapbt repair operation is running so
    that we can unlock the AGF buffer to scan the filesystem and keep the
    in-memory btree up to date during the scan.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/common.c
+++ fs/xfs/scrub/common.c
@@ -1302,5 +1302,8 @@ xchk_fsgates_enable(
 	if (scrub_fsgates & XCHK_FSGATES_DIRENTS)
 		xfs_dir_hook_enable();
 
+	if (scrub_fsgates & XCHK_FSGATES_RMAP)
+		xfs_rmap_hook_enable();
+
 	sc->flags |= scrub_fsgates;
 }
