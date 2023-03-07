3074725008ae3142d9a59b9f28648d34c33b3fee
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:39 -0800

    xfs: stabilize fs summary counters for online fsck
    If the fscounters scrubber notices incorrect summary counters, it's
    entirely possible that scrub is simply racing with other threads that
    are updating the incore counters.  There isn't a good way to stabilize
    percpu counters or set ourselves up to observe live updates with hooks
    like we do for the quotacheck or nlinks scanners, so we instead choose
    to freeze the filesystem long enough to walk the incore per-AG
    structures.
    
    Past me thought that it was going to be commonplace to have to freeze
    the filesystem to perform some kind of repair and set up a whole
    separate infrastructure to freeze the filesystem in such a way that
    userspace could not unfreeze while we were running.  This involved
    adding a mutex and freeze_super/thaw_super functions and dealing with
    the fact that the VFS freeze/thaw functions can free the VFS superblock
    references on return.
    
    This was all very overwrought, since fscounters turned out to be the
    only user of scrub freezes, and it doesn't require the log to quiesce,
    only the incore superblock counters.  We prevent other threads from
    changing the freeze level by adding a new SB_FREEZE_EXCLUSIVE level.
    
    The end result is that fscounters should be much more efficient.  When
    we're checking a busy system and we can't stabilize the counters, the
    custom freeze will do less work, which should result in less downtime.
    Repair should be similarly speedy, but that's in the next patch.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/scrub.h
+++ fs/xfs/scrub/scrub.h
@@ -123,6 +123,7 @@ struct xfs_scrub {
 #define XCHK_NEED_DRAIN		(1 << 3)  /* scrub needs to drain defer ops */
 #define XCHK_FSGATES_QUOTA	(1 << 4)  /* quota live update enabled */
 #define XCHK_FSGATES_DIRENTS	(1 << 5)  /* directory live update enabled */
+#define XCHK_HAVE_FREEZE_PROT	(1 << 6)  /* do we have freeze protection? */
 #define XREP_RESET_PERAG_RESV	(1 << 30) /* must reset AG space reservation */
 #define XREP_ALREADY_FIXED	(1 << 31) /* checking our repair work */
 
