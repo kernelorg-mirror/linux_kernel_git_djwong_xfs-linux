2f4de42d9a24b2ca08b6cb6ab3996e7e8d7d2483
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:44 -0800

    xfs: connect in-memory btrees to xfiles
    Add to our stubbed-out in-memory btrees the ability to connect them with
    an actual in-memory backing file (aka xfiles) and the necessary pieces
    to track free space in the xfile and flush dirty xfbtree buffers on
    demand, which we'll need for online repair.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/bitmap.h
+++ fs/xfs/scrub/bitmap.h
@@ -34,6 +34,9 @@ int xbitmap_walk(struct xbitmap *bitmap, xbitmap_walk_fn fn,
 bool xbitmap_empty(struct xbitmap *bitmap);
 bool xbitmap_test(struct xbitmap *bitmap, uint64_t start, uint64_t *len);
 
+int xbitmap_take_first_set(struct xbitmap *bitmap, uint64_t start,
+		uint64_t last, uint64_t *valp);
+
 /* u32 bitmap */
 
 struct xbitmap32 {
