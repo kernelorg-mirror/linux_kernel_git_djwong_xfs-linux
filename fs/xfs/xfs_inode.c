416ed711f78cd411cfbeee74132896fb0c8fa091
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:26 -0800

    xfs: implement live quotacheck inode scan
    Create a new trio of scrub functions to check quota counters.  While the
    dquots themselves are filesystem metadata and should be checked early,
    the dquot counter values are computed from other metadata and are
    therefore summary counters.  We don't plug these into the scrub dispatch
    just yet, because we still need to be able to watch quota updates while
    doing our scan.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -3767,3 +3767,24 @@ xfs_ifork_zapped(
 		return false;
 	}
 }
+
+/* Compute the number of data and realtime blocks used by a file. */
+void
+xfs_inode_count_blocks(
+	struct xfs_trans	*tp,
+	struct xfs_inode	*ip,
+	xfs_filblks_t		*dblocks,
+	xfs_filblks_t		*rblocks)
+{
+	struct xfs_ifork	*ifp = xfs_ifork_ptr(ip, XFS_DATA_FORK);
+
+	if (!XFS_IS_REALTIME_INODE(ip)) {
+		*dblocks = ip->i_nblocks;
+		*rblocks = 0;
+		return;
+	}
+
+	*rblocks = 0;
+	xfs_bmap_count_leaves(ifp, rblocks);
+	*dblocks = ip->i_nblocks - *rblocks;
+}
