6d5e5ce0c37cdaf188afd54b6bfbed7df4667252
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:36 -0800

    xfs: report XFS_IS_CORRUPT errors to the health system
    Whenever we encounter XFS_IS_CORRUPT failures, we should report that to
    the health monitoring system for later reporting.
    
    I started with this semantic patch and massaged everything until it
    built:
    
    @@
    expression mp, test;
    @@
    
    - if (XFS_IS_CORRUPT(mp, test)) return -EFSCORRUPTED;
    + if (XFS_IS_CORRUPT(mp, test)) { xfs_btree_mark_sick(cur); return -EFSCORRUPTED; }
    
    @@
    expression mp, test;
    identifier label, error;
    @@
    
    - if (XFS_IS_CORRUPT(mp, test)) { error = -EFSCORRUPTED; goto label; }
    + if (XFS_IS_CORRUPT(mp, test)) { xfs_btree_mark_sick(cur); error = -EFSCORRUPTED; goto label; }
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_discard.c
+++ fs/xfs/xfs_discard.c
@@ -18,6 +18,7 @@
 #include "xfs_trace.h"
 #include "xfs_log.h"
 #include "xfs_ag.h"
+#include "xfs_health.h"
 
 STATIC int
 xfs_trim_extents(
@@ -70,6 +71,7 @@ xfs_trim_extents(
 		if (error)
 			break;
 		if (XFS_IS_CORRUPT(mp, i != 1)) {
+			xfs_btree_mark_sick(cur);
 			error = -EFSCORRUPTED;
 			break;
 		}
