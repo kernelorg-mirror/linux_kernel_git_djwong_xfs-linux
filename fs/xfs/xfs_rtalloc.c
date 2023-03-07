333cb2a51ad2a5adc3ca416fe3a88da8030976ec
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:56:26 -0800

    xfs: report realtime refcount btree corruption errors to the health system
    Whenever we encounter corrupt realtime refcount btree blocks, we should
    report that to the health monitoring system for later reporting.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -1958,6 +1958,7 @@ xfs_rtmount_refcountbt(
 		goto out_path;
 
 	if (XFS_IS_CORRUPT(mp, ip->i_df.if_format != XFS_DINODE_FMT_REFCOUNT)) {
+		xfs_rtgroup_mark_sick(rtg, XFS_SICK_RT_REFCNTBT);
 		error = -EFSCORRUPTED;
 		goto out_rele;
 	}
