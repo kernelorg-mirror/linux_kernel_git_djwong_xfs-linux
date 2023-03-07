99b501541d2446dc4896279ec92fafbdc27fee45
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:48 -0800

    xfs: export some of the btree ops structures
    Expose these static btree ops structures so that we can reference them
    in the AG initialization code in the next patch.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/libxfs/xfs_alloc_btree.c
+++ fs/xfs/libxfs/xfs_alloc_btree.c
@@ -454,7 +454,7 @@ xfs_allocbt_keys_contiguous(
 				 be32_to_cpu(key2->alloc.ar_startblock));
 }
 
-static const struct xfs_btree_ops xfs_bnobt_ops = {
+const struct xfs_btree_ops xfs_bnobt_ops = {
 	.rec_len		= sizeof(xfs_alloc_rec_t),
 	.key_len		= sizeof(xfs_alloc_key_t),
 
@@ -477,7 +477,7 @@ static const struct xfs_btree_ops xfs_bnobt_ops = {
 	.keys_contiguous	= xfs_allocbt_keys_contiguous,
 };
 
-static const struct xfs_btree_ops xfs_crc_bnobt_ops = {
+const struct xfs_btree_ops xfs_crc_bnobt_ops = {
 	.rec_len		= sizeof(struct xfs_alloc_rec),
 	.key_len		= sizeof(xfs_alloc_key_t),
 	.geom_flags		= XFS_BTGEO_CRC_BLOCKS,
@@ -501,7 +501,7 @@ static const struct xfs_btree_ops xfs_crc_bnobt_ops = {
 	.keys_contiguous	= xfs_allocbt_keys_contiguous,
 };
 
-static const struct xfs_btree_ops xfs_cntbt_ops = {
+const struct xfs_btree_ops xfs_cntbt_ops = {
 	.rec_len		= sizeof(xfs_alloc_rec_t),
 	.key_len		= sizeof(xfs_alloc_key_t),
 	.geom_flags		= XFS_BTGEO_LASTREC_UPDATE,
@@ -525,7 +525,7 @@ static const struct xfs_btree_ops xfs_cntbt_ops = {
 	.keys_contiguous	= NULL, /* not needed right now */
 };
 
-static const struct xfs_btree_ops xfs_crc_cntbt_ops = {
+const struct xfs_btree_ops xfs_crc_cntbt_ops = {
 	.rec_len		= sizeof(struct xfs_alloc_rec),
 	.key_len		= sizeof(xfs_alloc_key_t),
 	.geom_flags		= XFS_BTGEO_LASTREC_UPDATE | XFS_BTGEO_CRC_BLOCKS,
