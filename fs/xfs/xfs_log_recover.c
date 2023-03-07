abe24ddcf8c7b8b218ccafcca1aae134afea7a9c
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:42 -0800

    xfs: support logging EFIs for realtime extents
    Teach the EFI mechanism how to free realtime extents.  We're going to
    need this to enforce proper ordering of operations when we enable
    realtime rmap.
    
    Declare a new log intent item type (XFS_LI_EFI_RT) and a separate defer
    ops for rt extents.  This keeps the ondisk artifacts and processing code
    completely separate between the rt and non-rt cases.  Hopefully this
    will make it easier to debug filesystem problems.
    
    Previous versions of this patch accomplished this by setting the high
    bit in each rt EFI extent.  This was found to be less transparent by
    reviewers.
    
    [Contains a bug fix and cleanups from hch]
    
    Cc: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_log_recover.c
+++ fs/xfs/xfs_log_recover.c
@@ -1792,6 +1792,8 @@ static const struct xlog_recover_item_ops *xlog_recover_item_ops[] = {
 	&xlog_attrd_item_ops,
 	&xlog_sxi_item_ops,
 	&xlog_sxd_item_ops,
+	&xlog_rtefi_item_ops,
+	&xlog_rtefd_item_ops,
 };
 
 static const struct xlog_recover_item_ops *
