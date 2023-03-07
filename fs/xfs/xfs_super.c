59980f2bccf8af24bab0e4ed39d50aaf02106285
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:19 -0800

    xfs: check the realtime superblock at mount time
    Check the realtime superblock at mount time, to ensure that the label
    actually matches the primary superblock.  If the rt superblock is good,
    attach it to the xfs_mount so that the log can use ordered buffers to
    keep this primary in sync with the primary super on the data device.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -44,6 +44,7 @@
 #include "scrub/rcbag_btree.h"
 #include "xfs_swapext_item.h"
 #include "xfs_rtbitmap.h"
+#include "xfs_rtalloc.h"
 
 #include <linux/magic.h>
 #include <linux/fs_context.h>
@@ -1127,6 +1128,7 @@ xfs_fs_put_super(
 	xfs_filestream_unmount(mp);
 	xfs_unmountfs(mp);
 
+	xfs_rtmount_freesb(mp);
 	xfs_freesb(mp);
 	free_percpu(mp->m_stats.xs_stats);
 	xfs_mount_list_del(mp);
@@ -1609,9 +1611,13 @@ xfs_fs_fill_super(
 		goto out_free_sb;
 	}
 
-	error = xfs_filestream_mount(mp);
+	error = xfs_rtmount_readsb(mp);
 	if (error)
 		goto out_free_sb;
+
+	error = xfs_filestream_mount(mp);
+	if (error)
+		goto out_free_rtsb;
 
 	/*
 	 * we must configure the block size in the superblock before we run the
@@ -1654,6 +1660,10 @@ xfs_fs_fill_super(
 	if (xfs_has_metadir(mp))
 		xfs_warn(mp,
 "EXPERIMENTAL metadata directory feature in use. Use at your own risk!");
+
+	if (xfs_has_rtgroups(mp))
+		xfs_warn(mp,
+"EXPERIMENTAL realtime allocation group feature in use. Use at your own risk!");
 
 	if (xfs_has_reflink(mp)) {
 		if (mp->m_sb.sb_rblocks) {
@@ -1699,6 +1709,8 @@ xfs_fs_fill_super(
 
  out_filestream_unmount:
 	xfs_filestream_unmount(mp);
+ out_free_rtsb:
+	xfs_rtmount_freesb(mp);
  out_free_sb:
 	xfs_freesb(mp);
  out_free_stats:
@@ -1720,7 +1732,7 @@ xfs_fs_fill_super(
  out_unmount:
 	xfs_filestream_unmount(mp);
 	xfs_unmountfs(mp);
-	goto out_free_sb;
+	goto out_free_rtsb;
 }
 
 static int
