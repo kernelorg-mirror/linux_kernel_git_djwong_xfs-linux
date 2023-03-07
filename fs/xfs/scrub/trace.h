b79cfc606b9b8457b3016da7cd071e4db74278ef
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:54:25 -0800

    xfs: scrub should set preen if attr leaf has holes
    If an attr block indicates that it could use compaction, set the preen
    flag to have the attr fork rebuilt, since the attr fork rebuilder can
    take care of that for us.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>

--- fs/xfs/scrub/trace.h
+++ fs/xfs/scrub/trace.h
@@ -366,6 +366,7 @@ DEFINE_EVENT(xchk_fblock_error_class, name, \
 
 DEFINE_SCRUB_FBLOCK_ERROR_EVENT(xchk_fblock_error);
 DEFINE_SCRUB_FBLOCK_ERROR_EVENT(xchk_fblock_warning);
+DEFINE_SCRUB_FBLOCK_ERROR_EVENT(xchk_fblock_preen);
 
 #ifdef CONFIG_XFS_QUOTA
 TRACE_EVENT(xchk_qcheck_error,
