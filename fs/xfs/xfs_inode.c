436e526d864a99ba55a09e5cb95e07f4beb521e0
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:54:51 -0800

    xfs: iget for metadata inodes
    Create a xfs_iget_meta function for metadata inodes to ensure that we
    always check that the inobt thinks a metadata inode is in use.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -41,6 +41,7 @@
 #include "xfs_health.h"
 #include "xfs_pnfs.h"
 #include "xfs_inode_util.h"
+#include "xfs_imeta.h"
 
 struct kmem_cache *xfs_inode_cache;
 
@@ -2547,6 +2548,13 @@ xfs_irele(
 {
 	trace_xfs_irele(ip, _RET_IP_);
 	iput(VFS_I(ip));
+}
+
+void
+xfs_imeta_irele(
+	struct xfs_inode	*ip)
+{
+	xfs_irele(ip);
 }
 
 /*
