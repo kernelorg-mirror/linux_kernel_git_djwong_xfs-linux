a02ae1a3173b8a1f23df9053f938cbe8d2d8e49f
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:56:00 -0800

    xfs: allow queued realtime intents to drain before scrubbing
    When a writer thread executes a chain of log intent items for the
    realtime volume, the ILOCKs taken during each step are for each rt
    metadata file, not the entire rt volume itself.  Although scrub takes
    all rt metadata ILOCKs, this isn't sufficient to guard against scrub
    checking the rt volume while that writer thread is in the middle of
    finishing a chain because there's no higher level locking primitive
    guarding the realtime volume.
    
    When there's a collision, cross-referencing between data structures
    (e.g. rtrmapbt and rtrefcountbt) yields false corruption events; if
    repair is running, this results in incorrect repairs, which is
    catastrophic.
    
    Fix this by adding to the mount structure the same drain that we use to
    protect scrub against concurrent AG updates, but this time for the
    realtime volume.
    
    [Contains a few cleanups from hch]
    
    Cc: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_bmap_item.c
+++ fs/xfs/xfs_bmap_item.c
@@ -327,10 +327,8 @@ xfs_bmap_update_get_group(
 {
 	if (xfs_ifork_is_realtime(bi->bi_owner, bi->bi_whichfork)) {
 		if (xfs_has_rtgroups(mp)) {
-			xfs_rgnumber_t	rgno;
-
-			rgno = xfs_rtb_to_rgno(mp, bi->bi_bmap.br_startblock);
-			bi->bi_rtg = xfs_rtgroup_get(mp, rgno);
+			bi->bi_rtg = xfs_rtgroup_intent_get(mp,
+						bi->bi_bmap.br_startblock);
 		} else {
 			bi->bi_rtg = NULL;
 		}
@@ -366,8 +364,9 @@ xfs_bmap_update_put_group(
 	struct xfs_bmap_intent	*bi)
 {
 	if (xfs_ifork_is_realtime(bi->bi_owner, bi->bi_whichfork)) {
-		if (xfs_has_rtgroups(bi->bi_owner->i_mount))
-			xfs_rtgroup_put(bi->bi_rtg);
+		if (xfs_has_rtgroups(bi->bi_owner->i_mount)) {
+			xfs_rtgroup_intent_put(bi->bi_rtg);
+		}
 		return;
 	}
 
--- fs/xfs/xfs_extfree_item.c
+++ fs/xfs/xfs_extfree_item.c
@@ -475,14 +475,12 @@ xfs_extent_free_defer_add(
 	else
 		optype = XFS_DEFER_OPS_TYPE_FREE;
 
-	if (xfs_efi_is_realtime(xefi)) {
-		xfs_rgnumber_t		rgno;
-
-		rgno = xfs_rtb_to_rgno(mp, xefi->xefi_startblock);
-		xefi->xefi_rtg = xfs_rtgroup_get(mp, rgno);
-	} else {
-		xefi->xefi_pag = xfs_perag_intent_get(mp, xefi->xefi_startblock);
-	}
+	if (xfs_efi_is_realtime(xefi))
+		xefi->xefi_rtg = xfs_rtgroup_intent_get(mp,
+						xefi->xefi_startblock);
+	else
+		xefi->xefi_pag = xfs_perag_intent_get(mp,
+						xefi->xefi_startblock);
 	*dfpp = xfs_defer_add(tp, optype, &xefi->xefi_list);
 }
 
@@ -614,11 +612,8 @@ xfs_efi_recover_work(
 	xefi->xefi_agresv = XFS_AG_RESV_NONE;
 	xefi->xefi_owner = XFS_RMAP_OWN_UNKNOWN;
 	if (isrt) {
-		xfs_rgnumber_t		rgno;
-
 		xefi->xefi_flags |= XFS_EFI_REALTIME;
-		rgno = xfs_rtb_to_rgno(mp, extp->ext_start);
-		xefi->xefi_rtg = xfs_rtgroup_get(mp, rgno);
+		xefi->xefi_rtg = xfs_rtgroup_intent_get(mp, extp->ext_start);
 	} else {
 		xefi->xefi_pag = xfs_perag_intent_get(mp, extp->ext_start);
 	}
@@ -775,7 +770,7 @@ xfs_rtextent_free_cancel_item(
 {
 	struct xfs_extent_free_item	*xefi = xefi_entry(item);
 
-	xfs_rtgroup_put(xefi->xefi_rtg);
+	xfs_rtgroup_intent_put(xefi->xefi_rtg);
 	kmem_cache_free(xfs_extfree_item_cache, xefi);
 }
 
