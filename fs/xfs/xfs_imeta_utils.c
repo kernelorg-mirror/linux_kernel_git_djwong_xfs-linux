8d532612c7ba12be15421e2497b239be2d5a7d6c
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:54:59 -0800

    xfs: record health problems with the metadata directory
    Make a report to the health monitoring subsystem any time we encounter
    something in the metadata directory tree that looks like corruption.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_imeta_utils.c
+++ fs/xfs/xfs_imeta_utils.c
@@ -23,6 +23,7 @@
 #include "xfs_imeta_utils.h"
 #include "xfs_trace.h"
 #include "xfs_parent.h"
+#include "xfs_health.h"
 
 /* Initialize a metadata update structure. */
 static inline int
@@ -47,8 +48,10 @@ xfs_imeta_init(
 	 * /quota already exists.
 	 */
 	error = xfs_imeta_dir_parent(mp, upd->path, &upd->dp);
-	if (error == -ENOENT)
+	if (error == -ENOENT) {
+		xfs_fs_mark_sick(mp, XFS_SICK_FS_METADIR);
 		return -EFSCORRUPTED;
+	}
 	if (error)
 		return error;
 
