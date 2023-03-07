5fd45ff33160a7889c5e5f36c9c5827704383e20
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:53:27 -0800

    xfs: repair dquots based on live quotacheck results
    Use the shadow quota counters that live quotacheck creates to reset the
    incore dquot counters.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/trace.h
+++ fs/xfs/scrub/trace.h
@@ -1823,6 +1823,7 @@ DEFINE_EVENT(xrep_dquot_class, name, \
 	TP_ARGS(mp, type, id))
 DEFINE_XREP_DQUOT_EVENT(xrep_dquot_item);
 DEFINE_XREP_DQUOT_EVENT(xrep_disk_dquot);
+DEFINE_XREP_DQUOT_EVENT(xrep_quotacheck_dquot);
 #endif /* CONFIG_XFS_QUOTA */
 
 #endif /* IS_ENABLED(CONFIG_XFS_ONLINE_REPAIR) */
