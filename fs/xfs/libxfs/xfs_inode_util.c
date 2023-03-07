ca71b0c0faeb8bde2386c8330b6c7725fb3b89e9
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:54:55 -0800

    xfs: read and write metadata inode directory
    Plumb in the bits we need to look up metadata inode numbers from the
    metadata inode directory and save them back.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/libxfs/xfs_inode_util.c
+++ fs/xfs/libxfs/xfs_inode_util.c
@@ -23,6 +23,7 @@
 #include "xfs_ag.h"
 #include "xfs_iunlink_item.h"
 #include "xfs_inode_item.h"
+#include "xfs_imeta.h"
 
 uint16_t
 xfs_flags2diflags(
@@ -632,6 +633,7 @@ xfs_droplink(
 	if (inode->i_nlink != XFS_NLINK_PINNED)
 		drop_nlink(VFS_I(ip));
 
+	xfs_imeta_droplink(ip);
 	xfs_trans_log_inode(tp, ip, XFS_ILOG_CORE);
 
 	if (VFS_I(ip)->i_nlink)
