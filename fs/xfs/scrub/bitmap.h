3df22584d228b18b7bcbd2b28e22a629a627d186
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:54:33 -0800

    xfs: repair AGI unlinked inode bucket lists
    Teach the AGI repair code to rebuild the unlinked buckets and lists.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/bitmap.h
+++ fs/xfs/scrub/bitmap.h
@@ -211,4 +211,58 @@ xdab_bitmap_test(
 	return ret;
 }
 
+/* Bitmaps, but for type-checked for xfs_agino_t */
+
+struct xagino_bitmap {
+	struct xbitmap	aginobitmap;
+};
+
+static inline void xagino_bitmap_init(struct xagino_bitmap *bitmap)
+{
+	xbitmap_init(&bitmap->aginobitmap);
+}
+
+static inline void xagino_bitmap_destroy(struct xagino_bitmap *bitmap)
+{
+	xbitmap_destroy(&bitmap->aginobitmap);
+}
+
+static inline int xagino_bitmap_clear(struct xagino_bitmap *bitmap,
+		xfs_agino_t agino, unsigned int len)
+{
+	return xbitmap_clear(&bitmap->aginobitmap, agino, len);
+}
+
+static inline int xagino_bitmap_set(struct xagino_bitmap *bitmap,
+		xfs_agino_t agino, unsigned int len)
+{
+	return xbitmap_set(&bitmap->aginobitmap, agino, len);
+}
+
+static inline bool
+xagino_bitmap_test(
+	struct xagino_bitmap	*bitmap,
+	xfs_agino_t		agino,
+	unsigned int		*len)
+{
+	uint64_t		biglen = *len;
+	bool			ret;
+
+	ret = xbitmap_test(&bitmap->aginobitmap, agino, &biglen);
+
+	if (agino + biglen >= UINT_MAX) {
+		ASSERT(0);
+		biglen = UINT_MAX - agino;
+	}
+
+	*len = biglen;
+	return ret;
+}
+
+static inline int xagino_bitmap_walk(struct xagino_bitmap *bitmap,
+		xbitmap_walk_fn fn, void *priv)
+{
+	return xbitmap_walk(&bitmap->aginobitmap, fn, priv);
+}
+
 #endif	/* __XFS_SCRUB_BITMAP_H__ */
