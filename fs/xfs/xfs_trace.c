5ae2c286b327bb62fa44da916a8b0dff45a1be42
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:18 -0800

    xfs: create incore realtime group structures
    Create an incore object that will contain information about a realtime
    allocation group.  This will eventually enable us to shard the realtime
    section in a similar manner to how we shard the data section.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_trace.c
+++ fs/xfs/xfs_trace.c
@@ -44,6 +44,7 @@
 #include "xfs_xchgrange.h"
 #include "xfs_parent.h"
 #include "xfs_imeta.h"
+#include "xfs_rtgroup.h"
 
 /*
  * We include this last to have the helpers above available for the trace
