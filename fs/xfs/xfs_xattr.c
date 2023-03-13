cf8159eac9037afb22790d42ac5eeaf254655aeb
Andrey Albershteyn <aalbersh@redhat.com>
2023-03-13 21:26:22 +0100

    xfs: add attribute type for fs-verity
    The Merkle tree blocks and descriptor are stored in the extended
    attributes of the inode. Add new attribute type for fs-verity
    metadata. Add XFS_ATTR_INTERNAL_MASK to skip parent pointer and
    fs-verity attributes as those are only for internal use. While we're
    at it add a few comments in relevant places that internally visible
    attributes are not suppose to be handled via interface defined in
    xfs_xattr.c.
    
    Signed-off-by: Andrey Albershteyn <aalbersh@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_xattr.c
+++ fs/xfs/xfs_xattr.c
@@ -21,6 +21,13 @@
 #include <linux/posix_acl_xattr.h>
 
 /*
+ * This file defines interface to work with externally visible extended
+ * attributes, such as those in user, system or security namespaces. This
+ * interface should not be used for internally used attributes (consider
+ * xfs_attr.c).
+ */
+
+/*
  * Get permission to use log-assisted atomic exchange of file extents.
  *
  * Callers must not be running any transactions or hold any inode locks, and
@@ -243,6 +250,9 @@ xfs_xattr_put_listent(
 	int prefix_len;
 
 	ASSERT(context->count >= 0);
+
+	if (flags & XFS_ATTR_INTERNAL_MASK)
+		return;
 
 	if (flags & XFS_ATTR_ROOT) {
 #ifdef CONFIG_XFS_POSIX_ACL
