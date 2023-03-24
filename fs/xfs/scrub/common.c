de4be3ed9e95efc14a37b522ca216406108f6326
Darrick J. Wong <djwong@kernel.org>
2023-03-24 13:34:19 -0700

    xfs: track file link count updates during live nlinks fsck
    Create the necessary hooks in the file create/unlink/rename code so that
    our live nlink scrub code can stay up to date with the rest of the
    filesystem.  This will be the means to keep our shadow link count
    information up to date while the scan runs in real time.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/common.c
+++ fs/xfs/scrub/common.c
@@ -966,3 +966,23 @@ xchk_start_reaping(
 	}
 	sc->flags &= ~XCHK_REAPING_DISABLED;
 }
+
+/*
+ * Enable filesystem hooks (i.e. runtime code patching) before starting a scrub
+ * operation.  Callers must not hold any locks that intersect with the CPU
+ * hotplug lock (e.g. writeback locks) because code patching must halt the CPUs
+ * to change kernel code.
+ */
+void
+xchk_fshooks_enable(
+	struct xfs_scrub	*sc,
+	unsigned int		scrub_fshooks)
+{
+	ASSERT(!(scrub_fshooks & ~XCHK_FSHOOKS_ALL));
+	ASSERT(!(sc->flags & scrub_fshooks));
+
+	if (scrub_fshooks & XCHK_FSHOOKS_DIRENTS)
+		xfs_dirent_hook_enable();
+
+	sc->flags |= scrub_fshooks;
+}
