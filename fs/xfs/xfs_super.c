f09616048402393dddd95e43cc5cea3080423033
Allison Henderson <allison.henderson@oracle.com>
2023-05-10 10:53:55 -0700

    xfs: Add the parent pointer support to the superblock version 5.
    Add the parent pointer superblock flag so that we can actually mount
    filesystems with this feature enabled.
    
    Signed-off-by: Mark Tinguely <tinguely@sgi.com>
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -1705,6 +1705,10 @@ xfs_fs_fill_super(
 		goto out_filestream_unmount;
 	}
 
+	if (xfs_has_parent(mp))
+		xfs_alert(mp,
+	"EXPERIMENTAL parent pointer feature enabled. Use at your own risk!");
+
 	error = xfs_mountfs(mp);
 	if (error)
 		goto out_filestream_unmount;
