023878aef53c5142117f12bc0f868cad68c3937f
Darrick J. Wong <djwong@kernel.org>
2023-07-06 14:31:43 -0700

    xfs: fix corruptions in the directory tree
    Repair corruptions in the directory tree itself.  Cycles are broken by
    removing an incoming parent->child link.  Multiply-owned directories are
    fixed by pruning the extra parent -> child links  Disconnected subtrees
    are reconnected to the lost and found.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/orphanage.h
+++ fs/xfs/scrub/orphanage.h
@@ -52,6 +52,14 @@ struct xrep_adoption {
 	/* Block reservations for orphanage and child (if directory). */
 	unsigned int		orphanage_blkres;
 	unsigned int		child_blkres;
+
+	/*
+	 * Does the caller want us to bump the child link count?  This is not
+	 * needed when reattaching files that have become disconnected but have
+	 * nlink > 1.  It is necessary when changing the directory tree
+	 * structure.
+	 */
+	bool			bump_child_nlink:1;
 };
 
 int xrep_adoption_init(struct xfs_scrub *sc, struct xrep_adoption *adopt);
