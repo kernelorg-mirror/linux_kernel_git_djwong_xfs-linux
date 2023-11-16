19df8e0d321f7e91b94d97a8bc018d572063e426
Darrick J. Wong <djwong@kernel.org>
2023-11-16 14:33:44 -0800

    xfs: reuse xfs_refcount_update_cancel_item
    Reuse xfs_refcount_update_cancel_item to put the AG/RTG and free the
    item in a few places that currently open code the logic.
    
    Inspired-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_refcount_item.c
+++ fs/xfs/xfs_refcount_item.c
@@ -376,6 +376,17 @@ xfs_refcount_update_put_group(
 	xfs_perag_intent_put(ri->ri_pag);
 }
 
+/* Cancel a deferred refcount update. */
+STATIC void
+xfs_refcount_update_cancel_item(
+	struct list_head		*item)
+{
+	struct xfs_refcount_intent	*ri = ci_entry(item);
+
+	xfs_refcount_update_put_group(ri);
+	kmem_cache_free(xfs_refcount_intent_cache, ri);
+}
+
 /* Process a deferred refcount update. */
 STATIC int
 xfs_refcount_update_finish_item(
@@ -397,8 +408,7 @@ xfs_refcount_update_finish_item(
 		return -EAGAIN;
 	}
 
-	xfs_refcount_update_put_group(ri);
-	kmem_cache_free(xfs_refcount_intent_cache, ri);
+	xfs_refcount_update_cancel_item(item);
 	return error;
 }
 
@@ -408,17 +418,6 @@ xfs_refcount_update_abort_intent(
 	struct xfs_log_item		*intent)
 {
 	xfs_cui_release(CUI_ITEM(intent));
-}
-
-/* Cancel a deferred refcount update. */
-STATIC void
-xfs_refcount_update_cancel_item(
-	struct list_head		*item)
-{
-	struct xfs_refcount_intent	*ri = ci_entry(item);
-
-	xfs_refcount_update_put_group(ri);
-	kmem_cache_free(xfs_refcount_intent_cache, ri);
 }
 
 const struct xfs_defer_op_type xfs_refcount_update_defer_type = {
