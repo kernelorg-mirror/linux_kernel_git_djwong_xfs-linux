6b40cbdb0348d7c82b1729fba6d5352da1b949d3
Darrick J. Wong <djwong@kernel.org>
2023-11-22 13:39:25 -0800

    xfs: move ->iop_recover to xfs_defer_op_type
    Finish off the series by moving the intent item recovery function
    pointer to the xfs_defer_op_type struct, since this is really a deferred
    work function now.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_log_recover.c
+++ fs/xfs/xfs_log_recover.c
@@ -2568,7 +2568,6 @@ xlog_recover_process_intents(
 
 	list_for_each_entry_safe(dfp, n, &log->l_dfops, dfp_list) {
 		struct xfs_log_item	*lip = dfp->dfp_intent;
-		const struct xfs_item_ops *ops = lip->li_ops;
 
 		ASSERT(xlog_item_is_intent(lip));
 
@@ -2587,12 +2586,10 @@ xlog_recover_process_intents(
 		 *
 		 * The recovery function can free @dfp.
 		 */
-		error = ops->iop_recover(dfp, &capture_list);
-		if (error) {
-			trace_xlog_intent_recovery_failed(log->l_mp, error,
-					ops->iop_recover);
+		error = xfs_defer_finish_recovery(log->l_mp, dfp,
+				&capture_list);
+		if (error)
 			break;
-		}
 	}
 	if (error)
 		goto err;
