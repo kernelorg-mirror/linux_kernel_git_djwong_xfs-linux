41115e5cb887a132ac027eece88582753689bceb
Darrick J. Wong <djwong@kernel.org>
2023-12-03 09:19:44 -0800

    xfs: fix 32-bit truncation in xfs_compute_rextslog
    It's quite reasonable that some customer somewhere will want to
    configure a realtime volume with more than 2^32 extents.  If they try to
    do this, the highbit32() call will truncate the upper bits of the
    xfs_rtbxlen_t and produce the wrong value for rextslog.  This in turn
    causes the rsumlevels to be wrong, which results in a realtime summary
    file that is the wrong length.  Fix that.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/libxfs/xfs_rtbitmap.c
+++ fs/xfs/libxfs/xfs_rtbitmap.c
@@ -1132,14 +1132,14 @@ xfs_rtbitmap_blockcount(
 
 /*
  * Compute the maximum level number of the realtime summary file, as defined by
- * mkfs.  The use of highbit32 on a 64-bit quantity is a historic artifact that
- * prohibits correct use of rt volumes with more than 2^32 extents.
+ * mkfs.  The historic use of highbit32 on a 64-bit quantity prohibited correct
+ * use of rt volumes with more than 2^32 extents.
  */
 uint8_t
 xfs_compute_rextslog(
 	xfs_rtbxlen_t		rtextents)
 {
-	return rtextents ? xfs_highbit32(rtextents) : 0;
+	return rtextents ? xfs_highbit64(rtextents) : 0;
 }
 
 /*
