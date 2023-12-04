d67085fee7a44977e2823d04b366b7bd90f72ffe
Darrick J. Wong <djwong@kernel.org>
2023-12-04 12:13:26 -0800

    xfs: document what LARP means
    Christoph requested a blurb somewhere explaining exactly what LARP
    means.  I don't know of a good place other than the source code (debug
    knobs aren't covered in Documentation/), so here it is.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_sysfs.c
+++ fs/xfs/xfs_sysfs.c
@@ -229,6 +229,13 @@ pwork_threads_show(
 }
 XFS_SYSFS_ATTR_RW(pwork_threads);
 
+/*
+ * The "LARP" (Logged extended Attribute Recovery Persistence) debugging knob
+ * sets the XFS_DA_OP_LOGGED flag on all xfs_attr_set operations performed on
+ * V5 filesystems.  As a result, the intermediate progress of all setxattr and
+ * removexattr operations are tracked via the log and can be restarted during
+ * recovery.
+ */
 static ssize_t
 larp_store(
 	struct kobject	*kobject,
