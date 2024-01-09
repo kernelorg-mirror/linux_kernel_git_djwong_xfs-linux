17f10a45c7c514c904d6df567e9478bcf57b2008
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:41:58 -0800

    xfs: implement live updates for parent pointer repairs
    While we're scanning the filesystem for dirents that we can turn into
    parent pointers, we cannot hold the IOLOCK or ILOCK of the file being
    repaired.  Therefore, we need to set up a dirent hook so that we can
    keep the temporary file's parent pionters up to date with the rest of
    the filesystem.  Hence we add the ability to *remove* pptrs from the
    temporary file.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/trace.h
+++ fs/xfs/scrub/trace.h
@@ -2814,6 +2814,7 @@ DEFINE_EVENT(xrep_pptr_class, name, \
 DEFINE_XREP_PPTR_EVENT(xrep_xattr_replay_parentadd);
 DEFINE_XREP_PPTR_EVENT(xrep_xattr_replay_parentremove);
 DEFINE_XREP_PPTR_EVENT(xrep_parent_replay_parentadd);
+DEFINE_XREP_PPTR_EVENT(xrep_parent_replay_parentremove);
 
 DECLARE_EVENT_CLASS(xrep_pptr_scan_class,
 	TP_PROTO(struct xfs_inode *ip, const struct xfs_inode *dp,
@@ -2849,6 +2850,7 @@ DEFINE_EVENT(xrep_pptr_scan_class, name, \
 		 const struct xfs_name *name), \
 	TP_ARGS(ip, dp, name))
 DEFINE_XREP_PPTR_SCAN_EVENT(xrep_parent_stash_parentadd);
+DEFINE_XREP_PPTR_SCAN_EVENT(xrep_parent_stash_parentremove);
 
 TRACE_EVENT(xrep_nlinks_set_record,
 	TP_PROTO(struct xfs_mount *mp, xfs_ino_t ino,
