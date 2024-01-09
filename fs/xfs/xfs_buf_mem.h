722ba4d53ffe60557452232cd0a94258c5f0d0e1
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:40:03 -0800

    xfs: connect in-memory btrees to xfiles
    Add to our stubbed-out in-memory btrees the ability to connect them with
    an actual in-memory backing file (aka xfiles) and the necessary pieces
    to track free space in the xfile and flush dirty xfbtree buffers on
    demand, which we'll need for online repair.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_buf_mem.h
+++ fs/xfs/xfs_buf_mem.h
@@ -22,6 +22,8 @@ void xmbuf_free(struct xmbuf *xmb);
 int xmbuf_map_page(struct xfs_buf *bp);
 void xmbuf_unmap_page(struct xfs_buf *bp);
 bool xmbuf_verify_daddr(struct xfs_buftarg *btp, xfs_daddr_t daddr);
+void xmbuf_trans_bdetach(struct xfs_trans *tp, struct xfs_buf *bp);
+int xmbuf_finalize(struct xfs_buf *bp);
 #else
 # define xmbuf_map_page(...)		(-ENOMEM)
 # define xmbuf_unmap_page(...)		((void)0)
