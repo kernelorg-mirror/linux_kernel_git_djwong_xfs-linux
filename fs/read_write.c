50587e857c24c82c69cc8da5e98634106593aa32
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:40:22 -0800

    xfs: introduce new file range exchange ioctl
    Introduce a new ioctl to handle swapping ranges of bytes between files.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/read_write.c
+++ fs/read_write.c
@@ -1667,6 +1667,7 @@ int generic_write_check_limits(struct file *file, loff_t pos, loff_t *count)
 
 	return 0;
 }
+EXPORT_SYMBOL(generic_write_check_limits);
 
 /* Like generic_write_checks(), but takes size of write instead of iter. */
 int generic_write_checks_count(struct kiocb *iocb, loff_t *count)
@@ -1735,3 +1736,4 @@ int generic_file_rw_checks(struct file *file_in, struct file *file_out)
 
 	return 0;
 }
+EXPORT_SYMBOL(generic_file_rw_checks);
