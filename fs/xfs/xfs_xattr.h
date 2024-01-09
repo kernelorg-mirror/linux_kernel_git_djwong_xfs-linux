fab80d9d8c75565c16228d0c056d19621527e9c5
Allison Henderson <allison.henderson@oracle.com>
2024-01-09 09:41:36 -0800

    xfs: parent pointer attribute creation
    Add parent pointer attribute during xfs_create, and subroutines to
    initialize attributes.  Note that the xfs_attr_intent object contains a
    pointer to the caller's xfs_da_args object, so the latter must persist
    until transaction commit.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    [djwong: shorten names, adjust to new format, set init_xattrs for parent
    pointers]
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/xfs_xattr.h
+++ fs/xfs/xfs_xattr.h
@@ -8,6 +8,8 @@
 
 enum xfs_attr_update;
 int xfs_attr_change(struct xfs_da_args *args, enum xfs_attr_update op);
+int xfs_attr_grab_log_assist(struct xfs_mount *mp);
+void xfs_attr_rele_log_assist(struct xfs_mount *mp);
 
 extern const struct xattr_handler * const xfs_xattr_handlers[];
 
