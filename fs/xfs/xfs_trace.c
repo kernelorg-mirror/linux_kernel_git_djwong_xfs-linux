a25c33181b3905348ef944cf6e1914ce2e19af99
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:42:26 -0800

    xfs: create imeta abstractions to get and set metadata inodes
    Create some helper routines to get and set metadata inode numbers
    instead of open-coding them throughout xfs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_trace.c
+++ fs/xfs/xfs_trace.c
@@ -42,6 +42,7 @@
 #include "xfs_exchmaps.h"
 #include "xfs_exchrange.h"
 #include "xfs_parent.h"
+#include "xfs_imeta.h"
 
 /*
  * We include this last to have the helpers above available for the trace
