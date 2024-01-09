85193ce82e7e693768be0e34e0f917af63ab30f4
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:45:11 -0800

    xfs: create a new inode flag to require extsize alignment of file data space
    Add a new inode flag to require that all file data extent mappings must
    be aligned (both the file offset range and the allocated space itself)
    to the extent size hint.  Having a separate COW extent size hint is no
    longer allowed.
    
    The goal here is to enable sysadmins and users to mandate that all space
    mappings in a file must have a startoff/blockcount that are aligned to
    (say) a 2MB alignment and that the startblock/blockcount will follow the
    same alignment.
    
    Co-developed-by: John Garry <john.g.garry@oracle.com>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -320,6 +320,7 @@ typedef struct xfs_mount {
 #define XFS_FEAT_METADIR	(1ULL << 27)	/* metadata directory tree */
 #define XFS_FEAT_RTGROUPS	(1ULL << 28)	/* realtime groups */
 #define XFS_FEAT_VERITY		(1ULL << 29)	/* fs-verity */
+#define XFS_FEAT_FORCEALIGN	(1ULL << 30)	/* aligned file data extents */
 
 /* Mount features */
 #define XFS_FEAT_ADD_LOG_FEAT	(1ULL << 47)	/* can add log incompat features */
@@ -387,6 +388,7 @@ __XFS_HAS_FEAT(large_extent_counts, NREXT64)
 __XFS_HAS_FEAT(metadir, METADIR)
 __XFS_HAS_FEAT(rtgroups, RTGROUPS)
 __XFS_HAS_FEAT(verity, VERITY)
+__XFS_HAS_FEAT(forcealign, FORCEALIGN)
 
 bool xfs_can_add_incompat_log_features(struct xfs_mount *mp, bool want_audit);
 
