d39eaa53cce371d6a9609e6f71b8ca8f1e04b7a1
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:40:53 -0800

    xfs: create a blob array data structure
    Create a simple 'blob array' data structure for storage of arbitrarily
    sized metadata objects that will be used to reconstruct metadata.  For
    the intended usage (temporarily storing extended attribute names and
    values) we only have to support storing objects and retrieving them.
    Use the xfile abstraction to store the attribute information in memory
    that can be swapped out.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/trace.h
+++ fs/xfs/scrub/trace.h
@@ -962,6 +962,7 @@ DEFINE_XFILE_EVENT(xfile_pwrite);
 DEFINE_XFILE_EVENT(xfile_seek_data);
 DEFINE_XFILE_EVENT(xfile_get_page);
 DEFINE_XFILE_EVENT(xfile_put_page);
+DEFINE_XFILE_EVENT(xfile_discard);
 
 TRACE_EVENT(xfarray_create,
 	TP_PROTO(struct xfarray *xfa, unsigned long long required_capacity),
