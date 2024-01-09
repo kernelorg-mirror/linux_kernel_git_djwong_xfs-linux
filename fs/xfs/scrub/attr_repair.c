be45d8b95abdb62d4d1e13e873e84e37928be978
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:42:00 -0800

    xfs: actually rebuild the parent pointer xattrs
    Once we've assembled all the parent pointers for a file, we need to
    commit the new dataset atomically to that file.  Parent pointer records
    are embedded in the xattr structure, which means that we must write a
    new extended attribute structure, again, atomically.  Therefore, we must
    copy the non-parent-pointer attributes from the file being repaired into
    the temporary file's extended attributes and then call the atomic extent
    swap mechanism to exchange the blocks.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/attr_repair.c
+++ fs/xfs/scrub/attr_repair.c
@@ -1033,7 +1033,7 @@ xrep_xattr_reset_fork(
  * fork.  The caller must ILOCK the tempfile and join it to the transaction.
  * This function returns with the inode joined to a clean scrub transaction.
  */
-STATIC int
+int
 xrep_xattr_reset_tempfile_fork(
 	struct xfs_scrub	*sc)
 {
@@ -1354,7 +1354,7 @@ xrep_xattr_swap_prep(
 }
 
 /* Exchange the temporary file's attribute fork with the one being repaired. */
-STATIC int
+int
 xrep_xattr_swap(
 	struct xfs_scrub	*sc,
 	struct xrep_tempexch	*tx)
