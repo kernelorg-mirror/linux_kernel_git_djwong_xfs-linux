9ecbe44369a321eb64fa2005fc7c740852dcfd08
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:42:54 -0800

    xfs: reduce rt summary file levels for rtgroups filesystems
    The rt summary file is supposed to be large enough to track the number
    of log2(rtextentcount) free space extents that start in a given rt
    bitmap block.  Prior to rt groups, there could be a single 2^52 block
    free extent, which implies a summary file with 53 levels.
    
    However, each rtgroup uses its first rt extent to hold a superblock,
    so there can't be any free extents longer than the length of a group.
    Groups are limited to 2^32-1 blocks, which means that the longest
    freespace will be counted in level 31.  Hence we only need 32 levels.
    
    Adjust the rextslog computation to create smaller rt summary files for
    rtgroups filesystems.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/rtsummary.c
+++ fs/xfs/scrub/rtsummary.c
@@ -93,7 +93,7 @@ xchk_setup_rtsummary(
 		int		rextslog;
 
 		rts->rextents = xfs_rtb_to_rtx(mp, mp->m_sb.sb_rblocks);
-		rextslog = xfs_compute_rextslog(rts->rextents);
+		rextslog = xfs_compute_rextslog(&mp->m_sb, rts->rextents);
 		rts->rsumlevels = rextslog + 1;
 		rts->rbmblocks = xfs_rtbitmap_blockcount(mp, rts->rextents);
 		rsumblocks = xfs_rtsummary_blockcount(mp, rts->rsumlevels,
