60750f2a9fd4f426479285f7bbde2e36d2de1bbd
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:44:36 -0800

    xfs: enable extent size hints for CoW operations
    Wire up the copy-on-write extent size hint for realtime files, and
    connect it to the rt allocator so that we avoid fragmentation on rt
    filesystems.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -2024,7 +2024,10 @@ xfs_bmap_rtalloc(
 	};
 	int			error;
 
-	align = xfs_get_extsz_hint(ap->ip);
+	if (ap->flags & XFS_BMAPI_COWFORK)
+		align = xfs_get_cowextsz_hint(ap->ip);
+	else
+		align = xfs_get_extsz_hint(ap->ip);
 retry:
 	error = xfs_bmap_extsize_align(mp, &ap->got, &ap->prev,
 					align, 1, ap->eof, 0,
