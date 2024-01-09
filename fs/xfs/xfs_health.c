0b8eaf80eabaeb3533fb33bd58c266cf1c6cd41f
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:40:02 -0800

    xfs: support in-memory btrees
    Adapt the generic btree cursor code to be able to create a btree whose
    buffers come from a (presumably in-memory) buftarg with a header block
    that's specific to in-memory btrees.  We'll connect this to other parts
    of online scrub in the next patches.
    
    Note that in-memory btrees always have a block size matching the system
    memory page size for efficiency reasons.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_health.c
+++ fs/xfs/xfs_health.c
@@ -512,6 +512,9 @@ xfs_btree_mark_sick(
 {
 	unsigned int			mask;
 
+	if (cur->bc_ops->type == XFS_BTREE_TYPE_MEM)
+		return;
+
 	switch (cur->bc_btnum) {
 	case XFS_BTNUM_BMAP:
 		xfs_bmap_mark_sick(cur->bc_ino.ip, cur->bc_ino.whichfork);
