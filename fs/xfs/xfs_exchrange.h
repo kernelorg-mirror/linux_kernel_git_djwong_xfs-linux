c74411306bd0fd9ac9a22f2cb6af630c9f7bf264
Darrick J. Wong <djwong@kernel.org>
2024-01-09 09:40:22 -0800

    xfs: introduce new file range exchange ioctls
    Introduce a pair of new ioctls to handle exchanging ranges of bytes
    between files.  The goal here is to perform the exchange atomically with
    respect to applications -- either they see the file contents before the
    exchange or they see that A-B is now B-A, even if the kernel crashes.
    
    The simpler of the two ioctls is XFS_IOC_EXCHANGE_RANGE, which performs
    the exchange unconditionally.  XFS_IOC_COMMIT_RANGE, on the other hand,
    requires the caller to sample the file attributes of one of the files
    participating in the exchange, and aborts the exchange if that file has
    changed in the meantime (presumably by another thread).
    
    For userspace programs the goal is to enable defragmentation of files
    (it's a replacement for the old swapext ioctl), the atomic commit of
    sparse updates to an indexed data file, or emulation of atomic writes on
    software defined storage servers.
    
    My original goal with all this code was to make it so that online repair
    can build a replacement directory or xattr structure in a temporary file
    and commit the repair by atomically exchanging all the data blocks
    between the two files.  However, I needed a way to test this mechanism
    thoroughly, so I've been evolving an ioctl interface since then.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

---
+++ fs/xfs/xfs_exchrange.h
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2020-2024 Oracle.  All Rights Reserved.
+ * Author: Darrick J. Wong <djwong@kernel.org>
+ */
+#ifndef __XFS_EXCHRANGE_H__
+#define __XFS_EXCHRANGE_H__
+
+/* Update the mtime/cmtime of file1 and file2 */
+#define __XFS_EXCHRANGE_UPD_CMTIME1	(1ULL << 63)
+#define __XFS_EXCHRANGE_UPD_CMTIME2	(1ULL << 62)
+
+/* Freshness check required */
+#define __XFS_EXCHRANGE_CHECK_FRESH2	(1ULL << 61)
+
+#define XFS_EXCHRANGE_PRIVATE_FLAGS	(__XFS_EXCHRANGE_UPD_CMTIME1 | \
+					 __XFS_EXCHRANGE_UPD_CMTIME2 | \
+					 __XFS_EXCHRANGE_CHECK_FRESH2)
+
+struct xfs_exchrange {
+	struct file		*file1;
+	struct file		*file2;
+
+	loff_t			file1_offset;
+	loff_t			file2_offset;
+	u64			length;
+
+	u64			flags;	/* XFS_EXCHRANGE flags */
+
+	/* file2 metadata for freshness checks if file1_ino != 0 */
+	u64			file2_ino;
+	struct timespec64	file2_mtime;
+	struct timespec64	file2_ctime;
+};
+
+int xfs_exchange_range(struct xfs_exchrange *fxr);
+
+#endif /* __XFS_EXCHRANGE_H__ */
