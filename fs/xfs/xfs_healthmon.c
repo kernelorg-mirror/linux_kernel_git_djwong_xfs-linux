0bbadbab1a416177d3fcc0a15e04850c7510d7c1
Darrick J. Wong <djwong@kernel.org>
2024-02-08 16:45:45 -0800

    xfs: report media errors through healthmon
    Set up a media error event hook so that we can send events to userspace.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_healthmon.c
+++ fs/xfs/xfs_healthmon.c
@@ -21,6 +21,7 @@
 #include "xfs_fsops.h"
 #include "xfs_health.h"
 #include "xfs_healthmon.h"
+#include "xfs_notify_failure.h"
 
 /*
  * Live Health Monitoring
@@ -153,6 +154,21 @@
  *     "bitmap":     free space bitmap contents for this group
  *     "rmapbt":     reverse mapping btree
  *     "refcountbt": reference count btree
+ *
+ * Media Failures
+ * --------------
+ *
+ * {
+ *	"type": "media",
+ *	"domain": "datadev" | "logdev" | "rtdev",
+ *	"daddr": integer,
+ *	"bbcount": integer,
+ *	"time_ns": integer
+ * }
+ *
+ * The domain element tells us which device reported a media failure.  The
+ * daddr and bbcount elements tell us where inside that device the failure was
+ * observed.
  */
 
 #define XFS_HEALTHMON_MAX_EVENTS \
@@ -180,6 +196,7 @@ struct xfs_healthmon {
 	/* live update hooks */
 	struct xfs_shutdown_hook	shook;
 	struct xfs_health_hook		hhook;
+	struct xfs_media_error_hook	mhook;
 
 	/* filesystem mount, or NULL if we've unmounted */
 	struct xfs_mount		*mp;
@@ -227,9 +244,11 @@ xfs_healthmon_exit(
 	trace_xfs_healthmon_exit(hm->mp, hm->events, hm->lost_prev_event);
 
 	if (hm->mp) {
+		xfs_media_error_hook_del(hm->mp, &hm->mhook);
 		xfs_health_hook_del(hm->mp, &hm->hhook);
 		xfs_shutdown_hook_del(hm->mp, &hm->shook);
 	}
+	xfs_media_error_hook_disable();
 	xfs_health_hook_disable();
 	xfs_shutdown_hook_disable();
 	mutex_destroy(&hm->lock);
@@ -526,6 +545,55 @@ out_unlock:
 	return NOTIFY_DONE;
 }
 
+#if defined(CONFIG_MEMORY_FAILURE) && defined(CONFIG_FS_DAX)
+/* Add a media error event to the reporting queue. */
+STATIC int
+xfs_healthmon_media_error_hook(
+	struct notifier_block		*nb,
+	unsigned long			action,
+	void				*data)
+{
+	struct xfs_healthmon		*hm;
+	struct xfs_healthmon_event	*event;
+	struct xfs_media_error_params	*p = data;
+	struct xfs_mount		*mp = p->btp->bt_mount;
+	enum xfs_healthmon_domain	domain;
+	int				error;
+
+	hm = container_of(nb, struct xfs_healthmon, mhook.error_hook.nb);
+
+	mutex_lock(&hm->lock);
+
+	trace_xfs_healthmon_media_error_hook(hm->mp, p, hm->events,
+			hm->lost_prev_event);
+
+	error = xfs_healthmon_start_live_update(hm);
+	if (error)
+		goto out_unlock;
+
+	if (mp->m_logdev_targp != mp->m_ddev_targp &&
+	    mp->m_logdev_targp == p->btp) {
+		domain = XFS_HEALTHMON_LOGDEV;
+	} else if (mp->m_rtdev_targp == p->btp) {
+		domain = XFS_HEALTHMON_RTDEV;
+	} else {
+		domain = XFS_HEALTHMON_DATADEV;
+	}
+
+	event = new_event(hm, XFS_HEALTHMON_MEDIA_ERROR, domain);
+	if (!event)
+		goto out_unlock;
+
+	event->daddr = p->daddr;
+	event->bbcount = p->bbcount;
+	xfs_healthmon_push(hm, event);
+
+out_unlock:
+	mutex_unlock(&hm->lock);
+	return NOTIFY_DONE;
+}
+#endif
+
 /* Render the health update type as a string. */
 STATIC const char *
 xfs_healthmon_typestring(
@@ -538,6 +606,7 @@ xfs_healthmon_typestring(
 		[XFS_HEALTHMON_SICK]		= "sick",
 		[XFS_HEALTHMON_CORRUPT]		= "corrupt",
 		[XFS_HEALTHMON_HEALTHY]		= "healthy",
+		[XFS_HEALTHMON_MEDIA_ERROR]	= "media",
 	};
 
 	if (event->type >= ARRAY_SIZE(type_strings))
@@ -558,6 +627,9 @@ xfs_healthmon_domstring(
 		[XFS_HEALTHMON_AG]		= "ag",
 		[XFS_HEALTHMON_INODE]		= "inode",
 		[XFS_HEALTHMON_RTGROUP]		= "rtgroup",
+		[XFS_HEALTHMON_DATADEV]		= "datadev",
+		[XFS_HEALTHMON_LOGDEV]		= "logdev",
+		[XFS_HEALTHMON_RTDEV]		= "rtdev",
 	};
 
 	if (event->domain >= ARRAY_SIZE(dom_strings))
@@ -783,6 +855,23 @@ xfs_healthmon_format_inode(
 			event->gen);
 }
 
+/* Render media error as a string set */
+static ssize_t
+xfs_healthmon_format_media_error(
+	struct stdio_redirect		*out,
+	const struct xfs_healthmon_event *event)
+{
+	ssize_t				ret;
+
+	ret = stdio_redirect_printf(out, false, "  \"daddr\":      %llu,\n",
+			event->daddr);
+	if (ret < 0)
+		return ret;
+
+	return stdio_redirect_printf(out, false, "  \"bbcount\":    %llu,\n",
+			event->bbcount);
+}
+
 /* Format an event into json. */
 STATIC int
 xfs_healthmon_format(
@@ -835,6 +924,11 @@ xfs_healthmon_format(
 		break;
 	case XFS_HEALTHMON_INODE:
 		ret = xfs_healthmon_format_inode(out, event);
+		break;
+	case XFS_HEALTHMON_DATADEV:
+	case XFS_HEALTHMON_LOGDEV:
+	case XFS_HEALTHMON_RTDEV:
+		ret = xfs_healthmon_format_media_error(out, event);
 		break;
 	}
 	if (ret < 0)
@@ -942,6 +1036,7 @@ xfs_healthmon_create(
 
 	xfs_shutdown_hook_enable();
 	xfs_health_hook_enable();
+	xfs_media_error_hook_enable();
 
 	xfs_shutdown_hook_setup(&hm->shook, xfs_healthmon_shutdown_hook);
 	ret = xfs_shutdown_hook_add(mp, &hm->shook);
@@ -953,18 +1048,26 @@ xfs_healthmon_create(
 	if (ret)
 		goto out_shutdown;
 
+	xfs_media_error_hook_setup(&hm->mhook, xfs_healthmon_media_error_hook);
+	ret = xfs_media_error_hook_add(mp, &hm->mhook);
+	if (ret)
+		goto out_health;
+
 	ret = run_thread_with_stdout(&hm->thread, &xfs_healthmon_ops);
 	if (ret < 0)
-		goto out_health;
+		goto out_media;
 
 	trace_xfs_healthmon_create(mp, hmo->flags, hmo->format);
 
 	return ret;
+out_media:
+	xfs_media_error_hook_del(mp, &hm->mhook);
 out_health:
 	xfs_health_hook_del(mp, &hm->hhook);
 out_shutdown:
 	xfs_shutdown_hook_del(mp, &hm->shook);
 out_hooks:
+	xfs_media_error_hook_disable();
 	xfs_health_hook_disable();
 	xfs_shutdown_hook_disable();
 	mutex_destroy(&hm->lock);
