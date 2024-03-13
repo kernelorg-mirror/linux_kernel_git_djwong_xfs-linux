dcb75d7e9bf74f64e12e5b78dd179136d8799ec4
Andrey Albershteyn <aalbersh@redhat.com>
2024-03-13 11:45:57 -0700

    xfs: add fs-verity ro-compat flag
    To mark inodes with fs-verity enabled the new XFS_DIFLAG2_VERITY flag
    will be added in further patch. This requires ro-compat flag to let
    older kernels know that fs with fs-verity can not be modified.
    
    Signed-off-by: Andrey Albershteyn <aalbersh@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -313,6 +313,7 @@ typedef struct xfs_mount {
 #define XFS_FEAT_NREXT64	(1ULL << 26)	/* large extent counters */
 #define XFS_FEAT_METADIR	(1ULL << 27)	/* metadata directory tree */
 #define XFS_FEAT_RTGROUPS	(1ULL << 28)	/* realtime groups */
+#define XFS_FEAT_VERITY		(1ULL << 29)	/* fs-verity */
 
 /* Mount features */
 #define XFS_FEAT_ADD_LOG_FEAT	(1ULL << 47)	/* can add log incompat features */
@@ -379,6 +380,7 @@ __XFS_HAS_FEAT(needsrepair, NEEDSREPAIR)
 __XFS_HAS_FEAT(large_extent_counts, NREXT64)
 __XFS_HAS_FEAT(metadir, METADIR)
 __XFS_HAS_FEAT(rtgroups, RTGROUPS)
+__XFS_HAS_FEAT(verity, VERITY)
 
 bool xfs_can_add_incompat_log_features(struct xfs_mount *mp, bool want_audit);
 
