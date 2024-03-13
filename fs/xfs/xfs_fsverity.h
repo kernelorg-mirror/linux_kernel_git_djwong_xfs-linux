0bca025ed5e1f825354139f700cf84777555d6b9
Darrick J. Wong <djwong@kernel.org>
2024-03-13 11:49:37 -0700

    xfs: create an icache tag for files with cached merkle tree blocks
    Create a radix tree tag for the inode cache so that merkle tree block
    shrinkers can find verity inodes quickly.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_fsverity.h
+++ fs/xfs/xfs_fsverity.h
@@ -13,6 +13,9 @@ void xfs_fsverity_cache_destroy(struct xfs_inode *ip);
 int xfs_fsverity_register_shrinker(struct xfs_mount *mp);
 void xfs_fsverity_unregister_shrinker(struct xfs_mount *mp);
 
+struct xfs_icwalk;
+int xfs_fsverity_scan_inode(struct xfs_inode *ip, struct xfs_icwalk *icw);
+
 extern const struct fsverity_operations xfs_fsverity_ops;
 #else
 # define xfs_fsverity_cache_init(ip)		((void)0)
@@ -20,6 +23,7 @@ extern const struct fsverity_operations xfs_fsverity_ops;
 # define xfs_fsverity_cache_destroy(ip)		((void)0)
 # define xfs_fsverity_register_shrinker(mp)	(0)
 # define xfs_fsverity_unregister_shrinker(mp)	((void)0)
+# define xfs_fsverity_scan_inode(ip, icw)	(0)
 #endif	/* CONFIG_FS_VERITY */
 
 #endif	/* __XFS_FSVERITY_H__ */
