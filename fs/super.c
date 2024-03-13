a5a751687ef62a4c9182a5d3e82eaecb97cffec9
Andrey Albershteyn <aalbersh@redhat.com>
2024-03-13 11:45:38 -0700

    fsverity: add per-sb workqueue for post read processing
    For XFS, fsverity's global workqueue is not really suitable due to:
    
    1. High priority workqueues are used within XFS to ensure that data
       IO completion cannot stall processing of journal IO completions.
       Hence using a WQ_HIGHPRI workqueue directly in the user data IO
       path is a potential filesystem livelock/deadlock vector.
    
    2. The fsverity workqueue is global - it creates a cross-filesystem
       contention point.
    
    This patch adds per-filesystem, per-cpu workqueue for fsverity
    work. This allows iomap to add verification work in the read path on
    BIO completion.
    
    Signed-off-by: Andrey Albershteyn <aalbersh@redhat.com>
    [djwong: make it clearer that this workqueue is for verity]
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/super.c
+++ fs/super.c
@@ -637,6 +637,13 @@ void generic_shutdown_super(struct super_block *sb)
 			sb->s_dio_done_wq = NULL;
 		}
 
+#ifdef CONFIG_FS_VERITY
+		if (sb->s_verify_wq) {
+			destroy_workqueue(sb->s_verify_wq);
+			sb->s_verify_wq = NULL;
+		}
+#endif
+
 		if (sop->put_super)
 			sop->put_super(sb);
 
