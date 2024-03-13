7eafd491a859d22e57d78f84af1deba8a7640c40
Darrick J. Wong <djwong@kernel.org>
2024-03-13 11:49:19 -0700

    xfs: create a per-mount shrinker for verity inodes merkle tree blocks
    Create a shrinker for an entire filesystem that will walk the inodes
    looking for inodes that are caching merkle tree blocks, and invoke
    shrink functions on that cache.  The actual details of shrinking merkle
    tree caches are left for subsequent patches.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_fsverity.h
+++ fs/xfs/xfs_fsverity.h
@@ -10,11 +10,16 @@ void xfs_fsverity_cache_init(struct xfs_inode *ip);
 void xfs_fsverity_destroy_inode(struct xfs_inode *ip);
 void xfs_fsverity_cache_destroy(struct xfs_inode *ip);
 
+int xfs_fsverity_register_shrinker(struct xfs_mount *mp);
+void xfs_fsverity_unregister_shrinker(struct xfs_mount *mp);
+
 extern const struct fsverity_operations xfs_fsverity_ops;
 #else
 # define xfs_fsverity_cache_init(ip)		((void)0)
 # define xfs_fsverity_destroy_inode(ip)		((void)0)
 # define xfs_fsverity_cache_destroy(ip)		((void)0)
+# define xfs_fsverity_register_shrinker(mp)	(0)
+# define xfs_fsverity_unregister_shrinker(mp)	((void)0)
 #endif	/* CONFIG_FS_VERITY */
 
 #endif	/* __XFS_FSVERITY_H__ */
