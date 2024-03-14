380fdf642d31c0b0084b705264e3a5ddd37c226a
Darrick J. Wong <djwong@kernel.org>
2024-03-14 11:22:58 -0700

    xfs: don't store trailing zeroes of merkle tree blocks
    As a minor space optimization, don't store trailing zeroes of merkle
    tree blocks to reduce space consumption and copying overhead.  This
    really only affects the rightmost blocks at each level of the tree.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Andrey Albershteyn <aalbersh@redhat.com>

--- fs/xfs/xfs_fsverity.c
+++ fs/xfs/xfs_fsverity.c
@@ -669,11 +669,6 @@ xfs_fsverity_read_merkle(
 	if (error)
 		goto out_new_mk;
 
-	if (!args.valuelen) {
-		error = -ENODATA;
-		goto out_new_mk;
-	}
-
 	mk = xfs_fsverity_cache_store(ip, key, new_mk);
 	if (mk != new_mk) {
 		/*
@@ -724,6 +719,12 @@ xfs_fsverity_write_merkle(
 		.value			= (void *)buf,
 		.valuelen		= size,
 	};
+	const char			*p = buf + size - 1;
+
+	/* Don't store trailing zeroes. */
+	while (p >= (const char *)buf && *p == 0)
+		p--;
+	args.valuelen = p - (const char *)buf + 1;
 
 	xfs_fsverity_init_merkle_args(ip, &name, pos, &args);
 	return xfs_attr_set(&args, XFS_ATTRUPDATE_UPSERT, false);
