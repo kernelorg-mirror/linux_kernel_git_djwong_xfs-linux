22f15aa9b1ffdd00dafebe77a9bc0c7c032bdb6a
Darrick J. Wong <djwong@kernel.org>
2024-03-16 08:51:50 -0700

    fsverity: box up the write_merkle_tree_block parameters too
    Box up the tree write request parameters into a structure so that we can
    add more in the next few patches.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/f2fs/verity.c
+++ fs/f2fs/verity.c
@@ -277,9 +277,12 @@ static struct page *f2fs_read_merkle_tree_page(struct inode *inode,
 	return page;
 }
 
-static int f2fs_write_merkle_tree_block(struct inode *inode, const void *buf,
-					u64 pos, unsigned int size)
+static int f2fs_write_merkle_tree_block(const struct fsverity_writemerkle *req,
+					const void *buf, u64 pos,
+					unsigned int size)
 {
+	struct inode *inode = req->inode;
+
 	pos += f2fs_verity_metadata_pos(inode);
 
 	return pagecache_write(inode, buf, size, pos);
