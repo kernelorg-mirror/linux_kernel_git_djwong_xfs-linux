0cd2b73c7eb7da47cb17fb36901b2fad31573faf
Kent Overstreet <kent.overstreet@linux.dev>
2024-03-25 18:05:55 -0700

    darray: lift from bcachefs
    dynamic arrays - inspired from CCAN darrays, basically c++ stl vectors.
    
    Used by thread_with_stdio, which is also being lifted from bcachefs for
    xfs.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>
    [djwong: remove bcachefs changes to reduce porting overhead]
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

---
+++ lib/darray.c
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * (C) 2022-2024 Kent Overstreet <kent.overstreet@linux.dev>
+ */
+
+#include <linux/darray.h>
+#include <linux/log2.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+
+int __darray_resize_slowpath(darray_char *d, size_t element_size, size_t new_size, gfp_t gfp)
+{
+	if (new_size > d->size) {
+		new_size = roundup_pow_of_two(new_size);
+
+		void *data = kvmalloc_array(new_size, element_size, gfp);
+		if (!data)
+			return -ENOMEM;
+
+		memcpy(data, d->data, d->size * element_size);
+		if (d->data != d->preallocated)
+			kvfree(d->data);
+		d->data	= data;
+		d->size = new_size;
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(__darray_resize_slowpath);
+
+MODULE_AUTHOR("Kent Overstreet");
+MODULE_LICENSE("GPL");
