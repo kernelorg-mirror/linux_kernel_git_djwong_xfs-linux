3b7e0486bdf5537640cecd55b6d3fabb05d2e11c
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:16:12 -0700

    xfs: create event queuing, formatting, and discovery infrastructure
    Create the basic infrastructure that we need to report health events to
    userspace.  We need a compact form for recording critical information
    about an event and queueing them; a means to notice that we've lost some
    events; and a means to format the events into something that userspace
    can handle.
    
    Here, we've chosen json to export information to userspace.  The
    structured key-value nature of json gives us enormous flexibility to
    modify the schema of what we'll send to userspace because we can add new
    keys at any time.  Userspace can use whatever json parsers are available
    to consume the events and will not be confused by keys they don't
    recognize.
    
    Note that we do NOT allow sending json back to the kernel, nor is there
    any intent to do that.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_trace.c
+++ fs/xfs/xfs_trace.c
@@ -48,6 +48,8 @@
 #include "xfs_rtgroup.h"
 #include "xfs_fsrefs.h"
 #include "xfs_fsverity.h"
+#include "xfs_health.h"
+#include "xfs_healthmon.h"
 
 static inline void
 xfs_rmapbt_crack_agno_opdev(
