b56c519d154a741a814ad619242364156edf8ee8
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:15:35 -0700

    xfs: support reflink with force align enabled
    Reuse the "big rt extents" code to do the necessary "COW around"
    behaviors so that we always reflink entire forcealign units.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_bmap_util.c
+++ fs/xfs/xfs_bmap_util.c
@@ -1725,7 +1725,6 @@ out_trans_cancel:
 	goto out_unlock_ilock;
 }
 
-#ifdef CONFIG_XFS_RT
 /*
  * Decide if this is an unwritten extent that isn't aligned to an allocation
  * unit boundary.
@@ -1904,7 +1903,6 @@ xfs_convert_rtbigalloc_file_space(
 
 	return 0;
 }
-#endif /* CONFIG_XFS_RT */
 
 /*
  * Reserve space and quota to this transaction to map in as much free space
