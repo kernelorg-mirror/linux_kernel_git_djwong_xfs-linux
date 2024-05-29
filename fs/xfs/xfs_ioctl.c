4ac127fc55b5d9bbb2b19f626601d40fc5e0a887
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:10:39 -0700

    xfs: introduce new file range commit ioctls
    This patch introduces two more new ioctls to manage atomic updates to
    file contents -- XFS_IOC_START_COMMIT and XFS_IOC_COMMIT_RANGE.  The
    commit mechanism here is exactly the same as what XFS_IOC_EXCHANGE_RANGE
    does, but with the additional requirement that file2 cannot have changed
    since some sampling point.  The start-commit ioctl performs the sampling
    of file attributes.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_ioctl.c
+++ fs/xfs/xfs_ioctl.c
@@ -1507,6 +1507,10 @@ xfs_file_ioctl(
 
 	case XFS_IOC_EXCHANGE_RANGE:
 		return xfs_ioc_exchange_range(filp, arg);
+	case XFS_IOC_START_COMMIT:
+		return xfs_ioc_start_commit(filp, arg);
+	case XFS_IOC_COMMIT_RANGE:
+		return xfs_ioc_commit_range(filp, arg);
 
 	default:
 		return -ENOTTY;
