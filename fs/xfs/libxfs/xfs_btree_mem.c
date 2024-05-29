daddc9be54b728c37e4c95c7c8adce5e7b5e1092
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:12:58 -0700

    xfs: create a shadow rmap btree during realtime rmap repair
    Create an in-memory btree of rmap records instead of an array.  This
    enables us to do live record collection instead of freezing the fs.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/libxfs/xfs_btree_mem.c
+++ fs/xfs/libxfs/xfs_btree_mem.c
@@ -18,6 +18,7 @@
 #include "xfs_ag.h"
 #include "xfs_buf_item.h"
 #include "xfs_trace.h"
+#include "xfs_rtgroup.h"
 
 /* Set the root of an in-memory btree. */
 void
@@ -60,6 +61,8 @@ xfbtree_dup_cursor(
 
 	if (cur->bc_mem.pag)
 		ncur->bc_mem.pag = xfs_perag_hold(cur->bc_mem.pag);
+	if (cur->bc_mem.rtg)
+		ncur->bc_mem.rtg = xfs_rtgroup_hold(cur->bc_mem.rtg);
 
 	return ncur;
 }
