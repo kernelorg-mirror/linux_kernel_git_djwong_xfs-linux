fda3e48fef615b2abab5268daf56ed45aa32f4b9
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:16:09 -0700

    xfs: create hooks for media errors
    Set up a media error event hook so that we can send events to userspace.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -2111,6 +2111,7 @@ static int xfs_init_fs_context(
 	xfs_hooks_init(&mp->m_dir_update_hooks);
 	xfs_hooks_init(&mp->m_shutdown_hooks);
 	xfs_hooks_init(&mp->m_health_update_hooks);
+	xfs_hooks_init(&mp->m_media_error_hooks);
 	xfs_timestats_init(mp);
 
 	fc->s_fs_info = mp;
