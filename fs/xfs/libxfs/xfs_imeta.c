977114e596b221b535417d34180f308ae85f4b77
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:10:53 -0700

    xfs: create transaction reservations for metadata inode operations
    Create transaction reservation types and block reservation helpers to
    help us calculate transaction requirements.  Right now the reservations
    are the same as always; we're just separating the symbols for a future
    patch.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/libxfs/xfs_imeta.c
+++ fs/xfs/libxfs/xfs_imeta.c
@@ -19,6 +19,10 @@
 #include "xfs_inode.h"
 #include "xfs_quota.h"
 #include "xfs_ialloc.h"
+#include "xfs_bmap_btree.h"
+#include "xfs_da_format.h"
+#include "xfs_da_btree.h"
+#include "xfs_trans_space.h"
 
 /*
  * Metadata File Management
