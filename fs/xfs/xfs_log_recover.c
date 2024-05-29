2a45e7550cf6be5c285f8721aece8c8c94e1a42d
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:13:10 -0700

    xfs: add a realtime flag to the refcount update log redo items
    Extend the refcount update (CUI) log items with a new realtime flag that
    indicates that the updates apply against the realtime refcountbt.  We'll
    wire up the actual refcount code later.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_log_recover.c
+++ fs/xfs/xfs_log_recover.c
@@ -1827,6 +1827,8 @@ static const struct xlog_recover_item_ops *xlog_recover_item_ops[] = {
 	&xlog_rtefd_item_ops,
 	&xlog_rtrui_item_ops,
 	&xlog_rtrud_item_ops,
+	&xlog_rtcui_item_ops,
+	&xlog_rtcud_item_ops,
 };
 
 static const struct xlog_recover_item_ops *
