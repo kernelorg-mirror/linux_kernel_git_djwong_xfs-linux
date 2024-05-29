58b1eb411e92fe3c9abdcbc5a27f972eb3580c6f
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:13:20 -0700

    xfs: add realtime refcount btree when adding rt volume
    If we're adding enough space to the realtime section to require the
    creation of new realtime groups, create the rt refcount btree inode
    before we start adding the space.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -1051,6 +1051,98 @@ out_path:
 	return error;
 }
 
+/* Add a metadata inode for a realtime refcount btree. */
+static int
+xfs_growfsrt_create_rtrefcount(
+	struct xfs_rtgroup	*rtg)
+{
+	struct xfs_imeta_update	upd;
+	struct xfs_mount	*mp = rtg->rtg_mount;
+	struct xfs_imeta_path	*path;
+	struct xfs_trans	*tp = NULL;
+	struct xfs_inode	*ip = NULL;
+	xfs_ino_t		ino;
+	int			error;
+
+	if (!xfs_has_rtreflink(mp) || rtg->rtg_refcountip)
+		return 0;
+
+	error = xfs_rtrefcountbt_create_path(mp, rtg->rtg_rgno, &path);
+	if (error)
+		return error;
+
+	error = xfs_imeta_ensure_dirpath(mp, path);
+	if (error)
+		goto out_path;
+
+	/* Does this file already exist at the end of the path? */
+	error = xfs_trans_alloc_empty(mp, &tp);
+	if (error)
+		goto out_path;
+
+	error = xfs_imeta_lookup(tp, path, &ino);
+	if (error)
+		goto out_trans;
+
+	if (ino != NULLFSINO) {
+		/* Inode already exists; load it. */
+		error = xfs_imeta_iget(tp, ino, XFS_DIR3_FT_REG_FILE, &ip);
+		xfs_trans_cancel(tp);
+		tp = NULL;
+		if (error)
+			goto out_path;
+
+		if (XFS_IS_CORRUPT(mp, ip->i_df.if_format !=
+							XFS_DINODE_FMT_REFCOUNT)) {
+			error = -EFSCORRUPTED;
+			xfs_imeta_irele(ip);
+			goto out_path;
+		}
+
+		lockdep_set_class_and_subclass(&ip->i_lock, &rtg->lock_class,
+				XFS_RTREFC_SUBCLASS);
+
+		rtg->rtg_refcountip = ip;
+		goto out_path;
+	}
+	xfs_trans_cancel(tp);
+	tp = NULL;
+
+	error = xfs_imeta_start_create(mp, path, &upd);
+	if (error)
+		goto out_path;
+
+	error = xfs_rtrefcountbt_create(&upd, &ip);
+	if (error)
+		goto out_cancel;
+
+	lockdep_set_class_and_subclass(&ip->i_lock, &rtg->lock_class,
+			XFS_RTREFC_SUBCLASS);
+
+	error = xfs_imeta_commit_update(&upd);
+	if (error)
+		goto out_path;
+
+	xfs_imeta_free_path(path);
+	xfs_finish_inode_setup(ip);
+	rtg->rtg_refcountip = ip;
+	return 0;
+
+out_cancel:
+	xfs_imeta_cancel_update(&upd, error);
+	/* Have to finish setting up the inode to ensure it's deleted. */
+	if (ip) {
+		xfs_finish_inode_setup(ip);
+		xfs_imeta_irele(ip);
+	}
+out_trans:
+	if (tp)
+		xfs_trans_cancel(tp);
+out_path:
+	xfs_imeta_free_path(path);
+	return error;
+}
+
 /* Add rtgroups as needed to deal with this phase of rt expansion. */
 STATIC int
 xfs_growfsrt_alloc_rtgroups(
@@ -1072,6 +1164,12 @@ xfs_growfsrt_alloc_rtgroups(
 			xfs_rtgroup_rele(rtg);
 			return error;
 		}
+
+		error = xfs_growfsrt_create_rtrefcount(rtg);
+		if (error) {
+			xfs_rtgroup_rele(rtg);
+			return error;
+		}
 	}
 
 	return 0;
@@ -1088,6 +1186,7 @@ xfs_growfsrt_free_rtgroups(
 
 	for_each_rtgroup_range(mp, rgno, nsbp->sb_rgcount, rtg) {
 		xfs_rtgroup_irele(&rtg->rtg_rmapip);
+		xfs_rtgroup_irele(&rtg->rtg_refcountip);
 	}
 
 	xfs_free_unused_rtgroup_range(mp, mp->m_sb.sb_rgcount + 1,
@@ -1201,9 +1300,11 @@ xfs_growfs_rt(
 		return -EINVAL;
 
 	/* Unsupported realtime features. */
-	if (!xfs_has_rtgroups(mp) && xfs_has_rmapbt(mp))
+	if (!xfs_has_rtgroups(mp) && (xfs_has_rmapbt(mp) || xfs_has_reflink(mp)))
 		return -EOPNOTSUPP;
-	if (xfs_has_reflink(mp) || xfs_has_quota(mp))
+	if (xfs_has_quota(mp))
+		return -EOPNOTSUPP;
+	if (xfs_has_reflink(mp) && in->extsize != 1)
 		return -EOPNOTSUPP;
 
 	nrblocks = in->newblocks;
