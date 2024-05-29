994a71aa56af274d518b099bbf86eb463646ab2a
Darrick J. Wong <djwong@kernel.org>
2024-05-28 21:13:34 -0700

    xfs: allow reflink on the rt volume when extent size is larger than 1 rt block
    Make the necessary tweaks to the reflink remapping code to support
    remapping on the realtime volume when the rt extent size is larger than
    a single rt block.  We need to check that the remap arguments from
    userspace are aligned to a rt extent boundary, and that the length
    is always aligned, even if the kernel tried to round it up to EOF for
    us.  XFS can only map and remap full rt extents, so we have to be a
    little more strict about the alignment there.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_trace.h
+++ fs/xfs/xfs_trace.h
@@ -3876,6 +3876,9 @@ TRACE_EVENT(xfs_reflink_remap_blocks,
 		  __entry->dest_lblk)
 );
 DEFINE_DOUBLE_IO_EVENT(xfs_reflink_remap_range);
+#ifdef CONFIG_XFS_RT
+DEFINE_DOUBLE_IO_EVENT(xfs_reflink_adjust_rtbigalloc_len);
+#endif /* CONFIG_XFS_RT */
 DEFINE_INODE_ERROR_EVENT(xfs_reflink_remap_range_error);
 DEFINE_INODE_ERROR_EVENT(xfs_reflink_set_inode_flag_error);
 DEFINE_INODE_ERROR_EVENT(xfs_reflink_update_inode_size_error);
