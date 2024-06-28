d9342882270fc229fa1d5918bd332ee8a67b714a
Darrick J. Wong <djwong@kernel.org>
2024-06-28 09:40:47 -0700

    xfs: clean up xfs_rtallocate_extent_exact a bit
    Before we start doing more surgery on the rt allocator, let's clean up
    the exact allocator so that it doesn't change its arguments and uses the
    helper introduced in the previous patch.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -350,10 +350,10 @@ xfs_rtallocate_extent_exact(
 	xfs_rtxlen_t		prod,	/* extent product factor */
 	xfs_rtxnum_t		*rtx)	/* out: start rtext allocated */
 {
-	int			error;
-	xfs_rtxlen_t		i;	/* extent length trimmed due to prod */
-	int			isfree;	/* extent is free */
 	xfs_rtxnum_t		next;	/* next rtext to try (dummy) */
+	xfs_rtxlen_t		alloclen; /* candidate length */
+	int			isfree;	/* extent is free */
+	int			error;
 
 	ASSERT(minlen % prod == 0);
 	ASSERT(maxlen % prod == 0);
@@ -364,25 +364,24 @@ xfs_rtallocate_extent_exact(
 	if (error)
 		return error;
 
-	if (!isfree) {
+	if (isfree) {
+		/* start to maxlen is all free; allocate it. */
+		alloclen = maxlen;
+	} else {
 		/*
 		 * If not, allocate what there is, if it's at least minlen.
 		 */
-		maxlen = next - start;
-		if (maxlen < minlen)
+		alloclen = next - start;
+		if (alloclen < minlen)
 			return -ENOSPC;
 
-		/*
-		 * Trim off tail of extent, if prod is specified.
-		 */
-		if (prod > 1 && (i = maxlen % prod)) {
-			maxlen -= i;
-			if (maxlen < minlen)
-				return -ENOSPC;
-		}
+		/* Ensure alloclen is a multiple of prod. */
+		alloclen = xfs_rtalloc_align_len(alloclen, prod);
+		if (alloclen < minlen)
+			return -ENOSPC;
 	}
 
-	*len = maxlen;
+	*len = alloclen;
 	*rtx = start;
 	return 0;
 }
