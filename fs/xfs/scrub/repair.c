e739912d5947dc989f080a873ff78c5faf8504f6
Darrick J. Wong <djwong@kernel.org>
2024-08-15 11:49:44 -0700

    xfs: check new rtbitmap records against rt refcount btree
    When we're rebuilding the realtime bitmap, check the proposed free
    extents against the rt refcount btree to make sure we don't commit any
    grievous errors.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/repair.c
+++ fs/xfs/scrub/repair.c
@@ -42,6 +42,7 @@
 #include "xfs_rtgroup.h"
 #include "xfs_rtalloc.h"
 #include "xfs_metafile.h"
+#include "xfs_rtrefcount_btree.h"
 #include "scrub/scrub.h"
 #include "scrub/common.h"
 #include "scrub/trace.h"
@@ -1009,6 +1010,13 @@ xrep_rtgroup_btcur_init(
 	    xfs_has_rtrmapbt(mp))
 		sr->rmap_cur = xfs_rtrmapbt_init_cursor(mp, sc->tp, sr->rtg,
 					sr->rtg->rtg_inodes[XFS_RTGI_RMAP]);
+
+	if (sc->sm->sm_type != XFS_SCRUB_TYPE_RTREFCBT &&
+	    (sr->rtlock_flags & XFS_RTGLOCK_REFCOUNT) &&
+	    xfs_has_rtreflink(mp))
+		sr->refc_cur = xfs_rtrefcountbt_init_cursor(mp, sc->tp,
+					sr->rtg,
+					sr->rtg->rtg_inodes[XFS_RTGI_REFCOUNT]);
 }
 
 /*
