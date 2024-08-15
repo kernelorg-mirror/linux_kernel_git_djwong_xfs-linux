d43196af78edf1b7b060a897afa45fdf8d038438
Darrick J. Wong <djwong@kernel.org>
2024-08-15 11:50:16 -0700

    xfs: present time stats for scrubbers
    Use the timestats code to report statistical information about how much
    time we spend in scrub and repair.  This augments the existing raw scrub
    counters.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_timestats.h
+++ fs/xfs/xfs_timestats.h
@@ -18,6 +18,7 @@ void xfs_timestats_destroy(struct xfs_mount *mp);
 # define DEFINE_XFS_TIMESTAT(name)	u64 name = local_clock()
 # define xfs_timestats_start(b)		do { *(b) = local_clock(); } while (0)
 # define xfs_timestats_end(a, b)	time_stats_update((a), (b))
+# define xfs_timestats_interval(a,b,c)	__time_stats_update((a), (b), (c))
 #else
 # define xfs_timestats_init(mp)		((void)0)
 # define xfs_timestats_export(mp)	((void)0)
@@ -28,6 +29,7 @@ void xfs_timestats_destroy(struct xfs_mount *mp);
 # define DEFINE_XFS_TIMESTAT(name)
 # define xfs_timestats_start(t)		((void)0)
 # define xfs_timestats_end(s, t)	((void)0)
+# define xfs_timestats_interval(...)	((void)0)
 #endif /* CONFIG_XFS_TIME_STATS */
 
 #endif /* __XFS_TIMESTATS_H__ */
