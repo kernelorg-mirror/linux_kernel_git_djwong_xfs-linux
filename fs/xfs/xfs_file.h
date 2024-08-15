16149d4e666fb276865510978ce9270ff9f71841
Darrick J. Wong <djwong@kernel.org>
2024-08-15 11:49:53 -0700

    xfs: enable CoW when rt extent size is larger than 1 block
    Copy on write encounters a major plot twist when the file being CoW'd
    lives on the realtime volume and the realtime extent size is larger than
    a single filesystem block.  XFS can only unmap and remap full rt
    extents, which means that allocations are always done in units of full
    rt extents, and a request to unmap less than one extent is treated as a
    request to convert an extent to unwritten status.
    
    This behavioral quirk is not compatible with the existing CoW mechanism,
    so we have to intercept every path through which files can be modified
    to ensure that we dirty an entire rt extent at once so that we can remap
    a full rt extent.  Use the existing VFS unshare functions to dirty the
    page cache to set that up.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_file.h
+++ fs/xfs/xfs_file.h
@@ -12,4 +12,7 @@ extern const struct file_operations xfs_dir_file_operations;
 bool xfs_is_falloc_aligned(struct xfs_inode *ip, loff_t pos,
 		long long int len);
 
+bool xfs_truncate_needs_cow_around(struct xfs_inode *ip, loff_t pos);
+int xfs_file_unshare_at(struct xfs_inode *ip, loff_t pos);
+
 #endif /* __XFS_FILE_H__ */
