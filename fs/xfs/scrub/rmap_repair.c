2080f2a1e4bf05e59304c2fafb04ca58ee9a97c7
Darrick J. Wong <djwong@kernel.org>
2024-09-23 13:42:19 -0700

    xfs: hook live realtime rmap operations during a repair operation
    Hook the regular realtime rmap code when an rtrmapbt repair operation is
    running so that we can unlock the AGF buffer to scan the filesystem and
    keep the in-memory btree up to date during the scan.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/scrub/rmap_repair.c
+++ fs/xfs/scrub/rmap_repair.c
@@ -1587,7 +1587,7 @@ xrep_rmapbt_live_update(
 	if (!xrep_rmapbt_want_live_update(&rr->iscan, &p->oinfo))
 		goto out_unlock;
 
-	trace_xrep_rmap_live_update(rr->sc->sa.pag, action, p);
+	trace_xrep_rmap_live_update(&rr->sc->sa.pag->pag_group, action, p);
 
 	error = xrep_trans_alloc_hook_dummy(mp, &txcookie, &tp);
 	if (error)
