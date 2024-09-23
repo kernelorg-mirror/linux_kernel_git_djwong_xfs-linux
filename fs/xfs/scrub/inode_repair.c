814eba78fef3502fefbcfe68681d43117337a60e
Darrick J. Wong <djwong@kernel.org>
2024-09-23 13:40:47 -0700

    xfs: define the on-disk format for the metadir feature
    Define the on-disk layout and feature flags for the metadata inode
    directory feature.  Add a xfs_sb_version_hasmetadir for benefit of
    xfs_repair, which needs to know where the new end of the superblock
    lies.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/scrub/inode_repair.c
+++ fs/xfs/scrub/inode_repair.c
@@ -521,10 +521,13 @@ STATIC void
 xrep_dinode_nlinks(
 	struct xfs_dinode	*dip)
 {
-	if (dip->di_version > 1)
-		dip->di_onlink = 0;
-	else
+	if (dip->di_version < 2) {
 		dip->di_nlink = 0;
+		return;
+	}
+
+	if (!(dip->di_flags2 & cpu_to_be64(XFS_DIFLAG2_METADATA)))
+		dip->di_onlink = 0;
 }
 
 /* Fix any conflicting flags that the verifiers complain about. */
