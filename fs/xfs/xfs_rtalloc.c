a5c9ddc77b892d36d4135d1a8a08c45154181b94
Darrick J. Wong <djwong@kernel.org>
2024-09-23 13:42:20 -0700

    xfs: enable realtime rmap btree
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- fs/xfs/xfs_rtalloc.c
+++ fs/xfs/xfs_rtalloc.c
@@ -1284,7 +1284,7 @@ xfs_growfs_rt(
 	error = -EOPNOTSUPP;
 	if (xfs_has_quota(mp) && !xfs_has_rtgroups(mp))
 		goto out_unlock;
-	if (xfs_has_rmapbt(mp) || xfs_has_reflink(mp))
+	if (xfs_has_reflink(mp))
 		goto out_unlock;
 
 	error = xfs_sb_validate_fsb_count(&mp->m_sb, in->newblocks);
