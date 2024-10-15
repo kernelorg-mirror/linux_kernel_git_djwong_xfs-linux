3ffaf0f98c84d04db9f447a61671cb73c522dd2f
Darrick J. Wong <djwong@kernel.org>
2024-10-15 12:39:30 -0700

    xfs: persist quota flags with metadir
    It's annoying that one has to keep reminding XFS about what quota
    options it should mount with, since the quota flags recording the
    previous state are sitting right there in the primary superblock.  Even
    more strangely, there exists a noquota option to disable quotas
    completely, so it's odder still that providing no options is the same as
    noquota.
    
    Starting with metadir, let's change the behavior so that if the user
    does not specify any quota-related mount options at all, the ondisk
    quota flags will be used to bring up quota.  In other words, the
    filesystem will mount in the same state and with the same functionality
    as it had during the last mount.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -497,6 +497,8 @@ __XFS_HAS_FEAT(nouuid, NOUUID)
 #define XFS_OPSTATE_WARNED_PPTR		15
 /* Kernel has logged a warning about metadata dirs being used on this fs. */
 #define XFS_OPSTATE_WARNED_METADIR	16
+/* Filesystem should use qflags to determine quotaon status */
+#define XFS_OPSTATE_RESUMING_QUOTAON	17
 
 #define __XFS_IS_OPSTATE(name, NAME) \
 static inline bool xfs_is_ ## name (struct xfs_mount *mp) \
@@ -521,8 +523,12 @@ __XFS_IS_OPSTATE(inodegc_enabled, INODEGC_ENABLED)
 __XFS_IS_OPSTATE(blockgc_enabled, BLOCKGC_ENABLED)
 #ifdef CONFIG_XFS_QUOTA
 __XFS_IS_OPSTATE(quotacheck_running, QUOTACHECK_RUNNING)
+__XFS_IS_OPSTATE(resuming_quotaon, RESUMING_QUOTAON)
 #else
 # define xfs_is_quotacheck_running(mp)	(false)
+# define xfs_is_resuming_quotaon(mp)	(false)
+# define xfs_set_resuming_quotaon(mp)	(false)
+# define xfs_clear_resuming_quotaon(mp)	(false)
 #endif
 __XFS_IS_OPSTATE(done_with_log_incompat, UNSET_LOG_INCOMPAT)
 __XFS_IS_OPSTATE(using_logged_xattrs, USE_LARP)
