fb8d0b27a0ce65692f6ead32ad9fe58538f5bbd0
Darrick J. Wong <djwong@kernel.org>
2024-11-20 16:21:24 -0800

    xfs: support realtime reflink with an extent size that isn't a power of 2
    Add the necessary alignment checking code to the reflink remap code to
    ensure that remap requests are aligned to rt extent boundaries if the
    realtime extent size isn't a power of two.  The VFS helpers assume that
    they can use the usual (blocksize - 1) masking to avoid slow 64-bit
    division, but since XFS is special we won't make everyone pay that cost
    for our weird edge case.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -1842,15 +1842,6 @@ xfs_fs_fill_super(
 	}
 
 	if (xfs_has_reflink(mp)) {
-		if (xfs_has_realtime(mp) &&
-		    !xfs_reflink_supports_rextsize(mp, mp->m_sb.sb_rextsize)) {
-			xfs_alert(mp,
-	"reflink not compatible with non-power-of-2 realtime extent size %u!",
-					mp->m_sb.sb_rextsize);
-			error = -EINVAL;
-			goto out_filestream_unmount;
-		}
-
 		if (xfs_has_zoned(mp)) {
 			xfs_alert(mp,
 	"reflink not compatible with zoned RT device!");
