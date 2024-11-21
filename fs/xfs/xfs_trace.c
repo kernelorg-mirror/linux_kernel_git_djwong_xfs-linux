4bcadf3cffc8f6665177e56067db96ecd8c7c8a8
Darrick J. Wong <djwong@kernel.org>
2024-11-20 16:21:42 -0800

    xfs: report media errors through healthmon
    Now that we have hooks to report media errors, connect this to the
    health monitor as well.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/xfs/xfs_trace.c
+++ fs/xfs/xfs_trace.c
@@ -54,6 +54,7 @@
 #include "xfs_fsrefs.h"
 #include "xfs_health.h"
 #include "xfs_healthmon.h"
+#include "xfs_notify_failure.h"
 
 /*
  * We include this last to have the helpers above available for the trace
