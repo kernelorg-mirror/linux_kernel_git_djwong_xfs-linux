641966b62ac3f39fde723b7fe5a938d769fc419e
Darrick J. Wong <djwong@kernel.org>
2024-11-20 16:21:38 -0800

    xfs: create file io error hooks
    Create hooks within XFS to deliver IO errors to callers.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -2084,6 +2084,7 @@ xfs_init_fs_context(
 	xfs_hooks_init(&mp->m_shutdown_hooks);
 	xfs_hooks_init(&mp->m_health_update_hooks);
 	xfs_hooks_init(&mp->m_media_error_hooks);
+	xfs_hooks_init(&mp->m_file_ioerror_hooks);
 
 	fc->s_fs_info = mp;
 	fc->ops = &xfs_context_ops;
