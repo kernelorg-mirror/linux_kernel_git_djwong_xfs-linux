e57478a1e77a37c40464f406f137ff3eb0f01889
Darrick J. Wong <djwong@kernel.org>
2024-11-20 16:21:32 -0800

    xfs: add an ioctl to map free space into a file
    Add a new ioctl to map free physical space into a file, at the same file
    offset as if the file were a sparse image of the physical device backing
    the filesystem.  The intent here is to use this to prototype a free
    space defragmentation tool.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/xfs/xfs_bmap_util.h
+++ fs/xfs/xfs_bmap_util.h
@@ -61,6 +61,7 @@ int	xfs_collapse_file_space(struct xfs_inode *, xfs_off_t offset,
 				xfs_off_t len);
 int	xfs_insert_file_space(struct xfs_inode *, xfs_off_t offset,
 				xfs_off_t len);
+int	xfs_map_free_space(struct xfs_inode *ip, xfs_off_t off, xfs_off_t len);
 
 /* EOF block manipulation functions */
 bool	xfs_can_free_eofblocks(struct xfs_inode *ip);
