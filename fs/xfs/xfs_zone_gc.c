303d1bf65f9329fa5c9e290eea25f43ea3266baf
Hans Holmberg <hans.holmberg@wdc.com>
2024-12-19 17:53:15 -0800

    xfs: support write life time based data placement
    Add a file write life time data placement allocation scheme that aims to
    minimize fragmentation and thereby to do two things:
    
     a) separate file data to different zones when possible.
     b) colocate file data of similar life times when feasible.
    
    To get best results, average file sizes should align with the zone
    capacity that is reported through the XFS_IOC_FSGEOMETRY ioctl.
    
    For RocksDB using leveled compaction, the lifetime hints can improve
    throughput for overwrite workloads at 80% file system utilization by
    ~10%.
    
    Lifetime hints can be disabled using the nolifetime mount option.
    
    Signed-off-by: Hans Holmberg <hans.holmberg@wdc.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/xfs_zone_gc.c
+++ fs/xfs/xfs_zone_gc.c
@@ -533,7 +533,7 @@ xfs_zone_gc_select_target(
 	if (!xfs_group_marked(mp, XG_TYPE_RTG, XFS_RTG_FREE))
 		oz = xfs_zone_gc_steal_open(zi);
 	else
-		oz = xfs_open_zone(mp, true);
+		oz = xfs_open_zone(mp, WRITE_LIFE_NOT_SET, true);
 	spin_unlock(&zi->zi_zone_list_lock);
 
 	if (oz)
