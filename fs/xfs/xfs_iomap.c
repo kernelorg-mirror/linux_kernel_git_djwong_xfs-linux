99acf8c123b34c28280900b7724aebfe8600daea
Christoph Hellwig <hch@lst.de>
2024-12-19 17:52:02 -0800

    xfs: define the zoned on-disk format
    Zone file systems reuse the basic RT group enabled XFS file system
    structure to support a mode where each RT group is always written from
    start to end and then reset for reuse (after moving out any remaining
    data).  There are few minor but important changes, which are indicated
    by a new incompat flag:
    
    1) there are no bitmap and summary inodes, thus the
       /rtgroups/{rgno}.{bitmap,summary} metadir files do not exist and the
       sb_rbmblocks superblock field must be cleared to zero.
    
    2) there is a new superblock field that specifies the start of an
       internal RT section.  This allows supporting SMR HDDs that have random
       writable space at the beginning which is used for the XFS data device
       (which really is the metadata device for this configuration), directly
       followed by a RT device on the same block device.  While something
       similar could be achieved using dm-linear just having a single device
       directly consumed by XFS makes handling the file systems a lot easier.
    
    3) Another superblock field that tracks the amount of reserved space (or
       overprovisioning) that is never used for user capacity, but allows GC
       to run more smoothly.
    
    4) an overlay of the cowextsize field for the rtrmap inode so that we
       can persistently track the total amount of rtblocks currently used in
       a RT group.  There is no data structure other than the rmap that
       tracks used space in an RT group, and this counter is used to decide
       when a RT group has been entirely emptied, and to select one that
       is relatively empty if garbage collection needs to be performed.
       While this counter could be tracked entirely in memory and rebuilt
       from the rmap at mount time, that would lead to very long mount times
       with the large number of RT groups implied by the number of hardware
       zones especially on SMR hard drives with 256MB zone sizes.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/xfs_iomap.c
+++ fs/xfs/xfs_iomap.c
@@ -1216,6 +1216,7 @@ retry:
 
 	fdblocks = indlen;
 	if (XFS_IS_REALTIME_INODE(ip)) {
+		ASSERT(!xfs_is_zoned_inode(ip));
 		error = xfs_dec_frextents(mp, xfs_blen_to_rtbxlen(mp, alen));
 		if (error)
 			goto out_unreserve_quota;
