259c56a69ec1e39b767623d34e46ac1d1c6347be
Christoph Hellwig <hch@lst.de>
2024-12-19 17:49:24 -0800

    iomap: split bios to zone append limits in the submission handlers
    Provide helpers for file systems to split bios in the direct I/O and
    writeback I/O submission handlers.
    
    This Follows btrfs' lead and don't try to build bios to hardware limits
    for zone append commands, but instead build them as normal unconstrained
    bios and split them to the hardware limits in the I/O submission handler.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- include/linux/iomap.h
+++ include/linux/iomap.h
@@ -358,6 +358,9 @@ struct iomap_ioend {
 	struct list_head	io_list;	/* next ioend in chain */
 	u16			io_flags;	/* IOMAP_IOEND_* */
 	struct inode		*io_inode;	/* file being written to */
+	atomic_t		io_remaining;	/* completetion defer count */
+	int			io_error;	/* stashed away status */
+	struct iomap_ioend	*io_parent;	/* parent for completions */
 	size_t			io_size;	/* size of the extent */
 	loff_t			io_offset;	/* offset in the file */
 	sector_t		io_sector;	/* start sector of ioend */
@@ -408,6 +411,10 @@ struct iomap_writepage_ctx {
 	u32			nr_folios;	/* folios added to the ioend */
 };
 
+struct iomap_ioend *iomap_init_ioend(struct inode *inode, struct bio *bio,
+		loff_t file_offset, u16 ioend_flags);
+struct iomap_ioend *iomap_split_ioend(struct iomap_ioend *ioend,
+		unsigned int max_len, bool is_append);
 void iomap_finish_ioends(struct iomap_ioend *ioend, int error);
 void iomap_ioend_try_merge(struct iomap_ioend *ioend,
 		struct list_head *more_ioends);
@@ -478,5 +485,7 @@ int iomap_swapfile_activate(struct swap_info_struct *sis,
 #else
 # define iomap_swapfile_activate(sis, swapfile, pagespan, ops)	(-EIO)
 #endif /* CONFIG_SWAP */
+
+extern struct bio_set iomap_ioend_bioset;
 
 #endif /* LINUX_IOMAP_H */
