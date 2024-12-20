697a494c04aebfe92b2447d0950de257e9ae72b3
Hans Holmberg <hans.holmberg@wdc.com>
2024-12-19 17:53:16 -0800

    xfs: export zone stats in /proc/*/mountstats
    Add the per-zone life time hint and the used block distribution
    for fully written zones, grouping reclaimable zones in fixed-percentage
    buckets spanning 0..9%, 10..19% and full zones as 100% used as well as a
    few statistics about the zone allocator and open and reclaimable zones
    in /proc/*/mountstats.
    
    This gives good insight into data fragmentation and data placement
    success rate.
    
    Signed-off-by: Hans Holmberg <hans.holmberg@wdc.com>
    Co-developed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -1246,6 +1246,10 @@ xfs_fs_show_stats(
 	struct seq_file		*m,
 	struct dentry		*root)
 {
+	struct xfs_mount	*mp = XFS_M(root->d_sb);
+
+	if (xfs_has_zoned(mp) && IS_ENABLED(CONFIG_XFS_RT))
+		xfs_zoned_show_stats(m, mp);
 	return 0;
 }
 
