4bc6e449d1db15626a8e6b9181b7c7963933ad9a
Christoph Hellwig <hch@lst.de>
2025-01-09 14:22:19 -0800

    iomap: add a io_private field to struct iomap_ioend
    Add a private data field to struct iomap_ioend so that the file system
    can attach information to it.  Zoned XFS will use this for a pointer to
    the open zone.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- include/linux/iomap.h
+++ include/linux/iomap.h
@@ -370,6 +370,7 @@ struct iomap_ioend {
 	size_t			io_size;	/* size of the extent */
 	loff_t			io_offset;	/* offset in the file */
 	sector_t		io_sector;	/* start sector of ioend */
+	void			*io_private;	/* file system private data */
 	struct bio		io_bio;		/* MUST BE LAST! */
 };
 
