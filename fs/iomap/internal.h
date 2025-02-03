8c790f8d00d29bf4e67d56de2f5d0591603c9bd7
Darrick J. Wong <djwong@kernel.org>
2025-02-03 12:10:10 -0800

    iomap, filemap: report buffered read and write io errors to the filesystem
    Provide a callback so that iomap can report read and write IO errors to
    the caller filesystem.  For now this is only wired up for iomap as a
    testbed for XFS.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/iomap/internal.h
+++ fs/iomap/internal.h
@@ -5,5 +5,7 @@
 #define IOEND_BATCH_SIZE	4096
 
 u32 iomap_finish_ioend_direct(struct iomap_ioend *ioend);
+void iomap_mapping_ioerror(struct address_space *mapping, int direction,
+		loff_t pos, u64 len, int error);
 
 #endif /* _IOMAP_INTERNAL_H */
