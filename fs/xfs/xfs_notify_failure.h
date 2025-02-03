aecbe95895425089038af1f4550b32d43404d628
Darrick J. Wong <djwong@kernel.org>
2025-02-03 12:10:18 -0800

    xfs: add media error reporting ioctl
    Add a new privileged ioctl so that xfs_scrub can report the media errors
    that it finds to the kernel for further processing.  Ideally this would
    be done by the kernel, but the kernel doesn't (yet) support initiating
    verification reads.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/xfs/xfs_notify_failure.h
+++ fs/xfs/xfs_notify_failure.h
@@ -8,4 +8,8 @@
 
 extern const struct dax_holder_operations xfs_dax_holder_operations;
 
+struct xfs_media_error_report;
+int xfs_ioc_report_media_error(struct file *file,
+		struct xfs_media_error_report __user *arg);
+
 #endif /* __XFS_NOTIFY_FAILURE_H__ */
