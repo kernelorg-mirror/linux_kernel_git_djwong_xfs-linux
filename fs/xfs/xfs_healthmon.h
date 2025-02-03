a1db356890f8d907b2fe64dbe2c981f073825f63
Darrick J. Wong <djwong@kernel.org>
2025-02-03 12:10:13 -0800

    xfs: create event queuing, formatting, and discovery infrastructure
    Create the basic infrastructure that we need to report health events to
    userspace.  We need a compact form for recording critical information
    about an event and queueing them; a means to notice that we've lost some
    events; and a means to format the events into something that userspace
    can handle.
    
    Here, we've chosen json to export information to userspace.  The
    structured key-value nature of json gives us enormous flexibility to
    modify the schema of what we'll send to userspace because we can add new
    keys at any time.  Userspace can use whatever json parsers are available
    to consume the events and will not be confused by keys they don't
    recognize.
    
    Note that we do NOT allow sending json back to the kernel, nor is there
    any intent to do that.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/xfs/xfs_healthmon.h
+++ fs/xfs/xfs_healthmon.h
@@ -6,6 +6,35 @@
 #ifndef __XFS_HEALTHMON_H__
 #define __XFS_HEALTHMON_H__
 
+enum xfs_healthmon_type {
+	XFS_HEALTHMON_RUNNING,	/* monitor running */
+	XFS_HEALTHMON_LOST,	/* message lost */
+};
+
+enum xfs_healthmon_domain {
+	XFS_HEALTHMON_MOUNT,	/* affects the whole fs */
+};
+
+struct xfs_healthmon_event {
+	struct xfs_healthmon_event	*next;
+
+	enum xfs_healthmon_type		type;
+	enum xfs_healthmon_domain	domain;
+
+	uint64_t			time_ns;
+
+	union {
+		/* lost events */
+		struct {
+			uint64_t	lostcount;
+		};
+		/* mount */
+		struct {
+			unsigned int	flags;
+		};
+	};
+};
+
 #ifdef CONFIG_XFS_HEALTH_MONITOR
 long xfs_ioc_health_monitor(struct xfs_mount *mp,
 		struct xfs_health_monitor __user *arg);
