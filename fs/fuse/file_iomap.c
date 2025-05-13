421acd1fb84f0e4ead04f12dc670751200623a94
Darrick J. Wong <djwong@kernel.org>
2025-05-13 12:13:35 -0700

    fuse: implement large folios for iomap pagecache files
    Use large folios when we're using iomap.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/file_iomap.c
+++ fs/fuse/file_iomap.c
@@ -1414,6 +1414,7 @@ static const struct address_space_operations fuse_iomap_aops = {
 static inline void fuse_iomap_set_fileio(struct inode *inode)
 {
 	struct fuse_inode *fi = get_fuse_inode(inode);
+	unsigned int min_order = 0;
 
 	ASSERT(get_fuse_conn_c(inode)->iomap_fileio);
 
@@ -1428,6 +1429,11 @@ static inline void fuse_iomap_set_fileio(struct inode *inode)
 	INIT_WORK(&fi->ioend_work, fuse_iomap_end_io);
 	INIT_LIST_HEAD(&fi->ioend_list);
 	spin_lock_init(&fi->ioend_lock);
+
+	if (inode->i_blkbits > PAGE_SHIFT)
+		min_order = inode->i_blkbits - PAGE_SHIFT;
+
+	mapping_set_folio_min_order(inode->i_mapping, min_order);
 	set_bit(FUSE_I_IOMAP_FILEIO, &fi->state);
 }
 
