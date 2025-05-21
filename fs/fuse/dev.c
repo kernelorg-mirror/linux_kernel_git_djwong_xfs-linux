551ac1a2ec502a15e4f5129a40089ecd2da7c0d9
Darrick J. Wong <djwong@kernel.org>
2025-05-20 20:01:14 -0700

    fuse: advertise support for iomap
    Advertise our new IO paths programmatically by creating an ioctl that
    can return the capabilities of the kernel.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -2658,6 +2658,9 @@ static long fuse_dev_ioctl(struct file *file, unsigned int cmd,
 	case FUSE_DEV_IOC_BACKING_CLOSE:
 		return fuse_dev_ioctl_backing_close(file, argp);
 
+	case FUSE_DEV_IOC_IOMAP_SUPPORT:
+		return fuse_dev_ioctl_iomap_support(file, argp);
+
 	default:
 		return -ENOTTY;
 	}
