bee6ba1ccfca2acd4df382c8ff49315b8c3b4d80
Darrick J. Wong <djwong@kernel.org>
2025-06-03 13:46:15 -0700

    fuse: enable caching of timestamps
    Cache the timestamps in the kernel so that the kernel sends FUSE_SETATTR
    calls to the fuse server after writes, because the iomap infrastructure
    won't do that for us.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/file_iomap.c
+++ fs/fuse/file_iomap.c
@@ -1389,6 +1389,12 @@ static inline void fuse_inode_set_iomap(struct inode *inode)
 
 	ASSERT(fuse_has_iomap(inode));
 
+	/*
+	 * Manage timestamps ourselves, don't make the fuse server do it.  This
+	 * is critical for mtime updates to work correctly with page_mkwrite.
+	 */
+	inode->i_flags &= ~S_NOCMTIME;
+	inode->i_flags &= ~S_NOATIME;
 	inode->i_data.a_ops = &fuse_iomap_aops;
 
 	INIT_WORK(&fi->ioend_work, fuse_iomap_end_io);
