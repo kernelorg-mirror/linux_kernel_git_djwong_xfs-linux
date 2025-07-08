1a77d691be1b52c1f78acf0c52e5a358d6585e2b
Darrick J. Wong <djwong@kernel.org>
2025-07-08 12:18:59 -0700

    fuse: flush events and send FUSE_SYNCFS and FUSE_DESTROY on unmount
    At unmount time, there are a few things that we need to ask the fuse
    server to do.
    
    First, we need to flush queued events to userspace to give the fuse
    server a chance to process the events.  This is how we make sure that
    the server processes FUSE_RELEASE events before the connection goes
    down.
    
    Second, to ensure that all those metadata updates are persisted to disk
    before tell the fuse server to destroy itself, send FUSE_SYNCFS after
    waiting for the queued events.
    
    Finally, we need to send FUSE_DESTROY to the fuse server so that it
    closes the filesystem and the device fds before unmount returns.  That
    way, a script that does something like "umount /dev/sda ; e2fsck -fn
    /dev/sda" will not fail the e2fsck because the fd closure races with
    e2fsck startup.  Obviously, we need to wait for FUSE_SYNCFS.
    
    This is a major behavior change and who knows what might break existing
    code, so we hide it behind iomap mode.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/file_iomap.c
+++ fs/fuse/file_iomap.c
@@ -593,3 +593,32 @@ int fuse_iomap_backing_close(struct fuse_conn *fc, struct fuse_backing *fb)
 	/* We only support closing iomap block devices at unmount */
 	return -EBUSY;
 }
+
+void fuse_iomap_mount(struct fuse_mount *fm)
+{
+	struct fuse_conn *fc = fm->fc;
+
+	/*
+	 * Enable syncfs for iomap fuse servers so that we can send a final
+	 * flush at unmount time.  This also means that we can support
+	 * freeze/thaw properly.
+	 */
+	fc->sync_fs = true;
+}
+
+void fuse_iomap_unmount(struct fuse_mount *fm)
+{
+	struct fuse_conn *fc = fm->fc;
+
+	/*
+	 * Flush all pending commands, syncfs, flush that, and send a destroy
+	 * command.  This gives the fuse server a chance to process all the
+	 * pending releases, write the last bits of metadata changes to disk,
+	 * and close the iomap block devices before we return from the umount
+	 * call.  The caller already flushed previously pending requests, so we
+	 * only need the flush to wait for syncfs.
+	 */
+	sync_filesystem(fm->sb);
+	fuse_flush_requests_and_wait(fc, secs_to_jiffies(60));
+	fuse_send_destroy(fm);
+}
