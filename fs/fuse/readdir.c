a592a0d38e6127f4aec2d47e125160dea9eebe7e
Darrick J. Wong <djwong@kernel.org>
2025-07-21 14:50:07 -0700

    fuse: always cache ACLs when using iomap
    Keep ACLs cached in memory when we're using iomap, so that we don't have
    to make a round trip to the fuse server.  This might want to become a
    FUSE_ATTR_ flag.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/readdir.c
+++ fs/fuse/readdir.c
@@ -224,7 +224,8 @@ retry:
 		fi->nlookup++;
 		spin_unlock(&fi->lock);
 
-		forget_all_cached_acls(inode);
+		if (!fuse_inode_has_iomap(inode))
+			forget_all_cached_acls(inode);
 		fuse_change_attributes(inode, &o->attr, NULL,
 				       ATTR_TIMEOUT(o),
 				       attr_version);
