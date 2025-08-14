623338534663df6dc62c8a213ab93156643a04f8
Darrick J. Wong <djwong@kernel.org>
2025-08-14 14:29:02 -0700

    fuse: make debugging configurable at runtime
    Use static keys so that we can configure debugging assertions and dmesg
    warnings at runtime.  By default this is turned off so the cost is
    merely scanning a nop sled.  However, fuse server developers can turn
    it on for their debugging systems.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -1692,6 +1692,14 @@ extern void fuse_sysctl_unregister(void);
 #define fuse_sysctl_unregister()	do { } while (0)
 #endif /* CONFIG_SYSCTL */
 
+#if IS_ENABLED(CONFIG_FUSE_IOMAP_DEBUG)
+int fuse_iomap_sysfs_init(struct kobject *kobj);
+void fuse_iomap_sysfs_cleanup(struct kobject *kobj);
+#else
+# define fuse_iomap_sysfs_init(...)		(0)
+# define fuse_iomap_sysfs_cleanup(...)		((void)0)
+#endif
+
 #if IS_ENABLED(CONFIG_FUSE_IOMAP)
 bool fuse_iomap_enabled(void);
 
