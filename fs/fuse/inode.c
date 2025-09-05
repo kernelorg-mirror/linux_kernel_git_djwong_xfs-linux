291231c8f34157de9740a337c7bf6b8ce8745ecd
Darrick J. Wong <djwong@kernel.org>
2025-09-05 09:19:17 -0700

    fuse: implement direct IO with iomap
    Start implementing the fuse-iomap file I/O paths by adding direct I/O
    support and all the signalling flags that come with it.  Buffered I/O
    is much more complicated, so we leave that to a subsequent patch.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -300,6 +300,8 @@ void fuse_change_attributes_common(struct inode *inode, struct fuse_attr *attr,
 	else
 		fi->cached_i_blkbits = inode->i_sb->s_blocksize_bits;
 
+	fuse_iomap_set_disk_size(fi, attr->size);
+
 	/*
 	 * Don't set the sticky bit in i_mode, unless we want the VFS
 	 * to check permissions.  This prevents failures due to the
@@ -349,6 +351,7 @@ static void fuse_change_attributes_i(struct inode *inode, struct fuse_attr *attr
 	 * inode.
 	 */
 	cache_mask = fuse_get_cache_mask(inode);
+	fuse_iomap_set_disk_size(fi, attr->size);
 	if (cache_mask & STATX_SIZE)
 		attr->size = i_size_read(inode);
 
