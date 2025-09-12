a6ca7db8a3ff2577276fbb79f7352e3deca44303
Darrick J. Wong <djwong@kernel.org>
2025-09-12 10:29:57 -0700

    fuse_trace: implement inline data file IO via iomap
    Add tracepoints for the previous patch.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/file_iomap.c
+++ fs/fuse/file_iomap.c
@@ -452,6 +452,8 @@ static int fuse_iomap_inline_read(struct inode *inode, loff_t pos,
 	if (BAD_DATA(!iomap_inline_data_valid(iomap)))
 		return -EFSCORRUPTED;
 
+	trace_fuse_iomap_inline_read(inode, pos, count, iomap);
+
 	args.opcode = FUSE_READ;
 	args.nodeid = fi->nodeid;
 	args.in_numargs = 1;
@@ -496,6 +498,8 @@ static int fuse_iomap_inline_write(struct inode *inode, loff_t pos,
 
 	if (BAD_DATA(!iomap_inline_data_valid(iomap)))
 		return -EFSCORRUPTED;
+
+	trace_fuse_iomap_inline_write(inode, pos, count, iomap);
 
 	args.opcode = FUSE_WRITE;
 	args.nodeid = fi->nodeid;
@@ -557,6 +561,9 @@ static int fuse_iomap_set_inline(struct inode *inode, unsigned opflags,
 		if (err)
 			return err;
 	}
+
+	trace_fuse_iomap_set_inline_iomap(inode, pos, count, iomap);
+	trace_fuse_iomap_set_inline_srcmap(inode, pos, count, srcmap);
 
 	return 0;
 }
