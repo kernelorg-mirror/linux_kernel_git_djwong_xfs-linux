591780da432724ac1de0c4071257fe9c6f5f0804
Darrick J. Wong <djwong@kernel.org>
2025-09-12 10:16:55 -0700

    fuse_trace: invalidate iomap cache after file updates
    Add tracepoints for the previous patch.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/file_iomap.c
+++ fs/fuse/file_iomap.c
@@ -2503,12 +2503,16 @@ void fuse_iomap_open_truncate(struct inode *inode)
 {
 	ASSERT(fuse_inode_has_iomap(inode));
 
+	trace_fuse_iomap_open_truncate(inode);
+
 	fuse_iomap_cache_invalidate(inode, 0);
 }
 
 void fuse_iomap_release_truncate(struct inode *inode)
 {
 	ASSERT(fuse_inode_has_iomap(inode));
+
+	trace_fuse_iomap_release_truncate(inode);
 
 	fuse_iomap_cache_invalidate(inode, 0);
 }
@@ -2517,6 +2521,8 @@ void fuse_iomap_copied_file_range(struct inode *inode, loff_t offset,
 				  size_t written)
 {
 	ASSERT(fuse_inode_has_iomap(inode));
+
+	trace_fuse_iomap_copied_file_range(inode, offset, written);
 
 	fuse_iomap_cache_invalidate_range(inode, offset, written);
 }
