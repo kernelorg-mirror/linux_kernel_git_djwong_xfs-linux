3d521793f5dbfd0a91176faabf7db7167fb03fc4
Darrick J. Wong <djwong@kernel.org>
2025-10-27 11:22:24 -0700

    xfs: merge health monitoring events when possible
    Reduce memory consumption and event traffic by merging healthmon events
    whenever we actually add an event to the queue.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/xfs/xfs_trace.h
+++ fs/xfs/xfs_trace.h
@@ -6144,6 +6144,7 @@ DEFINE_EVENT(xfs_healthmon_event_class, name, \
 	TP_PROTO(const struct xfs_mount *mp, const struct xfs_healthmon_event *event), \
 	TP_ARGS(mp, event))
 DEFINE_HEALTHMONEVENT_EVENT(xfs_healthmon_insert);
+DEFINE_HEALTHMONEVENT_EVENT(xfs_healthmon_merge);
 DEFINE_HEALTHMONEVENT_EVENT(xfs_healthmon_push);
 DEFINE_HEALTHMONEVENT_EVENT(xfs_healthmon_pop);
 DEFINE_HEALTHMONEVENT_EVENT(xfs_healthmon_format);
