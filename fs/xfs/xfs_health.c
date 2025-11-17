790a1e2ebc638447e20418d4e3e6e15ac2b150a3
Darrick J. Wong <djwong@kernel.org>
2025-11-17 10:59:13 -0800

    xfs: start creating infrastructure for health monitoring
    Start creating helper functions and infrastructure to pass filesystem
    health events to a health monitoring file.  Since this is an
    administrative interface, we only support a single health monitor
    process per filesystem, so we don't need to use anything fancy such as
    notifier chains (== tons of indirect calls).
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/xfs/xfs_health.c
+++ fs/xfs/xfs_health.c
@@ -19,6 +19,7 @@
 #include "xfs_da_btree.h"
 #include "xfs_quota_defs.h"
 #include "xfs_rtgroup.h"
+#include "xfs_healthmon.h"
 
 static void
 xfs_health_unmount_group(
@@ -46,12 +47,13 @@ xfs_health_unmount(
 {
 	struct xfs_perag	*pag = NULL;
 	struct xfs_rtgroup	*rtg = NULL;
+	struct xfs_healthmon	*hm;
 	unsigned int		sick = 0;
 	unsigned int		checked = 0;
 	bool			warn = false;
 
 	if (xfs_is_shutdown(mp))
-		return;
+		goto out_healthmon;
 
 	/* Measure AG corruption levels. */
 	while ((pag = xfs_perag_next(mp, pag)))
@@ -96,6 +98,13 @@ xfs_health_unmount(
 		 */
 		if (sick & XFS_SICK_FS_COUNTERS)
 			xfs_fs_mark_healthy(mp, XFS_SICK_FS_COUNTERS);
+	}
+
+out_healthmon:
+	hm = xfs_healthmon_get(mp);
+	if (hm) {
+		xfs_healthmon_unmount(hm);
+		xfs_healthmon_put(hm);
 	}
 }
 
