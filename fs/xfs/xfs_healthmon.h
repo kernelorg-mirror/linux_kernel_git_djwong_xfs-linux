6554a45bd8d91d983247a7bf86377b618e034f6e
Darrick J. Wong <djwong@kernel.org>
2025-11-17 11:18:02 -0800

    xfs: convey externally discovered fsdax media errors to the health monitor
    Connect the fsdax media failure notification code to the health monitor
    so that xfs can send events about that to the xfs_healer daemon.
    
    Later on we'll add the ability for the xfs_scrub media scan (phase 6) to
    report the errors that it finds to the kernel so that those are also
    logged by xfs_healer.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/xfs/xfs_healthmon.h
+++ fs/xfs/xfs_healthmon.h
@@ -79,6 +79,9 @@ enum xfs_healthmon_type {
 	XFS_HEALTHMON_SICK,	/* runtime corruption observed */
 	XFS_HEALTHMON_CORRUPT,	/* fsck reported corruption */
 	XFS_HEALTHMON_HEALTHY,	/* fsck reported healthy structure */
+
+	/* media errors */
+	XFS_HEALTHMON_MEDIA_ERROR,
 };
 
 enum xfs_healthmon_domain {
@@ -89,6 +92,11 @@ enum xfs_healthmon_domain {
 	XFS_HEALTHMON_AG,	/* allocation group metadata */
 	XFS_HEALTHMON_INODE,	/* inode metadata */
 	XFS_HEALTHMON_RTGROUP,	/* realtime group metadata */
+
+	/* media errors */
+	XFS_HEALTHMON_DATADEV,
+	XFS_HEALTHMON_RTDEV,
+	XFS_HEALTHMON_LOGDEV,
 };
 
 struct xfs_healthmon_event {
@@ -126,6 +134,11 @@ struct xfs_healthmon_event {
 			uint32_t	gen;
 			xfs_ino_t	ino;
 		};
+		/* media errors */
+		struct {
+			xfs_daddr_t	daddr;
+			uint64_t	bbcount;
+		};
 	};
 };
 
@@ -134,6 +147,10 @@ void xfs_healthmon_report_metadata(struct xfs_healthmon *hmon,
 		const struct xfs_health_update_params *p);
 
 void xfs_healthmon_report_shutdown(struct xfs_healthmon *hmon, uint32_t flags);
+
+struct xfs_media_error_params;
+void xfs_healthmon_report_media(struct xfs_healthmon *hmon,
+		const struct xfs_media_error_params *p);
 
 long xfs_ioc_health_monitor(struct file *file,
 		struct xfs_health_monitor __user *arg);
--- fs/xfs/xfs_notify_failure.h
+++ fs/xfs/xfs_notify_failure.h
@@ -8,4 +8,17 @@
 
 extern const struct dax_holder_operations xfs_dax_holder_operations;
 
+enum xfs_failed_device {
+	XFS_FAILED_DATADEV,
+	XFS_FAILED_LOGDEV,
+	XFS_FAILED_RTDEV,
+};
+
+struct xfs_media_error_params {
+	xfs_daddr_t			daddr;
+	uint64_t			bbcount;
+	enum xfs_failed_device		fdev;
+	bool				pre_remove;
+};
+
 #endif /* __XFS_NOTIFY_FAILURE_H__ */
