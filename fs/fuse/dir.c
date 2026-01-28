fab88aeb077db04c5f4fdbaff31a58f4dd9450c8
Darrick J. Wong <djwong@kernel.org>
2026-01-28 11:49:05 -0800

    fuse: implement direct IO with iomap
    Start implementing the fuse-iomap file I/O paths by adding direct I/O
    support and all the signalling flags that come with it.  Buffered I/O
    is much more complicated, so we leave that to a subsequent patch.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -906,6 +906,10 @@ static int fuse_create_open(struct mnt_idmap *idmap, struct inode *dir,
 	if (err)
 		goto out_acl_release;
 	fuse_dir_changed(dir);
+
+	if (fuse_inode_has_iomap(inode))
+		fuse_iomap_open(inode, file);
+
 	err = generic_file_open(inode, file);
 	if (!err) {
 		file->private_data = ff;
@@ -1943,6 +1947,9 @@ static int fuse_dir_open(struct inode *inode, struct file *file)
 	if (fuse_is_bad(inode))
 		return -EIO;
 
+	if (fuse_inode_has_iomap(inode))
+		fuse_iomap_open(inode, file);
+
 	err = generic_file_open(inode, file);
 	if (err)
 		return err;
@@ -2301,6 +2308,12 @@ int fuse_do_setattr(struct mnt_idmap *idmap, struct dentry *dentry,
 		fuse_make_bad(inode);
 		err = -EIO;
 		goto error;
+	}
+
+	if (is_iomap && is_truncate) {
+		err = fuse_iomap_setsize_finish(inode, outarg.attr.size);
+		if (err)
+			goto error;
 	}
 
 	spin_lock(&fi->lock);
