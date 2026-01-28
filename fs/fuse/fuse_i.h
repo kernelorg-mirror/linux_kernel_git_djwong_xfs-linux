601a7b74c7476181c1694fb79d093bad23e9ac11
Darrick J. Wong <djwong@kernel.org>
2026-01-28 11:49:13 -0800

    fuse: invalidate ranges of block devices being used for iomap
    Make it easier to invalidate the page cache for a block device that is
    being used in conjunction with iomap.  This allows a fuse server to kill
    all cached data for a block that is being freed, so that block reuse
    doesn't result in file corruption.  Right now, the only way to do this
    is with fadvise, which ignores and doesn't wait for pages undergoing
    writeback.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -1805,6 +1805,8 @@ int fuse_iomap_flush_unmap_range(struct inode *inode, loff_t pos,
 
 int fuse_dev_ioctl_iomap_support(struct file *file,
 				 struct fuse_iomap_support __user *argp);
+int fuse_iomap_dev_inval(struct fuse_conn *fc,
+			 const struct fuse_iomap_dev_inval_out *arg);
 
 int fuse_iomap_fadvise(struct file *file, loff_t start, loff_t end, int advice);
 #else
@@ -1832,6 +1834,7 @@ int fuse_iomap_fadvise(struct file *file, loff_t start, loff_t end, int advice);
 # define fuse_iomap_fallocate(...)		(-ENOSYS)
 # define fuse_iomap_flush_unmap_range(...)	(-ENOSYS)
 # define fuse_dev_ioctl_iomap_support(...)	(-EOPNOTSUPP)
+# define fuse_iomap_dev_inval(...)		(-ENOSYS)
 # define fuse_iomap_fadvise			NULL
 #endif
 
