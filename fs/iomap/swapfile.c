d78140ae7e41edad8f0971778c7c21a6f3da650c
Darrick J. Wong <djwong@kernel.org>
2026-02-22 14:23:01 -0800

    iomap: allow NULL swap info bdev when activating swapfile
    All current users of the iomap swapfile activation mechanism are block
    device filesystems.  This means that claim_swapfile will set
    swap_info_struct::bdev to inode->i_sb->s_bdev of the swap file.
    
    However, a subsequent patch to fuse will add iomap infrastructure so
    that fuse servers can be asked to provide file mappings specifically for
    swap files.  The fuse server isn't required to set s_bdev (by mounting
    as fuseblk) so s_bdev might be null.  For this case, we want to set
    sis::bdev from the first mapping.
    
    To make this work robustly, we must explicitly check that each mapping
    provides a bdev and that there's no way we can succeed at collecting
    swapfile pages without a block device.
    
    And just to be clear: fuse-iomap servers will have to respond to an
    explicit request for swapfile activation.  It's not like fuseblk, where
    responding to bmap means swapfiles work even if that wasn't expected.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/iomap/swapfile.c
+++ fs/iomap/swapfile.c
@@ -112,6 +112,17 @@ static int iomap_swapfile_iter(struct iomap_iter *iter,
 	if (iomap->flags & IOMAP_F_SHARED)
 		return iomap_swapfile_fail(isi, "has shared extents");
 
+	/* Swapfiles must be backed by a block device */
+	if (!iomap->bdev)
+		return iomap_swapfile_fail(isi, "is not on a block device");
+
+	/*
+	 * Not all filesystems can set sis->bdev before calling
+	 * iomap_swapfile_activate, so we allow setting it for pos 0.
+	 */
+	if (iter->pos == 0 && !isi->sis->bdev)
+		isi->sis->bdev = iomap->bdev;
+
 	/* Only one bdev per swap file. */
 	if (iomap->bdev != isi->sis->bdev)
 		return iomap_swapfile_fail(isi, "outside the main device");
@@ -181,6 +192,16 @@ int iomap_swapfile_activate(struct swap_info_struct *sis,
 	 */
 	if (isi.nr_pages == 0) {
 		pr_warn("swapon: Cannot find a single usable page in file.\n");
+		return -EINVAL;
+	}
+
+	/*
+	 * If this swapfile doesn't have a block device, reject this useless
+	 * swapfile to prevent confusion later on.
+	 */
+	if (!sis->bdev) {
+		pr_warn(
+"swapon: No block device for swap file but we configured swap pages?!\n");
 		return -EINVAL;
 	}
 
