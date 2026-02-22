79709e6c54b0ca2159709eb9a0e12c7c73fa97e6
Darrick J. Wong <djwong@kernel.org>
2026-02-22 14:22:59 -0800

    fuse: implement file attributes mask for statx
    Actually copy the attributes/attributes_mask from userspace.  Ignore
    file attributes bits that the VFS sets (or doesn't set) on its own.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>
    Reviewed-by: Joanne Koong <joannelkoong@gmail.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -1468,6 +1468,8 @@ static int fuse_do_statx(struct mnt_idmap *idmap, struct inode *inode,
 		stat->result_mask = sx->mask & (STATX_BASIC_STATS | STATX_BTIME);
 		stat->btime.tv_sec = sx->btime.tv_sec;
 		stat->btime.tv_nsec = min_t(u32, sx->btime.tv_nsec, NSEC_PER_SEC - 1);
+		stat->attributes |= fuse_statx_attributes(inode, sx);
+		stat->attributes_mask |= fuse_statx_attributes_mask(inode, sx);
 		fuse_fillattr(idmap, inode, &attr, stat);
 		stat->result_mask |= STATX_TYPE;
 	}
@@ -1572,6 +1574,8 @@ retry:
 			stat->btime = fi->i_btime;
 			stat->result_mask |= STATX_BTIME;
 		}
+		stat->attributes |= fi->statx_attributes;
+		stat->attributes_mask |= fi->statx_attributes_mask;
 	}
 
 	return err;
