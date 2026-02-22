4ec94b71cd9ebe784aa2822a6d092a0262acb1f8
Darrick J. Wong <djwong@kernel.org>
2026-02-22 14:23:11 -0800

    fuse: implement direct IO with iomap
    Start implementing the fuse-iomap file I/O paths by adding direct I/O
    support and all the signalling flags that come with it.  Buffered I/O
    is much more complicated, so we leave that to a subsequent patch.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -188,6 +188,11 @@ struct fuse_inode {
 
 			/* waitq for direct-io completion */
 			wait_queue_head_t direct_io_waitq;
+
+#ifdef CONFIG_FUSE_IOMAP
+			/* file size as reported by fuse server */
+			loff_t i_disk_size;
+#endif
 		};
 
 		/* readdir cache (directory only) */
@@ -656,6 +661,16 @@ struct fuse_sync_bucket {
 	struct rcu_head rcu;
 };
 
+#ifdef CONFIG_FUSE_IOMAP
+struct fuse_iomap_conn {
+	/* fuse server doesn't implement iomap_end */
+	unsigned int no_end:1;
+
+	/* fuse server doesn't implement iomap_ioend */
+	unsigned int no_ioend:1;
+};
+#endif
+
 /**
  * A Fuse connection.
  *
@@ -1005,6 +1020,11 @@ struct fuse_conn {
 #ifdef CONFIG_FUSE_BACKING
 	/** IDR for backing files ids */
 	struct idr backing_files_map;
+#endif
+
+#ifdef CONFIG_FUSE_IOMAP
+	/** iomap information */
+	struct fuse_iomap_conn iomap_conn;
 #endif
 
 #ifdef CONFIG_FUSE_IO_URING
