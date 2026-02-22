410ce778f7fdbc73c674684c9087879c2e1f2b68
Darrick J. Wong <djwong@kernel.org>
2026-02-22 14:23:18 -0800

    fuse: invalidate ranges of block devices being used for iomap
    Make it easier to invalidate the page cache for a block device that is
    being used in conjunction with iomap.  This allows a fuse server to kill
    all cached data for a block that is being freed, so that block reuse
    doesn't result in file corruption.  Right now, the only way to do this
    is with fadvise, which ignores and doesn't wait for pages undergoing
    writeback.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -247,6 +247,7 @@
  *  - add FUSE_ATTR_EXCLUSIVE to enable exclusive mode for specific inodes
  *  - add FUSE_ATTR_IOMAP to enable iomap for specific inodes
  *  - add FUSE_IOMAP_CONFIG so the fuse server can configure more fs geometry
+ *  - add FUSE_NOTIFY_IOMAP_DEV_INVAL to invalidate iomap bdev ranges
  */
 
 #ifndef _LINUX_FUSE_H
@@ -701,6 +702,8 @@ enum fuse_notify_code {
 	FUSE_NOTIFY_RESEND = 7,
 	FUSE_NOTIFY_INC_EPOCH = 8,
 	FUSE_NOTIFY_PRUNE = 9,
+	FUSE_NOTIFY_IOMAP_DEV_INVAL = 99,
+	FUSE_NOTIFY_CODE_MAX,
 };
 
 /* The read buffer is required to be at least 8k, but may be much larger */
@@ -1489,6 +1492,19 @@ struct fuse_iomap_config_out {
 	int64_t s_time_max;
 
 	int64_t s_maxbytes;	/* max file size */
+};
+
+struct fuse_range {
+	uint64_t offset;
+	uint64_t length;
+};
+
+struct fuse_iomap_dev_inval_out {
+	uint32_t dev;		/* device cookie */
+	uint32_t reserved;	/* zero */
+
+	/* range of bdev pagecache to invalidate, in bytes */
+	struct fuse_range range;
 };
 
 #endif /* _LINUX_FUSE_H */
