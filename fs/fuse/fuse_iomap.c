7fcf6deac9091c4e5abb1cb5149568d4f25771af
Darrick J. Wong <djwong@kernel.org>
2026-02-22 14:23:34 -0800

    fuse: invalidate iomap cache after file updates
    The kernel doesn't know what the fuse server might have done in response
    to truncate, fallocate, or ioend events.  Therefore, it must invalidate
    the mapping cache after those operations to ensure cache coherency.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/fuse_iomap.c
+++ fs/fuse/fuse_iomap.c
@@ -899,6 +899,8 @@ static int fuse_iomap_end(struct inode *inode, loff_t pos, loff_t count,
 			spin_lock(&fi->lock);
 			fi->i_disk_size = max(fi->i_disk_size, pos + written);
 			spin_unlock(&fi->lock);
+
+			fuse_iomap_cache_invalidate_range(inode, pos, written);
 		} else {
 			fuse_iomap_inline_free(iomap);
 		}
@@ -989,7 +991,7 @@ fuse_iomap_setsize_finish(
 	spin_lock(&fi->lock);
 	fi->i_disk_size = newsize;
 	spin_unlock(&fi->lock);
-	return 0;
+	return fuse_iomap_cache_invalidate(inode, newsize);
 }
 
 static int fuse_iomap_ioend(struct inode *inode, loff_t pos, size_t written,
@@ -1067,12 +1069,14 @@ static int fuse_iomap_ioend(struct inode *inode, loff_t pos, size_t written,
 
 	/*
 	 * If there weren't any ioend errors, update the incore isize, which
-	 * confusingly takes the new i_size as "pos".
+	 * confusingly takes the new i_size as "pos".  Invalidate cached
+	 * mappings for the file range that we just completed.
 	 */
 	spin_lock(&fi->lock);
 	fi->i_disk_size = outarg.newsize;
 	spin_unlock(&fi->lock);
 	fuse_write_update_attr(inode, pos + written, written);
+	fuse_iomap_cache_invalidate_range(inode, pos, written);
 	return 0;
 }
 
@@ -1781,6 +1785,8 @@ void fuse_iomap_open_truncate(struct inode *inode)
 	spin_lock(&fi->lock);
 	fi->i_disk_size = 0;
 	spin_unlock(&fi->lock);
+
+	fuse_iomap_cache_invalidate(inode, 0);
 }
 
 struct fuse_writepage_ctx {
@@ -2475,6 +2481,14 @@ fuse_iomap_fallocate(
 
 	trace_fuse_iomap_fallocate(inode, mode, offset, length, new_size);
 
+	if (mode & (FALLOC_FL_COLLAPSE_RANGE | FALLOC_FL_INSERT_RANGE))
+		error = fuse_iomap_cache_invalidate(inode, offset);
+	else
+		error = fuse_iomap_cache_invalidate_range(inode, offset,
+							  length);
+	if (error)
+		return error;
+
 	/*
 	 * If we unmapped blocks from the file range, then we zero the
 	 * pagecache for those regions and push them to disk rather than make
@@ -2492,6 +2506,8 @@ fuse_iomap_fallocate(
 	 */
 	if (new_size) {
 		error = fuse_iomap_setsize_start(inode, new_size);
+		if (!error)
+			error = fuse_iomap_setsize_finish(inode, new_size);
 		if (error)
 			return error;
 
@@ -2612,4 +2628,12 @@ int fuse_dev_ioctl_iomap_set_nofs(struct file *file, uint32_t __user *argp)
 	default:
 		return -EINVAL;
 	}
+}
+
+void fuse_iomap_copied_file_range(struct inode *inode, loff_t offset,
+				  u64 written)
+{
+	ASSERT(fuse_inode_has_iomap(inode));
+
+	fuse_iomap_cache_invalidate_range(inode, offset, written);
 }
