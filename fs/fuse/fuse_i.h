d9d2f83d36d79664fa87fee530afb276cd2a3982
Darrick J. Wong <djwong@kernel.org>
2026-02-22 14:23:39 -0800

    fuse: allow privileged mount helpers to pre-approve iomap usage
    For the upcoming safemount functionality in libfuse, we will create a
    privileged "mount.safe" helper that starts the fuse server in a
    completely unprivileged systemd container.  The mount helper will pass
    the mount options and fds for /dev/fuse and any other files requested by
    the fuse server into the container via a Unix socket.
    
    Currently, the ability to turn on iomap for fuse depends on a module
    parameter and the process that calls mount() having the CAP_SYS_RAWIO
    capability.  However, the unprivilged fuse server might want to query
    the /dev/fuse fd for iomap capabilities before mount or FUSE_INIT so
    that it can get ready.
    
    Similar to FUSE_DEV_SYNC_INIT, add a new bit for iomap that can be
    squirreled away in file->private_data and an ioctl to set that bit.
    That way the privileged mount helper can pass its iomap privilege to the
    contained fuse server without the fuse server needing to have
    CAP_SYS_RAWIO.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -617,6 +617,9 @@ struct fuse_dev {
 	/** Issue FUSE_INIT synchronously */
 	bool sync_init;
 
+	/** Allow fuse server to ask for IOMAP */
+	bool may_iomap;
+
 	/** Fuse connection for this device */
 	struct fuse_conn *fc;
 
@@ -972,6 +975,13 @@ struct fuse_conn {
 
 	/* Enable fs/iomap for file operations */
 	unsigned int iomap:1;
+
+	/*
+	 * Are filesystems using this connection allowed to use iomap?  This is
+	 * determined by the privilege level of the process that initiated the
+	 * mount() call.
+	 */
+	unsigned int may_iomap:1;
 
 	/* Use io_uring for communication */
 	unsigned int io_uring;
--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -1180,6 +1180,7 @@ struct fuse_iomap_support {
 #define FUSE_DEV_IOC_IOMAP_SUPPORT	_IOR(FUSE_DEV_IOC_MAGIC, 99, \
 					     struct fuse_iomap_support)
 #define FUSE_DEV_IOC_SET_NOFS		_IOW(FUSE_DEV_IOC_MAGIC, 100, uint32_t)
+#define FUSE_DEV_IOC_ADD_IOMAP		_IO(FUSE_DEV_IOC_MAGIC, 101)
 
 struct fuse_lseek_in {
 	uint64_t	fh;
