a9b7f97e5cee72d36a6ef76386af8582007a724c
Darrick J. Wong <djwong@kernel.org>
2026-02-22 14:23:42 -0800

    fuse: add kfuncs for iomap bpf programs to manage the cache
    Add a couple of kfuncs so that a BPF program that generates iomappings
    can add them to the inode's mapping cache, thereby avoiding the need to
    go into BPF program on the next access.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/fuse_iomap.c
+++ fs/fuse/fuse_iomap.c
@@ -2780,7 +2780,7 @@ fuse_iomap_upsert_validate_mappings(struct inode *inode,
 						  &outarg->write);
 }
 
-static int fuse_iomap_upsert_inode(struct inode *inode,
+int fuse_iomap_upsert_inode(struct inode *inode,
 		const struct fuse_iomap_upsert_mappings_out *outarg)
 {
 	int ret = fuse_iomap_upsert_validate_mappings(inode, outarg);
@@ -2875,7 +2875,7 @@ fuse_iomap_inval_validate_range(const struct inode *inode,
 	return true;
 }
 
-static int fuse_iomap_inval_inode(struct inode *inode,
+int fuse_iomap_inval_inode(struct inode *inode,
 		const struct fuse_iomap_inval_mappings_out *outarg)
 {
 	int ret = 0, ret2 = 0;
