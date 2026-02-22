505b67491687630a9fdd562de25a6d992fa1e991
Darrick J. Wong <djwong@kernel.org>
2026-02-22 14:23:37 -0800

    fuse: overlay iomap inode info in struct fuse_inode
    It's not possible for a regular file to use iomap mode and writeback
    caching at the same time, so we can save some memory in struct
    fuse_inode by overlaying them in the union.  This is a separate patch
    because C unions are rather unsafe and I prefer any errors to be
    bisectable to this patch.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -196,8 +196,11 @@ struct fuse_inode {
 
 			/* waitq for direct-io completion */
 			wait_queue_head_t direct_io_waitq;
+		};
 
 #ifdef CONFIG_FUSE_IOMAP
+		/* regular file iomap mode */
+		struct {
 			/* file size as reported by fuse server */
 			loff_t i_disk_size;
 
@@ -208,8 +211,8 @@ struct fuse_inode {
 
 			/* cached iomap mappings */
 			struct fuse_iomap_cache *cache;
-#endif
 		};
+#endif
 
 		/* readdir cache (directory only) */
 		struct {
