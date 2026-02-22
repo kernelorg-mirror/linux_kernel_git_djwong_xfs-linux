c438b61e448d96c76477498a00df4878eee40b8c
Darrick J. Wong <djwong@kernel.org>
2026-02-22 14:23:39 -0800

    fuse: allow privileged mount helpers to pre-approve iomap usage
    For the upcoming safemount functionality in libfuse, we will create a
    privileged "mount.safe" helper that starts the fuse server in a
    completely unprivileged systemd container.  The mount helper will pass
    the mount options and fds for /dev/fuse and any other files requested by
    the fuse server into the container via a Unix socket.
    
    Currently, the ability to turn on iomap for fuse depends on a module
    parameter and the process that calls mount() having the CAP_SYS_RAWIO
    capability.  However, the unprivilged fuse server might want to query
    the /dev/fuse fd for iomap capabilities before mount or FUSE_INIT so
    that it can get ready.
    
    Similar to FUSE_DEV_SYNC_INIT, add a new bit for iomap that can be
    squirreled away in file->private_data and an ioctl to set that bit.
    That way the privileged mount helper can pass its iomap privilege to the
    contained fuse server without the fuse server needing to have
    CAP_SYS_RAWIO.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -1076,6 +1076,7 @@ void fuse_conn_init(struct fuse_conn *fc, struct fuse_mount *fm,
 	fc->name_max = FUSE_NAME_LOW_MAX;
 	fc->timeout.req_timeout = 0;
 	fc->root_nodeid = FUSE_ROOT_ID;
+	fc->may_iomap = fuse_iomap_enabled();
 
 	if (IS_ENABLED(CONFIG_FUSE_BACKING))
 		fuse_backing_files_init(fc);
@@ -1610,7 +1611,7 @@ static void process_init_reply(struct fuse_mount *fm, struct fuse_args *args,
 			if (flags & FUSE_REQUEST_TIMEOUT)
 				timeout = arg->request_timeout;
 
-			if ((flags & FUSE_IOMAP) && fuse_iomap_enabled()) {
+			if ((flags & FUSE_IOMAP) && fc->may_iomap) {
 				fc->iomap = 1;
 				pr_warn(
  "EXPERIMENTAL iomap feature enabled.  Use at your own risk!");
@@ -1697,7 +1698,7 @@ static struct fuse_init_args *fuse_new_init(struct fuse_mount *fm)
 	 */
 	if (fuse_uring_enabled())
 		flags |= FUSE_OVER_IO_URING;
-	if (fuse_iomap_enabled())
+	if (fm->fc->may_iomap)
 		flags |= FUSE_IOMAP;
 
 	ia->in.flags = flags;
@@ -2093,6 +2094,8 @@ int fuse_fill_super_common(struct super_block *sb, struct fuse_fs_context *ctx)
 			goto err_unlock;
 		if (fud->sync_init)
 			fc->sync_init = 1;
+		if (fud->may_iomap)
+			fc->may_iomap = 1;
 	}
 
 	err = fuse_ctl_add_conn(fc);
