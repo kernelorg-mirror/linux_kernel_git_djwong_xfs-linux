680b6c29f7404df24a52d61d9aae818a11853ac7
Darrick J. Wong <djwong@kernel.org>
2026-02-22 14:23:36 -0800

    fuse_trace: enable iomap cache management
    Add tracepoints for the previous patch.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/fuse/fuse_iomap.c
+++ fs/fuse/fuse_iomap.c
@@ -2818,6 +2818,8 @@ int fuse_iomap_upsert_mappings(struct fuse_conn *fc,
 		goto out_sb;
 	}
 
+	trace_fuse_iomap_upsert_mappings(inode, outarg);
+
 	fi = get_fuse_inode(inode);
 	if (BAD_DATA(fi->orig_ino != outarg->attr_ino)) {
 		ret = -EINVAL;
@@ -2907,6 +2909,8 @@ int fuse_iomap_inval_mappings(struct fuse_conn *fc,
 		ret = -ESTALE;
 		goto out_sb;
 	}
+
+	trace_fuse_iomap_inval_mappings(inode, outarg);
 
 	fi = get_fuse_inode(inode);
 	if (BAD_DATA(fi->orig_ino != outarg->attr_ino)) {
