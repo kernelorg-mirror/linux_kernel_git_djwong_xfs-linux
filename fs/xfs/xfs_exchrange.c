35354a783362ea4563574a6319ef3ff55a57e591
Darrick J. Wong <djwong@kernel.org>
2026-07-07 15:28:07 -0700

    xfs: don't call xfs_exchange_range_finish for a dry run
    LOLLM noticed that we strip file privileges and whatnot even for a dry
    run.  We also shouldn't flush dirty data to disk or trim COW staging
    events for a dry run.  Neither of those behaviors are allowed by the
    manpage, so fix that by exiting early on DRY_RUN in various functions.
    
    Cc: <stable@vger.kernel.org> # v6.10
    Fixes: 42672471f938cd ("xfs: bind together the front and back ends of the file range exchange code")
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>
    Assisted-by: LOLLM # finding obvious bugs

--- fs/xfs/xfs_exchrange.c
+++ fs/xfs/xfs_exchrange.c
@@ -625,6 +625,9 @@ xfs_exchrange_prep(
 			return error;
 	}
 
+	if (fxr->flags & XFS_EXCHANGE_RANGE_DRY_RUN)
+		return 0;
+
 	/* Attach dquots to both inodes before changing block maps. */
 	error = xfs_qm_dqattach(ip2);
 	if (error)
@@ -709,9 +712,11 @@ xfs_exchrange_contents(
 	 * other file write would do.  This may involve turning on support for
 	 * logged xattrs if either file has security capabilities.
 	 */
-	error = xfs_exchange_range_finish(fxr);
-	if (error)
-		goto out_unlock;
+	if (!(fxr->flags & XFS_EXCHANGE_RANGE_DRY_RUN)) {
+		error = xfs_exchange_range_finish(fxr);
+		if (error)
+			goto out_unlock;
+	}
 
 out_unlock:
 	xfs_iunlock2_io_mmap(ip1, ip2);
