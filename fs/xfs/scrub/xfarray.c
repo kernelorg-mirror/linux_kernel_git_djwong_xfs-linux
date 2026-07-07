0342c0afef4e8cc75ddc447d56034d742c906620
Darrick J. Wong <djwong@kernel.org>
2026-07-07 15:28:28 -0700

    xfarray: don't crash when sorting if array element crosses a folio
    LOLLM points out that if an array element crosses a folio boundary,
    xfile_get_folio returns a NULL folio pointer.  If this happens,
    si->folio is also set to NULL, and calling folio_pos/folio_address will
    just crash the kernel.  Teach this function to handle this condition by
    falling back to reading the array element into scratchpad memory.
    
    Cc: <stable@vger.kernel.org> # v6.6
    Fixes: cf36f4f64c2d4e ("xfs: cache pages used for xfarray quicksort convergence")
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>
    Assisted-by: LOLLM # finding obvious bugs

--- fs/xfs/scrub/xfarray.c
+++ fs/xfs/scrub/xfarray.c
@@ -830,22 +830,24 @@ xfarray_sort_scan(
 			return PTR_ERR(folio);
 		si->folio = folio;
 
-		si->first_folio_idx = xfarray_idx(si->array,
-				folio_pos(si->folio) + si->array->obj_size - 1);
+		if (si->folio) {
+			si->first_folio_idx = xfarray_idx(si->array,
+					folio_pos(si->folio) + si->array->obj_size - 1);
 
-		next_pos = folio_next_pos(si->folio);
-		si->last_folio_idx = xfarray_idx(si->array, next_pos - 1);
-		if (xfarray_pos(si->array, si->last_folio_idx + 1) > next_pos)
-			si->last_folio_idx--;
+			next_pos = folio_next_pos(si->folio);
+			si->last_folio_idx = xfarray_idx(si->array, next_pos - 1);
+			if (xfarray_pos(si->array, si->last_folio_idx + 1) > next_pos)
+				si->last_folio_idx--;
 
-		trace_xfarray_sort_scan(si, idx);
+			trace_xfarray_sort_scan(si, idx);
+		}
 	}
 
 	/*
 	 * If this folio still doesn't cover the desired element, it must cross
 	 * a folio boundary.  Read into the scratchpad and we're done.
 	 */
-	if (idx < si->first_folio_idx || idx > si->last_folio_idx) {
+	if (!si->folio || idx < si->first_folio_idx || idx > si->last_folio_idx) {
 		void		*temp = xfarray_scratch(si->array);
 
 		error = xfile_load(si->array->xfile, temp, si->array->obj_size,
