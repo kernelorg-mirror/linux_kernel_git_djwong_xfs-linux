0641cdf593c32081764b5a5e4dd87c070968eee8
Darrick J. Wong <djwong@kernel.org>
2026-07-07 15:27:45 -0700

    xfs: drop unnecessary sr parameter from xchk_rtgroup init functions
    Drop the sr parameter from the scrub rtgroup functions because the value
    is always the same.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/xfs/scrub/rgsuper.c
+++ fs/xfs/scrub/rgsuper.c
@@ -62,7 +62,7 @@ xchk_rgsuperblock(
 	 * signal that the group no longer exists.  Take the rtbitmap in shared
 	 * mode so that the group can't change while we're doing things.
 	 */
-	error = xchk_rtgroup_init_existing(sc, rgno, &sc->sr);
+	error = xchk_rtgroup_init_existing(sc, rgno);
 	if (!xchk_xref_process_error(sc, 0, 0, &error))
 		return error;
 
@@ -71,7 +71,7 @@ xchk_rgsuperblock(
 	else
 		flags = XFS_RTGLOCK_BITMAP_SHARED;
 
-	error = xchk_rtgroup_lock(sc, &sc->sr, flags);
+	error = xchk_rtgroup_lock(sc, flags);
 	if (error)
 		return error;
 
