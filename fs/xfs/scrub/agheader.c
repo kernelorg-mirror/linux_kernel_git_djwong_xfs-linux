28f09ae7bc94b98029fb47a1743168b461e6dfb3
Darrick J. Wong <djwong@kernel.org>
2026-07-13 14:52:01 -0700

    xfs: read primary superblock from disk during scrub
    The primary superblock scrubber isn't that useful right now because the
    disk contents could have been corrupted after mount time.  Perform an
    uncached re-read of the super buffer to perform a fresh verification,
    and schedule a repair if it doesn't pass muster.
    
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

--- fs/xfs/scrub/agheader.c
+++ fs/xfs/scrub/agheader.c
@@ -119,8 +119,40 @@ xchk_superblock(
 	if (!pag)
 		return -ENOENT;
 
-	if (agno == 0)
+	if (agno == 0) {
+		/*
+		 * Reread the primary super from disk in case it's become
+		 * corrupted enough since mount time to fail the verifier.
+		 * Crashing now with a bad primary super will prevent remount
+		 * (which might recover the primary super from the log) so we
+		 * want to trigger a repair to write the incore primary
+		 * superblock out to disk ASAP.
+		 *
+		 * Note that we don't check the geometry (like we do for a
+		 * secondary super) because the incore copy is the source of
+		 * truth while the filesystem is mounted.
+		 *
+		 * If the read races with a write, we end up writing the
+		 * primary super unnecessarily, but that's not a big deal.
+		 */
+		error = xfs_buf_read_uncached(sc->mp->m_ddev_targp,
+				XFS_SB_DADDR, BTOBB(mp->m_sb.sb_sectsize), &bp,
+				&xfs_sb_buf_ops);
+		switch (error) {
+		case -EINVAL:	/* also -EWRONGFS */
+		case -ENOSYS:
+		case -EFBIG:
+			error = -EFSCORRUPTED;
+			fallthrough;
+		default:
+			break;
+		}
+		if (!xchk_process_error(sc, agno, XFS_SB_BLOCK(mp), &error))
+			goto out_pag;
+
+		xfs_buf_relse(bp);
 		goto out_xref;
+	}
 
 	error = xfs_sb_read_secondary(mp, sc->tp, agno, &bp);
 	/*
