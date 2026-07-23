68b92e4e9e844e7b47979716cbc9015a7a090a40
Darrick J. Wong <djwong@kernel.org>
2026-07-23 12:34:21 -0700

    xfs: improve dirent bounds checking in scrub and repair
    LOLLM complains that we don't do enough bounds checking of the directory
    entries in directory blocks when we're looking for errors or trying to
    salvage entries.  Let's improve that with more detailed checks.
    
    Cc: <stable@vger.kernel.org> # v4.16
    Fixes: ce92d29ddf9908 ("xfs: directory scrubber must walk through data block to offset")
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>
    Assisted-by: LOLLM # finding obvious bugs

--- fs/xfs/scrub/dir.c
+++ fs/xfs/scrub/dir.c
@@ -390,22 +390,51 @@ xchk_dir_rec(
 		xchk_fblock_set_corrupt(ds->sc, XFS_DATA_FORK, rec_bno);
 		goto out_relse;
 	}
-	for (;;) {
+	while (iter_off < end) {
 		struct xfs_dir2_data_entry	*dep = bp->b_addr + iter_off;
 		struct xfs_dir2_data_unused	*dup = bp->b_addr + iter_off;
+		unsigned int			advance;
 
-		if (iter_off >= end) {
+		/* must have freetag */
+		if (iter_off + offsetof(struct xfs_dir2_data_unused, length) >= end) {
 			xchk_fblock_set_corrupt(ds->sc, XFS_DATA_FORK, rec_bno);
 			goto out_relse;
 		}
 
 		if (be16_to_cpu(dup->freetag) == XFS_DIR2_DATA_FREE_TAG) {
-			iter_off += be16_to_cpu(dup->length);
+			if (iter_off + sizeof(*dup) > end) {
+				xchk_fblock_set_corrupt(ds->sc, XFS_DATA_FORK,
+						rec_bno);
+				goto out_relse;
+			}
+			advance = xfs_dir2_data_unusedsize(
+					be16_to_cpu(dup->length));
+			if (!advance) {
+				xchk_fblock_set_corrupt(ds->sc, XFS_DATA_FORK,
+						rec_bno);
+				goto out_relse;
+			}
+
+			iter_off += advance;
 			continue;
 		}
+
+		/* must have namelen */
+		if (iter_off + offsetof(struct xfs_dir2_data_entry, name) >= end) {
+			xchk_fblock_set_corrupt(ds->sc, XFS_DATA_FORK, rec_bno);
+			goto out_relse;
+		}
+
+		advance = xfs_dir2_data_entsize(mp, dep->namelen);
+		if (!advance) {
+			xchk_fblock_set_corrupt(ds->sc, XFS_DATA_FORK,
+					rec_bno);
+			goto out_relse;
+		}
+
 		if (dep == dent)
 			break;
-		iter_off += xfs_dir2_data_entsize(mp, dep->namelen);
+		iter_off += advance;
 	}
 
 	/* Retrieve the entry, sanity check it, and compare hashes. */
@@ -415,6 +444,13 @@ xchk_dir_rec(
 	if (!xfs_verify_dir_ino(mp, ino) || tag != off)
 		xchk_fblock_set_corrupt(ds->sc, XFS_DATA_FORK, rec_bno);
 	if (dent->namelen == 0) {
+		xchk_fblock_set_corrupt(ds->sc, XFS_DATA_FORK, rec_bno);
+		goto out_relse;
+	}
+
+	/* Name must not overflow end of block */
+	if ((char *)dent->name + dent->namelen >
+	    (char *)bp->b_addr + BBTOB(bp->b_length)) {
 		xchk_fblock_set_corrupt(ds->sc, XFS_DATA_FORK, rec_bno);
 		goto out_relse;
 	}
