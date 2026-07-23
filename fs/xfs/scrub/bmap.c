2661ade4fd8fddb04b55fc3ee5b5e380158235af
Darrick J. Wong <djwong@kernel.org>
2026-07-23 12:06:37 -0700

    xfs: jump out of xchk_bmap_check_rmap if fatal signals
    LOLLM observes that we never check for fatal signals inside the loop
    that checks file fork mappings against the rmapbt.  This can lead to
    lengthy delays in responding to a ^C, so add some bailout checks.
    
    Cc: <stable@vger.kernel.org> # v4.17
    Fixes: 5e777b62b0bcb6 ("xfs: bmap scrubber should do rmap xref with bmap for sparse files")
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>
    Assisted-by: LOLLM # finding obvious bugs

--- fs/xfs/scrub/bmap.c
+++ fs/xfs/scrub/bmap.c
@@ -625,6 +625,7 @@ xchk_bmap_check_rmap(
 	struct xfs_ifork		*ifp;
 	struct xfs_scrub		*sc = sbcri->sc;
 	bool				have_map;
+	int				error = 0;
 
 	/* Is this even the right fork? */
 	if (rec->rm_owner != I_INO(sc->ip))
@@ -634,6 +635,9 @@ xchk_bmap_check_rmap(
 		return 0;
 	if (rec->rm_flags & XFS_RMAP_BMBT_BLOCK)
 		return 0;
+
+	if (xchk_should_terminate(sc, &error))
+		return error;
 
 	/* Now look up the bmbt record. */
 	ifp = xfs_ifork_ptr(sc->ip, sbcri->whichfork);
@@ -656,6 +660,9 @@ xchk_bmap_check_rmap(
 	 */
 	check_rec = *rec;
 	while (have_map) {
+		if (xchk_should_terminate(sc, &error))
+			return error;
+
 		if (irec.br_startoff != check_rec.rm_offset)
 			xchk_fblock_set_corrupt(sc, sbcri->whichfork,
 					check_rec.rm_offset);
