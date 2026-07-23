1fb624edb0cadf3ec1e1e27488a0a1af18be1b71
Darrick J. Wong <djwong@kernel.org>
2026-07-23 12:15:58 -0700

    xfs: check fork type against dabtree block magic number
    LOLLM complains that we fail to flag dabtree blocks that should never be
    associated with the file fork -- data forks can't point to xattr leaves,
    and attr forks can't point to dirent blocks.  Add the missing checks.
    
    Cc: <stable@vger.kernel.org> # v4.15
    Fixes: 7c4a07a424c18d ("xfs: scrub directory/attribute btrees")
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>
    Assisted-by: LOLLM # finding obvious bugs

--- fs/xfs/scrub/dabtree.c
+++ fs/xfs/scrub/dabtree.c
@@ -410,6 +410,8 @@ xchk_da_btree_block(
 				XFS_BLFT_ATTR_LEAF_BUF);
 		blk->magic = XFS_ATTR_LEAF_MAGIC;
 		blk->hashval = xfs_attr_leaf_lasthash(blk->bp, pmaxrecs);
+		if (dargs->whichfork != XFS_ATTR_FORK)
+			xchk_da_set_corrupt(ds, level);
 		if (ds->tree_level != 0)
 			xchk_da_set_corrupt(ds, level);
 		break;
@@ -419,6 +421,8 @@ xchk_da_btree_block(
 				XFS_BLFT_DIR_LEAFN_BUF);
 		blk->magic = XFS_DIR2_LEAFN_MAGIC;
 		blk->hashval = xfs_dir2_leaf_lasthash(ip, blk->bp, pmaxrecs);
+		if (dargs->whichfork != XFS_DATA_FORK)
+			xchk_da_set_corrupt(ds, level);
 		if (ds->tree_level != 0)
 			xchk_da_set_corrupt(ds, level);
 		break;
@@ -428,6 +432,8 @@ xchk_da_btree_block(
 				XFS_BLFT_DIR_LEAF1_BUF);
 		blk->magic = XFS_DIR2_LEAF1_MAGIC;
 		blk->hashval = xfs_dir2_leaf_lasthash(ip, blk->bp, pmaxrecs);
+		if (dargs->whichfork != XFS_DATA_FORK)
+			xchk_da_set_corrupt(ds, level);
 		if (ds->tree_level != 0)
 			xchk_da_set_corrupt(ds, level);
 		break;
