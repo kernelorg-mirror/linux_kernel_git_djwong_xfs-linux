745195cb9d1df202cd7a25ee5d887f0e8e4b36e8
Darrick J. Wong <djwong@kernel.org>
2026-07-29 17:09:30 -0700

    xfs: init inode number for tracepoint
    LOLLM complains that if the xchk_dir_lookup call fails, it won't set ino
    to anything.  ino is then passed into the tracepoint function, which
    means that it reports random stack garbage.  Set it to NULLFSINO instead
    so that the circumstance is more obvious.
    
    Cc: <stable@vger.kernel.org> # v6.13
    Fixes: 0d2c636e489c11 ("xfs: repair metadata directory file path connectivity")
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>
    Assisted-by: LOLLM # finding obvious bugs

--- fs/xfs/scrub/metapath.c
+++ fs/xfs/scrub/metapath.c
@@ -522,7 +522,7 @@ xrep_metapath_try_link(
 	xfs_ino_t		*alleged_child)
 {
 	struct xfs_scrub	*sc = mpath->sc;
-	xfs_ino_t		ino;
+	xfs_ino_t		ino = NULLFSINO;
 	int			error;
 
 	/* Allocate transaction, lock inodes, join to transaction. */
@@ -621,7 +621,7 @@ xrep_metapath_try_unlink(
 {
 	struct xfs_scrub	*sc = mpath->sc;
 	struct xfs_inode	*ip = NULL;
-	xfs_ino_t		ino;
+	xfs_ino_t		ino = NULLFSINO;
 	int			error;
 
 	ASSERT(*alleged_child != I_INO(sc->ip));
